2d7c2c1f63b5465ad459324e9a1e32929804fe27
Jente Rosseel <rosseeljente@gmail.com>
2022-07-12 13:24:33 +0300

    :truck: @model/user

---
+++ libs/model/user/base/src/lib/ital-user.interface.ts
@@ -0,0 +1,22 @@
+import { User } from '@iote/bricks';
+
+import { iTalUserProfile } from './ital-user-profile.interface';
+import { iTalUserRoles } from './ital-user-roles.interface';
+
+export interface iTalUser extends User
+{
+  profile: iTalUserProfile;
+  roles:   iTalUserRoles;
+
+  /**
+   * Organisations the user has a role in (if not admin)
+   *
+   * @type {string[]}
+   * @memberof EleUser
+   */
+  orgs: string[];
+
+  /** Active Organisation ID. Users can only have one currently active org.
+   *    The active org ID is used to determine which screen to route non-admins too when selecting orgs. */
+  activeOrg: string;
+}
