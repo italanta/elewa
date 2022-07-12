dfb3046d343e93369d8213b401df4d6d63320b03
Jente Rosseel <rosseeljente@gmail.com>
2022-07-12 15:28:20 +0300

    :rocket: Bootstrap convl manager app

---
+++ apps/conv-learning-manager/src/app/app.routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route, PreloadAllModules }    from '@angular/router';
+
+import { IsLoggedInGuard } from '@app/elements/base/authorisation';
+
+export const APP_ROUTES: Route[] = [
+
+  // App Entry-Point - For now, we mock the normally to include paths such as org and flow selection and go
+  //    straight too the default active flow.
+  { path: '', redirectTo: `home`, pathMatch: 'full' },
+
+  //
+  // AUTH
+
+  {
+    path: 'auth',
+    loadChildren: () => import('@app/features/app/auth/login').then(m => m.AuthModule),
+  },
+
+  {
+    path: 'home',
+    loadChildren: () => import('@app/features/convs-mgr/home').then(m => m.ConvsMgrHomeModule),
+    canActivate: [IsLoggedInGuard]
+  },
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      APP_ROUTES,
+      {
+        enableTracing: true,
+        // useHash: true,
+        preloadingStrategy: PreloadAllModules
+      }
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
