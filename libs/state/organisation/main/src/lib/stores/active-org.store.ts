8958948e08ebe70752eef38741415439ee2ba870
Jente Rosseel <rosseeljente@gmail.com>
2022-07-12 15:40:21 +0300

    ⚡ Update org state layer to default to active user

--- libs/state/organisation/main/src/lib/stores/active-org.store.ts
+++ libs/state/organisation/main/src/lib/stores/active-org.store.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Router, NavigationEnd, Event } from '@angular/router';
 
 import { combineLatest } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 
+import { User } from '@iote/bricks';
 import { Store } from '@iote/state';
 
+import { UserStore } from '@app/state/user';
 import { Organisation } from '@app/model/organisation';
+
 import { OrgStore } from './organisation.store';
 
 @Injectable()
@@ -16,48 +18,57 @@ export class ActiveOrgStore extends Store<Organisation>
   _activeOrg : string;
 
   constructor(_orgStore: OrgStore,
-              _router: Router)
+              _user$$: UserStore)
+              // _router: Router)
   {
-    super(null);
+    super(null as any);
 
     const orgs$ = _orgStore.get();
-    const route$ = _router.events.pipe(filter((ev: Event) => ev instanceof NavigationEnd),
-                                       map(ev => ev as NavigationEnd));
+    // const route$ = _router.events.pipe(filter((ev: Event) => ev instanceof NavigationEnd),
+    //                                    map(ev => ev as NavigationEnd));
 
-    this._sbS.sink = combineLatest([orgs$,  route$])
-                        .subscribe(([orgs, route]) =>
+    this._sbS.sink = combineLatest([orgs$, _user$$.getUser()]) // route$])
+                        .subscribe(([orgs, user]) => //route
     {
-      const orgId = this._getRoute(route);
+      const orgId = (user as User).id as string;
 
-      if(orgId !== '__noop__')
+      const org = orgs.find(o => o.id === orgId);
+      if(org && this._activeOrg !== orgId)
       {
-        const org = orgs.find(o => o.id === orgId);
-
-        if(org && this._activeOrg !== orgId)
-        {
-          this._activeOrg = orgId;
-          this.set(org, 'UPDATE - FROM DB || ROUTE');
-        }
+        this._activeOrg = orgId;
+        this.set(org, 'UPDATE - FROM DB || ROUTE');
       }
+
+      // const orgId = this._getRoute(route);
+
+      // if(orgId !== '__noop__')
+      // {
+      //   const org = orgs.find(o => o.id === orgId);
+
+      //   if(org && this._activeOrg !== orgId)
+      //   {
+      //     this._activeOrg = orgId;
+      //     this.set(org, 'UPDATE - FROM DB || ROUTE');
+      //   }
+      // }
 
     });
   }
 
-  private _getRoute(route: NavigationEnd) : string
-  {
-    const elements = route.url.split('/');
-    const propId = elements.length >= 3 ? elements[2] : '__noop__';
+  // private _getRoute(route: NavigationEnd) : string
+  // {
+  //   const elements = route.url.split('/');
+  //   const propId = elements.length >= 3 ? elements[2] : '__noop__';
 
-    return propId;
-  }
+  //   return propId;
+  // }
 
   override get = () => super.get().pipe(filter(val => val != null));
 
-
-  /** Warning - Hack. Only use in property creation onboarding.
-   *                  Avoids challenge of already creating organisation-owned objects before navigating to org. */
-  __setTemp(org: Organisation) {
-    this._activeOrg = org.id;
-    this.set(org, 'UPDATE - FROM DB || ROUTE');
-  }
+  // /** Warning - Hack. Only use in property creation onboarding.
+  //  *                  Avoids challenge of already creating organisation-owned objects before navigating to org. */
+  // __setTemp(org: Organisation) {
+  //   this._activeOrg = org.id as string;
+  //   this.set(org, 'UPDATE - FROM DB || ROUTE');
+  // }
 }
