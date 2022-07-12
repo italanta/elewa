fb5b0edc6dd5f88b52faabdd10500c49c738420a
Jente Rosseel <rosseeljente@gmail.com>
2022-07-12 15:09:30 +0300

    :package: @model/organisation

---
+++ libs/model/organisation/main/src/lib/model-organisation-main.spec.ts
@@ -0,0 +1,7 @@
+import { modelOrganisationMain } from './model-organisation-main';
+
+describe('modelOrganisationMain', () => {
+  it('should work', () => {
+    expect(modelOrganisationMain()).toEqual('model-organisation-main');
+  });
+});
