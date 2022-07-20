d69617b1468cd808e63caa4688008e78bed40d55
Jente Rosseel <rosseeljente@gmail.com>
2022-07-20 18:34:43 +0300

    ⚡ Track element position during drag

--- libs/features/convs-mgr/stories/blocks/library/src/lib/components/block/block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/src/lib/components/block/block.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 import { Logger } from '@iote/bricks-angular';
 import { StoryBlock, StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
@@ -17,14 +17,48 @@ export class BlockComponent implements OnInit
   @Input() block: StoryBlock;
 
   type: StoryBlockTypes;
-
   messagetype = StoryBlockTypes.TextMessage;
 
-  constructor(private _logger: Logger) 
+  constructor(private _el: ElementRef,
+              private _logger: Logger) 
   { }
   
   ngOnInit(): void {
     this.type = this.block.type;
   }
 
+  /** 
+   * Track and update coordinates of block and update them in data model.
+   */
+  @HostListener('mouseout', ['$event']) // Mouseout always happens after the drag (though it also hapens a lot more but not enough for perf issues)
+  onDragEnd() 
+  {
+    const style = this._el.nativeElement.getAttribute('style');
+
+    const left = this._getPosFromStyle(style, 'left');
+    const top  = this._getPosFromStyle(style, 'top');
+
+    this.block.position = {
+      x: left ? left : this.block.position.x,
+      y: top  ? top  : this.block.position.y
+    };
+  }
+
+  /** 
+   * Fn which gets the block position from the style element. 
+   * jsPlumb sets the element position on the attribute style param during drag. */
+  private _getPosFromStyle(style: string, pos: 'left' | 'top') : number | false
+  {
+    const idx = style.indexOf(pos);
+    if(idx >= 0) {
+      const start = idx + pos.length + 2; // Start position of the number we want to read is +2 since normal str is e.g. 'left: ' so left = pos.lenght and +2 = ': '
+      const end = style.indexOf('px', start);        
+      
+      const posStr = style.substring(start, end);
+      const val = parseInt(posStr);
+
+      return !isNaN(val) ? val : false;
+    }
+    return false;
+  }
 }
