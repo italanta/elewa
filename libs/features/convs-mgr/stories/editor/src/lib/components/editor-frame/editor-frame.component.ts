1146abafbd03daf543a836dd8b86f0916da68bc9
Jente Rosseel <rosseeljente@gmail.com>
2022-07-20 09:34:27 +0300

    :wrench: Integrate external lib JS Plumb

--- libs/features/convs-mgr/stories/editor/src/lib/components/editor-frame/editor-frame.component.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/components/editor-frame/editor-frame.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnDestroy } from '@angular/core';
+import { BrowserJsPlumbInstance, newInstance as initJsPlumb } from '@jsplumb/browser-ui';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'convl-story-editor-frame',
   templateUrl: './editor-frame.component.html',
   styleUrls: ['./editor-frame.component.scss']
 })
-export class StoryEditorFrameComponent //implements OnDestroy
+export class StoryEditorFrameComponent implements AfterViewInit //implements OnDestroy
 {
+  @ViewChild('editor') editorVC: ElementRef;
+
+  private _jsplumb: BrowserJsPlumbInstance;
+
+  constructor(private _renderer: Renderer2) 
+  { }
+
+  ngAfterViewInit() {
+    this._jsplumb = initJsPlumb({
+      container: this.editorVC.nativeElement
+    });
+    
+  }
+  
 
 }
