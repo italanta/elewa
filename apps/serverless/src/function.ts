f71aec6400302bd5c08ffabbbd06fa0082867c55
Jente Rosseel <rosseeljente@gmail.com>
2022-07-29 13:26:25 +0300

    :rocket: Basic serverless example

---
+++ apps/serverless/src/function.ts
@@ -0,0 +1,16 @@
+import { GCFunction, FunctionRegistrar, Guard, FunctionHandler } from "@ngfi/functions";
+
+/**
+ * Implementation of GCFunction for the scope of this project. Adds in Environment.
+ *
+ * A conceptual representation of a Google Cloud function which contains the logic of a Firebase Function of type T (input) -> R (result).
+ *
+ * @param T: The data expected by the function
+ * @param R: The result returned from the function
+ */
+export class ServerlessFunction<T, R> extends GCFunction<T, R>
+{
+  constructor(name: string, registrar: FunctionRegistrar<T, R>, guards: Guard<T>[], handler: FunctionHandler<T, R>) {
+    super(name, registrar, guards, handler);
+  }
+}
---
+++ tools/scripts/build-gc-functions-package.js
@@ -0,0 +1,26 @@
+const packageJson = require('../../package.json'); // Take root package.json
+const fs = require('fs');
+const deps = packageJson['firebase-functions-dependencies'];
+
+// Template of package.json for Firebase Functions
+const firebaseFunctionsPackageJson = {
+  engines: { node: '10' },
+  main: 'main.js',
+
+  // filter only dependencies we need for Firebase Functions
+  dependencies: deps.reduce((acc, cur) => {
+    acc[cur] = packageJson.dependencies[cur];
+    return acc;
+  }, {})
+};
+
+// Only for demo purpose:
+console.log(
+  'Firebase Functions package.json:\n',
+  JSON.stringify(firebaseFunctionsPackageJson, null, 2)
+);
+
+const path = 'dist/apps/serverless/package.json'; // Where to save generated package.json file
+
+fs.writeFileSync(path, JSON.stringify(firebaseFunctionsPackageJson));
+console.log(`${path} written successfully.`);
