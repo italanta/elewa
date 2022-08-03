9daa095e5514ad5562c620c43f5c46f061752128
Jente Rosseel <rosseeljente@gmail.com>
2022-08-03 13:50:59 +0300

    :package: :wrench: Fix import path of conv-lm-backend

--- apps/conv-lm-backend/jest.config.ts
+++ apps/conv-lm-backend/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'convl-lm-backend',
+  displayName: 'conv-lm-backend',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/convl-lm-backend',
+  coverageDirectory: '../../coverage/apps/conv-lm-backend',
 };
