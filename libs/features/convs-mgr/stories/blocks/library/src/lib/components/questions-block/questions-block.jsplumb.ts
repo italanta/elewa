47fbb9561e2327963a40e1acde30b45680e59009
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-08-08 08:15:34 +0300

    :art: added the questions block component

---
+++ libs/features/convs-mgr/stories/blocks/library/src/lib/components/questions-block/questions-block.jsplumb.ts
@@ -0,0 +1,46 @@
+import { ComponentRef } from '@angular/core';
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+import { BezierConnector } from "@jsplumb/connector-bezier"
+
+
+import { QuestionMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
+import { BlockComponent } from "../block/block.component";
+
+/**
+ * Decorates MessageBlock with JS plumb connectors.
+ * 
+ * @param block   - TextMessageBlock data structure.
+ * @param comp    - Angular component within the viewport 
+ * @param jsPlumb - Active jsPlumb instance
+ * 
+ * @see {_JsPlumbComponentDecorator} - Should be the only one calling the component
+ */
+export function _QuestionsBlockDecoratePlumb(block: QuestionMessageBlock, comp: ComponentRef<BlockComponent>, jsPlumb: BrowserJsPlumbInstance) : ComponentRef<BlockComponent> 
+{
+  jsPlumb.addEndpoint(comp.location.nativeElement, {
+    // Whether the anchor is source (This Block -> Other Block)
+    source: true,
+
+    // Type of endpoint to render
+    endpoint: 'Dot',
+    // Where to position the anchor
+    anchor: "Right",
+    connector: {
+      type: BezierConnector.type,
+      options: { 
+        curviness: 100
+      }
+    }
+  });
+
+  jsPlumb.addEndpoint(comp.location.nativeElement, {
+    // Whether the anchor is target (Other Block -> This Block)
+    target: true,
+
+    endpoint: 'Rectangle',
+    anchor: "Left"
+  });
+
+  return comp;
+}
