04b3bd13644850fd5dc2271e46eec2869a941e6a
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-08-08 08:44:56 +0300

    :art: created options input field

---
+++ libs/elements/convs-mgr/controls/input-fields/src/lib/components/option-input-field/option-input-field.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-option-input-field',
+  templateUrl: './option-input-field.component.html',
+  styleUrls: ['./option-input-field.component.scss'],
+})
+export class OptionInputFieldComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
