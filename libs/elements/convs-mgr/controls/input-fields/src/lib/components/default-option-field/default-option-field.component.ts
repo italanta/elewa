c3cfd8c4e9a18a67a43976830d66ac66e2de1783
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-08-08 12:40:58 +0300

    :art: added elements default input field

---
+++ libs/elements/convs-mgr/controls/input-fields/src/lib/components/default-option-field/default-option-field.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-option-field',
+  templateUrl: './default-option-field.component.html',
+  styleUrls: ['./default-option-field.component.scss'],
+})
+export class DefaultOptionFieldComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
