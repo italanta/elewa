762b44b079610f5045b8f08c2968f37c6a88705e
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-08-08 12:37:56 +0300

    :art: added connector on options field

--- libs/elements/convs-mgr/controls/input-fields/src/lib/components/option-input-field/option-input-field.component.ts
+++ libs/elements/convs-mgr/controls/input-fields/src/lib/components/option-input-field/option-input-field.component.ts
@@ -1,17 +1,48 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormGroup } from '@angular/forms';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+import { BezierConnector } from '@jsplumb/connector-bezier';
 
 @Component({
   selector: 'app-option-input-field',
   templateUrl: './option-input-field.component.html',
   styleUrls: ['./option-input-field.component.scss'],
 })
-export class OptionInputFieldComponent implements OnInit {
+export class OptionInputFieldComponent implements OnInit, AfterViewInit {
 
   @Input() blockFormGroup: FormGroup;
   @Input() formGroupNameInput: number | string;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
+
+  inputUniqueId: string;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.inputUniqueId = `input-${this.formGroupNameInput}$-${this.blockFormGroup.value.id}`;
+  }
+
+  ngAfterViewInit(): void {
+    let input = document.getElementById(this.inputUniqueId) as Element;
+
+    if (this.jsPlumb) {
+      this.jsPlumb.addEndpoint(input, {
+        // Whether the anchor is source (This Block -> Other Block)
+        source: true,
+    
+        // Type of endpoint to render
+        endpoint: 'Dot',
+        // Where to position the anchor
+        anchor: "Right",
+        connector: {
+          type: BezierConnector.type,
+          options: { 
+            curviness: 100
+          }
+        }
+      });
+    }
+
+  }
 }
