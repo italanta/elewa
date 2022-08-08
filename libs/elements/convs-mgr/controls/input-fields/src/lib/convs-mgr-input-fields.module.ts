b844bed0e3c8acae472c02201a19e1c2729ea856
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-08-08 08:44:56 +0300

    :art: created options input field

--- libs/elements/convs-mgr/controls/input-fields/src/lib/convs-mgr-input-fields.module.ts
+++ libs/elements/convs-mgr/controls/input-fields/src/lib/convs-mgr-input-fields.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OptionInputFieldComponent } from './components/option-input-field/option-input-field.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [OptionInputFieldComponent],
 })
 export class ConvsMgrInputFieldsModule {}
