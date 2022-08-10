79ef86383f30c2fea1f698c9c333665a1775153b
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-08-10 12:19:53 +0300

    :pencil2: :recycle: refactored connections store

--- libs/state/convs-mgr/stories/block-connections/src/lib/stores/story-connections.store.ts
+++ libs/state/convs-mgr/stories/block-connections/src/lib/stores/story-connections.store.ts
@@ -9,22 +9,16 @@ import { tap, throttleTime, switchMap } from 'rxjs/operators';
 import { Logger } from '@iote/bricks-angular';
 
 import { Story } from '@app/model/convs-mgr/stories/main';
+import { StoryBlockConnection } from '@app/model/convs-mgr/stories/blocks/main';
 
 import { ActiveStoryStore } from '@app/state/convs-mgr/stories';
 
 @Injectable()
-export class StoryConnectionsStore extends DataStore<any>
+export class StoryConnectionsStore extends DataStore<StoryBlockConnection>
 {
   protected store = 'story-connections-store';
-  protected _activeRepo: Repository<any>;
-
-  private _activeStory: Story;
+  protected _activeRepo: Repository<StoryBlockConnection>;
   
-  // Question to dev's reviewing:
-  //   Will this always get all the organisations?
-  //     i.e. Even if no organisations need to be loaded for a specific piece of functionaly e.g. invites, do we still load all organisations?
-  //
-  // Answer: No, as Angular's DI engine is lazy, meaning it will only initialise services the first time they are called.
   constructor(_story$$: ActiveStoryStore,
               _repoFac: DataService,
               _logger: Logger)
@@ -33,7 +27,7 @@ export class StoryConnectionsStore extends DataStore<any>
 
     const data$ = _story$$.get()
                     .pipe(
-                      tap((story: Story) => this._activeRepo = _repoFac.getRepo<any>(`orgs/${story.orgId}/stories/${story.id}/connections`)),
+                      tap((story: Story) => this._activeRepo = _repoFac.getRepo<StoryBlockConnection>(`orgs/${story.orgId}/stories/${story.id}/connections`)),
                       switchMap((story: Story) => 
                         story ? this._activeRepo.getDocuments() : of([] as any[])),
                       throttleTime(400, undefined, { leading: true, trailing: true }));
