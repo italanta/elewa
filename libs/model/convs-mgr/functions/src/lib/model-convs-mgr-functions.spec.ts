a7792dbc1110ff49149798b378c632bf729bb493
Paul Chesa <paulchesa1@gmail.com>
2022-08-30 15:32:07 +0300

    🎉Create models library

---
+++ libs/model/convs-mgr/functions/src/lib/model-convs-mgr-functions.spec.ts
@@ -0,0 +1,7 @@
+import { modelConvsMgrFunctions } from './model-convs-mgr-functions';
+
+describe('modelConvsMgrFunctions', () => {
+  it('should work', () => {
+    expect(modelConvsMgrFunctions()).toEqual('model-convs-mgr-functions');
+  });
+});
