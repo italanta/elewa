ae4f905f7afccc58cb2861d29c9b4245c1e31278
Paul Chesa <paulchesa1@gmail.com>
2022-08-31 12:46:52 +0300

    🎉Create library for functions to handle whatsapp messages

---
+++ libs/functions/messages/whatsapp/src/lib/functions-messages-whatsapp.spec.ts
@@ -0,0 +1,7 @@
+import { functionsMessagesWhatsapp } from './functions-messages-whatsapp';
+
+describe('functionsMessagesWhatsapp', () => {
+  it('should work', () => {
+    expect(functionsMessagesWhatsapp()).toEqual('functions-messages-whatsapp');
+  });
+});
