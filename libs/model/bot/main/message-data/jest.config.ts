8abc412d8116d8ad94b757d3a3018619e7fb28c5
ReaganCn <rgncharana2@gmail.com>
2022-09-07 15:24:19 +0300

    :horse: create model for incoming message data

---
+++ libs/model/bot/main/message-data/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'model-bot-main-message-data',
+  preset: '../../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../../coverage/libs/model/bot/main/message-data',
+};
