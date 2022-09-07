c119676e99dc171546a89f110dcebcb70e46dcd1
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-07 08:22:26 +0300

    :house: (build) Added the name block model interface

---
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/name-message-block.interface.ts
@@ -0,0 +1,6 @@
+import { TextMessageBlock } from "./text-message-block.interface";
+
+/**
+ * Block which sends a message to ask the name.
+ */
+export interface NameMessageBlock extends TextMessageBlock{}
