89853d241303340fde794fe2e0bb470e081e1037
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-08 12:19:40 +0300

    :wrench: (build) Work on the image upload service. Create and add functionality

--- libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components /image-block/image-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components /image-block/image-block.component.ts
@@ -1,35 +1,64 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
-import { FormGroup,FormBuilder } from '@angular/forms';
 import { Logger } from '@iote/bricks-angular';
-
-import { ImageMessageBlock} from '@app/model/convs-mgr/stories/blocks/messaging';
-
-import { _JsPlumbComponentDecorator } from '../../../../../block-options/src/lib/providers/jsplumb-decorator.function';
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+
+import { ImageMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
+import { ImageUploadService } from '../../providers/image-upload.service';
+import { _JsPlumbComponentDecorator } from '../../providers/image-jsplumb-decorator.function';
+import { Storage,ref,uploadBytesResumable, getDownloadURL } from  '@angular/fire/storage';
+
+
 
 @Component({
   selector: 'app-image-block',
   templateUrl: './image-block.component.html',
   styleUrls: ['./image-block.component.scss'],
 })
-export class ImageBlockComponent implements OnInit 
-{
 
-  @Input () id:string;
-  @Input () block: ImageMessageBlock;
-  @Input () imageMessageForm: FormGroup;
-  @Input () jsPlumb: BrowserJsPlumbInstance;
+export class ImageBlockComponent implements OnInit {
 
-  constructor(private _fb:FormBuilder,
-              private _logger:Logger) {}
+  @Input() id: string;
+  @Input() block: ImageMessageBlock;
+  @Input() imageMessageForm: FormGroup;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
 
-  ngOnInit(): void {}
+  imageLink: string = "";
+  fileReader= new FileReader();
+  isLoadingImage: boolean = false;
+  file: File;
+
+
+  constructor(private _fb: FormBuilder,
+    private _logger: Logger,
+    private _imageUploadService: ImageUploadService) { }
+
+  ngOnInit(): void { }
 
   ngAfterViewInit(): void {
     if (this.jsPlumb) {
       this._decorateInput();
     }
+  }
+
+  processImage(event: any) {
+    this.file = event.target.files[0];
+    
+  }
+
+  onUpload() {
+
+    this.isLoadingImage = !this.isLoadingImage;
+    this._imageUploadService.upload(this.file).subscribe((event: any) => {
+      if (typeof (event) === 'object') {
+        this.imageLink  = event.link;
+        this.isLoadingImage = false;
+      }
+      ;
+    });
+
   }
 
   private _decorateInput() {
