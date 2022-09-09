af9d3c1a291b0cb5a202a6145f7e6ede355eea5b
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-09 11:38:23 +0300

    :lipstick: UI(style) Changed the height of the name block to follow the styles given

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/name-message-block.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/name-message-block.interface.ts
@@ -1,6 +1,7 @@
+import { StoryBlock } from '@app/model/convs-mgr/stories/blocks/main';
 import { TextMessageBlock } from "./text-message-block.interface";
 
 /**
  * Block which sends a message to ask the name.
  */
-export interface NameMessageBlock extends TextMessageBlock{}
+export interface NameMessageBlock extends StoryBlock{}
