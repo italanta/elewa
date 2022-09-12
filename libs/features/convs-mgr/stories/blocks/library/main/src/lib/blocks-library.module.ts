cb86c417e0f394ec1b8f8ef3509dc2c2acc59909
trevin-livele <liveletrevin6@gmail.com>
2022-09-12 13:49:54 +0300

    :lipstick:  {refactor} corrected the order of imports in the block-library.module.ts regarding issue #13

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/blocks-library.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/blocks-library.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import {
   FlexLayoutModule,
   MaterialBricksModule,
   MaterialDesignModule,
 } from '@iote/bricks-angular';
-
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
-
 import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/blocks/library/block-options';
 import { ConvsMgrTextMessageBlockModule } from '@app/features/convs-mgr/stories/blocks/library/text-message-block';
 import { ConvsMgrQuestionBlockModule } from '@app/features/convs-mgr/stories/blocks/library/question-message-block';
-
-import { BlockComponent } from './components/block/block.component';
-
-import { BlockInjectorService } from './providers/block-injector.service';
 import { MultiLangModule } from '@ngfi/multi-lang';
+import { BlockInjectorService } from './providers/block-injector.service';
+import { BlockComponent } from './components/block/block.component';
 
 @NgModule({
   imports: [
@@ -42,4 +37,4 @@ import { MultiLangModule } from '@ngfi/multi-lang';
   // Injector which creates all block types within the editor context.
   providers: [BlockInjectorService],
 })
-export class BlocksLibraryModule {}
+export class BlocksLibraryModule { }
