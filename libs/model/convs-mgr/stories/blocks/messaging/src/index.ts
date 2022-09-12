2c23af7ec2d4f845feeea83c64491e21c740de93
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-12 09:30:23 +0300

    :heavy_plus_sign: dependencies(build) Updated the dependencies to support phone-block component

--- libs/model/convs-mgr/stories/blocks/messaging/src/index.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/question-message-block.interface';
 export * from './lib/location-message-block.interface';
 export * from './lib/name-message-block.interface';
 export * from './lib/email-message-block.interface';
+export * from './lib/phone-message-block.interface';
