590ef3addca613f9909f68db158d0257ef994962
trevin-livele <liveletrevin6@gmail.com>
2022-09-13 13:31:42 +0300

    🎨 {refactor} moved the auth service logic and imports to the logout component regarding isssue #8

--- libs/elements/layout/user-menu/src/lib/components/user-menu/user-menu.component.ts
+++ libs/elements/layout/user-menu/src/lib/components/user-menu/user-menu.component.ts
@@ -1,31 +1,20 @@
 import { Observable } from 'rxjs';
-
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { User } from '@iote/bricks';
-import { UserService, AuthService} from '@ngfi/angular';
+import { UserService } from '@ngfi/angular';
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
   styleUrls: ['./user-menu.component.scss']
 })
-export class UserMenuComponent
-{
+export class UserMenuComponent {
   user$: Observable<User>;
-
   constructor(userService: UserService<User>,
-              private _router: Router,
-              private _authService: AuthService)
-  {
+    private _router: Router,) {
     this.user$ = userService.getUser();
   }
-
   isAdmin = (user: any) => user.roles.admin;
   toAdmin = () => this._router.navigate(['admin', 'translate']);
-
-  logout() {
-    this._authService.signOut('/auth/login');
-  }
 }
