31eb1404d4b49b0534c5e535b37a902c1fb40703
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-15 12:01:33 +0300

    :sparkles: :art: (feat) The jsPlumb decorator added to the audio block interface

---
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/audio-block.jsplumb.ts
@@ -0,0 +1,29 @@
+import { ComponentRef } from '@angular/core';
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+
+import { AudioMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
+import { BlockComponent } from "@app/features/convs-mgr/stories/blocks/library/main";
+
+/**
+ * Decorates MessageBlock with JS plumb connectors.
+ * 
+ * @param block   - AudioMessageBlock data structure.
+ * @param comp    - Angular component within the viewport 
+ * @param jsPlumb - Active jsPlumb instance
+ * 
+ * @see {_JsPlumbComponentDecorator} - Should be the only one calling the component
+ */
+export function _AudioBlockDecoratePlumb(block: AudioMessageBlock, comp: ComponentRef<BlockComponent>, jsPlumb: BrowserJsPlumbInstance) : ComponentRef<BlockComponent> 
+{
+
+  jsPlumb.addEndpoint(comp.location.nativeElement, {
+    // Whether the anchor is target (Other Block -> This Block)
+    target: true,
+
+    endpoint: 'Rectangle',
+    anchor: "Left"
+  });
+
+  return comp;
+}
