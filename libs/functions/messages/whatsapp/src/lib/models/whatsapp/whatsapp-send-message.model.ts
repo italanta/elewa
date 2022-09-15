18d9a7a37f4fa0a60d498b5d472ef89be7ca0f11
Paul Chesa <paulchesa1@gmail.com>
2022-09-15 16:50:45 +0300

    🚧Use axios to post message

--- libs/functions/messages/whatsapp/src/lib/models/whatsapp/whatsapp-send-message.model.ts
+++ libs/functions/messages/whatsapp/src/lib/models/whatsapp/whatsapp-send-message.model.ts
@@ -1,11 +1,21 @@
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+import { HandlerTools } from "@iote/cqrs";
+import axios from "axios";
 
 
-export class SendWhatsAppMessageModel{
-  constructor(_){ }
-  async sendMessage(message?:StoryBlock){
+export class SendWhatsAppMessageModel {
+
+  private _tools:HandlerTools;
+
+  constructor(tools: HandlerTools) { 
+    this._tools = tools;
+  }
+
+  async sendMessage(message?: StoryBlock) {
     const authorizationHeader = process.env.AUTHORIZATION_HEADER;
-    
+
+    this._tools.Logger.log(() => `[SendWhatsAppMessageModel].sendMessage: ${JSON.stringify(message)}`);
+
     const PHONE_NUMBER = 103844892462329 //Refers to business number to be used
     const url = `https://graph.facebook.com/v14.0/${PHONE_NUMBER}/messages`
     const data = {
@@ -13,33 +23,24 @@ export class SendWhatsAppMessageModel{
       "recipient_type": "individual",
       "to": "254706165412",
       "type": "text",
-      "text": { // the text object
+      "text": {
         "preview_url": false,
         "body": "test"
       }
     }
 
-    const response = await fetch(url,{
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin', 
+    const res = await axios.post(`${url}`, data, {
       headers: {
-        'Content-Type': 'application/json',
-        //Requires cronjob to get new token every 24hrs
-        //To be saved as environment variable
-        'Authorization': 'EAAIoZBI2uyCMBAK6iwQ47N6WY9Rv3pMe5PbA7dijoJViudsBY3CDoBBQYZCXUeRDycpUjU9PqVrXtktJ7yhUgL1faLUTMJTNQYoQ9l2msb6Y4e2KLHYyYWFy1KFmvLIjr2XmIQzZAopHmEr0LiXwNqrfriaM6xVtx8s8ZAz8j0dcDZA7olCrqM1tz9W5dHCB4PTsXskqNoQAZCT2SZAk251paUbhIVGuZBEZD',
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify(data)
-    })
+        'Authorization': `${authorizationHeader}`,
+        'content-type': 'text/json'
+      }
+    });
 
-    return response.json();
-    
+    return Promise.resolve(res.data.headers['Content-Type']); 
+
   }
 
-  
+
 
 
 }
