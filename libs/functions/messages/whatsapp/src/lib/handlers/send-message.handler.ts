e36c66f7408dd54afea62662997d52d34143f210
Paul Chesa <paulchesa1@gmail.com>
2022-09-15 16:50:21 +0300

    🚧Started handler to send message from whatsapp

--- libs/functions/messages/whatsapp/src/lib/handlers/send-message.handler.ts
+++ libs/functions/messages/whatsapp/src/lib/handlers/send-message.handler.ts
@@ -1,15 +1,18 @@
-import { botProvider } from "@app/model/convs-mgr/functions";
+import { BotProvider } from "@app/model/convs-mgr/functions";
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 import { HandlerTools } from "@iote/cqrs";
 import { FunctionHandler, HttpsContext } from "@ngfi/functions";
 import { SendWhatsAppMessageModel } from "../models/whatsapp/whatsapp-send-message.model";
 
-export class SendMessageHandler extends FunctionHandler<{val:StoryBlock, type: botProvider}, void>{
+export class SendMessageHandler extends FunctionHandler<{val:StoryBlock, type: BotProvider}, void>{
   
-  public async execute(data:{val:StoryBlock, type: botProvider}, context:HttpsContext, tools:HandlerTools)
+  public async execute(data:{val:StoryBlock, type: BotProvider}, context:HttpsContext, tools:HandlerTools)
   {
+    tools.Logger.log(() =>`[SendMessageHandler] Started execution with : ${data.type} ${data.val} ${data.type}`)
     const messageBlock = data.val;
     const provider = data.type;
+
+    return new SendWhatsAppMessageModel(tools).sendMessage(messageBlock)
 
 
   }
