c108dfaa6217a4f72a1551a033b13c88b1a889d2
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-15 11:59:31 +0300

    :heavy_plus_sign: (build) dependencies for the audio block interface

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/blocks-library.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/blocks-library.module.ts
@@ -16,6 +16,7 @@ import { ConvsMgrImageMessageBlockModule } from '@app/features/convs-mgr/stories
 import { ConvsMgrNameMessageBlockModule } from '@app/features/convs-mgr/stories/blocks/library/name-message-block';
 import { ConvsMgrEmailMessageBlockModule } from '@app/features/convs-mgr/stories/blocks/library/email-message-block';
 import { ConvsMgrPhoneMessageBlockModule } from '@app/features/convs-mgr/stories/blocks/library/phone-message-block';
+import { ConvsMgrAudioMessageBlockModule } from '@app/features/convs-mgr/stories/blocks/library/audio-message-block';
 
 import { BlockInjectorService } from './providers/block-injector.service';
 import { BlockComponent } from './components/block/block.component';
@@ -37,6 +38,7 @@ import { BlockComponent } from './components/block/block.component';
     ConvsMgrNameMessageBlockModule,
     ConvsMgrEmailMessageBlockModule,
     ConvsMgrPhoneMessageBlockModule,
+    ConvsMgrAudioMessageBlockModule,
     ConvsMgrBlockOptionsModule,
     MultiLangModule
   ],
