04874e613a07b8f7abc303ade4a9d191ed910925
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-15 13:29:07 +0300

    :sparkles: :lipstick: (feat) Adds jsPlumb to the video block interface

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/jsplumb-decorator.function.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/jsplumb-decorator.function.ts
@@ -4,15 +4,16 @@ import { StoryBlock, StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks
 
 import { BlockComponent } from '../components/block/block.component';
 
-import { EmailMessageBlock, ImageMessageBlock, LocationMessageBlock, NameMessageBlock, PhoneMessageBlock, QuestionMessageBlock, TextMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+import { EmailMessageBlock, ImageMessageBlock, LocationMessageBlock, NameMessageBlock, PhoneMessageBlock, QuestionMessageBlock, TextMessageBlock, VideoMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
-import { _MessageBlockDecoratePlumb } from './message-block.jsplumb'; 
+import { _MessageBlockDecoratePlumb } from './message-block.jsplumb';
 import { _QuestionsBlockDecoratePlumb } from './questions-block.jsplumb';
 import { _LocationBlockDecoratePlumb } from './location-block.jsplumb';
 import { _ImageBlockDecoratePlumb } from './image-block.jsplumb';
 import { _NameBlockDecoratePlumb } from './name-block.jsplumb';
 import { _EmailBlockDecoratePlumb } from './email-block.jsplumb';
 import { _PhoneBlockDecoratePlumb } from './phonenumber-block.jsplumb';
+import { _VideoBlockDecoratePlumb } from './video-block.jsplumb';
 
 /**
  * This function adds jsPlumb endpoints to rendered components. 
@@ -34,20 +35,24 @@ export function _JsPlumbComponentDecorator(block: StoryBlock, comp: ComponentRef
       return _ImageBlockDecoratePlumb(block as ImageMessageBlock, comp, jsPlumb);
       break;
     case StoryBlockTypes.Name:
-        return _NameBlockDecoratePlumb(block as NameMessageBlock, comp, jsPlumb);
-        break;
-     case StoryBlockTypes.Email:
-        return _EmailBlockDecoratePlumb(block as EmailMessageBlock, comp, jsPlumb);
-        break;
-      case StoryBlockTypes.PhoneNumber:
-          return _NameBlockDecoratePlumb(block as PhoneMessageBlock, comp, jsPlumb);
-          break;
+      return _NameBlockDecoratePlumb(block as NameMessageBlock, comp, jsPlumb);
+      break;
+    case StoryBlockTypes.Email:
+      return _EmailBlockDecoratePlumb(block as EmailMessageBlock, comp, jsPlumb);
+      break;
+    case StoryBlockTypes.PhoneNumber:
+      return _NameBlockDecoratePlumb(block as PhoneMessageBlock, comp, jsPlumb);
+      break;
     case StoryBlockTypes.QuestionBlock:
       return _QuestionsBlockDecoratePlumb(block as QuestionMessageBlock, comp, jsPlumb);
       break;
     case StoryBlockTypes.Location:
       return _LocationBlockDecoratePlumb(block as LocationMessageBlock, comp, jsPlumb);
       break;
+    case StoryBlockTypes.Video:
+      return _VideoBlockDecoratePlumb(block as VideoMessageBlock, comp, jsPlumb);
+      break;
+
   }
 
   // Default case
