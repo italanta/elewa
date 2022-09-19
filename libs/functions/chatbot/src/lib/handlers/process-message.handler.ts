edda87564fd2b0b5601de1ce7fba87f48bc055a7
ReaganCn <rgncharana2@gmail.com>
2022-09-19 16:26:43 +0300

    :horse_racing: :wrench: add more functionality to chatservice

--- libs/functions/chatbot/src/lib/handlers/process-message.handler.ts
+++ libs/functions/chatbot/src/lib/handlers/process-message.handler.ts
@@ -3,10 +3,10 @@ import { HandlerTools } from '@iote/cqrs';
 import { FunctionContext, FunctionHandler, RestResult200 } from '@ngfi/functions';
 
 import { RawMessageData } from '@app/model/convs-mgr/conversations/messages';
-import { createMessage } from '../model/create-message.model';
+
 import { ChatBotService } from '../services/main-chatbot.service';
 import { NextBlockFactory } from '../services/next-block.factory';
-import { ChatBotStore } from '../services/chatbot.store';
+import { ChatBotStore, EndUser } from '../services/chatbot.store';
 
 
 export class ProcessMessageHandler extends FunctionHandler<RawMessageData, RestResult200>
@@ -30,7 +30,6 @@ export class ProcessMessageHandler extends FunctionHandler<RawMessageData, RestR
   {
     tools.Logger.log(() => `[ProcessMessageHandler]._processMessage: Processing message ${JSON.stringify(msg)}.`);
 
-    const chatService =  new ChatBotService(tools.Logger)
     const chatBotRepo$ =  new ChatBotStore(tools)
 
     const endUser = await chatBotRepo$.getEndUser(msg.phoneNumber)
@@ -41,18 +40,21 @@ export class ProcessMessageHandler extends FunctionHandler<RawMessageData, RestR
     const userActivity =  await chatBotRepo$.getActivity(endUser);
 
     if(!userActivity){
-      return await this._initSession(endUser, chatService, msg, tools)
+      return await this._initSession(endUser, msg, tools)
     } else {
-      return await this._nextBlock(endUser, chatBotRepo$, msg, tools)
+      return await this._contSession(endUser, chatBotRepo$, msg, tools)
     }
   }
 
   /** If a chat session has not yet been recorded on this container, we create a new one and return the first block
    *  
   */
-  private async _initSession(endUser: any, chatService: ChatBotService, msg: RawMessageData, tools: HandlerTools)
-  {
+  private async _initSession(endUser: any, msg: RawMessageData, tools: HandlerTools)
+  {    
+    const chatService =  new ChatBotService(tools.Logger)
     const firstBlock = await chatService.init(endUser, tools)
+
+    tools.Logger.log(() => `[ProcessMessageHandler]._initSession: Session initialized ${JSON.stringify(msg)}.`);
 
     return firstBlock
   }
@@ -66,13 +68,13 @@ export class ProcessMessageHandler extends FunctionHandler<RawMessageData, RestR
    * @param tools 
    * @returns 
    */
-  private async _nextBlock(endUser: any, chatBotRepo$: ChatBotStore, msg: RawMessageData, tools: HandlerTools){
+  private async _contSession(endUser: EndUser, chatBotRepo$: ChatBotStore, msg: RawMessageData, tools: HandlerTools){
     const latestBlock = await chatBotRepo$.getLatestActivity(endUser)
     const nextBlockService = new NextBlockFactory().resoveBlockType(latestBlock.type, tools)
 
     const nextBlock = await nextBlockService.getNextBlock(endUser, msg.message, latestBlock)
 
-    const block = await chatBotRepo$.updateActivity(endUser, nextBlock)
+    const block = await chatBotRepo$.updateCursor(endUser, nextBlock)
 
     return block;
   }
--- libs/functions/chatbot/src/lib/services/main-chatbot.service.ts
+++ libs/functions/chatbot/src/lib/services/main-chatbot.service.ts
@@ -1,7 +1,7 @@
 import { HandlerTools } from '@iote/cqrs';
 import { Logger } from '@iote/cqrs';
 
-import { Block, ChatBotStore, DefaultBlock, EndUser } from './chatbot.store';
+import { Block, ChatBotStore, ChatStatus, DefaultBlock, EndUser } from './chatbot.store';
 
 
 /**
@@ -9,6 +9,7 @@ import { Block, ChatBotStore, DefaultBlock, EndUser } from './chatbot.store';
  */
 export class ChatBotService {
 
+  chatId: string;
   constructor(private _logger: Logger) {}
 
   async init(user: EndUser, tools: HandlerTools): Promise<Block> {
@@ -19,11 +20,44 @@ export class ChatBotService {
 
     // Get the next block using the default block
     const nextBlock: Block = await chatBotRepo$.getNextBlockFromDefault(user, defaultBlock);
+    const chatData = await chatBotRepo$.initChatStatus(user); 
 
+    this.chatId = chatData.chatId;
     // Save User Activity
-    await chatBotRepo$.updateActivity(user, defaultBlock);
+    await chatBotRepo$.updateCursor(user, nextBlock);
 
     return nextBlock;
   }
 
+  async pause(user: EndUser, tools: HandlerTools){
+    this._logger.log(()=> `[ChatBotService].pause - Pausing Chat`)
+
+    const chatBotRepo$ =  new ChatBotStore(tools)
+    await chatBotRepo$.updateChatStatus(user, ChatStatus.Paused)
+  }
+
+  async resume(user: EndUser, tools: HandlerTools){
+    this._logger.log(()=> `[ChatBotService].resume - Resuming Chat`)
+
+    const chatBotRepo$ =  new ChatBotStore(tools)
+    await chatBotRepo$.updateChatStatus(user, ChatStatus.Running)
+  }
+
+  async end(user: EndUser, tools: HandlerTools){
+    this._logger.log(()=> `[ChatBotService].end - Ending Session`)
+
+    const chatBotRepo$ =  new ChatBotStore(tools)
+    await chatBotRepo$.updateChatStatus(user, ChatStatus.Ended)
+  }
+
+  async jumpToBlock(blockId: string, user: EndUser, tools: HandlerTools){
+    this._logger.log(()=> `[ChatBotService].Jump - Jumping to block ${blockId}`)
+
+    const chatBotRepo$ =  new ChatBotStore(tools)
+    const newBlock = await chatBotRepo$.getBlockById(blockId, user)
+    
+    await chatBotRepo$.updateCursor(user, newBlock);
+
+    return newBlock
+  }
 }
