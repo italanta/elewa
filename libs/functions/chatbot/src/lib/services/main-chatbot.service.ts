10f7fd5337ba41c52f7c5245fd174609233cef79
ReaganCn <rgncharana2@gmail.com>
2022-09-19 11:03:45 +0300

    :construction: :horse_racing:  implement clm firebase service

--- libs/functions/chatbot/src/lib/services/main-chatbot.service.ts
+++ libs/functions/chatbot/src/lib/services/main-chatbot.service.ts
@@ -1,75 +1,119 @@
-import * as fetch from 'node-fetch';
+import { Query } from '@ngfi/firestore-qbuilder';
+import { HandlerTools } from '@iote/cqrs';
 import { Logger } from '@iote/cqrs';
+
+import { StoryBlock } from '@app/model/convs-mgr/stories/blocks/main';
+import { TextMessageBlock, QuestionMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
 import { config } from '../environment/environment';
 
 /**
- * Integrates with the Landbot API to exchange information back and forth.
+ * Handles all Firestore processes
  */
-export class ChatBotService
-{
-  constructor(private _logger: Logger) { }
+export class ChatBotService {
+  constructor(private _logger: Logger) {}
 
-   /**
-   * Pause bot and assign a user to an agent.
-   *
-   * @param chatId  - Chat to assign to operator
-   * 
-   * */
-  pauseBot(chatId: string)
-  {
-    throw new Error("Not yet implemented");
+  async init(user: EndUser, tools: HandlerTools): Promise<Block> {
+    // Get the default block
+    const defaultBlock: DefaultBlock = await this._getDefaultBlock(user, tools);
+
+    const activityRepo$ = tools.getRepository<DefaultBlock>(`user-activity/${user.id}/stories/${user.storyId}/milestones`);
+
+    // Create subject
+    await activityRepo$.write(defaultBlock, defaultBlock.id);
+
+    // Get the next block using the default block
+    const nextBlock: Block = await this._getNextBlockFromDefault(user, defaultBlock, tools);
+
+    return nextBlock;
   }
 
-   /**
-   * Send a message to a user over a channel.
-   *
-   * @param chatId  - Chat to send message through
-   * @param message - Message to send
-   * */
-  sendMessage(chatId: string, message: string)
-  {
-    throw new Error("Not yet implemented");
+  async getLatestActivity(user: EndUser, tools: HandlerTools): Promise<Block> {
+    const cursorRepo$ = tools.getRepository<Block>(`user-activity/${user.id}/stories/${user.storyId}/cursor`);
+
+    const latestBlock = await cursorRepo$.getDocuments(new Query().orderBy('createdOn', 'desc').limit(1));
+
+    return latestBlock[0];
   }
 
-  setOnline()
-  {
-    throw new Error("Not yet implemented");
+  async updateActivity(user: EndUser, newBlock: Block, tools: HandlerTools): Promise<Block> {
+    const cursorRepo$ = tools.getRepository<Block>(`user-activity/${user.id}/stories/${user.storyId}/cursor`);
+
+    //Update milestone
+    const block = await cursorRepo$.update(newBlock);
+
+    // Return next block
+    return block;
   }
 
-  assignToAgent(customer_id: string)
-  {
-    throw new Error("Not yet implemented");
+  async getActivity(user: EndUser, tools: HandlerTools) {
+    // Get subject
+    // TODO: Create a type for user-activity
+    const activityRepo$ = tools.getRepository<Block>(`user-activity/${user.id}/stories/${user.storyId}/milestones`);
+    const activity = await activityRepo$.getDocumentById(user.id);
+
+    return activity;
   }
 
-  ifOnline()
-  {
-    throw new Error("Not yet implemented");
+  async getUser(phoneNumber: string, tools: HandlerTools): Promise<EndUser> {
+    // Get users
+    const userRepo$ = tools.getRepository<EndUser>('end-users');
+    const user = await userRepo$.getDocumentById(phoneNumber);
+    if (!user) {
+      throw new Error('User does not exist!');
+    }
+    return user;
   }
 
+  private async _getNextBlockFromDefault(user: EndUser, defaultBlock: DefaultBlock, tools: HandlerTools): Promise<Block> {
+    const nextBlock: Block = await this._getBlockById(defaultBlock.nextBlock, user, tools);
 
-   /**
-   * Resume chat
-   *
-   * @param chatId   - Chat to send message through
-   * @param botId  - Bot to redirect the user to
-   * @param blockRef - Block Reference within chat
-   */
-  resumeChat(chatId: string, botId: number, blockRef?: string)
-  {
-    throw new Error("Not yet implemented");
+    const BlockRepo$ = tools.getRepository<Block>(`user-activity/${user.id}/stories/${user.storyId}/milestones`);
+
+    await BlockRepo$.write(nextBlock, nextBlock.id);
+
+    return nextBlock;
   }
 
+  private async _getDefaultBlock(user: EndUser, tools: HandlerTools): Promise<DefaultBlock> {
+    const orgRepo$ = tools.getRepository<DefaultBlock>(`orgs/${user.orgId}/stories/${user.storyId}/blocks`);
+    const id = 'default';
 
-  registerMessagehook(channelId: string, ref: string, name: string, cbUrl?: string)
-  {
-    throw new Error("Not yet implemented");
+    const defaultBlock = await orgRepo$.getDocumentById(id);
+
+    if (!defaultBlock) {
+      throw new Error('No default block set');
+    }
+    return defaultBlock;
   }
 
-  // Perform a call to Landbot
-  private async _callLandbot(res: string, body: any, method: string = 'post') : Promise<any>
-  {
-    throw new Error("Not yet implemented");
-  }
+  private async _getBlockById(id: string, user: any, tools: HandlerTools): Promise<Block> {
+    const orgRepo$ = tools.getRepository<Block>(`orgs/${user.orgId}/stories/${user.storyId}/blocks`);
 
+    const block: Block = await orgRepo$.getDocumentById(id);
+
+    if (!block) {
+      throw new Error('Block does not exist');
+    }
+
+    return block;
+  }
 }
+
+export interface EndUser {
+  id: string;
+  orgId: string;
+  storyId: string;
+  platform: Platforms;
+}
+
+export enum Platforms {
+  WhatsApp = 'whatsapp',
+  Telegram = 'telegram',
+}
+
+export interface DefaultBlock extends StoryBlock {
+  nextBlock: string;
+}
+
+export type Block = TextMessageBlock | QuestionMessageBlock;
