28c001f708b710efd182c04877204715c025cbac
ReaganCn <rgncharana2@gmail.com>
2022-09-20 12:02:23 +0300

    :construction: :art: add anchor block module and component

---
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/convs-mgr-stories-blocks-library-anchor-block.module';
---
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/convs-mgr-stories-blocks-library-anchor-block.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnchorBlockComponent } from './components/anchor-block/anchor-block.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    AnchorBlockComponent
+  ],
+})
+export class AnchorBlockModule {}
