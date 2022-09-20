e362e5ee2350f2a4e7ed7354ed9fe3e9f323883b
ReaganCn <rgncharana2@gmail.com>
2022-09-20 12:02:23 +0300

    :construction: :art: add anchor block module and component

---
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/components/anchor-block/anchor-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anchor-block',
+  templateUrl: './anchor-block.component.html',
+  styleUrls: ['./anchor-block.component.scss']
+})
+export class AnchorBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
