b56b133b6511d2aaa05c4d93059ef9499039fc3e
ReaganCn <rgncharana2@gmail.com>
2022-09-20 12:08:14 +0300

    :construction: :art: add content to anchor block

--- libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/convs-mgr-stories-blocks-library-anchor-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/convs-mgr-stories-blocks-library-anchor-block.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnchorBlockComponent } from './components/anchor-block/anchor-block.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialDesignModule, MaterialBricksModule } from '@iote/bricks-angular';
+import { ConvsMgrBlockOptionsModule } from '../../../block-options/src';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [
-    AnchorBlockComponent
+  imports: [
+    CommonModule, 
+    MaterialDesignModule, 
+    FlexLayoutModule, 
+    MaterialBricksModule, 
+    ConvsMgrBlockOptionsModule
   ],
+
+  declarations: [AnchorBlockComponent],
 })
 export class AnchorBlockModule {}
