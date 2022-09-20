75b00486e1fd6953e629414719e8d8670d55ab80
ReaganCn <rgncharana2@gmail.com>
2022-09-20 15:35:20 +0300

    :art: :bug: fix bugs on the anchor block

--- libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/convs-mgr-stories-blocks-library-anchor-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/convs-mgr-stories-blocks-library-anchor-block.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AnchorBlockComponent } from './components/anchor-block/anchor-block.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MaterialDesignModule, MaterialBricksModule } from '@iote/bricks-angular';
-import { ConvsMgrBlockOptionsModule } from '../../../block-options/src';
+import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+
+import { AnchorBlockComponent } from './components/anchor-block/anchor-block.component';
 
 @NgModule({
   imports: [
     CommonModule, 
     MaterialDesignModule, 
     FlexLayoutModule, 
-    MaterialBricksModule, 
+    MaterialBricksModule,
+    
+    ReactiveFormsModule,
+
     ConvsMgrBlockOptionsModule
   ],
 
   declarations: [AnchorBlockComponent],
+  exports: [AnchorBlockComponent],
 })
-export class AnchorBlockModule {}
+export class ConvsMgrAnchorBlockModule {}
