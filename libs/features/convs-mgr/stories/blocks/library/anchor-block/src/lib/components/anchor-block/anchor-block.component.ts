b720443997598885ce74f601c2a135107d58fd2b
ReaganCn <rgncharana2@gmail.com>
2022-09-21 17:53:05 +0300

    :construction: :art: add endpoint to anchor block

--- libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/components/anchor-block/anchor-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/components/anchor-block/anchor-block.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
 import { Logger } from '@iote/bricks-angular';
 
+import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+
 import { AnchorBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
 @Component({
   selector: 'app-anchor-block',
   templateUrl: './anchor-block.component.html',
   styleUrls: ['./anchor-block.component.scss'],
 })
-export class AnchorBlockComponent implements OnInit {
+export class AnchorBlockComponent implements OnInit, AfterViewInit {
   @Input() id: string;
   @Input() block: AnchorBlock;
   @Input() jsPlumb: BrowserJsPlumbInstance;
@@ -21,4 +24,17 @@ export class AnchorBlockComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void 
+  {
+    this._decorateInput();
+  }
+
+  private _decorateInput() 
+  {
+    let input = document.getElementById(this.id) as Element;
+    if (this.jsPlumb) {
+      input = _JsPlumbComponentDecorator(input, this.jsPlumb);
+    }
+  }
 }
