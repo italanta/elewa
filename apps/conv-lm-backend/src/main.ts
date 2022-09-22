f5c589458d194059a1cbacfbf2a196c71898c45c
ReaganCn <rgncharana2@gmail.com>
2022-09-22 08:24:23 +0300

    :rocket: :sparkles: deploy register user function

--- apps/conv-lm-backend/src/main.ts
+++ apps/conv-lm-backend/src/main.ts
@@ -1,1 +1,12 @@
 export * from './app/a-basic-function';
+
+import { config } from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+// Init Firebase Upload
+const conf = config().firebase;
+admin.initializeApp(conf);
+
+export * from './app/a-basic-function';
+export * from './app/functions/bot/processMessage.function';
+export * from './app/functions/bot/registerUser.function';
