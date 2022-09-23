83cecfbbeb24b0be553f1edd61ae9aa7d1d3864f
ReaganCn <rgncharana2@gmail.com>
2022-09-23 11:37:43 +0300

    :sparkles: :hammer: implement strategy pattern for matching inputs

---
+++ libs/functions/chatbot/src/lib/services/match-input/match-input.service.ts
@@ -0,0 +1,20 @@
+import { findIndex as __findIndex } from 'lodash';
+import { MatchStrategy } from './match-strategy.interface';
+
+/**
+ * [Work In Progress]
+ * Contains methods to match options and user input
+ */
+export class MatchInputService {
+
+    private matchStrategy: MatchStrategy;
+
+    setMatchStrategy(matchStrategy: MatchStrategy){
+        this.matchStrategy = matchStrategy
+    }
+
+    match(message: string, options: any[]){
+        return this.matchStrategy.match(message, options)
+    }
+
+}
