2677c210a90ddec66f5795a39493f44417c70274
Jente Rosseel <rosseeljente@gmail.com>
2022-09-26 15:50:07 +0200

    :truck: @ngfi/infinite-scroll

--- libs/util/ngfi/infinite-scroll/src/index.ts
+++ libs/util/ngfi/infinite-scroll/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/util-ngfi-infinite-scroll.module';
+export * from './lib/model/pagination-config.interface';
+export * from './lib/model/paginated-scroll.model';
+
+export * from './lib/directives/scrollbar.directive';
+
+export * from './lib/infinite-scroll.module';
