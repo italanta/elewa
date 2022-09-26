17699a90aaa51c5799fea38591715a0f03ffb3c1
ReaganCn <rgncharana2@gmail.com>
2022-09-26 10:26:29 +0300

    :horse_racing: :wrench: add a method to handle possible a race condition on firebase

--- libs/functions/chatbot/src/lib/services/main-chatbot.service.ts
+++ libs/functions/chatbot/src/lib/services/main-chatbot.service.ts
@@ -1,10 +1,11 @@
-import { Platforms } from '@app/model/convs-mgr/conversations/admin/system';
-import { ChatInfo, Block, DefaultBlock } from '@app/model/convs-mgr/conversations/chats';
-import { ChatStatus } from '@app/model/convs-mgr/conversations/messages';
 import { HandlerTools } from '@iote/cqrs';
 import { Logger } from '@iote/cqrs';
 
 import { ChatBotStore } from './chatbot.store';
+
+import { Platforms } from '@app/model/convs-mgr/conversations/admin/system';
+import { ChatInfo, Block, DefaultBlock } from '@app/model/convs-mgr/conversations/chats';
+import { ChatStatus, Message } from '@app/model/convs-mgr/conversations/messages';
 
 
 /**
@@ -16,7 +17,7 @@ export class ChatBotService {
 
   constructor(private _logger: Logger, private _platform: Platforms) {}
 
-  async init(chatInfo: ChatInfo, tools: HandlerTools): Promise<Block> {
+  async init(msg: Message, chatInfo: ChatInfo, tools: HandlerTools): Promise<Block> {
     this._logger.log(()=> `[ChatBotService].init - Initializing Chat`)
     const chatBotRepo$ =  new ChatBotStore(tools)
 
@@ -27,6 +28,8 @@ export class ChatBotService {
 
     // Use the connection.targetId, to get the data of the first block
     let firstBlock: Block = await chatBotRepo$.getBlockById(connection.targetId, chatInfo)
+
+    await this.handleDuplicates(msg, tools)
 
     const chatData = await chatBotRepo$.initChatStatus(chatInfo, this._platform); 
     this._logger.log(()=> `[ChatBotService].init - Initialized Chat Status`)
@@ -67,5 +70,20 @@ export class ChatBotService {
     await chatBotRepo$.moveCursor(chatInfo, newBlock, this._platform);
 
     return newBlock
+  }
+
+  /**
+   * Handles possible race condition, checks if a new message was added while processing the current message
+   * If a new message was added, then its possible another instance of the function is running, so this will stop the current function with an error
+   * @param msg 
+   * @param tools 
+   */
+  async handleDuplicates(msg: Message, tools: HandlerTools){
+    const chatBotRepo$ =  new ChatBotStore(tools)
+    const latestMessage = await chatBotRepo$.getLatestMessage(msg)
+
+    if(msg != latestMessage){
+      throw new Error(`[ProcessMessageHandler]._handleDuplicates: Another message has been added during this transaction`);
+    }
   }
 }
