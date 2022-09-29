12090af16ba1185fcd36e81820e2747de337a3b1
ReaganCn <rgncharana2@gmail.com>
2022-09-29 11:34:10 +0300

    :truck: update imports after merge

--- apps/conv-lm-backend/src/app/functions/bot/addMessage.function.ts
+++ apps/conv-lm-backend/src/app/functions/bot/addMessage.function.ts
@@ -1,12 +1,12 @@
 import { AddMessageHandler } from "@app/functions/conversations/messages/add-message";
 import { RestRegistrar } from "@ngfi/functions";
 
-import { GCPFunction } from "../../../function";
+import { ConvLearnFunction } from "../../../conv-learn-func.class";
 
 
 const handler = new AddMessageHandler();
 
-export const addMessage = new GCPFunction(
+export const addMessage = new ConvLearnFunction(
     "addMessage",
     new RestRegistrar(),
     [],
