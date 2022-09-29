67a4901f700313e0b92bcf902f308c7cd395a787
ReaganCn <rgncharana2@gmail.com>
2022-09-29 11:38:36 +0300

    :speech_balloon: change text to use multilang

--- libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/convs-mgr-stories-blocks-library-anchor-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/convs-mgr-stories-blocks-library-anchor-block.module.ts
@@ -5,6 +5,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialDesignModule, MaterialBricksModule } from '@iote/bricks-angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
 import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/blocks/library/block-options';
 
 import { AnchorBlockComponent } from './components/anchor-block/anchor-block.component';
@@ -15,6 +17,7 @@ import { AnchorBlockComponent } from './components/anchor-block/anchor-block.com
     MaterialDesignModule, 
     FlexLayoutModule, 
     MaterialBricksModule,
+    MultiLangModule,
     
     ReactiveFormsModule,
 
