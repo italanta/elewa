f0008115bf3b0bc0e4d63ced01b36e2d9d3f3c8a
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-30 10:23:27 +0300

    :house: (build) model on which the file upload is built on

---
+++ libs/state/file/store/src/lib/model/file-upload.interface.ts
@@ -0,0 +1,15 @@
+import { IObject } from '@iote/bricks';
+import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+
+/**
+ * The interface that supports the upload of files in firestore.
+ * Will allow storage of files and the interpretation of files to the chatbot
+ */
+export interface FileUpload extends IObject 
+{
+  fileId?:string;
+  fileType?: StoryBlockTypes;
+  filePath?: string;
+  size?: string;
+  
+}
