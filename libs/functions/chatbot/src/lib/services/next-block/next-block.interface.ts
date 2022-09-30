1b451187a2bc2c58cedcc45a779d09788b542e5a
Reagan Charana <rgncharana2@gmail.com>
2022-09-30 12:13:11 +0300

    :sparkles: :wrench: concrete classes now use abstract class NextBlockService

--- libs/functions/chatbot/src/lib/services/next-block/next-block.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Block, ChatInfo } from "@app/model/convs-mgr/conversations/chats";
-
-export interface NextBlockInterface {
-    userInput: string;
-    getNextBlock: (chatInfo: ChatInfo, message: string, lastBlock: Block) => Promise<Block>;
-}
