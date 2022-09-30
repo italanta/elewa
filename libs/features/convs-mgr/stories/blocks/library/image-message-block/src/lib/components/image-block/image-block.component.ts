361f1487035b2ccb1017c6889518d62e6006c908
Lucywambui <lmbugua63.lm@gmail.com>
2022-09-30 09:58:25 +0300

    :lipstick:  (style) Update files to allow the image to upload

--- libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
@@ -1,13 +1,20 @@
+
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 import { Logger } from '@iote/bricks-angular';
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+import { finalize } from 'rxjs/operators';
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
-import { ImageUploadService } from '../../providers/image-upload.service';
+
+import { UploadFileService } from '@app/state/file'; 
 
 import { ImageMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
 
 
 @Component({
@@ -23,17 +30,24 @@ export class ImageBlockComponent implements OnInit {
   @Input() imageMessageForm: FormGroup;
   @Input() jsPlumb: BrowserJsPlumbInstance;
 
-  imageLink: string = "";
-  fileReader = new FileReader();
+
+
   isLoadingImage: boolean = false;
-  file: File;
   imageInputId: string;
-  defaultImage: string ="assets/images/lib/block-builder/image-block-placeholder.jpg"
+  defaultImage: string = "assets/images/lib/block-builder/image-block-placeholder.jpg"
+  file: File;
+  imageLink: string = this.defaultImage;
+  imageName: string = '';
+  
+
 
 
   constructor(private _fb: FormBuilder,
     private _logger: Logger,
-    private _imageUploadService: ImageUploadService) { }
+    private _imageUploadService: UploadFileService,
+    public domSanitizer: DomSanitizer,
+    private storage: AngularFireStorage,
+    private _db: AngularFireDatabase) { }
 
   ngOnInit(): void {
     this.imageInputId = `img-${this.id}`
@@ -45,14 +59,34 @@ export class ImageBlockComponent implements OnInit {
     }
   }
 
-  processImage(event: any) {
-    this.file = event.target.files[0];
-    this.fileReader.readAsDataURL(this.file);
-    this.fileReader.onload = () => {
-      this.isLoadingImage = true;
-      this.imageLink = this.fileReader.result as string;
-    }
+  onKey(value: string) {
+    this.imageName += value;
+  }
 
+  processImage(event: any) {
+    if (event.target.files && event.target.files[0]){
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.imageLink = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.file = event.target.files[0];
+    } else {
+      this.imageLink = this.defaultImage;
+    }   
+    this.isLoadingImage = true;
+    var imgFilePath = `images/${this.file.name}_${new Date().getTime()}`;
+    const imgFileRef = this.storage.ref(imgFilePath);
+    this.storage.upload(imgFilePath, this.file).snapshotChanges().pipe(finalize(() => {
+      imgFileRef.getDownloadURL().subscribe((url) => {
+        this.imageMessageForm.controls['src'] = url;
+        this._imageUploadService.upload(this.block, url, this.block.type);
+      })
+    })
+    ).subscribe();
+  }
+
+
+  get formControl() {
+    return this.imageMessageForm.controls;
   }
 
   private _decorateInput() {
