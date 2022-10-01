f2cb6bdaafc8577985ddcccf090cb6c11933c974
Paul Chesa <paulchesa1@gmail.com>
2022-10-01 07:59:29 +0300

    🔧Use conv learn function

--- apps/conv-lm-backend/src/app/functions/bot/registerUser.function.ts
+++ apps/conv-lm-backend/src/app/functions/bot/registerUser.function.ts
@@ -1,13 +1,11 @@
 import { RegisterEndUserHandler } from "@app/functions/conversations/onboarding";
 import { RestRegistrar } from "@ngfi/functions";
-
-import { GCPFunction } from "../../../function";
-
+import { ConvLearnFunction } from "apps/conv-lm-backend/src/conv-learn-func.class";
 
 const handler = new RegisterEndUserHandler();
 
 // The function itself
-export const registerUser = new GCPFunction(
+export const registerUser = new ConvLearnFunction(
         "registerUser",
         new RestRegistrar(),
         [],
--- apps/conv-lm-backend/src/app/webhooks/whatsapp-message-webhook.function.ts
+++ apps/conv-lm-backend/src/app/webhooks/whatsapp-message-webhook.function.ts
@@ -2,6 +2,7 @@ import { WhatsAppMessageHookHandler } from "@app/functions/webhooks";
 import { EndpointRegistrar } from "@ngfi/functions";
 import { ConvLearnFunction } from "../../conv-learn-func.class";
 
+
 const handler = new WhatsAppMessageHookHandler();
 
 //Webhook used to register whatsapp bot on app on meta site
