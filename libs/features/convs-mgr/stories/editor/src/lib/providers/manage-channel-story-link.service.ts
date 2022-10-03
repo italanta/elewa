007e2be30e70d477262bb66fa063c816677ae58e
Paul Chesa <paulchesa1@gmail.com>
2022-10-03 05:54:48 +0300

    ✨Add bot to particular channel form finished

--- libs/features/convs-mgr/stories/editor/src/lib/providers/manage-channel-story-link.service.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/providers/manage-channel-story-link.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BaseChannel, ChannelOptions } from '@app/model/bot/channel';
+import { DataService } from '@ngfi/angular';
 
 @Injectable({providedIn: 'root'})
 
 export class ManageChannelStoryLinkService {
-  constructor() { }
+  
+  constructor(private _repoFac: DataService) { }
+
+
+  public addStoryToChannel(channel: BaseChannel)
+  {
+    const _channelRepo = this._repoFac.getRepo<BaseChannel>(`channels/${channel.channelName}`);
+    return _channelRepo.write(channel, channel.businessPhoneNumber);
+  }
+
   
 }
