e5b4ee504f193fb37f9b31729a92b41a06d03d31
Paul Chesa <paulchesa1@gmail.com>
2022-10-03 05:55:47 +0300

    🔧Edit interface to more general business id

--- libs/model/bot/channel/src/lib/whatsapp-channel.interface.ts
+++ libs/model/bot/channel/src/lib/whatsapp-channel.interface.ts
@@ -2,5 +2,5 @@ import { BaseChannel } from "./base-channel.interface";
 
 export interface WhatsappChannel extends BaseChannel {
   //For each number used in the channel, it must be have a whatsapp business identifier
-  whatsappBusinessId: string;
+  businessId: string;
 }
