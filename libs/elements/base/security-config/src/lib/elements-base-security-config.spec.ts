b82b1789a5fdb09d963022c2ed923ea8a31df8e7
Paul Chesa <paulchesa1@gmail.com>
2022-10-04 08:43:40 +0300

    🔥Remove unsed files

--- libs/elements/base/security-config/src/lib/elements-base-security-config.spec.ts
+++
@@ -1,9 +0,0 @@
-import { elementsBaseSecurityConfig } from './elements-base-security-config';
-
-describe('elementsBaseSecurityConfig', () => {
-  it('should work', () => {
-    expect(elementsBaseSecurityConfig()).toEqual(
-      'elements-base-security-config'
-    );
-  });
-});
