40666e73f3cef01bf679d3811c3b6ba9997080b7
Lucywambui <lmbugua63.lm@gmail.com>
2022-10-04 14:36:18 +0300

    :truck: :bug: (refactor) Arranged the code to follow certain standards

--- libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
@@ -1,3 +1,4 @@
+import { finalize, map } from 'rxjs/operators';
 
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
@@ -7,13 +8,12 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 import { Logger } from '@iote/bricks-angular';
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
-import { finalize } from 'rxjs/operators';
+
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
-import { FileUpload, UploadFileService } from '@app/state/file';
+import { UploadFileService } from '@app/state/file';
 
 import { ImageMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
-import { isArgumentsObject } from 'util/types';
 
 @Component({
   selector: 'app-image-block',
@@ -39,14 +39,11 @@ export class ImageBlockComponent implements OnInit {
   imageName: string = '';
 
 
-
-
-
   constructor(private _fb: FormBuilder,
     private _logger: Logger,
     private _imageUploadService: UploadFileService,
     public domSanitizer: DomSanitizer,
-    private storage: AngularFireStorage,
+    private _ngfiStorage: AngularFireStorage,
     private _db: AngularFireDatabase) { }
 
   ngOnInit(): void {
@@ -64,32 +61,23 @@ export class ImageBlockComponent implements OnInit {
     this.imageName += value;
   }
 
-  processImage(event: any) {
+  processImage(event: any)
+  {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
       reader.onload = (e: any) => this.imageLink = e.target.result;
       reader.readAsDataURL(event.target.files[0]);
       this.file = event.target.files[0];
-    } else {
+      this.isLoadingImage = true;
+    
+    }else 
+    {
       this.imageLink = this.defaultImage;
     }
-    this.isLoadingImage = true;
-    var imgFilePath = `images/${this.file.name}_${new Date().getTime()}`;
-    this.imageMessageForm.patchValue({src: imgFilePath})
-    const imgFileRef = this.storage.ref(imgFilePath);
-    this.storage.upload(imgFilePath, this.file).snapshotChanges().pipe(finalize(() => {
-    imgFileRef.getDownloadURL().subscribe((url: string) => {
-      this.imageLink = url;
-      debugger
-       this._imageUploadService.upload(this.block, url, this.block.type, this.file);
-      })
-    })
-    ).subscribe();
-  
+      this.isLoadingImage = true;
+      this._imageUploadService.uploadFile(this.file , this.block, this.block.type, this.imageMessageForm);
+        
   }
-
-
-
   
 
   private _decorateInput() {
--- libs/state/file/store/src/lib/providers/upload-file.service.ts
+++ libs/state/file/store/src/lib/providers/upload-file.service.ts
@@ -5,7 +5,7 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { Repository, DataService } from '@ngfi/angular';
 
 import { of } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { finalize, switchMap, map, tap } from 'rxjs/operators';
 
 import { Logger } from '@iote/bricks-angular';
 
@@ -15,18 +15,20 @@ import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 import { ActiveOrgStore } from '@app/state/organisation';
 
 import { FileUpload } from '../model/file-upload.interface';
+import { stringify } from 'querystring';
+import { url } from 'inspector';
+import { getDownloadURL } from 'firebase/storage';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UploadFileService
-{
+export class UploadFileService {
   protected store = 'file-store';
 
   protected _activeRepo: Repository<FileUpload>;
-  private _activeOrg: Organisation;
-
   org: Organisation;
+  imageLink: string;
+
 
   constructor(private _org$$: ActiveOrgStore,
     private _repoFac: DataService,
@@ -34,23 +36,22 @@ export class UploadFileService
     private _ngfiStorage: AngularFireStorage) { }
 
 
-  upload(block: FileUpload, url: string, blockType: StoryBlockTypes, file: File) {
-      const fileBlock = {
-        fileId: block.id,
-        filePath: url,
-        fileType: blockType,
-        size: '3MB'
-      };
-      
+  upload(block: FileUpload, url: string, blockType: StoryBlockTypes, form: FormGroup) {
 
-     
+    const fileBlock = {
+      fileId: block.id,
+      filePath: url,
+      fileType: blockType,
+      size: '3MB'
+    };
+
     this._org$$.get().pipe(
       map((org) => {
-        this.org = org
+        this.org = org;
         return (!!org ?? false)
       }),
       switchMap(org => {
-        if(!!org){
+        if (!!org) {
           this._activeRepo = this._repoFac.getRepo<FileUpload>(`orgs/${this.org.id}/files`);
           return this._activeRepo.create(fileBlock)
         } else {
@@ -58,20 +59,30 @@ export class UploadFileService
         }
       }),
       switchMap((rs) => {
-        console.log(rs);
-        this.uploadFile(file)
+        // this.uploadFile(file);
         return of([rs])
       })
-    ).subscribe(value =>{
+    ).subscribe(value => {
       return value;
     });
+    console.log(url);
+    form.controls['src'].setValue(url);
 
   }
 
-  public uploadFile(file: any){
-    this._ngfiStorage.upload('/files', file);
+  public uploadFile(file: File, block: FileUpload, blockType: StoryBlockTypes, form:FormGroup): any {
+    var imgFilePath = `images/${file.name}_${new Date().getTime()}`;
+    const imgFileRef = this._ngfiStorage.ref(imgFilePath);
+    const uploadTask = this._ngfiStorage.upload(imgFilePath, file);
+  
+    uploadTask.snapshotChanges().pipe(finalize(() => {
+      imgFileRef.getDownloadURL().subscribe((url: string) => {
+        this.imageLink = url
+        this.upload(block, url, blockType, form);  
+      })
+    })
+    ).subscribe();
   }
+
+
 }
-
-
-
