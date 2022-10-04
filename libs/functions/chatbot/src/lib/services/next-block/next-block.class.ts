8005ffd7cb52054673aaf944734d5105522574de
ReaganCn <rgncharana2@gmail.com>
2022-10-04 14:45:51 +0300

    :construction: :wrench: update function parameters

--- libs/functions/chatbot/src/lib/services/next-block/next-block.class.ts
+++ libs/functions/chatbot/src/lib/services/next-block/next-block.class.ts
@@ -4,6 +4,7 @@ import { ChatBotStore } from "@app/functions/chatbot";
 
 import { Block, ChatInfo } from "@app/model/convs-mgr/conversations/chats";
 import { TextMessageBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
+import { Message } from "@app/model/convs-mgr/conversations/messages";
 
 export abstract class NextBlockService {
 
@@ -17,11 +18,11 @@ export abstract class NextBlockService {
      * Applies for blocks which only have one target block e.g. Text Message Block
      * @returns Block
      */
-    async getNextBlock(chatInfo: ChatInfo, message: string, lastBlock: TextMessageBlock): Promise<Block>{
+    async getNextBlock(msg: Message, lastBlock: Block): Promise<Block>{
         const blockConnRepo$ =  new ChatBotStore(this.tools).blockConnections()
         
-        const connection = await blockConnRepo$.getConnBySourceId(lastBlock.id, chatInfo)
-        let nextBlock: Block = await blockConnRepo$.getBlockById(connection.targetId, chatInfo)
+        const connection = await blockConnRepo$.getConnBySourceId(lastBlock.id, msg)
+        let nextBlock: Block = await blockConnRepo$.getBlockById(connection.targetId, msg)
 
         return nextBlock
     }
