d02877fcd3be236fdd7a162d5173fa82b105b24e
Lucywambui <lmbugua63.lm@gmail.com>
2022-10-04 08:29:19 +0300

    :lipstick: (feat)(style)Updated the front-end of image block to support the file upload

--- libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
@@ -10,11 +10,10 @@ import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 import { finalize } from 'rxjs/operators';
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
-import { UploadFileService } from '@app/state/file'; 
+import { FileUpload, UploadFileService } from '@app/state/file';
 
 import { ImageMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
-
-
+import { isArgumentsObject } from 'util/types';
 
 @Component({
   selector: 'app-image-block',
@@ -31,13 +30,15 @@ export class ImageBlockComponent implements OnInit {
 
 
 
+
   isLoadingImage: boolean = false;
   imageInputId: string;
   defaultImage: string = "assets/images/lib/block-builder/image-block-placeholder.jpg"
   file: File;
   imageLink: string = this.defaultImage;
   imageName: string = '';
-  
+
+
 
 
 
@@ -49,7 +50,8 @@ export class ImageBlockComponent implements OnInit {
     private _db: AngularFireDatabase) { }
 
   ngOnInit(): void {
-    this.imageInputId = `img-${this.id}`
+    this.imageInputId = `img-${this.id}`;
+
   }
 
   ngAfterViewInit(): void {
@@ -63,30 +65,36 @@ export class ImageBlockComponent implements OnInit {
   }
 
   processImage(event: any) {
-    if (event.target.files && event.target.files[0]){
+    if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
       reader.onload = (e: any) => this.imageLink = e.target.result;
       reader.readAsDataURL(event.target.files[0]);
       this.file = event.target.files[0];
     } else {
       this.imageLink = this.defaultImage;
-    }   
+    }
     this.isLoadingImage = true;
     var imgFilePath = `images/${this.file.name}_${new Date().getTime()}`;
+    this.imageMessageForm.patchValue({src: imgFilePath})
     const imgFileRef = this.storage.ref(imgFilePath);
     this.storage.upload(imgFilePath, this.file).snapshotChanges().pipe(finalize(() => {
-      imgFileRef.getDownloadURL().subscribe((url) => {
-        this.imageMessageForm.controls['src'] = url;
-        this._imageUploadService.upload(this.block, url, this.block.type);
+    imgFileRef.getDownloadURL().subscribe((url: string) => {
+      this.imageLink = url;
+      debugger
+       this._imageUploadService.upload(this.block, url, this.block.type, this.file);
       })
     })
-    ).subscribe();
+    ).subscribe((acc)=> {console.log(';) ', this.imageLink);
+    this.imageMessageForm.patchValue({src: this.imageLink})
+    this.imageMessageForm.patchValue({src: imgFilePath})
+
+  });
+  
   }
 
 
-  get formControl() {
-    return this.imageMessageForm.controls;
-  }
+
+  
 
   private _decorateInput() {
     let input = document.getElementById(this.imageInputId) as Element;
