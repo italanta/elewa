de41c54329d40f7f8d1738a732d1db0536164826
Lucywambui <lmbugua63.lm@gmail.com>
2022-10-11 15:41:23 +0300

    :art: (UI) arranging code and getting rid of code that isn't used

--- libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/image-message-block/src/lib/components/image-block/image-block.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { DomSanitizer } from '@angular/platform-browser';;
-import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormGroup } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 
-import { Logger } from '@iote/bricks-angular';
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
-
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+
 import { UploadFileService } from '@app/state/file';
 
 import { ImageMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
@@ -27,41 +24,35 @@ export class ImageBlockComponent implements OnInit {
   @Input() jsPlumb: BrowserJsPlumbInstance;
 
 
-  isLoadingImage: boolean = false;
-  imageInputId: string;
-  defaultImage: string = "assets/images/lib/block-builder/image-block-placeholder.jpg"
   file: File;
-  imageLink = this.defaultImage;
+  imageInputId: string;
   imageName: string = '';
+  isLoadingImage: boolean = false;
+  defaultImage: string = "assets/images/lib/block-builder/image-block-placeholder.jpg"
+  imageLink = this.defaultImage;
 
 
-  constructor(private _fb: FormBuilder,
-    private _logger: Logger,
-    private _imageUploadService: UploadFileService,
-    public domSanitizer: DomSanitizer,
-    private document: AngularFirestore,
-    private _db: AngularFireDatabase) { }
 
-  ngOnInit(): void {
+  constructor(private _imageUploadService: UploadFileService,
+              public domSanitizer: DomSanitizer) 
+              { }
+
+  ngOnInit(): void
+  {
     this.imageInputId = `img-${this.id}`;
-    const imgId = this.document.createId();
-    this.block.fileId = imgId;
-    this.imageMessageForm.patchValue({ fileId: this.block.fileId })
-
   }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit(): void 
+  {
     if (this.jsPlumb) {
       this._decorateInput();
     }
   }
 
-  onKey(value: string) {
-    this.imageName += value;
-  }
 
-  processImage(event: any) {
 
+  async processImage(event: any) 
+  {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
       reader.onload = (e: any) => this.imageLink = e.target.result;
@@ -72,11 +63,12 @@ export class ImageBlockComponent implements OnInit {
       this.imageLink = this.defaultImage;
     }
     this.isLoadingImage = true;
-    this._imageUploadService.uploadFile(this.file, this.block, this.block.type, this.imageMessageForm, this.block.fileId!);
+    (await this._imageUploadService.uploadFile(this.file, this.block)).subscribe();
 
   }
 
-  private _decorateInput() {
+  private _decorateInput() 
+  {
     let input = document.getElementById(this.imageInputId) as Element;
     if (this.jsPlumb) {
       input = _JsPlumbComponentDecorator(input, this.jsPlumb);
