d417473fe842d1f4887a9a49635d91216f3a3346
ReaganCn <rgncharana2@gmail.com>
2022-10-11 11:06:01 +0300

    :truck: export message data service class

--- libs/functions/chatbot/src/index.ts
+++ libs/functions/chatbot/src/index.ts
@@ -2,4 +2,5 @@ export * from './lib/services/main-chatbot.service';
 
 export * from './lib/handlers/process-message.handler';
 export * from './lib/stores/chatbot.store';
+export * from './lib/services/data-services/messages.service';
 // export * from './lib/handlers/cancel-operator-request.handler';
