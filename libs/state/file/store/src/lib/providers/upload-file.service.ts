37d15d57eda119ca3f15f18203b061ccf1da0099
Lucywambui <lmbugua63.lm@gmail.com>
2022-10-11 16:08:27 +0300

    :zap: :art: (refactor) The updated version of the upload file service

--- libs/state/file/store/src/lib/providers/upload-file.service.ts
+++ libs/state/file/store/src/lib/providers/upload-file.service.ts
@@ -1,112 +1,122 @@
-import { StoryBlock } from './../../../../../../model/convs-mgr/stories/blocks/main/src/lib/story-block.interface';
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 import { Repository, DataService } from '@ngfi/angular';
 
 import { of } from 'rxjs';
-import { finalize, switchMap, map, tap, take } from 'rxjs/operators';
+import { switchMap, map, take, catchError } from 'rxjs/operators';
 
 import { Logger } from '@iote/bricks-angular';
 
 import { StoryBlocksStore } from '@app/state/convs-mgr/stories/blocks';
-
-import { Organisation } from '@app/model/organisation'
-import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
-import { FileMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
-
 import { ActiveOrgStore } from '@app/state/organisation';
 
+import { Organisation } from '@app/model/organisation';
+import { FileMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+import { StoryBlock } from '@app/model/convs-mgr/stories/blocks/main';
+
 import { FileUpload } from '../model/file-upload.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class UploadFileService {
+
+/**This service handles the upload of files on firestorage and firestore*/
+export class UploadFileService
+{
+
+  //The filepath on firestore for the files
+  protected _activeRepo: Repository<FileUpload>;
   protected store = 'file-store';
 
-  protected _activeRepo: Repository<FileUpload>;
   org: Organisation;
   imageLink: string;
 
-
-
-  constructor(
-    private _storyBlockService$: StoryBlocksStore,
-    private _org$$: ActiveOrgStore,
-    private _repoFac: DataService,
-    _logger: Logger,
-    private _ngfiStorage: AngularFireStorage,
-    private document: AngularFirestore) { }
+  downloadUrl: string;
+  constructor(private _storyBlockService$: StoryBlocksStore,
+              private _org$$: ActiveOrgStore,
+              private _repoFac: DataService,
+              private _ngfiStorage: AngularFireStorage,
+              _logger: Logger)
+              { }
 
 
 
-  upload(block: FileUpload, url: string, blockType: StoryBlockTypes, form: FormGroup, id: string) {
-
-
+  /**
+   * Function that does the upload of all files
+   */
+  upload(url: string, block: StoryBlock) 
+  {
+    //Step 1 - create a descriptor and set its values
     const fileBlock:FileUpload = {
-      fileId: id,
       filePath: url,
-      fileType: blockType,
+      fileType: block.type,
       size: '3MB'
     };
 
-    this._org$$.get().pipe(
+    //Step 2 - get the organisation creating the files
+    return this._org$$.get().pipe(take(1),
       map((org) => {
         this.org = org;
-        return (!!org ?? false)
       }),
+      //Step 3 - create the path in firestore that will lead to files 
       switchMap(org => {
-        if (!!org) {
+        if (!!this.org) {
           this._activeRepo = this._repoFac.getRepo<FileUpload>(`orgs/${this.org.id}/files`);
           return this._activeRepo.create(fileBlock);
         } else {
-          return of([true]);
+          return of([]);
         }
       }),
-      switchMap((rs) => {
-        // this.uploadFile(file);
-        return of([rs])
+      //Step 4 - update The field fileSrc for the block holding the file
+      switchMap(() => {
+        return this._updateSrc(url, block.id!).pipe(take(1));
+      }),
+      catchError(err => {
+        throw err;
       })
-    ).subscribe(value => {
-      return value;
-    });
-
+    )
 
   }
 
-  private _updateSrc(blockId: string, url: string){
-    const fileBlock = this._storyBlockService$.getOne(blockId);
-    return fileBlock.pipe(switchMap((block)=>{
-      let newBlock: FileMessageBlock = {
+  /**
+   * Updates the blocks src field once the url has been set from firestorage
+   */
+  private _updateSrc(url: string, id: string)
+  {
+    //Step 1 - Get the block in the story that has the same id 
+    const fileBlock = this._storyBlockService$.getOne(id)
+
+    //Step 2 - returns the block with the updated fileSrc field 
+   return fileBlock.pipe(
+     take(1),
+     switchMap((block)=>{
+      const newBlock: FileMessageBlock = {
         ...block as FileMessageBlock,
-        fileSrc: url
+        fileSrc: url,
       }
-      this._storyBlockService$.update(newBlock).subscribe();
-      return fileBlock;
-    }))
+      //Step 3 - update the block 
+      return this._storyBlockService$.update(newBlock);
+    }));
   }
 
-  public uploadFile(file: File, block: FileUpload, blockType: StoryBlockTypes, form: FormGroup, id: string): any {
-
-    
+  /**
+   * Adds the file on firestorage
+   */
+  public async uploadFile(file: File, block: StoryBlock) 
+  { 
+    //Step 1 - Create the file path that will be in firebase storage
     var imgFilePath = `images/${file.name}_${new Date().getTime()}`;
-    const imgFileRef = this._ngfiStorage.ref(imgFilePath);
-    const uploadTask = this._ngfiStorage.upload(imgFilePath, file);
-    
 
+    //Step 2 - Upload the file 
+    var uploadTask = (await this._ngfiStorage.upload(imgFilePath, file)).ref;
 
-    uploadTask.snapshotChanges().pipe(finalize(() => {
-      imgFileRef.getDownloadURL().subscribe((url: string) => {  
-        this.imageLink = url
-        console.log(form.value.fileSrc);
-        this._updateSrc(block.id as string, this.imageLink).pipe(take(1)).subscribe()
-        this.upload(block, url, blockType, form, id);
-      })
-    })
-    ).subscribe();
+    //Step 3 - Get the url in firebase storage
+    var reference = await uploadTask.getDownloadURL();
+
+    //Step 4 - Call the upload function 
+    return this.upload(reference, block).pipe(take(1));
   }
 
 
