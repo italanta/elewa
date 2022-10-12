f820622a62a07bc089418f99909205c2e9c2209c
ReaganCn <rgncharana2@gmail.com>
2022-10-12 10:49:27 +0300

    :wrench: update channel data to correspond to bot flow

--- libs/functions/chatbot/src/lib/services/data-services/channel-info.service.ts
+++ libs/functions/chatbot/src/lib/services/data-services/channel-info.service.ts
@@ -1,36 +1,48 @@
 import { HandlerTools } from '@iote/cqrs';
 
-import { Platforms } from '@app/model/convs-mgr/conversations/admin/system';
+// import { BaseChannel } from '@app/model/bot/channel';
 import { ChatInfo } from '@app/model/convs-mgr/conversations/chats';
 import { BotDataService } from './data-service-abstract.class';
-import { BaseMessage } from '@app/model/convs-mgr/conversations/messages';
+import { BaseMessage, RawMessageData } from '@app/model/convs-mgr/conversations/messages';
+import { BaseChannel } from 'libs/model/bot/channel/src';
 
 /**
  * Contains all the required database flow methods for the ChatInfo collection
  */
 export class ChannelDataService extends BotDataService<BaseChannel> {
   private _docPath: string;
-  private _msg: BaseMessage;
+  private _msg: RawMessageData;
   
-  constructor(tools: HandlerTools) 
+  constructor(msg: RawMessageData,tools: HandlerTools) 
   {
     super(tools)
+    this._init(msg)
   }
 
-  protected _init(msg: BaseMessage){
-    this._docPath = `channels/${msg.platform}/${msg.storyId}/${msg.phoneNumber}`
+  protected _init(msg: RawMessageData){
+    this._docPath = `channels/${msg.platform}/${msg.botUserPhoneNumber}`
     this._msg = msg
   }
 
 
-  async getChannelInfo(phoneNumber: string): Promise<ChatInfo> {
+  async getChannelInfo<T>(phoneNumber: string): Promise<T> {
     // Get users
-    const channelInfo =  await this.getDocumentById(phoneNumber, this._docPath)
+    const channelInfo =  (await this.getDocumentById(phoneNumber, this._docPath)) as T
 
     if (!channelInfo) {
       throw new Error('The user has not been registered to a channel');
     }
     return channelInfo;
+  }
+
+  
+  async getChannels(): Promise<BaseChannel> {
+    const channels =  await this.getDocuments(this._docPath)
+
+    if (!channels[0]) {
+      throw new Error('The user has not been registered to any channel');
+    }
+    return channels[0];
   }
   
 }
