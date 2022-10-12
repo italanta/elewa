acd9ddcca088baf2ebb5d3b66f31d331fa440e5d
ReaganCn <rgncharana2@gmail.com>
2022-10-12 14:14:30 +0300

    :bulb: add comment to send message model

--- libs/functions/messages/whatsapp/src/lib/models/send-message-main.model.ts
+++ libs/functions/messages/whatsapp/src/lib/models/send-message-main.model.ts
@@ -1,6 +1,10 @@
 import { BaseMessage } from "@app/model/convs-mgr/conversations/messages";
 import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
 
+/** Abstract Class for sending messages 
+ *  Outlines all methods that can be used to send messages across different platforms 
+ * 
+ * */
 export abstract class SendMessageModel {
     
     abstract sendMessage(message: BaseMessage, blockType: StoryBlockTypes, env:any): Promise<any>
