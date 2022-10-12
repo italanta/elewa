c7df5a8c0355ee442a175368088ca7ec81737ad8
ReaganCn <rgncharana2@gmail.com>
2022-10-12 14:06:04 +0300

    :heavy_plus_sign: :wrench: add sendTextMessage method to abstract class

--- libs/functions/messages/whatsapp/src/lib/models/send-message-main.model.ts
+++ libs/functions/messages/whatsapp/src/lib/models/send-message-main.model.ts
@@ -4,4 +4,6 @@ import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
 export abstract class SendMessageModel {
     
     abstract sendMessage(message: BaseMessage, blockType: StoryBlockTypes, env:any): Promise<any>
+
+    protected abstract _sendTextMessage(message: BaseMessage, blockType: StoryBlockTypes, env:any): Promise<any>
 }
