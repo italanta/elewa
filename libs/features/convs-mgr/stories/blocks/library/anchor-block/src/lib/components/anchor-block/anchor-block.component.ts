488187353ee96ef715b3cb664df68397dd85516f
Lucywambui <lmbugua63.lm@gmail.com>
2022-10-18 15:13:57 +0300

    :lipstick: :sparkles: (feat) Added the anchor block styling and jsPlumb

--- libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/components/anchor-block/anchor-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/anchor-block/src/lib/components/anchor-block/anchor-block.component.ts
@@ -1,13 +1,15 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
 import { Logger } from '@iote/bricks-angular';
+import{ ActiveStoryStore } from '@app/state/convs-mgr/stories';
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
-
-import { AnchorBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+import { getLocaleId } from '@angular/common';
+import { map } from 'rxjs';
+import { Story } from '@app/model/convs-mgr/stories/main';
 
 @Component({
   selector: 'app-anchor-block',
@@ -15,24 +17,31 @@ import { AnchorBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
   styleUrls: ['./anchor-block.component.scss'],
 })
 export class AnchorBlockComponent implements OnInit, AfterViewInit {
+
   @Input() id: string;
-  @Input() block: AnchorBlock;
   @Input() jsPlumb: BrowserJsPlumbInstance;
+  anchorInput: string;
 
-  @Input() anchorBlockForm: FormGroup;
 
-  constructor() {}
+  constructor(private _story$$: ActiveStoryStore) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { 
+    this.getId().subscribe();
+    console.log(this.anchorInput)
+  }
 
-  ngAfterViewInit(): void 
-  {
+  ngAfterViewInit(): void {
     this._decorateInput();
   }
 
-  private _decorateInput() 
-  {
-    let input = document.getElementById(this.id) as Element;
+  getId(){
+    return this._story$$.get()
+                        .pipe(
+                          map((story:Story) => this.anchorInput = story.id!));
+  }
+
+  private _decorateInput() {
+    let input = document.getElementById(this.anchorInput) as Element;
     if (this.jsPlumb) {
       input = _JsPlumbComponentDecorator(input, this.jsPlumb);
     }
