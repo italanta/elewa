6ddbe13a13dd7ea79c2dc1ec94f5428bc893bf55
Reagan Charana <34684844+ReaganCn@users.noreply.github.com>
2022-10-18 11:04:21 +0300

    :wrench: clean up the chat flow for the bot

--- libs/functions/chatbot/src/lib/services/chat-manager.service.ts
+++ libs/functions/chatbot/src/lib/services/chat-manager.service.ts
@@ -1,22 +1,18 @@
-import { __DateFromStorage } from '@iote/time';
+import { HandlerTools } from '@iote/cqrs';
 
 import { AddMessageFactory } from '@app/functions/conversations/messages/add-message';
 
 import { MessagesDataService } from './data-services/messages.service';
 import { ChatStatusDataService } from './data-services/chat-status.service';
 import { ChannelDataService } from './data-services/channel-info.service';
+import { CursorDataService } from './data-services/cursor.service';
+import { BlockDataService } from './data-services/blocks.service';
+import { ConnectionsDataService } from './data-services/connections.service';
+import { ChatBotMainService } from './main-chatbot.service';
 
 import { Platforms } from '@app/model/convs-mgr/conversations/admin/system';
 import { BaseMessage, ChatStatus, RawMessageData } from '@app/model/convs-mgr/conversations/messages';
 import { WhatsappChannel } from '@app/model/bot/channel';
-import { ProcessMessageService } from './process-message/process-message.service';
-import { HandlerTools } from '@iote/cqrs';
-import { Block } from '@app/model/convs-mgr/conversations/chats';
-import { CursorDataService } from './data-services/cursor.service';
-import { BlockDataService } from './data-services/blocks.service';
-import { ConnectionsDataService } from './data-services/connections.service';
-import { SendMessageFactory } from '@app/functions/messages/whatsapp';
-import { ChatBotMainService } from './main-chatbot.service';
 
 /**
  * Handles the main processes of the ChatBot
@@ -30,9 +26,6 @@ export class ChatManagerService {
     private _msgDataService$: MessagesDataService,
     private _chatStatusService$: ChatStatusDataService,
     private _channelService$: ChannelDataService,
-    private _blocksService$: BlockDataService,
-    private _connService$: ConnectionsDataService,
-    private _cursorDataService$: CursorDataService,
     private _tools: HandlerTools,
     platform: Platforms
   ) {
@@ -41,26 +34,72 @@ export class ChatManagerService {
 
   async main(rawMessage: RawMessageData){
 
-    const currentChat  = await this._chatStatusService$.getChatStatus(storyId)
+    const baseMessage = await this._init(rawMessage)
 
-    const chat = new ChatBotMainService()
+    // To later use a DI container to manage instances and dynamically inject approtiate dependencies
+    const connDataService  = new ConnectionsDataService(baseMessage, this._tools)
+    const cursorDataService = new CursorDataService(baseMessage, this._tools)
+    const blockDataService = new BlockDataService(baseMessage, connDataService, this._tools)
 
-    switch (currentChat.status) {
+
+    const currentChatInfo  = await this._chatStatusService$.getChatStatus(baseMessage.storyId)
+
+    const chat = new ChatBotMainService(blockDataService, connDataService, cursorDataService, this._tools, this.platform)
+
+    switch (currentChatInfo.status) {
       case ChatStatus.Running:
-        
+        await chat.run(baseMessage)
         break;
       case ChatStatus.Paused:
-        
+        await chat.sendTextMessage(baseMessage, "Chat has been paused.")
         break;      
-      case ChatStatus.TakingToOperator:
-        
-        break;
       default:
         break;
     }
   }
 
-  async init(rawMessage: RawMessageData){
+    /** Checks if the message is from a new user and then initializes chat */
+    private async _init(req: RawMessageData) {
+      // Check if the enduser is registered to a channel
+      this.messageChannel = await this._getChannelInfo(req, this._channelService$);
+  
+      let baseMessage: BaseMessage;
+  
+      if (this.messageChannel) {
+        // Add message to collection
+        baseMessage = await this._addMessage(req);
+        return baseMessage;
+      } else {
+        // Initialize chat status
+        await this._chatStatusService$.initChatStatus(this.messageChannel, this.platform);
+  
+        // Add message to collection
+        baseMessage = await this._addMessage(req);
+  
+        return baseMessage;
+      }
+    }
 
+    private async _getChannelInfo(msg: RawMessageData, channelDataService: ChannelDataService) {
+      switch (msg.platform) {
+        case Platforms.WhatsApp:
+          return await channelDataService.getChannelInfo<WhatsappChannel>(msg.botUserPhoneNumber);
+  
+        default:
+          return await channelDataService.getChannelInfo<WhatsappChannel>(msg.botUserPhoneNumber);
+      }
+    }
+
+  /**
+   * Inteprates the raw message received from the hook and saves it to firestore
+   * @param req - Raw Message data received from the webhook
+   */
+  private async _addMessage(req: RawMessageData) {
+    // Use factory to resolve the platform
+    const AddMessageService = new AddMessageFactory(this._msgDataService$).resolveAddMessagePlatform(req.platform);
+
+    const baseMessage = await AddMessageService.addMessage(req, this.messageChannel);
+
+    return baseMessage;
   }
 }
