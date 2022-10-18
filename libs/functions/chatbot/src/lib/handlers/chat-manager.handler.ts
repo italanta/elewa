18296940219c8d450c7210c9484688f46b6dad5c
Reagan Charana <34684844+ReaganCn@users.noreply.github.com>
2022-10-18 11:04:44 +0300

    :horse_racing: :sparkles: create chat manager handler

---
+++ libs/functions/chatbot/src/lib/handlers/chat-manager.handler.ts
@@ -0,0 +1,42 @@
+import { HandlerTools } from '@iote/cqrs';
+import { FunctionContext, FunctionHandler, RestResult200 } from '@ngfi/functions';
+
+import { RawMessageData } from '@app/model/convs-mgr/conversations/messages';
+
+import { ChatManagerService } from '../services/chat-manager.service';
+import { MessagesDataService } from '../services/data-services/messages.service';
+import { ChatStatusDataService } from '../services/data-services/chat-status.service';
+import { ChannelDataService } from '../services/data-services/channel-info.service';
+
+/**
+ * Triggered by document.create in 'messages/{phoneNumber}/platforms/{platform}/msgs/{messageId}'
+ * Processes the message and returns the next block.
+ */
+export class EngineChatManagerHandler extends FunctionHandler<RawMessageData, RestResult200>
+{
+  public async execute(req: RawMessageData, context: FunctionContext, tools: HandlerTools)
+  {
+    tools.Logger.log(() => `[EngineChatManagerHandler].execute: New incoming chat from channels.`);
+    tools.Logger.log(() => JSON.stringify(req));
+
+    try {
+    // To later use a DI container to manage instances and dynamically inject approtiate dependencies
+    const messageDataService  = new MessagesDataService(tools)
+    const chatStatusDataService = new ChatStatusDataService(tools)
+    const channelDataService = new ChannelDataService(req, tools)
+
+
+    const chatManager = new ChatManagerService(messageDataService, chatStatusDataService, channelDataService, tools, req.platform)
+
+    await chatManager.main(req)
+
+    return { success: true} as RestResult200
+
+    } catch (error) {
+
+      tools.Logger.error(() => `[EngineChatManagerHandler].execute: Chat Manager encountered an error: ${error}`);
+      
+    }
+  }
+
+}
