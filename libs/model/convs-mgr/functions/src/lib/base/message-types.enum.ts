af2af803bddf9c86731cb2b87ff5102312640ebd
ReaganCn <rgncharana2@gmail.com>
2022-10-21 17:51:31 +0300

    :horse: create standard message types that should be used across different platforms

---
+++ libs/model/convs-mgr/functions/src/lib/base/message-types.enum.ts
@@ -0,0 +1,11 @@
+// Refer to guide for message types from whatsapp 
+// https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages#send-messages
+
+export enum MessageTypes {
+    TEXT = "text",
+    REACTION = "reaction",
+    IMAGE = "image",
+    LOCATION = "location",
+    CONTACTS = "contacts",
+    INTERACTIVE = "interactive",
+}
