d899c98743ce128b0fe919b4f59341625b2afe6e
ReaganCn <rgncharana2@gmail.com>
2022-10-24 17:20:47 +0300

    :wrench: add proper interpretation of media blocks

--- libs/functions/chatbot/src/lib/services/interpreter/send-message-interpreter/whatsapp/whatsapp-api-send-message-interpreter.class.ts
+++ libs/functions/chatbot/src/lib/services/interpreter/send-message-interpreter/whatsapp/whatsapp-api-send-message-interpreter.class.ts
@@ -1,5 +1,5 @@
-import { StoryBlock } from '@app/model/bot/blocks/story-block';
-import { BaseChannel, WhatsappChannel } from '@app/model/bot/channel';
+import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+
 import { BaseMessage } from '@app/model/convs-mgr/conversations/messages';
 import { ActionButtonsInfo, InteractiveButtonMessage, MetaMessagingProducts, RecepientType, TextMessagePayload, WhatsAppBaseMessage, WhatsAppInteractiveMessage, WhatsAppMediaMessage, WhatsAppMessageType } from '@app/model/convs-mgr/functions';
 import { QuestionMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
@@ -12,7 +12,6 @@ import { SendMessageInterpreter } from '../send-message-interpreter-abstract.cla
  * @see https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples
  */
 export class WhatsappSendMessageInterpreter extends SendMessageInterpreter {
-
   /**
    * @Description Used to send message of type text to whatsapp api
    * @param message 
@@ -91,7 +90,7 @@ export class WhatsappSendMessageInterpreter extends SendMessageInterpreter {
 
   }
 
-  interpretMediaBlock (message: BaseMessage, block: StoryBlock){
+  interpretImageBlock (message: BaseMessage, block: StoryBlock){
     let link: string;
 
     if(block){
@@ -119,5 +118,129 @@ export class WhatsappSendMessageInterpreter extends SendMessageInterpreter {
       ...mediaMessage
     }
     return generatedMessage
+  }
+
+  interpretAudioBlock (message: BaseMessage, block: StoryBlock){
+    let link: string;
+
+    if(block){
+      link = block.message
+    } else {
+      link = message.message
+    }
+    // Create the text payload which will be sent to api
+    const mediaMessage = { 
+      type: WhatsAppMessageType.AUDIO,
+      image: {
+        link
+      }
+    } as WhatsAppMediaMessage
+
+    /**
+     * Add the required fields for the whatsapp api
+     * @see https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages
+     */
+    const generatedMessage: WhatsAppBaseMessage = {
+      messaging_product: MetaMessagingProducts.WHATSAPP,
+      recepient_type: RecepientType.INDIVIDUAL,
+      to: message.phoneNumber,
+      type: WhatsAppMessageType.AUDIO,
+      ...mediaMessage
+    }
+    return generatedMessage
+  }
+
+  interpretDocumentBlock (message: BaseMessage, block: StoryBlock){
+    let link: string;
+
+    if(block){
+      link = block.message
+    } else {
+      link = message.message
+    }
+    // Create the text payload which will be sent to api
+    const mediaMessage = { 
+      type: WhatsAppMessageType.DOCUMENT,
+      image: {
+        link
+      }
+    } as WhatsAppMediaMessage
+
+    /**
+     * Add the required fields for the whatsapp api
+     * @see https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages
+     */
+    const generatedMessage: WhatsAppBaseMessage = {
+      messaging_product: MetaMessagingProducts.WHATSAPP,
+      recepient_type: RecepientType.INDIVIDUAL,
+      to: message.phoneNumber,
+      type: WhatsAppMessageType.DOCUMENT,
+      ...mediaMessage
+    }
+    return generatedMessage
+  }
+
+  interpretVideoBlock (message: BaseMessage, block: StoryBlock){
+    let link: string;
+
+    if(block){
+      link = block.message
+    } else {
+      link = message.message
+    }
+    // Create the text payload which will be sent to api
+    const mediaMessage = { 
+      type: WhatsAppMessageType.VIDEO,
+      image: {
+        link
+      }
+    } as WhatsAppMediaMessage
+
+    /**
+     * Add the required fields for the whatsapp api
+     * @see https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages
+     */
+    const generatedMessage: WhatsAppBaseMessage = {
+      messaging_product: MetaMessagingProducts.WHATSAPP,
+      recepient_type: RecepientType.INDIVIDUAL,
+      to: message.phoneNumber,
+      type: WhatsAppMessageType.VIDEO,
+      ...mediaMessage
+    }
+    return generatedMessage
+  }
+
+  interpretStickerBlock (message: BaseMessage, block: StoryBlock){
+    let link: string;
+
+    if(block){
+      link = block.message
+    } else {
+      link = message.message
+    }
+    // Create the text payload which will be sent to api
+    const mediaMessage = { 
+      type: WhatsAppMessageType.STICKER,
+      image: {
+        link
+      }
+    } as WhatsAppMediaMessage
+
+    /**
+     * Add the required fields for the whatsapp api
+     * @see https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages
+     */
+    const generatedMessage: WhatsAppBaseMessage = {
+      messaging_product: MetaMessagingProducts.WHATSAPP,
+      recepient_type: RecepientType.INDIVIDUAL,
+      to: message.phoneNumber,
+      type: WhatsAppMessageType.STICKER,
+      ...mediaMessage
+    }
+    return generatedMessage
+  }
+
+  interpretLocationBlock(message: BaseMessage, block: StoryBlock): BaseMessage {
+    throw new Error("Method not implemented.");
   }
 }
