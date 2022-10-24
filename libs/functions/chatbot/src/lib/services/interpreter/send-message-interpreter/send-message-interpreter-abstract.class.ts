7557ed71cbf3bd22cd2684a853a9b10328774ad3
ReaganCn <rgncharana2@gmail.com>
2022-10-24 17:20:47 +0300

    :wrench: add proper interpretation of media blocks

--- libs/functions/chatbot/src/lib/services/interpreter/send-message-interpreter/send-message-interpreter-abstract.class.ts
+++ libs/functions/chatbot/src/lib/services/interpreter/send-message-interpreter/send-message-interpreter-abstract.class.ts
@@ -1,5 +1,4 @@
 import { StoryBlock } from "@app/model/bot/blocks/story-block";
-import { BaseChannel } from "@app/model/bot/channel";
 import { BaseMessage } from "@app/model/convs-mgr/conversations/messages";
 import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
 
@@ -9,11 +8,21 @@ import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
  */
 export abstract class SendMessageInterpreter {
 
-    abstract interpretTextBlock(message: BaseMessage,block: StoryBlock): BaseMessage
+    abstract interpretTextBlock(message: BaseMessage, block: StoryBlock): BaseMessage
 
-    abstract interpretQuestionBlock(message: BaseMessage,block: StoryBlock): BaseMessage
+    abstract interpretQuestionBlock(message: BaseMessage, block: StoryBlock): BaseMessage
 
-    abstract interpretMediaBlock(message: BaseMessage,block: StoryBlock): BaseMessage
+    abstract interpretImageBlock(message: BaseMessage, block: StoryBlock): BaseMessage
+
+    abstract interpretAudioBlock(message: BaseMessage, block: StoryBlock): BaseMessage
+
+    abstract interpretVideoBlock(message: BaseMessage, block: StoryBlock): BaseMessage
+
+    abstract interpretStickerBlock(message: BaseMessage, block: StoryBlock): BaseMessage
+
+    abstract interpretDocumentBlock(message: BaseMessage, block: StoryBlock): BaseMessage
+
+    abstract interpretLocationBlock(message: BaseMessage, block: StoryBlock): BaseMessage
 
     interpretBlock(blockType: StoryBlockTypes){
         switch (blockType) {
@@ -28,17 +37,15 @@ export abstract class SendMessageInterpreter {
             case StoryBlockTypes.QuestionBlock:
                 return this.interpretQuestionBlock
             case StoryBlockTypes.Image:
-                return this.interpretMediaBlock
+                return this.interpretImageBlock
             case StoryBlockTypes.Document:
-                return this.interpretMediaBlock
+                return this.interpretDocumentBlock
             case StoryBlockTypes.Audio:
-                return this.interpretMediaBlock
+                return this.interpretAudioBlock
             case StoryBlockTypes.Video:
-                return this.interpretMediaBlock
+                return this.interpretVideoBlock
             case StoryBlockTypes.Sticker:
-                return this.interpretMediaBlock
-            case StoryBlockTypes.Audio:
-                return this.interpretMediaBlock
+                return this.interpretStickerBlock
             default:
                 return this.interpretTextBlock
         }
