e022b4854c7119d9188d9dd8253962b5ccb6fbca
ReaganCn <rgncharana2@gmail.com>
2022-10-25 11:11:44 +0300

    :wrench: corrent implementation of whatsapp media message payload

--- libs/model/convs-mgr/functions/src/lib/whatsapp/whatsapp-media-message.interface.ts
+++ libs/model/convs-mgr/functions/src/lib/whatsapp/whatsapp-media-message.interface.ts
@@ -6,9 +6,39 @@ import { WhatsAppMessageType } from "./whatsapp-message-types.interface";
  * @extends {WhatsAppBaseMessage}
  * @see https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages#media-messages
  */
-export interface WhatsAppMediaMessage extends WhatsAppBaseMessage {
+export interface WhatsAppImageMessage extends WhatsAppBaseMessage {
   type: WhatsAppMessageType,
   image: {
+    link: string;
+  }
+
+}
+
+export interface WhatsAppAudioMessage extends WhatsAppBaseMessage {
+  type: WhatsAppMessageType,
+  audio: {
+    link: string;
+  }
+
+}
+export interface WhatsAppVideoMessage extends WhatsAppBaseMessage {
+  type: WhatsAppMessageType,
+  video: {
+    link: string;
+  }
+
+}
+export interface WhatsAppStickerMessage extends WhatsAppBaseMessage {
+  type: WhatsAppMessageType,
+  sticker: {
+    link: string;
+  }
+
+}
+
+export interface WhatsAppDocumentMessage extends WhatsAppBaseMessage {
+  type: WhatsAppMessageType,
+  document: {
     link: string;
   }
 
