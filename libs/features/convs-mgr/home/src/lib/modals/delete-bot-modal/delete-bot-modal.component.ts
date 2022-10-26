53fcbfbb4bff3c237ecba38806afe2b286cb00a9
lemmy mwaura <lemminton@gmail.com>
2022-10-26 03:51:44 -0700

    ✨ inject mat dialog to access payload

--- libs/features/convs-mgr/home/src/lib/modals/delete-bot-modal/delete-bot-modal.component.ts
+++ libs/features/convs-mgr/home/src/lib/modals/delete-bot-modal/delete-bot-modal.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { Story } from '@app/model/convs-mgr/stories/main';
+import { NewStoryService } from '../../services/new-story.service';
 
 @Component({
   selector: 'convl-italanta-apps-delete-bot-modal',
   templateUrl: './delete-bot-modal.component.html',
-  styleUrls: ['./delete-bot-modal.component.css'],
+  styleUrls: ['./delete-bot-modal.component.scss'],
 })
 export class DeleteBotModalComponent implements OnInit {
-  constructor(private _dialog:MatDialog) {}
+  constructor(
+    private _addStory$: NewStoryService,
+    @Inject(MAT_DIALOG_DATA) public data: { payload?: Story},
+  ) {}
 
   ngOnInit(): void {
     ''
   }
+
+  // delete() {
+  //   this._addStory$.remove(this.data.payload);
+  // }
 }
