c8ff58ed426354db87c0095e04d290c5e5ca0c0f
ReaganCn <rgncharana2@gmail.com>
2022-10-27 18:24:24 +0300

    :pencil: :wrench: rename methods in the Outgoing Message Parser

--- libs/functions/bot-engine/main/src/lib/io/outgoing-message.parser.ts
+++ libs/functions/bot-engine/main/src/lib/io/outgoing-message.parser.ts
@@ -1,65 +1,65 @@
-import { StoryBlock } from "@app/model/bot/blocks/story-block";
-import { BaseMessage } from "@app/model/convs-mgr/conversations/messages";
-import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
-
-/** TODO: Refactor the function to handle the message being passed */
+import { Message } from "@app/model/convs-mgr/conversations/messages";
+import { StoryBlock, StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
 
 /**
- * Defines methods that translates storyblocks to a format that can be handled by different platforms
+ * Our chatbot can send different types of messages, be it a text message, a location, an image, ...
+ * 
+ * The channel API (responsible for sending and receiving messages) requires each of these messages to be sent in a certain format/via a certain contract.
+ * 
+ * Here we define methods that 
  * 
  * @param {block} - This is passed to determine the fields 
  *
  */
 export abstract class OutgoingMessageParser
 {
-  /** Take */
-  abstract getTextBlockParserOut     (message: BaseMessage, block: StoryBlock): BaseMessage
 
-  abstract getQuestionBlockParserOut (message: BaseMessage, block: StoryBlock): BaseMessage
+  abstract getTextBlockParserOut     (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getImageBlockParserOut    (message: BaseMessage, block: StoryBlock): BaseMessage
+  abstract getQuestionBlockParserOut (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getAudioBlockParserOut    (message: BaseMessage, block: StoryBlock): BaseMessage
+  abstract getImageBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getVideoBlockParserOut    (message: BaseMessage, block: StoryBlock): BaseMessage
+  abstract getAudioBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getStickerBlockParserOut  (message: BaseMessage, block: StoryBlock): BaseMessage
+  abstract getVideoBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getDocumentBlockParserOut (message: BaseMessage, block: StoryBlock): BaseMessage
+  abstract getStickerBlockParserOut  (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getLocationBlockParserOut (message: BaseMessage, block: StoryBlock): BaseMessage
+  abstract getDocumentBlockParserOut (storyBlock: StoryBlock, phone: string): Message
+
+  abstract getLocationBlockParserOut (storyBlock: StoryBlock, phone: string): Message
 
   /**
    * Our chatbot can send different types of messages, be it a text message, a location, an image, ...
    * 
    * The channel API (responsible for sending and receiving messages) requires each of these messages to be sent in a certain format/via a certain contract.
    * 
-   * @param {StoryBlockType} - Type of the message that we want to send. This is key to determining how to send it.
    * @param {StoryBlock}     - The StoryBlock to send. StoryBlocks are prepared messages in a format our chatbot can understand.
    * @param {string}         - Phonenumber to which to send the message
    * 
    * @returns {Message}
    * A prepared  message which can be sent over the line to its specific channel API.
    */
-  parse(storyBlockType: StoryBlockTypes, s: StoryBlock, phone: string) : BaseMessage
+  parse(block: StoryBlock, phone: string) : Message
   {
-    let parser!: (message: BaseMessage, phone: string) => BaseMessage;
+    let parser!: (storyBlock: StoryBlock, phone: string) => Message;
     
-    switch (storyBlockType) {
-      case StoryBlockTypes.TextMessage:   parser = this.interpretTextBlock;     break;
-      case StoryBlockTypes.Name:          parser = this.interpretTextBlock;     break;
-      case StoryBlockTypes.Email:         parser = this.interpretTextBlock;     break;
-      case StoryBlockTypes.PhoneNumber:   parser = this.interpretTextBlock;     break;   
-      case StoryBlockTypes.QuestionBlock: parser = this.interpretQuestionBlock; break;
-      case StoryBlockTypes.Image:         parser = this.interpretImageBlock;    break;
-      case StoryBlockTypes.Document:      parser = this.interpretDocumentBlock; break;
-      case StoryBlockTypes.Audio:         parser = this.interpretAudioBlock;    break;
-      case StoryBlockTypes.Video:         parser = this.interpretVideoBlock;    break;
-      case StoryBlockTypes.Sticker:       parser = this.interpretStickerBlock;  break;
+    switch (block.type) {
+      case StoryBlockTypes.TextMessage:   parser = this.getTextBlockParserOut;     break;
+      case StoryBlockTypes.Name:          parser = this.getTextBlockParserOut;     break;
+      case StoryBlockTypes.Email:         parser = this.getTextBlockParserOut;     break;
+      case StoryBlockTypes.PhoneNumber:   parser = this.getTextBlockParserOut;     break;   
+      case StoryBlockTypes.QuestionBlock: parser = this.getQuestionBlockParserOut; break;
+      case StoryBlockTypes.Image:         parser = this.getImageBlockParserOut;    break;
+      case StoryBlockTypes.Document:      parser = this.getDocumentBlockParserOut; break;
+      case StoryBlockTypes.Audio:         parser = this.getAudioBlockParserOut;    break;
+      case StoryBlockTypes.Video:         parser = this.getVideoBlockParserOut;    break;
+      case StoryBlockTypes.Sticker:       parser = this.getStickerBlockParserOut;  break;
       default:
-          parser = this.interpretTextBlock;
+          parser = this.getTextBlockParserOut;
     }
 
-    return parser(s, phone);
+    return parser(block, phone);
   }
 }
