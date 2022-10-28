50b2d44e2ef5a6ceba5d233c40ba51f15390033f
ReaganCn <rgncharana2@gmail.com>
2022-10-28 12:20:04 +0300

    :house: :wrench: update the WhatsappResponse to include access token and business phone number id

--- libs/model/convs-mgr/functions/src/lib/whatsapp/whatsapp-response.interface.ts
+++ libs/model/convs-mgr/functions/src/lib/whatsapp/whatsapp-response.interface.ts
@@ -1,28 +1,23 @@
 import { IncomingMessage } from "@app/model/convs-mgr/conversations/messages";
 import { WhatsAppMessagePayLoad } from "./raw-whatsapp-payload.interface";
 
-// export interface WhatsAppResponse extends IObject {
-//   //Phone number of business/manager of the bot displayed on chat of bot user
-//   botAccountDisplayPhoneNumber:string,
-
-//   //id of phone number of business/manager of the bot
-//   botAccountphonNumberId:string,
-
-//   //Name of the person sending the message to the bot
-//   botUserName: string,
-
-//   //Phone number of the person sending the message to the bot
-//   botUserPhoneNumber:string,
-
-//   //Actual message being sent by the user to the bot
-//   message: WhatsAppMessagePayLoad
-
-//   messageType?: WhatsAppMessageType
-
-// }
-
 export interface WhatsAppResponse extends IncomingMessage {
 
-  //Actual message being sent by the user to the bot
+ /** 
+  * Unique id assigned by WhatsApp to the phone number an organization uses for the chatbot
+  *  
+  * We need to store the businessPhoneNumberId because it is required by the bot engine to 
+  *     send messages to whatsapp
+  * */ 
+  businessPhoneNumberId: string;
+
+ /** Actual message being sent by the user to the bot */
   message: WhatsAppMessagePayLoad
+
+  /** This is a unique token given by Whatsapp and required to 
+   *    send and receive messages on whatsapp endpoints
+   *  
+   *  We pass the access token in the Authorization header when sending a http request 
+   * */
+  accessToken?: string;
 }
