f4f81e0fdd5985ac4bbd2658eb4c76be4cd40593
ReaganCn <rgncharana2@gmail.com>
2022-10-28 09:45:39 +0300

    :art: :bulb: clean up methods and document io parsers

--- libs/functions/bot-engine/main/src/lib/io/incoming-message.parser.ts
+++ libs/functions/bot-engine/main/src/lib/io/incoming-message.parser.ts
@@ -1,22 +1,37 @@
-import { BaseChannel } from "@app/model/bot/channel";
 import { Message } from "@app/model/convs-mgr/conversations/messages";
 import { MessageTypes } from "@app/model/convs-mgr/functions";
 
 /**
- * Base class for interpreting the received raw message from the Webhook to Base Message 
+ * Our chatbot recieves different types of messages, be it a text message, a location, an image, ...
+ * 
+ * We therefore need to convert this message to a standardized format so that our chatbot can read and process the message
+ * 
+ * Here we define methods that parse in messages and return a standardized format required by the chatbot
+ * 
+ * @param {messageType} -  Type of the message that we receive form the end user. This is key to determining how to interpret it
+ * @param {incomingMessage} - This is the incoming message from the end user via the communication channel @see {CommunicationChannel}
+ * 
+ * @returns {Message} - This is standardized format that our chatbot can read and process
  */
 export abstract class IncomingMessageParser
 { 
-  /** Interprets a text message to BaseMessage */
-  protected abstract parseInTextMessage(msg: any, channel: BaseChannel): Message
+  protected abstract parseInTextMessage(incomingMessage: any): Message
 
-  /** Interprets an interactive message to BaseMessage */
-  protected abstract parseInInteractiveButtonMessage(msg: any, channel: BaseChannel): Message
+  protected abstract parseInInteractiveButtonMessage(incomingMessage: any): Message
 
-  /** Returns the appropriate interprating method based on message type */
-  parse(messageType: MessageTypes, msg: any, channel: BaseChannel): Message
+/**
+ * Our chatbot recieves different types of messages, be it a text message, a location, an image, ...
+ * 
+ * We therefore need to convert this message to a standardized format so that our chatbot can read and process the message
+ *  
+ * @param {messageType} -  Type of the message that we receive form the end user. This is key to determining how to interpret it
+ * @param {incomingMessage} - This is the incoming message from the end user via the communication channel @see {CommunicationChannel}
+ * 
+ * @returns {Message} - This is standardized format that our chatbot can read and process
+ */
+  parse(messageType: MessageTypes, incomingMessage: any): Message
   {
-    let parser!: ((msg: any, channel: BaseChannel) => Message);
+    let parser!: ((incomingMessage: any) => Message);
 
     switch (messageType) 
     {
@@ -26,6 +41,6 @@ export abstract class IncomingMessageParser
       //     break;
     }
 
-    return parser(msg, channel);
+    return parser(incomingMessage);
   }
 }
