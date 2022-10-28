97e64f7fe4e8795ab74b4883020f8bf24a3766c7
ReaganCn <rgncharana2@gmail.com>
2022-10-28 09:45:39 +0300

    :art: :bulb: clean up methods and document io parsers

--- libs/functions/bot-engine/main/src/lib/io/outgoing-message.parser.ts
+++ libs/functions/bot-engine/main/src/lib/io/outgoing-message.parser.ts
@@ -8,7 +8,8 @@ import { StoryBlock, StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks
  * 
  * Here we define methods that 
  * 
- * @param {block} - This is passed to determine the fields 
+ * @param {storyBlock}     - The StoryBlock to send. StoryBlocks are prepared messages in a format our chatbot can understand.
+ * @param {phone}          - Phonenumber to which to send the message
  *
  */
 export abstract class OutgoingMessageParser
@@ -20,46 +21,46 @@ export abstract class OutgoingMessageParser
 
   abstract getImageBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getAudioBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
+  // abstract getAudioBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getVideoBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
+  // abstract getVideoBlockParserOut    (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getStickerBlockParserOut  (storyBlock: StoryBlock, phone: string): Message
+  // abstract getStickerBlockParserOut  (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getDocumentBlockParserOut (storyBlock: StoryBlock, phone: string): Message
+  // abstract getDocumentBlockParserOut (storyBlock: StoryBlock, phone: string): Message
 
-  abstract getLocationBlockParserOut (storyBlock: StoryBlock, phone: string): Message
+  // abstract getLocationBlockParserOut (storyBlock: StoryBlock, phone: string): Message
 
   /**
    * Our chatbot can send different types of messages, be it a text message, a location, an image, ...
    * 
    * The channel API (responsible for sending and receiving messages) requires each of these messages to be sent in a certain format/via a certain contract.
    * 
-   * @param {StoryBlock}     - The StoryBlock to send. StoryBlocks are prepared messages in a format our chatbot can understand.
-   * @param {string}         - Phonenumber to which to send the message
+   * @param {storyBlock}     - The StoryBlock to send. StoryBlocks are prepared messages in a format our chatbot can understand.
+   * @param {phone}         - Phonenumber to which to send the message
    * 
    * @returns {Message}
    * A prepared  message which can be sent over the line to its specific channel API.
    */
-  parse(block: StoryBlock, phone: string) : Message
+  parse(storyBlock: StoryBlock, phone: string) : Message
   {
     let parser!: (storyBlock: StoryBlock, phone: string) => Message;
     
-    switch (block.type) {
+    switch (storyBlock.type) {
       case StoryBlockTypes.TextMessage:   parser = this.getTextBlockParserOut;     break;
       case StoryBlockTypes.Name:          parser = this.getTextBlockParserOut;     break;
       case StoryBlockTypes.Email:         parser = this.getTextBlockParserOut;     break;
       case StoryBlockTypes.PhoneNumber:   parser = this.getTextBlockParserOut;     break;   
       case StoryBlockTypes.QuestionBlock: parser = this.getQuestionBlockParserOut; break;
       case StoryBlockTypes.Image:         parser = this.getImageBlockParserOut;    break;
-      case StoryBlockTypes.Document:      parser = this.getDocumentBlockParserOut; break;
-      case StoryBlockTypes.Audio:         parser = this.getAudioBlockParserOut;    break;
-      case StoryBlockTypes.Video:         parser = this.getVideoBlockParserOut;    break;
-      case StoryBlockTypes.Sticker:       parser = this.getStickerBlockParserOut;  break;
+      // case StoryBlockTypes.Document:      parser = this.getDocumentBlockParserOut; break;
+      // case StoryBlockTypes.Audio:         parser = this.getAudioBlockParserOut;    break;
+      // case StoryBlockTypes.Video:         parser = this.getVideoBlockParserOut;    break;
+      // case StoryBlockTypes.Sticker:       parser = this.getStickerBlockParserOut;  break;
       default:
           parser = this.getTextBlockParserOut;
     }
 
-    return parser(block, phone);
+    return parser(storyBlock, phone);
   }
 }
