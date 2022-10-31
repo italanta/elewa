2f22196483cc7133500d6244b955a2fbae3a6def
ReaganCn <rgncharana2@gmail.com>
2022-10-31 14:18:32 +0300

    :pencil: rename to WhatsappOutgoingMessage

--- libs/model/convs-mgr/functions/src/lib/whatsapp/whatsapp-base-message.interface.ts
+++ libs/model/convs-mgr/functions/src/lib/whatsapp/whatsapp-base-message.interface.ts
@@ -1,11 +1,11 @@
-import { Message } from "@app/model/convs-mgr/conversations/messages";
+import { OutgoingMessagePayload } from "@app/model/convs-mgr/conversations/messages";
 
 import { WhatsAppMessageType } from "./whatsapp-message-types.interface";
 
 //All the fields that are required for all types of whatsapp messages
 //see : https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages#send-messages
 
-export interface WhatsAppMessage extends Message {
+export interface WhatsAppOutgoingMessage extends OutgoingMessagePayload{
   messaging_product: MetaMessagingProducts,
   context?: { message_id: string },
   recepient_type: RecepientType,
