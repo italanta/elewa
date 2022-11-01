ee8d4dc55803f9382799aed471aca58759048bed
ReaganCn <rgncharana2@gmail.com>
2022-11-01 13:22:36 +0300

    :truck: :wrench: fix imports

--- libs/model/bot/channel/src/index.ts
+++ libs/model/bot/channel/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/base-channel.interface';
 export * from './lib/whatsapp-channel.interface';
 export * from './lib/telegram-channel.interface';
--- libs/model/bot/channel/src/lib/whatsapp-channel.interface.ts
+++ libs/model/bot/channel/src/lib/whatsapp-channel.interface.ts
@@ -1,5 +1,4 @@
-import { CommunicationChannel } from "./base-channel.interface";
-
+import { CommunicationChannel } from "@app/model/convs-mgr/conversations/admin/system";
 export interface WhatsappChannel extends CommunicationChannel {
   //For each number used in the channel, it must be have a whatsapp business identifier
   // businessAccountId: string;
