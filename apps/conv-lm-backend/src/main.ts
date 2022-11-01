3ff1ce8aad0cb0e8d30dfa39cbbdb08e866a6603
ReaganCn <rgncharana2@gmail.com>
2022-11-01 10:19:29 +0300

    :truck: export cloud function

--- apps/conv-lm-backend/src/main.ts
+++ apps/conv-lm-backend/src/main.ts
@@ -5,10 +5,4 @@ import * as admin from 'firebase-admin';
 const conf = config().firebase;
 admin.initializeApp(conf);
 
-export * from './functions/fns-webhooks';
-// export * from './functions/fns-messages';
-
-// export * from './app/functions/bot/engineProcessMessage.function';
-// export * from './app/functions/bot/registerEndUser.function';
-// export * from './app/functions/bot/engineAddMessage.function';
-export * from './app/functions/bot/engineChatManager.function';
+export * from './app/webhooks/whatsapp-receive-incoming-message.function';
