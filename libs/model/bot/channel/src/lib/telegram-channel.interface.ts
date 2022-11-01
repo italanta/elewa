bc29a3355aede85693e3dfc2585d5efed7d0d94e
ReaganCn <rgncharana2@gmail.com>
2022-11-01 13:22:36 +0300

    :truck: :wrench: fix imports

--- libs/model/bot/channel/src/lib/telegram-channel.interface.ts
+++ libs/model/bot/channel/src/lib/telegram-channel.interface.ts
@@ -1,4 +1,4 @@
-import { CommunicationChannel } from "./base-channel.interface";
+import { CommunicationChannel } from "@app/model/convs-mgr/conversations/admin/system";
 
 export interface TelegramChannel extends CommunicationChannel {
   //For each number used in the channel, it must be have a
