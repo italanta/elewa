363b71c539fcb9b51d290660f05eefa601b47734
ReaganCn <rgncharana2@gmail.com>
2022-11-01 12:19:08 +0300

    :bulb: add comments to whatsapp receive message cloud function

--- apps/conv-lm-backend/src/app/webhooks/whatsapp-receive-incoming-message.function.ts
+++ apps/conv-lm-backend/src/app/webhooks/whatsapp-receive-incoming-message.function.ts
@@ -8,9 +8,15 @@ import { ConvLearnFunction } from "../../conv-learn-func.class";
 const handler = new WhatsAppReceiveIncomingMsgHandler();
 
 /**
- * @Desctiption : Webhook used to register whatsapp bot on app on meta site
+ * @Description : When an end user sends a message to the chatbot, this function is triggered, 
+ *      handles the message and potentially responds to it
+ * 
+ * It is specifically for Whatsapp @see {PlatformType}, and subscribes to the 'messages' webhook on
+ *      the Meta Developers platform
+ * 
+ * @see https://developers.facebook.com/docs/whatsapp/cloud-api/guides/set-up-webhooks
+ * 
  */
-
 export const channelWhatsAppMsgReceiveIncmngMsg = new ConvLearnFunction('channelWhatsAppMsgReceiveIncmngMsg', 
                                                   new EndpointRegistrar(), 
                                                   [], 
