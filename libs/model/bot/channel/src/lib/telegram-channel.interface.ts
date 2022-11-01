fa62fe0ac2d3b66a2607dacc587d275cd5cf16f5
ReaganCn <rgncharana2@gmail.com>
2022-11-01 16:27:24 +0300

    :bug: :wrench: fix channel causing errors on ng serve

--- libs/model/bot/channel/src/lib/telegram-channel.interface.ts
+++ libs/model/bot/channel/src/lib/telegram-channel.interface.ts
@@ -1,6 +1,11 @@
 import { CommunicationChannel } from "@app/model/convs-mgr/conversations/admin/system";
 
-export interface TelegramChannel extends CommunicationChannel {
-  //For each number used in the channel, it must be have a
-  // businessAccountId: string;
+export interface TelegramCommunicationChannel extends CommunicationChannel
+{
+    /** This is a unique token given by Whatsapp and required to 
+     *    send and receive messages on whatsapp endpoints
+     *  
+     *  We pass the access token in the Authorization header when sending a http request 
+     * */
+    accessToken?: string;
 }
