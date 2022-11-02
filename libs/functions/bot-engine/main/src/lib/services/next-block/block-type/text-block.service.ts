873d679d1e7278e6fb251bb268ca6b9fdd957903
Reagan Charana <rgncharana2@gmail.com>
2022-11-02 15:43:19 +0300

    :art: clean up code

--- libs/functions/bot-engine/main/src/lib/services/next-block/block-type/text-block.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/next-block/block-type/text-block.service.ts
@@ -9,11 +9,13 @@ import { BlockDataService } from "../../data-services/blocks.service";
 import { ConnectionsDataService } from "../../data-services/connections.service";
 import { NextBlockService } from "../next-block.class";
 
-export class TextMessageService extends NextBlockService {
+export class TextMessageService extends NextBlockService
+{
     userInput: string;
     _logger: Logger;
     tools: HandlerTools;
-    constructor(private _blockDataService: BlockDataService, private _connDataService: ConnectionsDataService, tools: HandlerTools){
+    constructor(private _blockDataService: BlockDataService, private _connDataService: ConnectionsDataService, tools: HandlerTools)
+    {
         super(tools)
         this.tools = tools
     }
@@ -22,10 +24,12 @@ export class TextMessageService extends NextBlockService {
      * Returns the block connected to the default option of the text block
      * @returns Next StoryBlock
      */
-    async getNextBlock(msg: Message, lastBlock: TextMessageBlock): Promise<StoryBlock>{
+    async getNextBlock(msg: Message, lastBlock: TextMessageBlock): Promise<StoryBlock>
+    {
+        let nextBlock: StoryBlock
 
         const connection = await this._connDataService.getConnBySourceId(lastBlock.id)
-        let nextBlock: StoryBlock = await this._blockDataService.getBlockById(connection.targetId)
+        nextBlock = await this._blockDataService.getBlockById(connection.targetId)
 
         return nextBlock
     }
