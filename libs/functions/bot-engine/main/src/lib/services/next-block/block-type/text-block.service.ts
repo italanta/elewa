c0b8af0e5f2d7734a4d58e36cb211b92dfcd49ec
Reagan Charana <rgncharana2@gmail.com>
2022-11-02 17:04:53 +0300

    :bulb: add more documentation

--- libs/functions/bot-engine/main/src/lib/services/next-block/block-type/text-block.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/next-block/block-type/text-block.service.ts
@@ -9,31 +9,40 @@ import { BlockDataService } from "../../data-services/blocks.service";
 import { ConnectionsDataService } from "../../data-services/connections.service";
 import { NextBlockService } from "../next-block.class";
 
-export class TextMessageService extends NextBlockService
+/**
+ * When an end user send a message to the bot, we need to know the type of block @see {StoryBlockTypes} we sent 
+ *  so that we can process the response based on that block.
+ * 
+ * This service manages those blocks that have only the default option(Only one connection originates from them)
+ *  Therefore for these blocks, we already know the next block to send regardless of the user response
+ * 
+ */
+export class DefaultOptionMessageService extends NextBlockService
 {
-    userInput: string;
-    _logger: Logger;
-    tools: HandlerTools;
-    
-    constructor(private _blockDataService: BlockDataService, private _connDataService: ConnectionsDataService, tools: HandlerTools)
-    {
-        super(tools);
-        this.tools = tools;
-    }
+	userInput: string;
+	_logger: Logger;
+	tools: HandlerTools;
 
-    /**
-     * Returns the block connected to the default option of the text block
-     * @returns Next StoryBlock
-     */
-    async getNextBlock(msg: Message, lastBlock: TextMessageBlock): Promise<StoryBlock>
-    {
-        let nextBlock: StoryBlock;
+	constructor(private _blockDataService: BlockDataService, private _connDataService: ConnectionsDataService, tools: HandlerTools)
+	{
+		super(tools);
+		this.tools = tools;
+	}
 
-        const connection = await this._connDataService.getConnBySourceId(lastBlock.id);
+	/**
+	 * Gets the next block in the story linked to the default option
+	 * 
+	 * @note We can potentially know if the block is the last one if no connection originates from it (connnection == null)
+	 */
+	async getNextBlock(msg: Message, lastBlock: TextMessageBlock): Promise<StoryBlock>
+	{
+		let nextBlock: StoryBlock;
+		// Get the connection
+		const connection = await this._connDataService.getConnBySourceId(lastBlock.id);
+		// Get the next block using the id. Connection.targetId == id of the next block
+		if (connection)
+			nextBlock = await this._blockDataService.getBlockById(connection.targetId);
 
-        if (connection)
-            nextBlock = await this._blockDataService.getBlockById(connection.targetId);
-
-        return nextBlock;
-    }
+		return nextBlock;
+	}
 }
