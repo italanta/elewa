8978935c224900d33b8dbfbecdde19cec1340439
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-11-08 11:08:26 +0300

    :wrench: temporarily routed analytics back to dashboard

--- libs/features/convs-mgr/stories/home/src/lib/stories.router.ts
+++ libs/features/convs-mgr/stories/home/src/lib/stories.router.ts
@@ -8,6 +8,8 @@ import { StoriesDashboardComponent } from './pages/stories-dashboard/stories-das
 const STORIES_ROUTES: Route[] = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: 'dashboard', component: StoriesDashboardComponent, canActivate: [IsLoggedInGuard] },
+  // TODO: Update analytics to analytics page
+  { path: 'analytics', component: StoriesDashboardComponent, canActivate: [IsLoggedInGuard] },
   {
     path: ':id',
     loadChildren: () => import('libs/features/convs-mgr/stories/editor/src/lib/convl-story-editor.module').then(m => m.ConvlStoryEditorModule),
