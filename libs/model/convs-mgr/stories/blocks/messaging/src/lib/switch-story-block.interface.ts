7d4d3257611510aec62a527a06b9d2f492878d6c
Reagan Charana <rgncharana2@gmail.com>
2022-12-01 12:49:50 +0300

    :pencil: rename to EndStoryBlock

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/switch-story-block.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/switch-story-block.interface.ts
@@ -3,7 +3,7 @@ import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 /**
  * Block which links the current story to the next one
  */
-export interface SwitchStoryBlock extends StoryBlock
+export interface EndStoryBlock extends StoryBlock
 {
   /** Id of the story to switch to */
   storyId: string;
