36943c642b1d6175ec24542f86d2d94867db5b37
conceptacherono <cocotesot254@gmail.com>
2022-12-02 12:36:08 +0300

    final edit
    final edit

---
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/icons-and-titles.ts
@@ -0,0 +1,66 @@
+export const iconsAndTitles: any = {
+  1 : {
+		title: 'Message Block',
+		icon: 'fas fa-comment-alt'
+	},
+  2 : {
+		title: 'Input Block',
+		icon: 'fas fa-envelope'
+	},
+  3 : {
+		title: 'IO Block',
+		icon: 'fas fa-envelope'
+	},
+  4 : {
+		title: 'Location Block',
+		icon: 'fas fa-map-marker-alt'
+	},
+  5 : {
+		title: 'Image Block',
+		icon: 'fas fa-image'
+	},
+  6 : {
+		title: 'Question Block',
+		icon: 'fas fa-question-circle'
+	},
+  7 : {
+		title: 'Document Block',
+		icon: 'fas fa-file-alt'
+	},
+  8 : {
+		title: 'Audio Block',
+		icon: 'fas fa-microphone-alt'
+	},
+  9 : {
+		title: 'Structural Block',
+		icon: 'fas fa-envelope'
+	},
+  10 : {
+		title: 'Name Block',
+		icon: 'fas fa-user-alt'
+	},
+  11 : {
+		title: 'Email Block',
+		icon: 'fas fa-envelope'
+	},
+  12 : {
+		title: 'Phone Block',
+		icon: 'fas fa-phone-alt'
+	},
+  13 : {
+		title: 'Video Block',
+		icon: 'fas fa-video'
+	},
+  15 : {
+		title: 'Sticker Block',
+		icon: 'fas fa-sticky-note'
+	},
+  16 : {
+		title: 'List Block',
+		icon: 'fas fa-list-ul'
+	},
+  17 : {
+		title: 'Reply Block',
+		icon: 'fas fa-reply'
+	},
+}
