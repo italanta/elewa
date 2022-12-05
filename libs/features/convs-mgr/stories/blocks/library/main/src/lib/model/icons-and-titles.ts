018c9988b0b053599be888f4fd0038ef1abfd64d
ianodhiambo9 <ianodhiambo9@gmail.com>
2022-12-05 15:21:09 +0300

    :art: fixed blocks structure

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/icons-and-titles.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/icons-and-titles.ts
@@ -1,66 +1,66 @@
 export const iconsAndTitles: any = {
   1 : {
-		title: 'Message Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.MESSAGE',
 		icon: 'fas fa-comment-alt'
 	},
   2 : {
-		title: 'Input Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.INPUT',
 		icon: 'fas fa-envelope'
 	},
   3 : {
-		title: 'IO Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.IO',
 		icon: 'fas fa-envelope'
 	},
   4 : {
-		title: 'Location Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.MESSAGE.LOCATION',
 		icon: 'fas fa-map-marker-alt'
 	},
   5 : {
-		title: 'Image Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.IMAGE',
 		icon: 'fas fa-image'
 	},
   6 : {
-		title: 'Question Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.QUESTION',
 		icon: 'fas fa-question-circle'
 	},
   7 : {
-		title: 'Document Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.DOCUMENT',
 		icon: 'fas fa-file-alt'
 	},
   8 : {
-		title: 'Audio Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.AUDIO',
 		icon: 'fas fa-microphone-alt'
 	},
   9 : {
-		title: 'Structural Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.STRUCTURAL',
 		icon: 'fas fa-envelope'
 	},
   10 : {
-		title: 'Name Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.NAME',
 		icon: 'fas fa-user-alt'
 	},
   11 : {
-		title: 'Email Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.EMAIL',
 		icon: 'fas fa-envelope'
 	},
   12 : {
-		title: 'Phone Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.PHONE',
 		icon: 'fas fa-phone-alt'
 	},
   13 : {
-		title: 'Video Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.VIDEO',
 		icon: 'fas fa-video'
 	},
   15 : {
-		title: 'Sticker Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.STICKER',
 		icon: 'fas fa-sticky-note'
 	},
   16 : {
-		title: 'List Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.LIST',
 		icon: 'fas fa-list-ul'
 	},
   17 : {
-		title: 'Reply Block',
+		title: 'PAGE-CONTENT.BLOCK.TITLES.REPLY',
 		icon: 'fas fa-reply'
 	},
 }
