015654e8fc38a19c2dd131bf013b833fabe174fc
Nyash-Mauro <mauricee423@gmail.com>
2022-12-05 13:40:28 +0300

    removed decorators and after init lifecycle method

--- libs/features/convs-mgr/stories/blocks/library/name-message-block/src/lib/components/name-block/name-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/name-message-block/src/lib/components/name-block/name-block.component.ts
@@ -1,18 +1,16 @@
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 import { NameMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
-
-import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
 
 @Component({
   selector: 'app-name-block',
   templateUrl: './name-block.component.html',
   styleUrls: ['./name-block.component.scss'],
 })
-export class NameBlockComponent implements OnInit, AfterViewInit 
+export class NameBlockComponent implements OnInit
 {
   @Input() id: string;
   @Input() block: NameMessageBlock;
@@ -26,18 +24,4 @@ export class NameBlockComponent implements OnInit, AfterViewInit
   ngOnInit(): void {
     this.nameInputId = `name-${this.id}`
   }
-
-  ngAfterViewInit(): void {
-    if (this.jsPlumb) {
-      this._decorateInput();
-    }
-  }
-
-  private _decorateInput() {
-    let input = document.getElementById(this.nameInputId) as Element;
-    if (this.jsPlumb) {
-      input = _JsPlumbComponentDecorator(input, this.jsPlumb);
-    }
-  }
-
 }
