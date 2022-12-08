68dbe5a57389ab1549795d07c007642b533a73e0
Reagan Charana <rgncharana2@gmail.com>
2022-12-08 08:52:48 +0300

    :bulb: :art: clean up code and add comments

--- libs/functions/bot-engine/main/src/lib/services/next-block/block-type/multiple-options-block.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/next-block/block-type/multiple-options-block.service.ts
@@ -2,21 +2,22 @@ import { HandlerTools, Logger } from "@iote/cqrs";
 
 import { Message, QuestionMessage } from "@app/model/convs-mgr/conversations/messages";
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+import { QuestionMessageBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
 
 import { BlockDataService } from "../../data-services/blocks.service";
 import { ConnectionsDataService } from "../../data-services/connections.service";
 import { NextBlockService } from "../next-block.class";
 import { MatchInputService } from "../../match-input/match-input.service";
 import { ExactMatch } from "../../match-input/strategies/exact-match.strategy";
-import { QuestionMessageBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
 
 /**
  * When an end user send a message to the bot, we need to know the type of block @see {StoryBlockTypes} we sent 
  *  so that we can process the response based on that block.
  * 
- * This service manages those blocks that have only the default option(Only one connection originates from them)
- *  Therefore for these blocks, we already know the next block to send regardless of the user response
+ * This service manages those blocks that have multiple options. It determines the next block in the story based
+ * 	on the option selected by the end user.
  * 
+ * Used for @type {QuestionMessageBlock} and @type {ListMessageBlock}
  */
 export class MultipleOptionsMessageService extends NextBlockService
 {
@@ -31,40 +32,49 @@ export class MultipleOptionsMessageService extends NextBlockService
 	}
 
 	/**
-	 * Gets the next block in the story linked to the default option
+	 * Gets the next block in the story linked to the option selected by the end user
 	 * 
 	 * @note We can potentially know if the block is the last one if no connection originates from it (connnection == null)
 	 */
 	async getNextBlock(msg: Message, lastBlock: QuestionMessageBlock, orgId: string, currentStory: string, endUserId: string): Promise<StoryBlock>
 	{
 		const response = msg as QuestionMessage;
-		
+
 		const matchInput = new MatchInputService();
 
 		// Set the match strategy to exactMatch
 		// TODO: Add a dynamic way of selecting matching strategies
 		matchInput.setMatchStrategy(new ExactMatch());
-	
+
 		const selectedOptionIndex = matchInput.matchId(response.options[0].optionId, lastBlock.options);
-	
-		if (selectedOptionIndex == -1)
-		{
-				this._logger.error(() => `The message did not match any option found`);
+
+		if (selectedOptionIndex == -1) {
+			this._logger.error(() => `The message did not match any option found`);
 		}
-	
+
 		const sourceId = `i-${selectedOptionIndex}-${lastBlock.id}`;
-	
+
 		const connection = await this._connDataService.getConnByOption(sourceId, orgId, currentStory);
-	
+
 		const nextBlock = await this._blockDataService.getBlockById(connection.targetId, orgId, currentStory);
-	
+
 		return nextBlock;
 	}
 
+ /**
+ 	* The user/organisation creating the bot might choose to save the response of the end user for later use.
+ 	* 
+ 	* This method takes the response(value) and saves it to a temporary collection. This data can then
+ 	*  be retrieved from this collection later e.g. when the story ends the user creating the bot might choose to 
+ 	*    post the data to a REST endpoint.
+ 	*/
 	async saveUserResponse(msg: Message, lastBlock: StoryBlock, orgId: string, endUserId: string): Promise<any>
 	{
 		const response = msg as QuestionMessage;
 
-		if (lastBlock.milestone) return this.saveData(lastBlock.tag, orgId, lastBlock.milestone, response.options[0].optionText, endUserId);
+		// Get the text value of the option the end user has clicked
+		const value = response.options[0].optionText;
+
+		if (lastBlock.milestone) return this.saveData(lastBlock.tag, orgId, lastBlock.milestone, value, endUserId);
 	}
 }
