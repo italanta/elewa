43d3079058b07bdf3c4daf38f1dc109415c83ebd
Nyash-Mauro <mauricee423@gmail.com>
2022-12-10 10:27:53 +0300

    solved errors,removee unused codes and merged

--- libs/features/convs-mgr/stories/blocks/library/email-message-block/src/lib/components/email-block/email-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/email-message-block/src/lib/components/email-block/email-block.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
-import { StoryBlock, StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+import {  StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 
 import { EmailMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
