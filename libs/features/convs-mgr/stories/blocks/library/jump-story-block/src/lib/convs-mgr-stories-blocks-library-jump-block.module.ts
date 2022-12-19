5a18e9e0a17dbfac477773714ac0915cef101a93
Reagan Charana <rgncharana2@gmail.com>
2022-12-19 15:44:14 +0300

    :art: :heavy_plus_sign: add jump block component

--- libs/features/convs-mgr/stories/blocks/library/jump-story-block/src/lib/convs-mgr-stories-blocks-library-jump-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/jump-story-block/src/lib/convs-mgr-stories-blocks-library-jump-block.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { JumpBlockComponent } from './components/jump-block/jump-block.component';
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [JumpBlockComponent]
 })
 export class ConvsMgrJumpBlockModule {}
