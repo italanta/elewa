75f1234bbcef0068311bf9bdcc52cc22c52d2137
Reagan Charana <rgncharana2@gmail.com>
2022-12-19 15:56:13 +0300

    :art: :sparkles:  add jump block to block component

--- libs/features/convs-mgr/stories/blocks/library/jump-story-block/src/lib/components/jump-block/jump-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/jump-story-block/src/lib/components/jump-block/jump-block.component.ts
@@ -1,20 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
-import { Logger } from '@iote/bricks-angular';
-import { StoryBlock, StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
-// import { TextMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
-import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+
+import { Logger } from '@iote/bricks-angular';
+
+import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 import { JumpBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
+import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+
 
 /**
  * Block which sends a message from bot to user.
  */
 @Component({
   selector: 'app-jump-block',
-  templateUrl: './message-block.component.html',
-  styleUrls: ['./message-block.component.scss']
+  templateUrl: './jump-block.component.html',
+  styleUrls: ['./jump-block.component.scss']
 })
 export class JumpBlockComponent implements OnInit
 {
@@ -27,7 +30,7 @@ export class JumpBlockComponent implements OnInit
   @Input() blocksGroup: FormArray;
  
   type: StoryBlockTypes;
-  messagetype = StoryBlockTypes.TextMessage;
+  messagetype = StoryBlockTypes.JumpBlock;
   blockFormGroup: FormGroup;
 
   constructor(private _fb: FormBuilder,
--- libs/features/convs-mgr/stories/blocks/library/jump-story-block/src/lib/convs-mgr-stories-blocks-library-jump-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/jump-story-block/src/lib/convs-mgr-stories-blocks-library-jump-block.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialDesignModule, MaterialBricksModule } from '@iote/bricks-angular';
+
 import { JumpBlockComponent } from './components/jump-block/jump-block.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [JumpBlockComponent]
+  imports: [
+    CommonModule,
+    MaterialDesignModule,
+    FlexLayoutModule,
+    MaterialBricksModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    // ConvsMgrBlockOptionsModule
+  ],
+
+  declarations: [JumpBlockComponent],
+
+  exports: [JumpBlockComponent],
 })
 export class ConvsMgrJumpBlockModule {}
