68eee0c6d92caa92ec32ec6fc73ac73b2e6d7008
Erastus Njoroge <85213281+indomitable-core@users.noreply.github.com>
2023-01-16 13:06:11 +0300

    :art:  add interface for webhook block

---
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/webhook-message-block.interface.ts
@@ -0,0 +1,17 @@
+import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+import { HttpMethodTypes } from '@app/model/convs-mgr/stories/blocks/main';
+export interface WebhookMessageBlock extends StoryBlock {
+defaultTarget?: string;
+  /** The http url of the endpoint */
+httpUrl?: string;
+
+/** An array of variables defined in the previous input blocks e.g. variable for a name input block can be `name`*/
+variablesToPost?: string[];
+
+/** Http Method to be choosen by the user */
+httpMethod?: HttpMethodTypes;
+
+/** Optional token if required by the api we are posting to */
+authorizationToken?: string;
+
+}
