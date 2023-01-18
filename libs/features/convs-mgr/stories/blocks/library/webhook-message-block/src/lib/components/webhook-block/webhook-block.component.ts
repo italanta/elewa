b093722db87044edab831e6eaefcdbd0b0caddfb
Erastus Njoroge <85213281+indomitable-core@users.noreply.github.com>
2023-01-18 15:34:02 +0300

    :art: review the webhook user interface

--- libs/features/convs-mgr/stories/blocks/library/webhook-message-block/src/lib/components/webhook-block/webhook-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/webhook-message-block/src/lib/components/webhook-block/webhook-block.component.ts
@@ -1,20 +1,17 @@
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
-import { Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 
-import { BehaviorSubject, Observable, combineLatest, of, map, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, of, Subscription } from 'rxjs';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
 
 import { WebhookMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
-import { StoryBlock, StoryBlockTypes, HttpMethodTypes, VariablesConfig } from '@app/model/convs-mgr/stories/blocks/main';
-import {
-  ImageMessageBlock, LocationMessageBlock, NameMessageBlock, QuestionMessageBlock,
-  TextMessageBlock, EmailMessageBlock, PhoneMessageBlock, DocumentMessageBlock, StickerMessageBlock,
-  VoiceMessageBlock, VideoMessageBlock, ListMessageBlock, ReplyMessageBlock
-} from '@app/model/convs-mgr/stories/blocks/messaging';
+import { StoryBlock, StoryBlockTypes, HttpMethodTypes, VariablesConfig, Variables } from '@app/model/convs-mgr/stories/blocks/main';
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+import { VariablesConfigStore } from '@app/state/convs-mgr/stories/variables-config';
+
 
 @Component({
   selector: 'app-webhook-block',
@@ -27,7 +24,7 @@ export class WebhookBlockComponent implements OnInit, AfterViewInit {
   @Input() block: WebhookMessageBlock;
   @Input() webhookMessageForm: FormGroup;
   @Input() jsPlumb: BrowserJsPlumbInstance;
-  @Input() blockFormGroup: FormGroup;
+
 
 
   webhookInputId: string;
@@ -50,52 +47,32 @@ export class WebhookBlockComponent implements OnInit, AfterViewInit {
 
   variables = new FormControl();
 
-  inputVariables: StoryBlock[] = [
-    { type: StoryBlockTypes.TextMessage, message: 'Message' } as TextMessageBlock,
-    { type: StoryBlockTypes.Location, message: 'Location' } as LocationMessageBlock,
-    { type: StoryBlockTypes.Image, message: 'Image' } as ImageMessageBlock,
-    { type: StoryBlockTypes.QuestionBlock, message: 'Question' } as QuestionMessageBlock,
-    { type: StoryBlockTypes.Document, message: 'Document' } as DocumentMessageBlock,
-    { type: StoryBlockTypes.Audio, message: 'Audio' } as VoiceMessageBlock,
-    { type: StoryBlockTypes.Name, message: 'Name' } as NameMessageBlock,
-    { type: StoryBlockTypes.Email, message: 'Email' } as EmailMessageBlock,
-    { type: StoryBlockTypes.PhoneNumber, message: 'Phone' } as PhoneMessageBlock,
-    { type: StoryBlockTypes.Video, message: 'Video' } as VideoMessageBlock,
-    { type: StoryBlockTypes.Sticker, message: 'Sticker' } as StickerMessageBlock,
-    { type: StoryBlockTypes.List, message: 'List' } as ListMessageBlock,
-  ];
+  variables$: Observable<any>
 
   selectedVariables: any;
-  inputVariables$: Observable<StoryBlock[]> = of(this.inputVariables);
-  constructor(private _fb: FormBuilder) { }
+
+  constructor(private _fb: FormBuilder,
+    private _variablesStore$$: VariablesConfigStore
+    ) { }
 
   ngOnInit() {
 
+    this.variables$ = this._variablesStore$$.get();
 
     this.webhookInputId = `webhook-${this.id}`;
     this.httpUrl;
-    // this.method;
-    this.filteredVariables;
+    this.httpMethods
     this.subscription = this.httpDropdown.valueChanges
-    .subscribe(value => console.log(value));
-
-  }
-  filteredVariables() {
-    this.inputVariables$ = combineLatest([this.filterVariableInput$$, this.inputVariables$])
-      .pipe(map(([filter, variablesArray]) => variablesArray
-        .filter((variable: StoryBlock) => {
-          return variable.message?.toString().toLowerCase().includes(filter)
-          
-        })))
+      .subscribe(value => console.log(value));
 
   }
 
-  filterVariables(event: any) {
-    this.filterVariableInput$$.next(event.target.value);
+  getVariables(variables: Variables): string[] {
+    return Object.keys(variables);
   }
-
 
   ngAfterViewInit(): void {
     ///
   }
+
 }
