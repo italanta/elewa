2ba3ada4d8f4c74d4ba67c9f41dee94009c43dfa
Reagan Charana <rgncharana2@gmail.com>
2023-01-19 13:18:42 +0300

    :wrench: :sparkles: implement service to handle jump block

--- libs/functions/bot-engine/main/src/lib/services/next-block/block-type/jump-story-block.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/next-block/block-type/jump-story-block.service.ts
@@ -4,12 +4,17 @@ import { EndUser } from "@app/model/convs-mgr/conversations/chats";
 import { JumpBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 import { Message } from "@app/model/convs-mgr/conversations/messages";
+import { Stack } from "@app/model/convs-mgr/functions";
+import { Cursor, EndUserPosition, RoutedCursor } from "@app/model/convs-mgr/conversations/admin/system";
 
 import { BlockDataService } from "../../data-services/blocks.service";
 import { ConnectionsDataService } from "../../data-services/connections.service";
 import { EndUserDataService } from "../../data-services/end-user.service";
 
 import { NextBlockService } from "../next-block.class";
+
+import { CursorDataService } from "../../data-services/cursor.service";
+
 
 /**
  * A subroutine is a conversational flow within another story. 
@@ -42,13 +47,58 @@ export class JumpStoryBlockService extends NextBlockService
    *  - Add function to get the first block in the new story if the block is not provided
    *  - Add function to jump to the same story, if the story is not provided
    */
-  async getNextBlock(msg: Message, lastBlock: JumpBlock, orgId: string, currentStory: string, endUserId: string): Promise<StoryBlock>
+  async getNextBlock(msg: Message, currentCursor: Cursor, currentBlock: JumpBlock, orgId: string, currentStory: string, endUserId: string): Promise<StoryBlock>
   {
+    const cursorService = new CursorDataService(this.tools);
+
+    // Get the connections pointing to block success or block fail blocks in the story
+    // Then we use the connections to get the blocks id and construct our RoutedCursor
+
+    // The jump block only has two options, one to use in case the child story is successful
+    //  and another incase the story fails
+    const blockSuccessSourceId = `i-${0}-${currentBlock.id}`;
+    const blockFailSourceId = `i-${1}-${currentBlock.id}`;
+
+    const blockSuccessConn = await this._connDataService.getConnBySourceId(blockSuccessSourceId, orgId, currentStory);
+
+    const blockFailConn = await this._connDataService.getConnBySourceId(blockFailSourceId, orgId, currentStory);
+
     // Get the next block by passing the blockId and the storyId and the blockId specified in the story.
-    const nextBlock = await this._blockDataService.getBlockById(lastBlock.targetBlockId, orgId, lastBlock.targetStoryId);
+    const nextBlock = await this._blockDataService.getBlockById(currentBlock.targetBlockId, orgId, currentBlock.targetStoryId);
+
+    // Create routed cursor
+    const routedCursor: RoutedCursor = {
+      storyId: currentBlock.targetStoryId,
+      blockSuccess: blockSuccessConn.targetId,
+      blockFail: blockFailConn.targetId,
+    };
+
+    // Update the EndUser 
+    const newUserPosition: EndUserPosition = {
+      storyId: currentBlock.targetStoryId,
+      blockId: nextBlock.id
+    };
+
+    // Create new stack if it does not exist or 
+    //  push the new routed cursor to the top existing stack
+    if (currentCursor.parentStack) {
+      currentCursor.parentStack = new Stack<RoutedCursor>(routedCursor);
+    } else {
+      currentCursor.parentStack.push(routedCursor);
+    }
+
+    // Update the current cursor
+    const newCursor: Cursor = {
+      ...currentCursor,
+      position: newUserPosition
+    };
+
+    await cursorService.updateCursor(endUserId, orgId, newCursor);
 
     // Update the story if we have jumped to another one
-    if (currentStory !== lastBlock.targetBlockId) await this._updateStory(lastBlock.targetStoryId, orgId, endUserId);
+    // TODO: Remove story from end user document and to fetch from cursor
+    if (currentStory !== currentBlock.targetBlockId) await this._updateStory(currentBlock.targetStoryId, orgId, endUserId);
+
 
     return nextBlock;
   }
