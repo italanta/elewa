712621819b1b312ea09529220a5b0d4d849dfd08
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-01-23 10:21:21 -0800

    :art: :

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/multiple-input-message-block.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/multiple-input-message-block.interface.ts
@@ -1,7 +1,12 @@
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 
+/**
+ * Block that accepts multiple inputs.
+ */
+
 export interface MultipleInputMessageBlock extends StoryBlock
 {
+  /**Multiple Input that is expected as input */
   message?: string;
 
   defaultTarget?: string;
@@ -12,10 +17,12 @@ export interface MultipleInputMessageBlock extends StoryBlock
   // Implement generic tags
   tag?: string;
 
+  // Stores the data from the multiple input
   context?: string;
 
 }
 
 interface MultipleOptions {
+  /** Value the answer holds. */
   values: string[]
 }
