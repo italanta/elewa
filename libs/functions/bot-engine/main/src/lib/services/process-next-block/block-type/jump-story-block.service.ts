b32ef00be54408d40293650532015b243ba24a4a
Reagan Charana <rgncharana2@gmail.com>
2023-01-25 19:45:38 +0300

    :recycle: :wrench: move operational story blocks to separate folder

--- libs/functions/bot-engine/main/src/lib/services/process-next-block/block-type/jump-story-block.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/process-next-block/block-type/jump-story-block.service.ts
@@ -2,19 +2,13 @@ import { HandlerTools, Logger } from "@iote/cqrs";
 
 import { EndUser } from "@app/model/convs-mgr/conversations/chats";
 import { JumpBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
-import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
-import { Message } from "@app/model/convs-mgr/conversations/messages";
 
 import { Cursor, EndUserPosition, RoutedCursor } from "@app/model/convs-mgr/conversations/admin/system";
 
 import { BlockDataService } from "../../data-services/blocks.service";
 import { ConnectionsDataService } from "../../data-services/connections.service";
 import { EndUserDataService } from "../../data-services/end-user.service";
-
-import { NextBlockService } from "../next-block.class";
-
-import { CursorDataService } from "../../data-services/cursor.service";
-
+import { IProcessNextBlock } from "../models/process-next-block.interface";
 
 /**
  * A subroutine is a conversational flow within another story. 
@@ -27,87 +21,71 @@ import { CursorDataService } from "../../data-services/cursor.service";
  *  - Add function to get the first block in the new story if the block is not provided
  *  - Add function to jump to the same story, if the story is not provided
  */
-export class JumpStoryBlockService extends NextBlockService
+export class JumpStoryBlockService implements IProcessNextBlock
 {
   userInput: string;
   _logger: Logger;
 
-  constructor(private _blockDataService: BlockDataService, private _connDataService: ConnectionsDataService, tools: HandlerTools)
-  {
-    super(tools);
-    this.tools = tools;
-  }
+  constructor(private _blockDataService: BlockDataService, private _connDataService: ConnectionsDataService, private tools: HandlerTools)
+  { }
 
-  /**
-   * When a user hits a jump block we:
-   *  1. Get the current cursor
-   *  2. Create a RoutedCursor from the block configuration (based on next steps (success connection, fail connection, error config) 
-   *      within the current story
-   *  3. Push the RoutedCursor onto the parent stack 
-   *  4. Update the current cursor to point to the new story and it's first block (the one connected to the starting block)
-   * 
-   * We get the next block in the storyline by using the targetStoryId and targetBlockId saved in the @type {JumpBlock}
-   * 
-   * TODO: 
-   *  - Add function to get the first block in the new story if the block is not provided
-   *  - Add function to jump to the same story, if the story is not provided
-   */
-  async getNextBlock(msg: Message, currentCursor: Cursor, currentBlock: JumpBlock, orgId: string, currentStory: string, endUserId: string): Promise<Cursor>
-  {
-    const cursorService = new CursorDataService(this.tools);
-
+  public async handleBlock(storyBlock: JumpBlock, updatedCursor: Cursor, orgId: string, endUserId: string) {
+      
+    const currentStory = updatedCursor.position.storyId;
+    
     // 1. Get the connections pointing to block success or block fail blocks in the story
     // Then we use the connections to get the blocks id and construct our RoutedCursor
 
     // The jump block only has two options, one to use in case the child story is successful
     //  and another incase the story fails
-    const blockSuccessSourceId = `i-0-${currentBlock.id}`;
-    const blockFailSourceId = `i-1-${currentBlock.id}`;
+    const blockSuccessSourceId = `i-0-${storyBlock.id}`;
+    const blockFailSourceId = `i-1-${storyBlock.id}`;
 
     const blockSuccessConn = await this._connDataService.getConnBySourceId(blockSuccessSourceId, orgId, currentStory);
 
     const blockFailConn = await this._connDataService.getConnBySourceId(blockFailSourceId, orgId, currentStory);
 
     // Get the next block by passing the blockId and the storyId and the blockId specified in the story.
-    const nextBlock = await this._blockDataService.getBlockById(currentBlock.targetBlockId, orgId, currentBlock.targetStoryId);
+    const nextBlock = await this._blockDataService.getBlockById(storyBlock.targetBlockId, orgId, storyBlock.targetStoryId);
 
     // 2. Create routed cursor
     const routedCursor: RoutedCursor = {
-      storyId: currentBlock.targetStoryId,
+      storyId: storyBlock.targetStoryId,
       blockSuccess: blockSuccessConn.targetId,
       blockFail: blockFailConn.targetId,
     };
 
     // Update the EndUser 
     const newUserPosition: EndUserPosition = {
-      storyId: currentBlock.targetStoryId,
+      storyId: storyBlock.targetStoryId,
       blockId: nextBlock.id
     };
 
     // 3. Create new stack if it does not exist or 
     //  push the new routed cursor to the top existing stack
-    if (currentCursor.parentStack) {
+    if (updatedCursor.parentStack) {
       const parentStack = [];
       parentStack.unshift(routedCursor);
 
-      currentCursor.parentStack = parentStack;
+      updatedCursor.parentStack = parentStack;
     } else {
-      currentCursor.parentStack.push(routedCursor);
+      updatedCursor.parentStack.push(routedCursor);
     }
 
     // 4. Update the current cursor
     const newCursor: Cursor = {
-      ...currentCursor,
+      ...updatedCursor,
       position: newUserPosition
     };
 
-    // await cursorService.updateCursor(endUserId, orgId, newCursor);
-
     // Update the story if we have jumped to another one
     // TODO: Remove story from end user document and to fetch from cursor
-    if (currentStory !== currentBlock.targetBlockId) await this._updateStory(currentBlock.targetStoryId, orgId, endUserId);
+    if (currentStory !== storyBlock.targetBlockId) await this._updateStory(storyBlock.targetStoryId, orgId, endUserId);
 
-    return newCursor;
+    return {
+      storyBlock: nextBlock,
+      newCursor
+    };
   }
 
   /**
