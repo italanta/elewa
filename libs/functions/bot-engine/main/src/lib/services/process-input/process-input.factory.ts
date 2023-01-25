082daf424a02ebd946bc4cc42f1f37b520a0e342
Reagan Charana <rgncharana2@gmail.com>
2023-01-25 11:25:48 +0300

    :wrench: implement separate class to handle inputs

---
+++ libs/functions/bot-engine/main/src/lib/services/process-input/process-input.factory.ts
@@ -0,0 +1,37 @@
+import { HandlerTools } from "@iote/cqrs";
+
+import { Message } from "@app/model/convs-mgr/conversations/messages";
+import { MessageTypes } from "@app/model/convs-mgr/functions";
+import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+
+import { ProcessLocationInput } from "./block-type/process-location-input.service";
+import { ProcessMediaInput } from "./block-type/process-media-input.service";
+import { ProcessTextInput } from "./block-type/process-text-input.service";
+import { ProcessOptionsInput } from "./block-type/process-options-input.service";
+
+
+export class ProcessInputFactory 
+{
+  constructor(private tools: HandlerTools){}
+
+  processInput(message: Message, lastBlock: StoryBlock, orgId: string, endUserId: string) {
+    switch (message.type) {
+      case MessageTypes.TEXT:
+        return new ProcessTextInput(this.tools).handleInput(message, lastBlock, orgId, endUserId);
+      case MessageTypes.LOCATION:
+        return new ProcessLocationInput(this.tools).handleInput(message, lastBlock, orgId, endUserId);
+      case MessageTypes.QUESTION:
+        return new ProcessOptionsInput(this.tools).handleInput(message, lastBlock, orgId, endUserId);
+      case MessageTypes.IMAGE:
+        return new ProcessMediaInput(this.tools).handleInput(message, lastBlock, orgId, endUserId);
+        case MessageTypes.IMAGE:
+        return new ProcessMediaInput(this.tools).handleInput(message, lastBlock, orgId, endUserId);
+      case MessageTypes.VIDEO:
+        return new ProcessMediaInput(this.tools).handleInput(message, lastBlock, orgId, endUserId);
+      case MessageTypes.AUDIO:
+        return new ProcessMediaInput(this.tools).handleInput(message, lastBlock, orgId, endUserId);
+      default:
+        break;
+    }
+  }
+}
