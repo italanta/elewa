29d456b24f6de59d365e61b09ccad415c6bafcf7
Reagan Charana <rgncharana2@gmail.com>
2023-01-26 09:44:26 +0300

    :wrench: :horse_racing: implement main incoming message parser class

---
+++ libs/functions/bot-engine/main/src/lib/io/incoming-message-parsers/incoming-message.parser.ts
@@ -0,0 +1,33 @@
+import { Message } from "@app/model/convs-mgr/conversations/messages";
+
+import { ActiveChannel } from "../../model/active-channel.service";
+import { MessagesDataService } from "../../services/data-services/messages.service";
+
+/**
+ * Our chatbot recieves different types of messages, be it a text message, a location, an image, ...
+ * 
+ * We therefore need to convert this message to a standardized format so that our chatbot can read and process the message
+ * 
+ * Here we define methods that parse in messages and return a standardized format required by the chatbot
+ * 
+ * @param {messageType} -  Type of the message that we receive form the end user. This is key to determining how to interpret it
+ * @param {incomingMessage} - This is the incoming message from the end user via the communication channel @see {CommunicationChannel}
+ * 
+ * @returns {Message} - This is standardized format that our chatbot can read and process
+ */ 
+export class IncomingMessageParser
+{ 
+  orgId: string;
+
+  constructor(activeChannel: ActiveChannel, private _msgService$: MessagesDataService)
+  {
+    this.orgId = activeChannel.channel.orgId;
+  }
+
+  protected getMessageId = () => Date.now().toString();
+
+  protected saveMessage(message: Message, endUserId: string) {
+
+    return this._msgService$.saveMessage(message, this.orgId, endUserId);
+  }
+}
