1c9e5addd0855b6f97c3182fa880ebd8a0d96261
Erastus Njoroge <85213281+indomitable-core@users.noreply.github.com>
2023-01-26 14:26:23 +0300

    :art: document the interface

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/fail-message-block.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/fail-message-block.interface.ts
@@ -1,6 +1,11 @@
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
-
+/**
+ * Block that sends error message in the event of a failure
+ */
 export interface FailMessageBlock extends StoryBlock {
+  /**
+   * Messsage emitted about the failure
+   */
   message?: string;
 
   defaultTarget?: string;
