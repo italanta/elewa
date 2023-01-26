d827fe5331b51e73f13dd23f7e39488100014d6b
Erastus Njoroge <85213281+indomitable-core@users.noreply.github.com>
2023-01-26 14:27:12 +0300

    :art: document the interface

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/image-input-block.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/image-input-block.interface.ts
@@ -3,9 +3,10 @@ import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 /**
  * Block which sends a message in the form of text.
  */
-export interface ImageInputBlock extends StoryBlock
-{
-  /** Message to accompany image. */
+export interface ImageInputBlock extends StoryBlock {
+  /**
+   * Message to accompany image.
+   */
   message?: string;
 
   defaultTarget?: string;
