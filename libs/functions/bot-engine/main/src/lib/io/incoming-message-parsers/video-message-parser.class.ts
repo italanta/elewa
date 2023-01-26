7ea8d90e2a9941d97399cbbfd9be688a70737ffe
Reagan Charana <rgncharana2@gmail.com>
2023-01-26 09:43:38 +0300

    :wrench: create abstract classes for parsing in messages

---
+++ libs/functions/bot-engine/main/src/lib/io/incoming-message-parsers/video-message-parser.class.ts
@@ -0,0 +1,18 @@
+import { HandlerTools } from "@iote/cqrs";
+
+import { IncomingMessagePayload, Message } from "@app/model/convs-mgr/conversations/messages";
+
+import { ActiveChannel } from "../../model/active-channel.service";
+import { MessagesDataService } from "../../services/data-services/messages.service";
+import { IncomingFileMessageParser } from "./file-message-parser.class";
+
+
+export abstract class IncomingVideoMessageParser extends IncomingFileMessageParser
+{
+  constructor(activeChannel: ActiveChannel, msgService$: MessagesDataService, tools: HandlerTools)
+  {
+    super(activeChannel, msgService$, tools);
+  }
+
+  protected abstract parseInVideoMessage(incomingMessage: IncomingMessagePayload): Message;
+}
