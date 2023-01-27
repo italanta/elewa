91bfb113e1dec9af68188b59bae1d01ed76a2a9a
Reagan Charana <rgncharana2@gmail.com>
2023-01-27 18:44:34 +0300

    :art: add location input block component

---
+++ libs/features/convs-mgr/stories/blocks/library/location-input-block/src/lib/convs-mgr-location-input-block.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LocationInputBlockComponent } from './location-input-block/location-input-block.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LocationInputBlockComponent],
+})
+export class ConvsMgrLocationInputBlockModule {}
