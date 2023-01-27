06968e8a4a1d08fc3a4c2dfd3c0d49e103a362bd
Reagan Charana <rgncharana2@gmail.com>
2023-01-27 17:38:51 +0300

    :art: add audio input block component

---
+++ libs/features/convs-mgr/stories/blocks/library/audio-input-block/src/lib/audio-input-message-block/audio-input-message-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-input-message-block',
+  templateUrl: './audio-input-message-block.component.html',
+  styleUrls: ['./audio-input-message-block.component.scss'],
+})
+export class AudioInputMessageBlockComponent {}
---
+++ libs/features/convs-mgr/stories/blocks/library/audio-input-block/src/lib/convs-mgr-stories-blocks-library-audio-input-block.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AudioInputMessageBlockComponent } from './audio-input-message-block/audio-input-message-block.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [AudioInputMessageBlockComponent],
+  exports: [AudioInputMessageBlockComponent],
+})
+export class ConvsMgrStoriesBlocksLibraryAudioInputBlockModule {}
