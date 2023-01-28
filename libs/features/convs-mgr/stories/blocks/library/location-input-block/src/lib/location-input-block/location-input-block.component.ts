aff30d9963e7b6b884f0ccb7fac37311d30c7fe5
Reagan Charana <rgncharana2@gmail.com>
2023-01-28 12:23:04 +0300

    :art: :wrench: implement location input block component

--- libs/features/convs-mgr/stories/blocks/library/location-input-block/src/lib/location-input-block/location-input-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/location-input-block/src/lib/location-input-block/location-input-block.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+
+import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+import { LocationInputBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
 @Component({
   selector: 'app-location-input-block',
   templateUrl: './location-input-block.component.html',
   styleUrls: ['./location-input-block.component.scss'],
 })
-export class LocationInputBlockComponent {}
+export class LocationInputBlockComponent {
+  @Input() id: string;
+  @Input() block: LocationInputBlock;
+  @Input() locationInputForm: FormGroup;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
+
+  locationInputId: string;
+
+  type: StoryBlockTypes;
+  locationInputType = StoryBlockTypes.LocationInputBlock;
+  blockFormGroup: FormGroup;
+
+  constructor(private _fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.locationInputId = `location-${this.id}`
+  }
+}
