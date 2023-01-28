df71861e03e3d36e42c8aef595c3888edf2e6979
Reagan Charana <rgncharana2@gmail.com>
2023-01-28 12:23:04 +0300

    :art: :wrench: implement location input block component

--- libs/features/convs-mgr/stories/blocks/library/location-input-block/src/lib/convs-mgr-location-input-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/location-input-block/src/lib/convs-mgr-location-input-block.module.ts
@@ -1,9 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MultiLangModule } from '@ngfi/multi-lang';
+import { MaterialDesignModule, MaterialBricksModule } from '@iote/bricks-angular';
+
+import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+
 import { LocationInputBlockComponent } from './location-input-block/location-input-block.component';
 
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MultiLangModule,
+    MaterialDesignModule,
+    FlexLayoutModule,
+    MaterialBricksModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    HttpClientModule,
+
+    ConvsMgrBlockOptionsModule
+  ],
   declarations: [LocationInputBlockComponent],
+  exports: [LocationInputBlockComponent]
 })
 export class ConvsMgrLocationInputBlockModule {}
