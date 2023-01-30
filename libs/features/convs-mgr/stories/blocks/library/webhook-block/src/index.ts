ffe52f899cd455ca5107d5a570ec4918e15bdd59
Reagan Charana <rgncharana2@gmail.com>
2023-01-30 16:12:10 +0300

    :house: :truck: export interfaces

--- libs/features/convs-mgr/stories/blocks/library/webhook-block/src/index.ts
+++ libs/features/convs-mgr/stories/blocks/library/webhook-block/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/convs-mgr-stories-blocks-library-webhook-block.module';
+export * from './lib/convs-mgr-webhook-block.module';
--- libs/model/convs-mgr/stories/blocks/main/src/index.ts
+++ libs/model/convs-mgr/stories/blocks/main/src/index.ts
@@ -4,3 +4,6 @@ export * from './lib/story-block.interface';
 export * from './lib/story-block-types.enum';
 export * from './lib/story-block-connection.interface';
 export * from './lib/http-method-types.enum';
+export * from './lib/variables.interface';
+export * from './lib/story-block-variables-config.interface';
+export * from './lib/http-methods.interface';
