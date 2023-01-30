5d696891a7f0854a55845d32507cb1f1d83e0db8
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-01-30 08:57:19 -0800

    :WIP: working on tests

---
+++ libs/functions/bot-engine/main/src/lib/services/next-block/tests/webhook-block.test.ts
@@ -0,0 +1,18 @@
+import { WebhookBlockService } from "../block-type/webhook-block.service";
+
+describe("WebhookBlockService", () =>
+{
+  let incomingHttpRequest: WebhookBlockService;
+
+  beforeEach(() =>
+  {
+    incomingHttpRequest = new WebhookBlockService();
+
+  });
+  describe("incomingHttpRequest", () =>
+  {
+    // Create a sample incoming whatsapp sticker message payload
+    const dataCollection 
+    };
+
+});
