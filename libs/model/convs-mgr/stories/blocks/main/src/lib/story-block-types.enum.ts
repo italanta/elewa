df8b5216079459ba52c14bbd0940e36e8c9486b7
Reagan Charana <rgncharana2@gmail.com>
2023-01-30 15:49:06 +0300

    :wrench: add webhook block type

--- libs/model/convs-mgr/stories/blocks/main/src/lib/story-block-types.enum.ts
+++ libs/model/convs-mgr/stories/blocks/main/src/lib/story-block-types.enum.ts
@@ -111,5 +111,8 @@ export enum StoryBlockTypes {
   /** */
   FailBlock = 20,
   LocationInputBlock = 22,
-  AudioInput = 21
+  AudioInput = 21,
+
+  /** Block that calls a specified URL endpoint when hit */
+  WebhookBlock = 25
 }
