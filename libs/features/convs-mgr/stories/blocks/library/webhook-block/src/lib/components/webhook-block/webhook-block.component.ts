054cac31906fb5a40f939a4e89062cad259befda
Reagan Charana <rgncharana2@gmail.com>
2023-01-30 16:10:31 +0300

    :art: create webhook block component

---
+++ libs/features/convs-mgr/stories/blocks/library/webhook-block/src/lib/components/webhook-block/webhook-block.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+import { Subscription, Observable } from 'rxjs';
+
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+
+import { HttpMethods, HttpMethodTypes, StoryBlockTypes, Variable, VariablesConfig } from '@app/model/convs-mgr/stories/blocks/main';
+import { WebhookBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
+@Component({
+  selector: 'app-webhook-block',
+  templateUrl: './webhook-block.component.html',
+  styleUrls: ['./webhook-block.component.scss'],
+})
+export class WebhookBlockComponent {
+  @Input() id: string;
+  @Input() block: WebhookBlock;
+  @Input() webhookMessageForm: FormGroup;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
+
+  webhookInputId: string;
+  httpUrl: VariablesConfig;
+
+
+  httpCategories: HttpMethods[] = [
+    { method: HttpMethodTypes.POST, name: 'POST' },
+    { method: HttpMethodTypes.GET, name: 'GET' },
+    { method: HttpMethodTypes.DELETE, name: 'DELETE' }
+  ];
+  preSelectedMethod: number = HttpMethodTypes.POST;
+  subscription: Subscription;
+
+  type: StoryBlockTypes;
+  webhookType = StoryBlockTypes.WebhookBlock;
+  httpMethod = new FormControl(this.preSelectedMethod);
+  variables = new FormControl();
+  variables$: Observable<Variable[]>
+
+  constructor(private _fb: FormBuilder,
+    private _variablesStore$$: VariablesConfigStore
+  ) { }
+
+  ngOnInit() {
+
+    this.variables$ = this._variablesStore$$.get();
+    this.webhookInputId = `webhook-${this.id}`;
+    this.httpUrl;
+    this.httpCategories;
+    this.subscription = this.httpMethod.valueChanges
+      .subscribe(value => console.log(value));
+
+  }
+}
