90fbf6bff2db5626cb9a8e5911b6e51fd271eb4a
Reagan Charana <rgncharana2@gmail.com>
2023-01-31 14:46:02 +0300

    :wrench: add getnextblock for lists and questions

--- libs/functions/bot-engine/main/src/lib/services/next-block/next-block.factory.ts
+++ libs/functions/bot-engine/main/src/lib/services/next-block/next-block.factory.ts
@@ -1,11 +1,11 @@
 import { HandlerTools } from "@iote/cqrs";
 
-import { DefaultOptionMessageService } from "./block-type/default-block.service";
-
 import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
+
+import { DefaultOptionMessageService } from "./block-type/default-block.service";
 import { BlockDataService } from "../data-services/blocks.service";
 import { ConnectionsDataService } from "../data-services/connections.service";
-
+import { MultipleOptionsMessageService } from "./block-type/multiple-options-block.service";
 
 /**
  * Factory to resolve block type and return the appropriate service that gets the next block
@@ -16,6 +16,15 @@ export class NextBlockFactory
 {
     resoveBlockType(blockType: StoryBlockTypes, tools: HandlerTools, blockDataService: BlockDataService, connDataService: ConnectionsDataService)
     {
-        return new DefaultOptionMessageService(blockDataService, connDataService, tools);
+        switch (blockType) {
+            case StoryBlockTypes.QuestionBlock:
+                return new MultipleOptionsMessageService(blockDataService, connDataService, tools);
+            case StoryBlockTypes.ListBlock:
+                return new MultipleOptionsMessageService(blockDataService, connDataService, tools);
+            case StoryBlockTypes.TextMessage:
+                return new DefaultOptionMessageService(blockDataService, connDataService, tools);
+            default:
+                return new DefaultOptionMessageService(blockDataService, connDataService, tools);
+        }
     }
 }
