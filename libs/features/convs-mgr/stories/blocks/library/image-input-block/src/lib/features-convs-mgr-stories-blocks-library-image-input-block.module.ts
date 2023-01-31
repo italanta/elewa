0f29e2ee7c92a672f304f4952f26a9cce9ad1d42
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-01-31 06:07:45 -0800

    Delete features-convs-mgr-stories-blocks-library-image-input-block.module.ts

--- libs/features/convs-mgr/stories/blocks/library/image-input-block/src/lib/features-convs-mgr-stories-blocks-library-image-input-block.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ImageInputBlockComponent } from './components/image-input-block/image-input-block.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [ImageInputBlockComponent],
-})
-export class FeaturesConvsMgrStoriesBlocksLibraryImageInputBlockModule {}
