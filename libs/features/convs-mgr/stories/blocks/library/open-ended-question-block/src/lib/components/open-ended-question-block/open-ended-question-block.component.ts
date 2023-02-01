78096ee6d4c5904982a5f74fd63572effbccaa12
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-02-01 10:42:15 -0800

    :art: open ended question block

--- libs/features/convs-mgr/stories/blocks/library/open-ended-question-block/src/lib/components/open-ended-question-block/open-ended-question-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/open-ended-question-block/src/lib/components/open-ended-question-block/open-ended-question-block.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+import { Logger } from '@iote/bricks-angular';
+import { StoryBlock, StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+import { OpenEndedQuestionBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
 
 @Component({
   selector: 'app-open-ended-question-block',
   templateUrl: './open-ended-question-block.component.html',
   styleUrls: ['./open-ended-question-block.component.scss'],
 })
-export class OpenEndedQuestionBlockComponent {}
+export class OpenEndedQuestionBlockComponent implements OnInit{
+  @Input() id: string;
+  @Input() block: OpenEndedQuestionBlock;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
+
+  @Input() openEndedQuestionForm: FormGroup
+
+  @Input() blocksGroup: FormArray;
+ 
+  type: StoryBlockTypes;
+  openQuestiontype = StoryBlockTypes.OpenEndedQuestion;
+  blockFormGroup: FormGroup;
+
+  constructor(private _fb: FormBuilder,
+              private _logger: Logger) 
+  { }
+  
+  ngOnInit(): void {}
+}
--- libs/features/convs-mgr/stories/blocks/library/open-ended-question-block/src/lib/features-convs-mgr-stories-blocks-library-open-ended-question-block.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { OpenEndedQuestionBlockComponent } from './components/open-ended-question-block/open-ended-question-block.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [OpenEndedQuestionBlockComponent],
-})
-export class FeaturesConvsMgrStoriesBlocksLibraryOpenEndedQuestionBlockModule {}
