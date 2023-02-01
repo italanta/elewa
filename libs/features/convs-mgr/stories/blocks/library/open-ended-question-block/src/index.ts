298a60da2465f035e68ff3db86be14a34b4230dd
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2023-02-01 09:18:55 -0800

    :construction:  open ended question block

---
+++ libs/features/convs-mgr/stories/blocks/library/open-ended-question-block/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/features-convs-mgr-stories-blocks-library-open-ended-question-block.module';
---
+++ libs/features/convs-mgr/stories/blocks/library/open-ended-question-block/src/lib/components/open-ended-question-block/open-ended-question-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-ended-question-block',
+  templateUrl: './open-ended-question-block.component.html',
+  styleUrls: ['./open-ended-question-block.component.scss'],
+})
+export class OpenEndedQuestionBlockComponent {}
