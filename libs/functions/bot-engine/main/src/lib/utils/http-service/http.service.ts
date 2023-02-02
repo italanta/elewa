5f9997c5078e47ed45f5ea3aa5ab2923f7838abe
Reagan Charana <rgncharana2@gmail.com>
2023-02-02 17:55:35 +0300

    :art: clean up the http service logging

--- libs/functions/bot-engine/main/src/lib/utils/http-service/http.service.ts
+++ libs/functions/bot-engine/main/src/lib/utils/http-service/http.service.ts
@@ -8,45 +8,47 @@ export class HttpService
   {
     const payload = { data: dataPayload || {} };
 
-    tools.Logger.log(() => `[makeHttpRequest] - URL: ${URL}`);
+    tools.Logger.log(() => `[HttpService].post - Attempting to post: ${JSON.stringify(payload)}`);
 
-    tools.Logger.log(() => `[makeHttpRequest] - Data to post: ${JSON.stringify(payload)}`);
-
-      const resp = await axios.post(URL, payload, {
-        headers: {
-          'ContentType': 'application/json'
-        }
-      });
-
-      if (resp.status === 200 || 201 || 202) {
-        tools.Logger.log(() => `[makeHttpRequest] - Post data Success: ${JSON.stringify(resp.status)}`);
-
-        return resp.data
-      } else {
-        tools.Logger.error(() => 
-          `[BotEngine].httpPostRequest - Error while posting data. CODE :: ${JSON.stringify(resp.status)} - ${JSON.stringify(resp.data)}`);
+    const resp = await axios.post(URL, payload, {
+      headers: {
+        'ContentType': 'application/json'
       }
+    });
+
+    if (resp.status === 200 || 201 || 202) {
+      tools.Logger.log(() => `[HttpService].post - Response: ${JSON.stringify(resp.status)}`);
+      tools.Logger.log(() => `[HttpService].post - Post data Success: ${JSON.stringify(resp.data)}`);
+      return resp.data;
+    } else {
+      tools.Logger.log(() => `[HttpService].post - Response: ${JSON.stringify(resp.status)}`);
+      tools.Logger.error(() =>
+        `[BotEngine].httpPostRequest - Error while posting data: ${JSON.stringify(resp.data)}`);
+    }
   }
 
   public async get(URL: string, tools: HandlerTools)
   {
 
-    tools.Logger.log(() => `[BotEngine].httpGetRequest - Attempting to fetch Data`);
+    tools.Logger.log(() => `[HttpService].get - Attempting to fetch Data`);
 
-      const resp = await axios.get(URL, {
-        headers: {
-          'ContentType': 'application/json'
-        }
-      });
-
-       if (resp.status === 200 || 201 || 202) {
-        tools.Logger.log(() => `[makeHttpRequest] - Fetch data Success: ${JSON.stringify(resp.status)}`);
-
-        return resp.data
-      } else {
-        tools.Logger.error(() => 
-          `[BotEngine].httpPostRequest - Error while fetching data. CODE :: ${JSON.stringify(resp.status)} - ${JSON.stringify(resp.data)}`);
+    const resp = await axios.get(URL, {
+      headers: {
+        'ContentType': 'application/json'
       }
+    });
+
+    if (resp.status === 200 || 201 || 202) {
+
+      tools.Logger.log(() => `[HttpService].get - Response: ${JSON.stringify(resp.status)}`);
+      tools.Logger.log(() => `[HttpService].get - Fetch data Success: ${JSON.stringify(resp.status)}`);
+
+      return resp.data;
+    } else {
+      tools.Logger.log(() => `[HttpService].get - Response: ${JSON.stringify(resp.status)}`);
+      tools.Logger.error(() =>
+        `[HttpService].get - Error while fecthing data: ${JSON.stringify(resp.data)}`);
+    }
 
   }
 }
