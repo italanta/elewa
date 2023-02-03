8956ce6670717babcabb4c896ce69df33f82eb82
Reagan Charana <rgncharana2@gmail.com>
2023-02-03 15:09:18 +0300

    :wrench: add side operations to process next block

--- libs/functions/bot-engine/main/src/lib/services/process-next-block/models/process-next-block.interface.ts
+++ libs/functions/bot-engine/main/src/lib/services/process-next-block/models/process-next-block.interface.ts
@@ -2,5 +2,6 @@ import { Cursor } from "@app/model/convs-mgr/conversations/admin/system";
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 
 export interface IProcessNextBlock {
+  sideOperations: Promise<any>[];
   handleBlock(storyBlock: StoryBlock, updatedCursor: Cursor, orgId: string, endUserId: string)
 }
