f8633248637d57c8736e933b6cb214971266abb4
Erastus Njoroge <eramwangiz@gmail.com>
2023-02-07 13:28:43 +0300

    :art: add a connection service to the state

--- libs/features/convs-mgr/stories/editor/src/lib/providers/story-editor-initialiser.service.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/providers/story-editor-initialiser.service.ts
@@ -1,34 +1,36 @@
 import { ElementRef, Injectable, ViewContainerRef } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { newInstance as initJsPlumb } from '@jsplumb/browser-ui';
 
 import { BlockInjectorService } from '@app/features/convs-mgr/stories/blocks/library/main';
 
+import { BlockConnectionsService } from '@app/state/convs-mgr/stories/block-connections';
+
 import { StoryEditorFrame } from '../model/story-editor-frame.model';
-import { FormBuilder } from '@angular/forms';
 
 @Injectable()
-export class StoryEditorInitialiserService 
-{
+export class StoryEditorInitialiserService {
   constructor(private _fb: FormBuilder,
-              private _blocksInjector: BlockInjectorService) 
-  {}
+    private _blocksInjector: BlockInjectorService,
+    private _connectionsService: BlockConnectionsService
+  ) { }
 
-  initialiseEditor(editorContainer: ElementRef<HTMLElement>, 
-                   viewport: ViewContainerRef)
-  {
+  initialiseEditor(editorContainer: ElementRef<HTMLElement>,
+    viewport: ViewContainerRef) {
     // Get inner HTML element from reference
     const container = editorContainer.nativeElement;
-    
+
     // Initialise div as a _jsPlumb instance
-    const _jsplumb = initJsPlumb({ 
+    const _jsplumb = initJsPlumb({
       container,
-      
+
       // paintStyle: { strokeWidth: 1 },
       // anchors: [["Left", "Right", "Bottom"], ["Top", "Bottom"]],
     });
 
-    _jsplumb.addClass(container,"jsplumb_instance")
+    _jsplumb.addClass(container, "jsplumb_instance")
 
-    return new StoryEditorFrame(this._fb, _jsplumb, this._blocksInjector, viewport);
+    return new StoryEditorFrame(this._fb, _jsplumb, this._blocksInjector, viewport, this._connectionsService);
   }
 }
