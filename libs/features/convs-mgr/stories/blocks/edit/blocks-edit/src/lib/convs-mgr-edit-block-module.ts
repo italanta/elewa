a8b8cb2f2ecfbbf62f84ecc1e82d4ddbc45106cb
lemmyMwaura <lemminton@gmail.com>
2023-02-08 03:34:43 -0800

    :bricks: feat: set default edit component - will be replaced with new form edit blocks

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/convs-mgr-edit-block-module.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/convs-mgr-edit-block-module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MessageBlockEditComponent } from './components/message-block-edit/message-block-edit.component';
+import { ImageBlockEditComponent } from './components/image-block-edit/image-block-edit.component';
+import { DefaultComponent } from './components/default/default.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 
-  declarations: [MessageBlockEditComponent],
+  declarations: [
+    MessageBlockEditComponent,
+    ImageBlockEditComponent,
+    DefaultComponent,
+  ],
 })
 export class ConvsMgrEditBlockModule {}
--- libs/features/convs-mgr/stories/editor/src/lib/providers/fetch-active-block-component.function.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/providers/fetch-active-block-component.function.ts
@@ -1,54 +1,54 @@
 import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
-import { ImageBlockEditComponent } from '@app/features/convs-mgr/stories/blocks/edit/blocks-edit';
 import { MessageBlockEditComponent } from '@app/features/convs-mgr/stories/blocks/edit/blocks-edit';
+import { DefaultComponent } from '@app/features/convs-mgr/stories/blocks/edit/blocks-edit';
 
 export function getActiveBlock(type: StoryBlockTypes) {
   switch (type) {
     case StoryBlockTypes.TextMessage:
       return MessageBlockEditComponent;
     case StoryBlockTypes.Image:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Name:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Email:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.PhoneNumber:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.QuestionBlock:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Location:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.List:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Document:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Audio:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Video:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Sticker:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.Reply:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.JumpBlock:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.MultipleInput:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.FailBlock:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.ImageInput:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.LocationInputBlock:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.AudioInput:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.WebhookBlock:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.EndStoryAnchorBlock:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     case StoryBlockTypes.OpenEndedQuestion:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
     default:
-      return ImageBlockEditComponent;
+      return DefaultComponent;
   }
 }
