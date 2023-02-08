60f52e82042bc6809effe5f384a0cccf50a46898
Reagan Charana <rgncharana2@gmail.com>
2023-02-08 13:01:45 +0300

    :wrench: clean up service and fetch url once

--- libs/functions/bot-engine/main/src/lib/services/media/process-media-service.ts
+++ libs/functions/bot-engine/main/src/lib/services/media/process-media-service.ts
@@ -17,6 +17,8 @@ import { ActiveChannel } from '../../model/active-channel.service';
  */
 export class BotMediaProcessService 
 {
+  private mediaFileURL: string = "";
+
   constructor(private _tools: HandlerTools) { }
 
   /**
@@ -24,35 +26,35 @@ export class BotMediaProcessService
    * Takes the media message, uploads the file, saves the information to the database and returns the firebase storage url
    * 
    */
-  async processMediaFile(message: Message, endUserId: string, activeChannel: ActiveChannel)
+  async saveFileInformation(message: Message, endUserId: string, activeChannel: ActiveChannel)
   {
-    const mediaFileMessage = message as FileMessage;
+    const orgId = activeChannel.channel.orgId;
 
-    const mediaFileURL = await this._getFileURL(mediaFileMessage, endUserId, activeChannel);
+    const mediaFileMessage = message as FileMessage;
 
     const mediaFileInfo: FileUpload = {
       id: mediaFileMessage.mediaId,
-      filePath: mediaFileURL as string || null,
+      filePath: this.mediaFileURL,
       mime_type: mediaFileMessage.mime_type
     };
 
-    await this._saveFileInformation(endUserId, activeChannel.channel.orgId, mediaFileInfo);
-
-    return mediaFileURL;
-  }
-
-  private async _getFileURL(message: FileMessage, endUserId: string, activeChannel: ActiveChannel)
-  {
-    const file = await activeChannel.getMediaFile(message.mediaId, message.mime_type);
-
-    if (file) return this._uploadFile(endUserId, message.type, file.filePath, file.fileName);
-  }
-
-  private async _saveFileInformation(endUserId: string, orgId: string, file: FileUpload)
-  {
     const fileRepo$ = this._tools.getRepository<FileUpload>(`orgs/${orgId}/end-users/${endUserId}/files`);
 
-    return fileRepo$.create(file, file.id);
+    await fileRepo$.create(mediaFileInfo, mediaFileInfo.id);
+
+    return this.mediaFileURL;
+  }
+
+  async getFileURL(message: FileMessage, endUserId: string, activeChannel: ActiveChannel)
+  {
+    if(!this.mediaFileURL) {
+      const file = await activeChannel.getMediaFile(message.mediaId, message.mime_type);
+
+      if (file) this.mediaFileURL = await this._uploadFile(endUserId, message.type, file.filePath, file.fileName) as string;
+
+    }
+
+    return this.mediaFileURL;
   }
 
   /** Uploads the file to firebase storage */
