db55596d9c102c42c527019aa0d3e6aace8bffb4
lemmyMwaura <lemminton@gmail.com>
2023-02-08 03:34:43 -0800

    :bricks: feat: set default edit component - will be replaced with new form edit blocks

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/index.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/index.ts
@@ -1,3 +1,4 @@
-export * from './lib/convs-mgr-edit-block-module'
+export * from './lib/convs-mgr-edit-block-module';
+export * from './lib/components/default/default.component';
 export * from './lib/components/image-block-edit/image-block-edit.component';
 export * from './lib/components/message-block-edit/message-block-edit.component';
