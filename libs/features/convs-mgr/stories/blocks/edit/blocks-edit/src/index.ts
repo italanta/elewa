52568dab2610e63783a8ed2052ab40cedd6d85b9
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-09 10:03:00 +0300

    :bug:  fix: image edit block import bug

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/index.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/convs-mgr-edit-block-module';
 export * from './lib/components/default/default.component';
-export * from './lib/components/image-block-edit/image-block-edit.component';
 export * from './lib/components/message-block-edit/message-block-edit.component';
