0ad3302ba3fdfa39d9d97df01e05c2f7e2c43be9
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-09 10:55:01 +0300

    :pencil2::bricks: feat: add name block edit form

---
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/name-block-edit/name-block-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-name-block-edit',
+  templateUrl: './name-block-edit.component.html',
+  styleUrls: ['./name-block-edit.component.scss'],
+})
+export class NameBlockEditComponent {
+  @Input() form: FormGroup;
+}
