87aaf6b9443ba44192c8a1d83b75ea3515246e8b
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-10 15:00:01 +0300

    :bricks: :memo: feat: add webhook edit form

---
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/webhook-edit/webhook-edit.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpMethods, HttpMethodTypes, Variable } from '@app/model/convs-mgr/stories/blocks/main';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-webhook-edit',
+  templateUrl: './webhook-edit.component.html',
+  styleUrls: ['./webhook-edit.component.scss'],
+})
+export class WebhookEditComponent implements OnInit {
+  @Input() form: FormGroup
+  
+  httpCategories: HttpMethods[] = [
+    { method: HttpMethodTypes.POST, name: 'POST' },
+    { method: HttpMethodTypes.GET, name: 'GET' },
+    { method: HttpMethodTypes.DELETE, name: 'DELETE' }
+  ];
+
+  variables = new FormControl();
+  variables$: Observable<Variable[]>;
+
+  ngOnInit() {
+    console.log(this.form)
+  }
+}
