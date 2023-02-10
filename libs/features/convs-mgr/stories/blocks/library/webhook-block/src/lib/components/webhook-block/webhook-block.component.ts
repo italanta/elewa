0098125be42a6452cc257119be9da737525bff50
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-10 14:56:50 +0300

    :twisted_rightwards_arrows: :bricks: feat: move webhook fields from webhook to it's edit block

--- libs/features/convs-mgr/stories/blocks/library/webhook-block/src/lib/components/webhook-block/webhook-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/webhook-block/src/lib/components/webhook-block/webhook-block.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Subscription, Observable } from 'rxjs';
-
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
-
 import { VariablesConfigStore } from '@app/state/convs-mgr/stories/variables-config';
-
-import { HttpMethods, HttpMethodTypes, StoryBlockTypes, Variable, VariablesConfig } from '@app/model/convs-mgr/stories/blocks/main';
+import {
+  StoryBlockTypes,
+  Variable,
+  VariablesConfig,
+} from '@app/model/convs-mgr/stories/blocks/main';
 import { WebhookBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
 @Component({
@@ -15,7 +15,7 @@ import { WebhookBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
   templateUrl: './webhook-block.component.html',
   styleUrls: ['./webhook-block.component.scss'],
 })
-export class WebhookBlockComponent {
+export class WebhookBlockComponent implements OnInit {
   @Input() id: string;
   @Input() block: WebhookBlock;
   @Input() webhookForm: FormGroup;
@@ -24,31 +24,16 @@ export class WebhookBlockComponent {
   webhookInputId: string;
   httpUrl: VariablesConfig;
 
-
-  httpCategories: HttpMethods[] = [
-    { method: HttpMethodTypes.POST, name: 'POST' },
-    { method: HttpMethodTypes.GET, name: 'GET' },
-    { method: HttpMethodTypes.DELETE, name: 'DELETE' }
-  ];
-  preSelectedMethod: number = HttpMethodTypes.POST;
   subscription: Subscription;
-
   type: StoryBlockTypes;
   webhookType = StoryBlockTypes.WebhookBlock;
-  httpMethod = new FormControl(this.preSelectedMethod);
   variables = new FormControl();
-  variables$: Observable<Variable[]>
+  variables$: Observable<Variable[]>;
 
-  constructor(private _fb: FormBuilder,
-    private _variablesStore$$: VariablesConfigStore
-  ) { }
+  constructor(private _variablesStore$$: VariablesConfigStore) {}
 
   ngOnInit() {
-
     this.variables$ = this._variablesStore$$.get();
     this.webhookInputId = `webhook-${this.id}`;
-    this.subscription = this.httpMethod.valueChanges
-      .subscribe(value => console.log(value));
-
   }
 }
