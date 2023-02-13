90f6cf1a1a220e4158fe160f1df9a3d1bfe5a160
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-13 09:30:44 +0300

    :twisted_rightwards_arrows::bricks: feat: export and select question btns edit form

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/index.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/index.ts
@@ -11,3 +11,4 @@ export * from './lib/components/audio-input-block-edit/audio-input-block-edit.co
 export * from './lib/components/location-input-block-edit/location-input-block-edit.component'
 export * from './lib/components/video-input-block-edit/video-input-block-edit.component'
 export * from './lib/components/image-input-block-edit/image-input-block-edit.component'
+export * from './lib/components/question-buttons-edit-forms/question-buttons-edit-forms.component'
