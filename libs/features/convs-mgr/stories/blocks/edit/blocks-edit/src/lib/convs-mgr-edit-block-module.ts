762c53246a317bfb06d06e97515ee0c59be39bfd
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-13 10:50:53 +0300

    :page_facing_up: feat: add multi lang translations

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/convs-mgr-edit-block-module.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/convs-mgr-edit-block-module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
 import { MessageBlockEditComponent } from './components/message-block-edit/message-block-edit.component';
 import { EmailBlockEditComponent } from './components/email-block-edit/email-block-edit.component';
 import { PhoneBlockEditComponent } from './components/phone-block-edit/phone-block-edit.component';
@@ -8,7 +10,7 @@ import { NameBlockEditComponent } from './components/name-block-edit/name-block-
 import { DefaultComponent } from './components/default/default.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MultiLangModule],
 
   declarations: [
     MessageBlockEditComponent,
