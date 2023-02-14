607ea61fd50a732ce771fe96279be67d3fb4625f
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-14 11:21:05 +0300

    :sparkles: feat: add keyword-jump lib and component

---
+++ libs/features/convs-mgr/stories/blocks/library/keyword-jump-block/src/lib/components/keyword-jump-block/keyword-jump-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keyword-jump-block',
+  templateUrl: './keyword-jump-block.component.html',
+  styleUrls: ['./keyword-jump-block.component.scss'],
+})
+export class KeywordJumpBlockComponent {}
---
+++ libs/features/convs-mgr/stories/blocks/library/keyword-jump-block/src/lib/features-convs-mgr-stories-blocks-library-keyword-jump-block.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KeywordJumpBlockComponent } from './components/keyword-jump-block/keyword-jump-block.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [KeywordJumpBlockComponent],
+})
+export class FeaturesConvsMgrStoriesBlocksLibraryKeywordJumpBlockModule {}
