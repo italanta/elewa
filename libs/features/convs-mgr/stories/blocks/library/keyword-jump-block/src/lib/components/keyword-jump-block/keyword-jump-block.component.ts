11dcc3ef13c3211e2fae38cfd34a11a3e578b10d
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-14 14:36:54 +0300

    :sparkles: feat: update keyword jump block

--- libs/features/convs-mgr/stories/blocks/library/keyword-jump-block/src/lib/components/keyword-jump-block/keyword-jump-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/keyword-jump-block/src/lib/components/keyword-jump-block/keyword-jump-block.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+import { KeywordMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+import { ButtonsBlockButton } from '@app/model/convs-mgr/stories/blocks/scenario';
+
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
 @Component({
   selector: 'app-keyword-jump-block',
   templateUrl: './keyword-jump-block.component.html',
   styleUrls: ['./keyword-jump-block.component.scss'],
 })
-export class KeywordJumpBlockComponent {}
+export class KeywordJumpBlockComponent<T> implements AfterViewInit {
+  @Input() id: string;
+  @Input() block: KeywordMessageBlock;
+  @Input() keywordJumpBlock: FormGroup;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
+
+  type: StoryBlockTypes;
+  listType = StoryBlockTypes.List;
+
+  constructor(private _fb: FormBuilder) {}
+
+  ngAfterViewInit(): void {
+    this.block.options?.forEach((listItem) => {
+      this.listItems.push(this.addListOptions(listItem));
+    });
+  }
+
+  get listItems(): FormArray {
+    return this.keywordJumpBlock.controls['options'] as FormArray;
+  }
+
+  addListOptions(listItem?: ButtonsBlockButton<T>) {
+    return this._fb.group({
+      id: [listItem?.id ?? `${this.id}-${this.listItems.length + 1}`],
+      message: [listItem?.message ?? ''],
+      value: [listItem?.value ?? ''],
+    });
+  }
+
+  addNewOption() {
+    this.listItems.push(this.addListOptions());
+  }
+  deleteInput(i: number) {
+    this.listItems.removeAt(i);
+  }
+}
