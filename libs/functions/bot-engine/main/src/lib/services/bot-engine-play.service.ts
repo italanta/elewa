aac08df85e2a29fc7a7d2d4c9605610b561a077f
Reagan Charana <rgncharana2@gmail.com>
2023-02-20 08:34:04 +0300

    :wrench: :horse_racing: update nextblock delay to 1500ms

--- libs/functions/bot-engine/main/src/lib/services/bot-engine-play.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/bot-engine-play.service.ts
@@ -81,7 +81,7 @@ export class BotEnginePlay implements IBotEnginePlay
     // If the last block sent was media, we wait for 500ms 
     //  before replying as media processing can take a while
     if(this.blockSent && isMediaBlock(this.blockSent.type) && !isMediaBlock(nextBlock.type)) {
-      await new Promise(resolve => setTimeout(resolve, 500));
+      await new Promise(resolve => setTimeout(resolve, 1500));
     } 
 
     await this.__reply(nextBlock, endUser, message);
