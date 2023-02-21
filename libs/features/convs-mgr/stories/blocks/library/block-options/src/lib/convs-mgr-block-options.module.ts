68c2027d1b1378eccad23e6594612b312c6e7bf7
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-02-21 17:33:21 +0300

    :recycle: refactor: delete option view component - not needed

--- libs/features/convs-mgr/stories/blocks/library/block-options/src/lib/convs-mgr-block-options.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/block-options/src/lib/convs-mgr-block-options.module.ts
@@ -11,7 +11,7 @@ import { OptionInputFieldComponent } from './components/option-input-field/optio
 import { DefaultOptionFieldComponent } from './components/default-option-field/default-option-field.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListOptionComponent } from './components/list-option/list-option.component';
-import { OptionViewFieldComponent } from './components/option-view-field/option-view-field.component';
+
 
 @NgModule({
   imports: [
@@ -19,7 +19,6 @@ import { OptionViewFieldComponent } from './components/option-view-field/option-
     MaterialDesignModule,
     FlexLayoutModule,
     MaterialBricksModule,
-
     FormsModule,
     ReactiveFormsModule,
   ],
@@ -28,14 +27,12 @@ import { OptionViewFieldComponent } from './components/option-view-field/option-
     OptionInputFieldComponent,
     DefaultOptionFieldComponent,
     ListOptionComponent,
-    OptionViewFieldComponent,
   ],
 
   exports: [
     OptionInputFieldComponent,
     DefaultOptionFieldComponent,
     ListOptionComponent,
-    OptionViewFieldComponent
   ],
 })
 export class ConvsMgrBlockOptionsModule {}
