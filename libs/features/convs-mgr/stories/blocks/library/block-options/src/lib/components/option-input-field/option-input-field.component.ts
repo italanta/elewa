33c62bf25198cf98e0d4e2bea65928a885813f42
Rokyt69 <royokite@gmail.com>
2023-02-23 12:03:48 +0300

    :sparkles: feat: Add option input maxlength validation and error message
    Add maxlength variable to option component.
    Add custom error message to display on invalid input

--- libs/features/convs-mgr/stories/blocks/library/block-options/src/lib/components/option-input-field/option-input-field.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/block-options/src/lib/components/option-input-field/option-input-field.component.ts
@@ -10,8 +10,7 @@ import { _JsPlumbComponentDecorator } from '../../providers/jsplumb-decorator.fu
   templateUrl: './option-input-field.component.html',
   styleUrls: ['./option-input-field.component.scss'],
 })
-export class OptionInputFieldComponent implements OnInit, AfterViewInit 
-{
+export class OptionInputFieldComponent implements OnInit, AfterViewInit {
 
   @Input() blockFormGroup: FormGroup;
   @Input() formGroupNameInput: number | string;
@@ -19,25 +18,24 @@ export class OptionInputFieldComponent implements OnInit, AfterViewInit
   @Input() optionClass: string;
   @Input() isEndpoint: boolean;
   @Input() isReadOnly: boolean;
+  @Input() charMaxlength: number;
 
   inputUniqueId: string;
+  optionValue: string = "";
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void 
-  {
+  ngOnInit(): void {
     if (this.isEndpoint) {
       this.inputUniqueId = `i-${this.formGroupNameInput}-${this.blockFormGroup.value.id}`;
     }
   }
 
-  ngAfterViewInit(): void 
-  {
+  ngAfterViewInit(): void {
     this._decorateInput();
   }
 
-  private _decorateInput() 
-  {
+  private _decorateInput() {
     let input = document.getElementById(this.inputUniqueId) as Element;
     if (this.jsPlumb) {
       input = _JsPlumbComponentDecorator(input, this.jsPlumb);
