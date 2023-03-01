23bccf21cf006188051fd064024a04184929b87a
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-01 09:58:30 +0300

    :sparkles: create variable form component

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { VariableInputComponent } from './components/variable-input/variable-input.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
+  declarations: [VariableInputComponent],
+  exports: [VariableInputComponent],
 })
 export class ConvsMgrProcessInputsModule {}
