555fdc7e171117c74c757182ff722eff58127085
Rokyt69 <royokite@gmail.com>
2023-03-02 20:29:49 +0300

    :fire: fix: Remove default target from list and question block interfaces

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/list-message-block.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/list-message-block.interface.ts
@@ -1,11 +1,9 @@
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 import { ButtonsBlockButton } from "../../../scenario/src";
-export interface ListMessageBlock extends StoryBlock{
+export interface ListMessageBlock extends StoryBlock {
 
   /** Actual question */
   message?: string;
-
-  defaultTarget?: string;
 
   /** Response options */
   options?: ButtonsBlockButton<Button>[];
