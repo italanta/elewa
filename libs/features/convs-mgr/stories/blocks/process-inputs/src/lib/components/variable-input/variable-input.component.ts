eb3ceb90dca1f35074c06513a64c6794bbdd1ff0
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-06 10:58:53 +0300

    :sparkles: feat: process inputs and add to parent form group

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/variable-input/variable-input.component.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/variable-input/variable-input.component.ts
@@ -1,7 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { VariablesConfigService } from '@app/state/convs-mgr/stories/variables-config';
 import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+import { VariableTypes } from '@app/model/convs-mgr/stories/blocks/main';
+
+import { _CreateNameBlockVariableForm } from '../../model/name-variables-form.model';
 
 @Component({
   selector: 'app-variable-input',
@@ -9,23 +13,40 @@ import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
   styleUrls: ['./variable-input.component.scss'],
 })
 export class VariableInputComponent implements OnInit {
-  @Input() blockType: StoryBlockTypes;
-  variableForm: FormGroup;
+  @Input() setValidation: boolean;
+  @Input() BlockFormGroup: FormGroup;
+  
+  blockType: StoryBlockTypes;
+  variablesForm: FormGroup;
+  variablesTypesList = [
+    { name: VariableTypes[1], value: 1 },
+    { name: VariableTypes[2], value: 2 },
+    { name: VariableTypes[3], value: 3 },
+    { name: VariableTypes[4], value: 4 },
+  ];
 
-  constructor(private _fb: FormBuilder) {}
+  nametype = StoryBlockTypes.Name;
+  emailtype = StoryBlockTypes.Email;
+  phonetype = StoryBlockTypes.PhoneNumber;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _variableService: VariablesConfigService
+  ) {}
 
   ngOnInit(): void {
-    const variable = this.getVariableName();
-
-    this.variableForm = this._fb.group({
-      variable,
-      validationMessage:
-        "I'm afraid I didn't understand, could you try again, please?⁠",
-    });
+    this.blockType = this.BlockFormGroup.value.type;
+    
+    const variable = this.getVariableName(this.blockType);
+    this.variablesForm = _CreateNameBlockVariableForm(
+      this._fb,
+      this.BlockFormGroup,
+      variable
+    );
   }
 
-  getVariableName() {
-    switch (this.blockType) {
+  getVariableName(blockType: StoryBlockTypes) {
+    switch (blockType) {
       case StoryBlockTypes.Name:
         return 'name';
       case StoryBlockTypes.Email:
@@ -37,8 +58,25 @@ export class VariableInputComponent implements OnInit {
     }
   }
 
-  createValues() {
-    console.log(this.variableForm);
-    console.log(this.variableForm.value.variable);
+  /** pass properties to block's formGroup */
+  setVariable() {
+    this.BlockFormGroup.value.variable = {
+      name: this.variablesForm.get('name')?.value,
+      type: parseInt(this.variablesForm.get('type')?.value),
+      validators: this.variablesForm.get('validators')?.value ?? {},
+    };
+
+    // update list of avalilable variables
+    const variable = this.variablesForm.get('name')?.value
+    
+    if(variable) {
+      // todo check in store for variable
+
+    }
+  }
+
+  onSubmit() {
+    this.setVariable();
+    console.log(this.BlockFormGroup);
   }
 }
