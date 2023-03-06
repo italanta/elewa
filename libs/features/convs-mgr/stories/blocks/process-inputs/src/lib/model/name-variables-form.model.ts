0b71954fc58558794d49b5bf21e50394f5125512
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-06 10:56:46 +0300

    :sparkles: feat: create NameBlock Variables Form

---
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/model/name-variables-form.model.ts
@@ -0,0 +1,27 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+/**
+ *
+ * @param _fb instance of formbuilder that creates formgroups controls etc
+ * @param blockData the formGroup that belongs to selected block.
+ * @param variable the default variable name to set if block is fresh (no variable has been set).
+ * @returns builds the formgroup with data if available and returns the Formgroup
+ */
+export function _CreateNameBlockVariableForm(
+  _fb: FormBuilder,
+  form: FormGroup<any>,
+  variable: string
+): FormGroup {
+  return _fb.group({
+    name: [form.value.variable.name || variable],
+    type: [form.value.variable.type || 1],
+    validators: _fb.group({
+      regex: [form.value.variable.validators.regex],
+      max: [form.value.variable.validators.max],
+      min: [form.value.variable.validators.min],
+      validationMessage: [
+        form.value.variable.validators.validationMessage ||
+          "I'm afraid I didn't understand, could you try again, please?",
+      ],
+    }),
+  });
+}
