2eb6f06234a256505d4c703b54fe6ec842b34aa2
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-06 11:04:21 +0300

    :sparkles: feat: add name variables validation component

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { VariableInputComponent } from './components/variable-input/variable-input.component';
+import { NameValidationsComponent } from './components/name-validations/name-validations.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [VariableInputComponent],
+  declarations: [VariableInputComponent, NameValidationsComponent],
   exports: [VariableInputComponent],
 })
 export class ConvsMgrProcessInputsModule {}
