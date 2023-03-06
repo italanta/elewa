7ac373043b2c631b186283b6b88008ba05de3c84
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-06 11:04:21 +0300

    :sparkles: feat: add name variables validation component

---
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/name-validations/name-validations.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-name-validations',
+  templateUrl: './name-validations.component.html',
+  styleUrls: ['./name-validations.component.scss'],
+})
+export class NameValidationsComponent {
+  @Input() formGroup: FormGroup;
+  @Input() setValidation: boolean;
+  @Input() formGroupName: string;
+}
