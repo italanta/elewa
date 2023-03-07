2e9c29256527ca9c11c44d535c369eb151525d00
Beulah Mathenge <beulahmathenge@gmail.com>
2023-03-07 23:07:11 +0300

    :sparkles: Starter code for the video block modal, adding the base html and logic

--- libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/modals/video-block-modal/video-block-modal.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/modals/video-block-modal/video-block-modal.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'italanta-apps-video-block-modal',
+  selector: 'app-video-block-modal',
   templateUrl: './video-block-modal.component.html',
   styleUrls: ['./video-block-modal.component.css']
 })
 export class VideoBlockModalComponent {
 
+  @Output() applied = new EventEmitter<any>();
+  title = 'Upload Video';
+  name: string;
+  video: File;
+  size: string;
+
+  // make this an array of objects, having the size in pixels and given dimensions
+  sizeOptions = ['360p', '480p', '720p (Reccomeneded)', '1080p', '4K'];;
+
+  onVideoChange(event: any) {
+    this.video = event.target.files[0];
+  }
+
+  apply() {
+    const videoBlock = {
+      name: this.name,
+      video: this.video,
+      size: this.size
+    };
+    this.applied.emit(videoBlock);
+  }
 }
