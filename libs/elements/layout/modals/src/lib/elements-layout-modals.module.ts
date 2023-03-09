d696dba355eb893c1a638b1e21f971cdd4b2bebd
calvin <ckodalo@gmail.com>
2023-03-09 21:50:17 +0300

    added error-prompt-modal component

--- libs/elements/layout/modals/src/lib/elements-layout-modals.module.ts
+++ libs/elements/layout/modals/src/lib/elements-layout-modals.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ErrorPromptModalComponent } from './modals/error-prompt-modal/error-prompt-modal.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ErrorPromptModalComponent],
 })
 export class ElementsLayoutModalsModule {}
---
+++ libs/elements/layout/modals/src/lib/modals/error-prompt-modal/error-prompt-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-prompt-modal',
+  templateUrl: './error-prompt-modal.component.html',
+  styleUrls: ['./error-prompt-modal.component.scss'],
+})
+export class ErrorPromptModalComponent {}
