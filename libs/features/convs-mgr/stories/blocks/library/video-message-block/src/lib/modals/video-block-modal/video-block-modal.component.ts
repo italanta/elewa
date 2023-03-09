682a69aad863d6722bac29f8ce4d4e3fc35b132b
Beulah Mathenge <beulahmathenge@gmail.com>
2023-03-09 10:29:44 +0300

    :sparkles: Implementing the opening of the video block modal on click of the play button.

--- libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/modals/video-block-modal/video-block-modal.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/modals/video-block-modal/video-block-modal.component.ts
@@ -1,31 +1,90 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+
+import { VideoMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+
+import { UploadFileService } from '@app/state/file';
+
+import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
 
 @Component({
   selector: 'app-video-block-modal',
   templateUrl: './video-block-modal.component.html',
   styleUrls: ['./video-block-modal.component.css']
 })
-export class VideoBlockModalComponent {
+export class VideoBlockModalComponent implements OnInit {
 
+  @Input() id: string;
+  @Input() block: VideoMessageBlock;
+  @Input() videoMessageForm: FormGroup;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
+
+  type: StoryBlockTypes;
+  videoType = StoryBlockTypes.Video;
+
+  blockFormGroup: FormGroup;
+
+  file: File;
+  videoLink: string = "";
+  videoInputId: string;
+  isLoadingVideo: boolean;
+  hasVideo: boolean;
+  videoUrl: string;
+
+  videoInputUpload: string = '';
+   
   @Output() applied = new EventEmitter<any>();
+  name: string
+  size: string
   title = 'Upload Video';
-  name: string;
-  video: File;
-  size: string;
+  sizeOptions = ['4K', '1080p', '720p (Recommended)', '480p', '360p']
 
-  // make this an array of objects, having the size in pixels and given dimensions
-  sizeOptions = ['360p', '480p', '720p (Reccomeneded)', '1080p', '4K'];;
-
-  onVideoChange(event: any) {
-    this.video = event.target.files[0];
-  }
-
-  apply() {
+    apply() {
     const videoBlock = {
       name: this.name,
-      video: this.video,
       size: this.size
+      // VideoUrl should go here?
     };
     this.applied.emit(videoBlock);
+  }
+
+  constructor(private _videoUploadService: UploadFileService,
+              private _ngfiStorage:AngularFireStorage,
+  ) 
+  {
+    this.block = this.block as VideoMessageBlock;
+  }
+
+  ngOnInit(): void {
+    this.videoInputId = `vid-${this.id}`;
+    this.videoInputUpload = `vid-${this.id}-upload`;
+    
+    this.checkIfVideoExists();
+  }
+
+  checkIfVideoExists(){
+    this.videoUrl = this.videoMessageForm.value.fileSrc;
+    this.hasVideo = this.videoUrl && this.videoUrl != '' ? true : false;
+  }
+
+  async processVideo(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.videoLink = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.file = event.target.files[0];
+      this.isLoadingVideo = true;
+    }
+    //Step 1 - Create the file path that will be in firebase storage
+    const vidFilePath = `videos/${this.file.name}_${new Date().getTime()}`;
+    this.isLoadingVideo = true;
+    this.videoMessageForm.get('fileName')?.setValue(this.file.name);
+
+    this.videoUrl =await (await this._ngfiStorage.upload(vidFilePath, this.file)).ref.getDownloadURL();
+    (await this._videoUploadService.uploadFile(this.file, this.block, vidFilePath)).subscribe();
   }
 }
