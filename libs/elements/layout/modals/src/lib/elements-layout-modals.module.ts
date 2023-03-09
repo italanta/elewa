c66f4871b2bec1cf18fe6e701989fbc6d6735f9c
calvin <ckodalo@gmail.com>
2023-03-10 02:37:30 +0300

    FEAT: added reusable error prompt component

--- libs/elements/layout/modals/src/lib/elements-layout-modals.module.ts
+++ libs/elements/layout/modals/src/lib/elements-layout-modals.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 import { ErrorPromptModalComponent } from './modals/error-prompt-modal/error-prompt-modal.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   declarations: [ErrorPromptModalComponent],
 })
 export class ElementsLayoutModalsModule {}
