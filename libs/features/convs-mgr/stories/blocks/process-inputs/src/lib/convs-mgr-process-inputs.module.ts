0d86b448bb2b18194ded5c65f48da1f91746cc64
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-09 20:14:52 +0300

    :memo: feat: add multilang translations

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MultiLangModule } from '@ngfi/multi-lang';
+
 import { VariableInputComponent } from './components/variable-input/variable-input.component';
 import { NameValidationsComponent } from './components/name-validations/name-validations.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MultiLangModule],
   declarations: [VariableInputComponent, NameValidationsComponent],
   exports: [VariableInputComponent],
 })
