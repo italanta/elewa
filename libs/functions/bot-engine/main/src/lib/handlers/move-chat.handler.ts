6898d74b9035d7bfae12c948709fd53906007a78
Reagan Charana <rgncharana2@gmail.com>
2023-03-09 19:17:27 +0300

    :wrench: :horse_racing: reuse bot play service after sending the next block

--- libs/functions/bot-engine/main/src/lib/handlers/move-chat.handler.ts
+++ libs/functions/bot-engine/main/src/lib/handlers/move-chat.handler.ts
@@ -6,7 +6,7 @@ import { JumpStoryBlockService } from '../services/process-next-block/block-type
 import { CommunicationChannel, Cursor, __PlatformTypeToPrefix, __PrefixToPlatformType } from "@app/model/convs-mgr/conversations/admin/system";
 
 import { JumpBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
-import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+import { isOutputBlock, StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 
 import { BlockDataService } from '../services/data-services/blocks.service';
 import { ConnectionsDataService } from '../services/data-services/connections.service';
@@ -14,12 +14,17 @@ import { CursorDataService } from '../services/data-services/cursor.service';
 
 import { ActiveChannelFactory } from '../factories/active-channel/active-channel.factory';
 import { ChannelDataService } from '../services/data-services/channel-info.service';
-import { BlockToStandardMessage } from '../io/block-to-message-parser.class';
 import { MessagesDataService } from '../services/data-services/messages.service';
+import { ProcessMessageService } from '../services/process-message/process-message.service';
+import { BotMediaProcessService } from '../services/media/process-media-service';
+import { BotEngineJump } from '../services/bot-engine-jump.service';
+import { EndUserDataService } from '../services/data-services/end-user.service';
+import { ChatStatus, EndUser } from '@app/model/convs-mgr/conversations/chats';
 
 export class MoveChatHandler extends FunctionHandler<{ storyId: string, orgId: string, endUserId: string, blockId?: string}, RestResult200>
 {
   jumpBlockService$: JumpStoryBlockService;
+  sideOperations: Promise<any>[] = [];
   /**
    * Put a break on execution and halt the system to talk to a Human agent. */
   public async execute(req: { storyId: string, orgId: string, endUserId: string, blockId?: string}, context: FunctionContext, tools: HandlerTools)
@@ -29,58 +34,39 @@ export class MoveChatHandler extends FunctionHandler<{ storyId: string, orgId: s
 
     const splitEndUserId = req.endUserId.split('_');
     const n = parseInt(splitEndUserId[1]);
-
     const phoneNumber = splitEndUserId[2];
 
     const _channelService$ = new ChannelDataService(tools);
 
     const communicationChannel: CommunicationChannel = await _channelService$.getChannelByConnection(n) as CommunicationChannel;
 
+    console.log(`[MoveChatHandler].execute: Communication Channel: ${JSON.stringify(communicationChannel)}`);
+
     const connDataService = new ConnectionsDataService(communicationChannel, tools);
     const blockDataService = new BlockDataService(communicationChannel, connDataService, tools);
     const cursorDataService = new CursorDataService(tools);
     const msgDataService = new MessagesDataService(tools);
+    const processMediaService = new BotMediaProcessService(tools);
+
+
+    const endUser: EndUser = {
+      id: req.endUserId,
+      phoneNumber,
+      status: ChatStatus.Running
+    }
 
     const currentCursor = await cursorDataService.getLatestCursor(req.endUserId, req.orgId);
-
-    this.jumpBlockService$ = new JumpStoryBlockService(blockDataService, connDataService, tools);
 
     const activeChannelFactory = new ActiveChannelFactory();
 
     const activeChannel = activeChannelFactory.getActiveChannel(communicationChannel, tools);
+    
+    const processMessageService = new ProcessMessageService(cursorDataService, connDataService, blockDataService, tools, activeChannel, processMediaService);
 
-    const {storyBlock, newCursor} = await this._moveChat(req.storyId, req.orgId, currentCursor as Cursor, req.endUserId, req.blockId);
+    const bot = new BotEngineJump(processMessageService, cursorDataService, msgDataService, processMediaService, activeChannel, tools);
 
-    const outgoingMessage = activeChannel.parseOutMessage(storyBlock, phoneNumber);
-
-    await activeChannel.send(outgoingMessage as any);
-
-    const blockToMessage = new BlockToStandardMessage().convert(storyBlock);
-
-    await msgDataService.saveMessage(blockToMessage, req.endUserId, req.orgId);
-
-    await cursorDataService.updateCursor(req.endUserId, req.orgId, newCursor);
+    await bot.jump(req.storyId, req.orgId, endUser, currentCursor as Cursor, req.blockId);
 
     return { success: true } as RestResult200;
-  }
-
-  // Pause the chat
-  private async _moveChat(storyId: string, orgId: string, currentCursor: Cursor, endUserId: string, blockId?: string) {
-
-    const jumpBlock: JumpBlock = {
-
-      type: StoryBlockTypes.JumpBlock,
-      position: undefined,
-      deleted: false,
-      blockTitle: '',
-      blockIcon: '',
-      targetStoryId: storyId,
-      targetBlockId: blockId || "",
-    }
-
-   const {storyBlock, newCursor}  =  await this.jumpBlockService$.handleBlock(jumpBlock, currentCursor, orgId, endUserId);
-
-   return {storyBlock, newCursor};
-
   }
 }
---
+++ libs/functions/bot-engine/main/src/lib/services/bot-engine-jump.service.ts
@@ -0,0 +1,74 @@
+import { HandlerTools } from "@iote/cqrs";
+
+import { Cursor } from "@app/model/convs-mgr/conversations/admin/system";
+import { EndUser } from "@app/model/convs-mgr/conversations/chats";
+import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
+import { JumpBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
+
+import { ActiveChannel } from "../model/active-channel.service";
+import { BotEnginePlay } from "./bot-engine-play.service";
+import { BlockDataService } from "./data-services/blocks.service";
+import { ConnectionsDataService } from "./data-services/connections.service";
+import { CursorDataService } from "./data-services/cursor.service";
+import { MessagesDataService } from "./data-services/messages.service";
+import { BotMediaProcessService } from "./media/process-media-service";
+import { ProcessMessageService } from "./process-message/process-message.service";
+import { JumpStoryBlockService } from "./process-next-block/block-type/jump-story-block.service";
+
+export class BotEngineJump extends BotEnginePlay
+{
+  cursorDataService$: CursorDataService;
+
+  constructor(
+    _processMessageService$: ProcessMessageService,
+    _cursorDataService$: CursorDataService,
+    protected _msgService$: MessagesDataService,
+    _processMediaService$: BotMediaProcessService,
+    protected _activeChannel: ActiveChannel,
+    protected _tools: HandlerTools)
+  {
+    super(_processMessageService$, _cursorDataService$, _msgService$, _processMediaService$, _activeChannel, _tools);
+
+    this.orgId = _activeChannel.channel.orgId;
+    this.cursorDataService$ = _cursorDataService$;
+  }
+
+  async jump(storyId: string, orgId: string, endUser: EndUser, currentCursor: Cursor, blockId?: string) 
+  {
+    const {storyBlock, newCursor} = await this.__moveChat(storyId, orgId, currentCursor, endUser.id, blockId);
+
+    await this.__reply(storyBlock, endUser);
+    
+    this.__move(newCursor, endUser.id);
+
+    await this.play(null, endUser, newCursor);
+
+    // Resolve all pending operations.
+    await Promise.all(this.sideOperations);
+  }
+
+  private async __moveChat(storyId: string, orgId: string, currentCursor: Cursor, endUserId: string, blockId?: string) 
+  {
+
+    const connDataService = new ConnectionsDataService(this._activeChannel.channel, this._tools);
+    const blockDataService = new BlockDataService(this._activeChannel.channel, connDataService, this._tools);
+
+    const jumpBlockService$ = new JumpStoryBlockService(blockDataService, connDataService, this._tools);
+
+    const jumpBlock: JumpBlock = {
+
+      type: StoryBlockTypes.JumpBlock,
+      position: undefined,
+      deleted: false,
+      blockTitle: '',
+      blockIcon: '',
+      targetStoryId: storyId,
+      targetBlockId: blockId || "",
+    }
+
+   const {storyBlock, newCursor}  =  await jumpBlockService$.handleBlock(jumpBlock, currentCursor, orgId, endUserId);
+
+   return {storyBlock, newCursor};
+
+  }
+}
