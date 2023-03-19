89fa2879dd880a9dc4b23ae6240f79d0491e7ba0
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-19 16:22:55 +0300

    :sparkles: feat: move the text area component to the reusable text area module

--- libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/convs-mgr-reusable-text-area.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/convs-mgr-reusable-text-area.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { TextMessageComponent } from './components/text-message-block/text-message.component';
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
+  declarations: [TextMessageComponent],
+  exports: [TextMessageComponent]
 })
 export class ConvsMgrReusableTextAreaModule {}
