debae2902ca27679d7f847bf69f6857ceadb9dd3
Kangonga <guchu413@gmail.com>
2023-03-20 10:43:38 +0300

    feat: Import the classic editor and set the config options

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/message-block-edit/message-block-edit.component.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/message-block-edit/message-block-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-message-block-edit',
@@ -8,4 +9,13 @@ import { FormGroup } from '@angular/forms';
 })
 export class MessageBlockEditComponent {
   @Input() form: FormGroup;
+  public Editor:any = ClassicEditor;
+  config: any = {
+    toolbar: {
+      items:[ 'heading', '|', 'Undo', 'Redo', '|', 'Bold', 'Italic', 'Underline', 'numberedList' , 'bulletedList','|', 'link' ],
+      shouldNotGroupWhenFull: false
+    },
+    language: 'en'
+  };
+
 }
