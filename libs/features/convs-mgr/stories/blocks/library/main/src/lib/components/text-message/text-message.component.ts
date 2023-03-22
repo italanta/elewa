8a646bfc2aa80437d9b35874094f7414884f3bec
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-03-22 15:50:06 +0300

    :bug: fix: movetext area component into the reusable module

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/components/text-message/text-message.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-text-message',
-  templateUrl: './text-message.component.html',
-  styleUrls: ['./text-message.component.scss'],
-})
-export class TextMessageComponent {}
