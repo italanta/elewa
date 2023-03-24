03ad0a253aae322ff09062a3b534578f9fc322b4
kiprotich <kiprotichmichael570@gmail.com>
2023-03-24 17:32:04 +0300

    clean code up code

--- libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/components/video-block/video-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/components/video-block/video-block.component.ts
@@ -59,26 +59,13 @@ export class VideoBlockComponent implements OnInit {
   }
 
   async processVideo(event: any) {
+    this.isLoading = true; // set isLoading to true to show the spinner
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
       reader.onload = (e: any) => this.videoLink = e.target.result;
       reader.readAsDataURL(event.target.files[0]);
       this.file = event.target.files[0];
       this.isLoadingVideo = true;
-
-      const xhr = new XMLHttpRequest();
-      xhr.upload.addEventListener("progress", (event: ProgressEvent) => {
-        this.isLoading = true; // set isLoading to true while uploading
-      });
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          this.videoUrl = URL.createObjectURL(this.file);
-          this.hasVideo = true;
-          this.isLoading = false; // set isLoading to false when upload is complete
-        }
-      };
-      xhr.open("POST", "url-to-upload-video");
-      xhr.send(this.file);
     }
     //Step 1 - Create the file path that will be in firebase storage
     const vidFilePath = `videos/${this.file.name}_${new Date().getTime()}`;
@@ -87,6 +74,7 @@ export class VideoBlockComponent implements OnInit {
 
     this.videoUrl =await (await this._ngfiStorage.upload(vidFilePath, this.file)).ref.getDownloadURL();
     (await this._videoUploadService.uploadFile(this.file, this.block, vidFilePath)).subscribe();
+    this.isLoading = false; // set isLoading to false to hide the spinner
   }
   
 }
