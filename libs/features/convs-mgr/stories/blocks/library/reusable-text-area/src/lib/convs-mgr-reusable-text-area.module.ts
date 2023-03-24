c19f7440be0e9812e3e4011b471bb397ce9d3aa7
martin mithi <gathitumithi9000@gmail.com>
2023-03-24 12:52:17 +0300

    Add CKEditor module and define it in imports

--- libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/convs-mgr-reusable-text-area.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/convs-mgr-reusable-text-area.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 import { TextMessageComponent } from './components/text-message/text-message.component';
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, CKEditorModule],
   declarations: [TextMessageComponent],
   exports: [TextMessageComponent],
 })
