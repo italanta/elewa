2e6cd6b41b22acad47e703ffb0420daf40cdb2b1
Jente Rosseel <rosseeljente@gmail.com>
2023-03-30 14:56:58 +0300

    :wrench: Fix module exports

--- libs/features/analytics/class-based/progress/src/index.ts
+++ libs/features/analytics/class-based/progress/src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/class-based-progress-chart/class-based-progress-chart.component';
+export * from './lib/components/class-based-progress-chart/class-based-progress-chart.component';
 
 export * from './lib/class-based-progress.module';
