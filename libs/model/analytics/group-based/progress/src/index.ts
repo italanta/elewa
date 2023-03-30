794454c75ef0e3ed24aae0d7e08a467d74b9ea3c
Jente Rosseel <rosseeljente@gmail.com>
2023-03-30 16:05:27 +0300

    :wrench: Fix and simplify model ontology

--- libs/model/analytics/group-based/progress/src/index.ts
+++ libs/model/analytics/group-based/progress/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/participant-progress.model'
-export * from './lib/group-based-progress.model';
+export * from './lib/group-progress.model';
 
 export * from './lib/measure-progress.command';
 export * from './lib/measure-group-progress.command';
