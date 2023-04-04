a4b58982ea812666791de5bf0c680edc630b7eaa
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-04-04 14:39:46 +0300

    :recycle: refactor: create nested formgroup for variable processing and  validation

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/name-block-form.model.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/name-block-form.model.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 import { NameMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
@@ -18,19 +18,24 @@ export function _CreateNameMessageBlockForm(
     defaultTarget: [blockData.defaultTarget ?? ''],
     message: [blockData?.message! ?? ''],
     type: [blockData.type ?? StoryBlockTypes.Name],
-    variable: [
-      blockData.variable ?? {
-        name: 'name',
-        type: 1,
-        validate: 'false',
-        validators: {
-          regex: '',
-          min: '',
-          max: '',
-          validationMessage: '',
-        },
-      },
-    ],
     position: [blockData.position ?? { x: 200, y: 50 }],
+
+    // variables FormGroup
+    variable: _fb.group({
+      name: [blockData.variable?.name ?? '', [Validators.required]],
+      type: [blockData.variable?.type ?? 1, [Validators.required]],
+      validate: [blockData.variable?.validate ?? false, [Validators.required]],
+
+      // validators FormGroup
+      validators: _fb.group({
+        regex: [blockData.variable?.validators?.regex ?? ''],
+        min: [blockData.variable?.validators?.min ?? ''],
+        max: [blockData.variable?.validators?.max ?? ''],
+        validationMessage: [
+          blockData.variable?.validators?.validationMessage ??
+            "I'm afraid I didn't understand, could you try again, please?",
+        ],
+      }),
+    }),
   });
 }
