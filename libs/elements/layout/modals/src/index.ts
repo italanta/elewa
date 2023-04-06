d4aa4dec69acbc6dbfa3deafa78e51c36b4a1646
calvin <ckodalo@gmail.com>
2023-04-06 15:41:13 +0300

    re-added modals lib

--- libs/elements/layout/modals/src/index.ts
+++ libs/elements/layout/modals/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/elements-layout-modals';
+export * from './lib/elements-layout-modals.module';
---
+++ libs/elements/layout/modals/src/lib/elements-layout-modals.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorModalComponentComponent } from './error-modal-component/error-modal-component.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ErrorModalComponentComponent],
+})
+export class ElementsLayoutModalsModule {}
