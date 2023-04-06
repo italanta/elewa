a7e6e35215a87019ed96f224d97c641d54e6379f
Snow <onyangogeorge954@gmail.com>
2023-04-06 11:04:21 +0300

    add phone validations component

---
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/phone-validations/phone-validations.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-validations',
+  templateUrl: './phone-validations.component.html',
+  styleUrls: ['./phone-validations.component.scss'],
+})
+export class PhoneValidationsComponent {}
