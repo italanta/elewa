8ff3ae1d8df85f87c5bab6c11ba0412b3f31f11a
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-06 12:58:07 +0300

    ✨ Generating a video upload modal

---
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/modals/video-upload-modal/video-upload-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-upload-modal',
+  templateUrl: './video-upload-modal.component.html',
+  styleUrls: ['./video-upload-modal.component.scss']
+})
+export class VideoUploadModalComponent {
+
+}
