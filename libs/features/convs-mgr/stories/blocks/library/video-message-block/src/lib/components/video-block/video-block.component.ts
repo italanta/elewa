ec761051bca129d293e31ed09f826f1a791a8cda
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-06 12:58:39 +0300

    🎨 Opening the modal

--- libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/components/video-block/video-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/components/video-block/video-block.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 
+import { take } from 'rxjs';
+
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
 import { VideoMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 
-import { UploadFileService } from '@app/state/file';
+import { FileStorageService, UploadFileService } from '@app/state/file';
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+import { MatDialog } from '@angular/material/dialog';
+import { VideoUploadModalComponent } from '../../modals/video-upload-modal/video-upload-modal.component';
 
 @Component({
   selector: 'app-video-block',
@@ -26,7 +30,6 @@ export class VideoBlockComponent implements OnInit {
 
   type: StoryBlockTypes;
   videoType = StoryBlockTypes.Video;
-
   blockFormGroup: FormGroup;
 
   file: File;
@@ -35,11 +38,10 @@ export class VideoBlockComponent implements OnInit {
   isLoadingVideo: boolean;
   hasVideo: boolean;
   videoUrl: string;
-
   videoInputUpload: string = '';
 
-  constructor(private _videoUploadService: UploadFileService,
-              private _ngfiStorage:AngularFireStorage
+  constructor(private _videoUploadService: FileStorageService,
+              private _dialog: MatDialog
   ) 
   {
     this.block = this.block as VideoMessageBlock;
@@ -57,6 +59,14 @@ export class VideoBlockComponent implements OnInit {
     this.hasVideo = this.videoUrl && this.videoUrl != '' ? true : false;
   }
 
+  getFileNameFromFbUrl(fbUrl: string): string {
+    return fbUrl.split('%2F')[1].split("?")[0];
+  }
+
+  openVideoModal(){
+    this._dialog.open(VideoUploadModalComponent, {minWidth: '600px', minHeight: 'fit-content'})
+  }
+
   async processVideo(event: any) {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
@@ -68,9 +78,14 @@ export class VideoBlockComponent implements OnInit {
     //Step 1 - Create the file path that will be in firebase storage
     const vidFilePath = `videos/${this.file.name}_${new Date().getTime()}`;
     this.isLoadingVideo = true;
-    this.videoMessageForm.get('fileName')?.setValue(this.file.name);
 
-    this.videoUrl =await (await this._ngfiStorage.upload(vidFilePath, this.file)).ref.getDownloadURL();
-    (await this._videoUploadService.uploadFile(this.file, this.block, vidFilePath)).subscribe();
+      this._videoUploadService.uploadSingleFile(this.file, vidFilePath).then((url) => {
+      url.pipe(take(1)).subscribe((url) => this._autofillUrl(url));
+    })
+  }
+
+    private _autofillUrl(url: string) {
+    this.videoMessageForm.patchValue({fileSrc: url});
+    this.isLoadingVideo = false;
   }
 }
