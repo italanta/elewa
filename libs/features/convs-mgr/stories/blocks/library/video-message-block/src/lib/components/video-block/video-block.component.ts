6a417904636c6175f6d552358c3ab2cb7f5c0d28
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-07 15:29:18 +0300

    :cookie: Adding the code to open a modal and exchange data using mat_dialog_data
    Adding the code to open a modal and exchange data using mat_dialog_data

--- libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/components/video-block/video-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/components/video-block/video-block.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
-
-import { take } from 'rxjs';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
 import { VideoMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 
-import { FileStorageService, UploadFileService } from '@app/state/file';
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
-import { MatDialog } from '@angular/material/dialog';
+
+
+import { MatDialog} from '@angular/material/dialog';
+//import { UploadFileService } from '@app/state/file';
 import { VideoUploadModalComponent } from '../../modals/video-upload-modal/video-upload-modal.component';
+
 
 @Component({
   selector: 'app-video-block',
@@ -21,7 +21,7 @@ import { VideoUploadModalComponent } from '../../modals/video-upload-modal/video
   styleUrls: ['./video-block.component.scss'],
 })
 
-export class VideoBlockComponent implements OnInit {
+export class VideoBlockComponent implements OnInit{
 
   @Input() id: string;
   @Input() block: VideoMessageBlock;
@@ -30,62 +30,29 @@ export class VideoBlockComponent implements OnInit {
 
   type: StoryBlockTypes;
   videoType = StoryBlockTypes.Video;
-  blockFormGroup: FormGroup;
+  videoInputUpload: string
+  videoLink: string
 
   file: File;
-  videoLink: string = "";
-  videoInputId: string;
-  isLoadingVideo: boolean;
-  hasVideo: boolean;
-  videoUrl: string;
-  videoInputUpload: string = '';
 
-  constructor(private _videoUploadService: FileStorageService,
-              private _dialog: MatDialog
-  ) 
-  {
+  constructor(
+              private matdialog:MatDialog,
+              //private dialogueRef: MatDialogRef
+  ) {
     this.block = this.block as VideoMessageBlock;
   }
-
+  
   ngOnInit(): void {
-    this.videoInputId = `vid-${this.id}`;
-    this.videoInputUpload = `vid-${this.id}-upload`;
-    
-    this.checkIfVideoExists();
-  }
+    this.videoLink = this.videoMessageForm.value.fileSrc
+   }
 
-  checkIfVideoExists(){
-    this.videoUrl = this.videoMessageForm.value.fileSrc;
-    this.hasVideo = this.videoUrl && this.videoUrl != '' ? true : false;
-  }
-
-  getFileNameFromFbUrl(fbUrl: string): string {
-    return fbUrl.split('%2F')[1].split("?")[0];
-  }
-
+  
   openVideoModal(){
-    this._dialog.open(VideoUploadModalComponent, {minWidth: '600px', minHeight: 'fit-content'})
-  }
-
-  async processVideo(event: any) {
-    if (event.target.files && event.target.files[0]) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => this.videoLink = e.target.result;
-      reader.readAsDataURL(event.target.files[0]);
-      this.file = event.target.files[0];
-      this.isLoadingVideo = true;
-    }
-    //Step 1 - Create the file path that will be in firebase storage
-    const vidFilePath = `videos/${this.file.name}_${new Date().getTime()}`;
-    this.isLoadingVideo = true;
-
-      this._videoUploadService.uploadSingleFile(this.file, vidFilePath).then((url) => {
-      url.pipe(take(1)).subscribe((url) => this._autofillUrl(url));
+   this.matdialog.open(VideoUploadModalComponent, {minWidth: '1000px',
+    data: {
+      block: this.block,
+      videoMessageForm: this.videoMessageForm,
+      }
     })
-  }
-
-    private _autofillUrl(url: string) {
-    this.videoMessageForm.patchValue({fileSrc: url});
-    this.isLoadingVideo = false;
   }
 }
