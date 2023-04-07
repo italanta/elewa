0c30004a97d86feb6dfaf6a3880ecb62558f8928
calvin <ckodalo@gmail.com>
2023-04-07 23:18:31 +0300

    EDITS: refactor and error handling

--- libs/features/convs-mgr/stories/blocks/library/document-message-block/src/lib/components/document-block/document-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/document-message-block/src/lib/components/document-block/document-block.component.ts
@@ -6,9 +6,10 @@ import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 import { DocumentMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 
-import { UploadFileService } from '@app/state/file';
+import { FileStorageService, UploadFileService } from '@app/state/file';
 
 import { _JsPlumbComponentDecorator } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-document-block',
@@ -32,7 +33,9 @@ export class DocumentBlockComponent implements OnInit {
   type: StoryBlockTypes;
   documentType = StoryBlockTypes.Document;
 
-  constructor(private _docUploadService: UploadFileService)
+  constructor(private _docUploadService: UploadFileService,
+    private _fileStorageService: FileStorageService
+    )
   { }
 
   ngOnInit(): void 
@@ -44,6 +47,16 @@ export class DocumentBlockComponent implements OnInit {
 
   async processDocs(event: any)
   {   
+
+    const allowedFileTypes = ['application/pdf'];
+
+    if (!allowedFileTypes.includes(event.target.files[0].type)) {
+      //error modal displayed here
+      this._fileStorageService.openErrorModal("Invalid File Type", "Please select a .pdf only.");
+      event.target.value = '' //clear input
+       return;
+    }
+
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
       reader.onload = (e: any) => this.docLink = e.target.result;
@@ -56,6 +69,14 @@ export class DocumentBlockComponent implements OnInit {
     this.isDocLoading= true;
       //Step 1 - Create the file path that will be in firebase storage
      const docFilePath = `docs/${this.file.name}_${new Date().getTime()}`;
-    (await this._docUploadService.uploadFile(this.file, this.block, docFilePath)).subscribe();
+     (await this._docUploadService.uploadFile(this.file, this.block, docFilePath)).pipe(
+      catchError(error => {
+        console.error('Error uploading file:', error);
+        // show error message to user
+        return of(null);
+      })
+    ).subscribe(() => {
+      this.isDocLoading = false;
+    });
   }
 }
