fd5821e3d7f75dda8544710b96effad1cc02e020
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-12 11:09:05 +0300

    :sparkles:  Generating a location validations component

---
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/location-validations/location-validations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-validations',
+  templateUrl: './location-validations.component.html',
+  styleUrls: ['./location-validations.component.scss']
+})
+export class LocationValidationsComponent {
+
+}
