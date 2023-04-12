41ed08685b70fe5412f8fd136441e921e2855df3
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-12 11:09:05 +0300

    :sparkles:  Generating a location validations component

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -6,10 +6,11 @@ import { MultiLangModule } from '@ngfi/multi-lang';
 
 import { VariableInputComponent } from './components/variable-input/variable-input.component';
 import { NameValidationsComponent } from './components/name-validations/name-validations.component';
+import { LocationValidationsComponent } from './components/location-validations/location-validations.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, MultiLangModule],
-  declarations: [VariableInputComponent, NameValidationsComponent],
+  declarations: [VariableInputComponent, NameValidationsComponent, LocationValidationsComponent],
   exports: [VariableInputComponent],
 })
 export class ConvsMgrProcessInputsModule {}
