571d5435abda2dc010a1363c8a637d160b1664f1
martin mithi <gathitumithi9000@gmail.com>
2023-04-14 02:44:17 +0300

    refractor: Import and implement Coordinate interface to the coordinate subject

--- libs/features/convs-mgr/stories/editor/src/lib/providers/drag-drop.service.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/providers/drag-drop.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Coordinate } from '../model/coordinates.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DragDropService {
-  private _coordinate = new Subject<object>();
+  private _coordinate = new Subject<Coordinate>();
   coord$ = this._coordinate.asObservable();
 
-  set coordinates(coord: object) {
+  set coordinates(coord: Coordinate) {
     this._coordinate.next(coord);
   }
 }
