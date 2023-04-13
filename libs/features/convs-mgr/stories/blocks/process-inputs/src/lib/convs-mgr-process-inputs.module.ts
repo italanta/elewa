6deae2bd86186092a5e287cd5c1e06b43d441b1d
Snow <onyangogeorge954@gmail.com>
2023-04-13 10:11:06 +0300

    import phone module

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -6,10 +6,15 @@ import { MultiLangModule } from '@ngfi/multi-lang';
 
 import { VariableInputComponent } from './components/variable-input/variable-input.component';
 import { NameValidationsComponent } from './components/name-validations/name-validations.component';
+import { PhoneValidationsComponent } from './components/phone-validations/phone-validations.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, MultiLangModule],
-  declarations: [VariableInputComponent, NameValidationsComponent],
+  declarations: [
+    VariableInputComponent,
+    NameValidationsComponent,
+    PhoneValidationsComponent,
+  ],
   exports: [VariableInputComponent],
 })
 export class ConvsMgrProcessInputsModule {}
