3b1a3fe81cad36d8e168fa8875df6a4d2e8f419a
Snow <onyangogeorge954@gmail.com>
2023-04-13 10:09:21 +0300

    add validation design

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/phone-validations/phone-validations.component.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/phone-validations/phone-validations.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-phone-validations',
   templateUrl: './phone-validations.component.html',
   styleUrls: ['./phone-validations.component.scss'],
 })
-export class PhoneValidationsComponent {}
+export class PhoneValidationsComponent {
+  @Input() validate: boolean;
+  @Input() variablesForm: FormGroup;
+}
