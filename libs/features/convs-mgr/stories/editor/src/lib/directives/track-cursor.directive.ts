bffd2850723dafbba97e34f1cabb950946e63f03
martin mithi <gathitumithi9000@gmail.com>
2023-04-13 20:21:09 +0300

    Initializa directive to track cursor position on editor frame when dragging

---
+++ libs/features/convs-mgr/stories/editor/src/lib/directives/track-cursor.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, HostListener } from '@angular/core';
+import { DragDropService } from '../providers/drag-drop.service';
+
+@Directive({
+  selector: '[convlTrackCursor]',
+})
+export class TrackCursorDirective {
+  constructor(private dragService: DragDropService) {}
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: MouseEvent) {
+    this.dragService.coordinates = {
+      ...this.dragService.coordinates,
+      x: event.offsetX,
+      y: event.offsetY,
+    };
+  }
+}
