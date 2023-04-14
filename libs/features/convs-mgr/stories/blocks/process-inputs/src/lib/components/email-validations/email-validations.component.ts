21c6cddfc0e25374e34a419b7af43f69bce2c241
Snow <onyangogeorge954@gmail.com>
2023-04-14 09:22:05 +0300

    create email validations view

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/email-validations/email-validations.component.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/email-validations/email-validations.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-email-validations',
   templateUrl: './email-validations.component.html',
   styleUrls: ['./email-validations.component.scss'],
 })
-export class EmailValidationsComponent {}
+export class EmailValidationsComponent {
+  @Input() validate: boolean;
+  @Input() variablesForm: FormGroup;
+}
