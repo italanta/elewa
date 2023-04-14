074588d9fe9f606df6b67117d1b3b2d4aebe04c6
Snow <onyangogeorge954@gmail.com>
2023-04-14 09:09:51 +0300

    add email model variable and validator

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/email-block-form.model.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/email-block-form.model.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
 import { EmailMessageBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
@@ -15,6 +15,20 @@ export function _CreateEmailMessageBlockForm(_fb: FormBuilder, blockData: EmailM
     defaultTarget: [blockData.defaultTarget ?? ''],
     message: [blockData?.message! ?? ''],
     type: [blockData.type ?? StoryBlockTypes.Email],
-    position: [blockData.position ?? { x: 200, y: 50 }]
+    position: [blockData.position ?? { x: 200, y: 50 }],
+
+    variable: _fb.group({
+      name: [blockData.variable?.name ?? '', [Validators.required]],
+      type: [blockData.variable?.type ?? 1, [Validators.required]],
+      validate: [blockData.variable?.validate ?? false, [Validators.required]],
+
+      validators: _fb.group({
+        regex: [blockData.variable?.validators?.regex ?? ''],
+        validationMessage: [
+          blockData.variable?.validators?.validationMessage ??
+            "Invalid email, could you try again, please?",
+        ],
+      })
+    })
   })
 }
