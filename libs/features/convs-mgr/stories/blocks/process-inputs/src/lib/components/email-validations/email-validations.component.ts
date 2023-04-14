f3d99a301dabbefab6c65b1c717a7c37de57c122
Snow <onyangogeorge954@gmail.com>
2023-04-14 09:19:13 +0300

    add email component

---
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/email-validations/email-validations.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-validations',
+  templateUrl: './email-validations.component.html',
+  styleUrls: ['./email-validations.component.scss'],
+})
+export class EmailValidationsComponent {}
--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -6,10 +6,15 @@ import { MultiLangModule } from '@ngfi/multi-lang';
 
 import { VariableInputComponent } from './components/variable-input/variable-input.component';
 import { NameValidationsComponent } from './components/name-validations/name-validations.component';
+import { EmailValidationsComponent } from './components/email-validations/email-validations.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, MultiLangModule],
-  declarations: [VariableInputComponent, NameValidationsComponent],
+  declarations: [
+    VariableInputComponent,
+    NameValidationsComponent,
+    EmailValidationsComponent,
+  ],
   exports: [VariableInputComponent],
 })
 export class ConvsMgrProcessInputsModule {}
