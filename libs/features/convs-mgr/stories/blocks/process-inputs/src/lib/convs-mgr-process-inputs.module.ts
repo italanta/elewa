da973b0b29b1cdd8a7da82ca265ebd758b630353
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-04-17 16:38:05 +0300

    :bug: fix - remove validation component not-needed

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -8,7 +8,6 @@ import { VariableInputComponent } from './components/variable-input/variable-inp
 import { NameValidationsComponent } from './components/name-validations/name-validations.component';
 import { PhoneValidationsComponent } from './components/phone-validations/phone-validations.component';
 import { LocationValidationsComponent } from './components/location-validations/location-validations.component';
-import { AudioValidationsComponent } from './components/audio-validations/audio-validations.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, MultiLangModule],
@@ -17,7 +16,6 @@ import { AudioValidationsComponent } from './components/audio-validations/audio-
     NameValidationsComponent,
     PhoneValidationsComponent,
     LocationValidationsComponent,
-    AudioValidationsComponent
   ],
 
   exports: [VariableInputComponent],
