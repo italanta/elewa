71f221813acd427e1ef2a9c917a8d03a92b56a31
Jane Njoroge <jwnjoroge4@gmail.com>
2023-04-17 11:55:11 +0300

    Revert "🎨 🖌️ As a user I should be able to use a text editor to format my text #331"

--- libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/convs-mgr-reusable-text-area.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/convs-mgr-reusable-text-area.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 import { TextMessageComponent } from './components/text-message/text-message.component';
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, CKEditorModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [TextMessageComponent],
   exports: [TextMessageComponent],
 })
