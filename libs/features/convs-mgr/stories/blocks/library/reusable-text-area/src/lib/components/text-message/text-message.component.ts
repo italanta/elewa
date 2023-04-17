4be414d3ac9a4aeff16a04a2afefcb3fe4afb2b5
Jane Njoroge <jwnjoroge4@gmail.com>
2023-04-17 11:55:11 +0300

    Revert "🎨 🖌️ As a user I should be able to use a text editor to format my text #331"

--- libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/components/text-message/text-message.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/reusable-text-area/src/lib/components/text-message/text-message.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-text-message',
@@ -9,22 +8,4 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class TextMessageComponent {
   @Input() formgroup: FormGroup;
-  public editor: any = ClassicEditor;
-  config = {
-    toolbar: [
-      'heading',
-      '|',
-      'Bold',
-      'Italic',
-      'Link',
-      'bulletedlist',
-      'numberedList',
-      'undo',
-      'redo',
-    ],
-    shouldNotGroupWhenFull:false
-  };
-  onReady(val: any) {
-    val.focus();
-  }
 }
