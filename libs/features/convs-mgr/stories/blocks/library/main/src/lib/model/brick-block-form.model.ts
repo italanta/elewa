f2f187a5936b2d986912118f8adf67df1e84b52f
martin mithi <gathitumithi9000@gmail.com>
2023-04-19 07:52:02 +0300

    Create and initialize brick form model

---
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/brick-block-form.model.ts
@@ -0,0 +1,23 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
+import { BrickBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
+/**
+ *
+ * @param _fb instance of formbuilder that creates formgroups controls etc
+ * @param blockData the data being patched into the FormGroup
+ * @returns builds the formgroup with data if available and returns the Formgroup
+ */
+export function _CreateBrickBlockForm(
+  _fb: FormBuilder,
+  blockData: BrickBlock
+): FormGroup {
+  return _fb.group({
+    id: [blockData?.id! ?? ''],
+    defaultTarget: [blockData.defaultTarget ?? ''],
+    message: [blockData?.message! ?? ''],
+    type: [blockData.type ?? StoryBlockTypes.BrickBlock],
+    position: [blockData.position ?? { x: 200, y: 50 }],
+  });
+}
