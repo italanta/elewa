9fab391c69d44d147c7527cf68ad9b3e462ed341
Reagan Charana <rgncharana2@gmail.com>
2023-04-25 12:06:54 +0300

    :package: create messenger library

---
+++ libs/functions/bot-engine/messenger/src/lib/functions-bot-engine-messenger.spec.ts
@@ -0,0 +1,9 @@
+import { functionsBotEngineMessenger } from './functions-bot-engine-messenger';
+
+describe('functionsBotEngineMessenger', () => {
+  it('should work', () => {
+    expect(functionsBotEngineMessenger()).toEqual(
+      'functions-bot-engine-messenger'
+    );
+  });
+});
