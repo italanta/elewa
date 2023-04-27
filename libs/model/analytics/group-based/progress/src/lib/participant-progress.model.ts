656e2fc07cda4cc938ca291a577f2ab57651361f
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-04-27 12:45:57 +0300

    :bulb: feat: update JSDoc comments

--- libs/model/analytics/group-based/progress/src/lib/participant-progress.model.ts
+++ libs/model/analytics/group-based/progress/src/lib/participant-progress.model.ts
@@ -1,17 +1,21 @@
 
 /**
- * Model for analysing and visualing participant progress. 
+ * Model representing an individual participant and their current progress (storyID reached at set calculation time).
  * 
  * Used in the participant progress barchart.
  */
 
 export interface ParticipantProgressMilestone
-{
+{ 
+  /** Individual participant/user */
   participant: { id:string, name:string, phone:string };
-  /** Unix time interval at which is measured */
+
+  /** group/class the user is part of */
   group: string; 
+
   /** Milestone the user has reached */
   milestone: string; 
+
   /** Story ID of the milestone the user has reached */
   storyId: string;
 }
