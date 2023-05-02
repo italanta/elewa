d091471e9ca8ea5215f5b93059c90df7dd054cae
Beulah Mathenge <beulahmathenge@gmail.com>
2023-05-02 17:16:43 +0300

    :recycle: Refactor code after new video load spinner on merge

--- libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/modals/video-upload-modal/video-upload-modal.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/modals/video-upload-modal/video-upload-modal.component.ts
@@ -1,86 +1,74 @@
-import { Component, Inject, OnInit} from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { FileStorageService } from '@app/state/file';
+import { SubSink } from 'subsink';
 
 import { Story } from '@app/model/convs-mgr/stories/main';
 
 @Component({
   selector: 'app-video-upload-modal',
   templateUrl: './video-upload-modal.component.html',
-  styleUrls: ['./video-upload-modal.component.scss']
+  styleUrls: ['./video-upload-modal.component.scss'],
 })
-export class VideoUploadModalComponent implements OnInit{
-  blockFormGroup: FormGroup;
-  
-  file: File;
-  videoLink:"";
-  videoInputId: string;
-  isLoadingVideo: boolean;
-  hasVideo: boolean;
-  videoUrl: string;
-  vidFilePath: string
-
-  videoInputUpload = '';
-
+export class VideoUploadModalComponent {
   title = 'Upload Video';
+  videoName: string;
+  videoFile: File;
   sizeOptions = [
-  { vidSize: '4K', resolution: '3840px x 2160px' },
-  { vidSize: '1080p', resolution: '1920px x 1080px' },
-  { vidSize: '720p (Recommended)', resolution: '1280px x 720px' },
-  { vidSize: '540p', resolution: '960px x 540px' },
-  { vidSize: "Don't Encode Media", resolution: 'This will use the original media you provided' },
-  ]
+    { vidSize: '4K', resolution: '3840px x 2160px' },
+    { vidSize: '1080p', resolution: '1920px x 1080px' },
+    { vidSize: '720p (Recommended)', resolution: '1280px x 720px' },
+    { vidSize: '540p', resolution: '960px x 540px' },
+    {
+      vidSize: "Don't Encode Media",
+      resolution: 'This will use the original media you provided',
+    },
+  ];
+  videoUrl = '';
+  videoInputId = 'videoInput';
+  videoInputUpload = 'videoInputUpload';
+  private _sBs = new SubSink();
 
   constructor(
-    private _videoUploadService: FileStorageService,
     private dialogRef: MatDialogRef<VideoUploadModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { videoMessageForm: FormGroup, block: Story },
-  ) { }
-
-  ngOnInit(): void {
-    this.videoInputId = `vid-${this.data.block.id}`;
-    this.videoInputUpload = `vid-${this.data.block.id}-upload`;
-    
-    this.checkIfVideoExists();
-  }
-
-  checkIfVideoExists(){
-    this.videoUrl = this.data.videoMessageForm.value.fileSrc;
-    this.hasVideo = this.videoUrl && this.videoUrl != '' ? true : false;
-  }
-
-   async apply() {
-    if(this.vidFilePath){
-      const res = await this._videoUploadService.uploadSingleFile(this.file, this.vidFilePath);
-      res.subscribe(url => this._autofillUrl(url))
-    } else {
-      alert('not uploaded')
-    }
-  
-    this.closeModal()
-  }
+    @Inject(MAT_DIALOG_DATA) public data: { videoMessageForm: FormGroup; block: Story },
+    private _videoUploadService: FileStorageService,
+  ) {}
 
   closeModal(): void {
     this.dialogRef.close();
   }
 
-  async processVideo(event: any) {
-    if (event.target.files && event.target.files[0]) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => this.videoLink = e.target.result;
-      reader.readAsDataURL(event.target.files[0]);
-      this.file = event.target.files[0];
-      this.isLoadingVideo = true;
-    }
+  async apply(): Promise<void> {
     //Step 1 - Create the file path that will be in firebase storage
-    this.vidFilePath = `videos/${this.file.name}_${new Date().getTime()}`;
-    this.isLoadingVideo = true;
-    this.data.videoMessageForm.get('fileName')?.setValue(this.file.name);
+    const vidFilePath = `videos/${this.videoFile.name}_${new Date().getTime()}`;
+
+    const response = await this._videoUploadService.uploadSingleFile(this.videoFile, vidFilePath);
+
+    this._sBs.sink = response.subscribe((url) => {
+      this.data.videoMessageForm.patchValue({ fileSrc: url });
+      this.data.videoMessageForm.patchValue({ fileName: this.videoFile.name });
+      this.dialogRef.close({
+        name: this.videoFile.name,
+        file: this.videoFile,
+        url,
+      });
+    });
   }
 
-  private _autofillUrl(url: string) {
-    this.data.videoMessageForm.patchValue({fileSrc: url});
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      this.videoUrl = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+    this.videoFile = file;
+  }
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
   }
 }
