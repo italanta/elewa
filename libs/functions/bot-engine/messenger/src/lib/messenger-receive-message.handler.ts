0320b2a0edb04b07d89e77a6475ae504f072fab7
Reagan Charana <rgncharana2@gmail.com>
2023-05-02 12:13:47 +0300

    :sparkles: :horse_racing: connect messenger to the bot engine

--- libs/functions/bot-engine/messenger/src/lib/messenger-receive-message.handler.ts
+++ libs/functions/bot-engine/messenger/src/lib/messenger-receive-message.handler.ts
@@ -3,12 +3,14 @@ import { HandlerTools } from '@iote/cqrs';
 import { FunctionHandler, RestResult, HttpsContext } from '@ngfi/functions';
 
 import { IncomingWhatsAppMessage } from '@app/model/convs-mgr/functions';
+import { ChannelDataService, EngineBotManager, generateEndUserId } from '@app/functions/bot-engine';
+import { MessengerCommunicationChannel, MessengerEndUser, PlatformType } from '@app/model/convs-mgr/conversations/admin/system';
+
+import { MessengerActiveChannel } from './models/messenger-active-channel.model';
+
+import { MessengerIncomingMessageParser } from './io/incoming-message-parser/messenger-api-incoming-message-parser.class';
 
 import { __ValidateVerificationRequest } from './utils/validate-webhook.function';
-import { ChannelDataService, EngineBotManager, MessagesDataService } from '@app/functions/bot-engine';
-import { WhatsappActiveChannel } from '@app/functions/bot-engine/whatsapp';
-import { WhatsAppCommunicationChannel } from '@app/model/convs-mgr/conversations/admin/system';
-import { WhatsappIncomingMessageParser } from 'libs/functions/bot-engine/whatsapp/src/lib/io/incoming-message-parser/whatsapp-api-incoming-message-parser.class';
 import { __ConvertMessengerApiPayload } from './utils/convert-messanger-payload.util';
 
 /**
@@ -50,7 +52,7 @@ export class MessengerReceiveMsgHandler extends FunctionHandler<IncomingWhatsApp
     if (!payload) return { status: 400, message: "No messages in incoming payload to process" } as RestResult;
 
     tools.Logger.log(() => `Received Messenger msg ${JSON.stringify(payload)}`);
-    console.log(`Received Messenger msg ${payload}`);
+    console.log(`Received Messenger msg ${JSON.stringify(payload)}`);
 
     // STEP 2: We have validated.
     //         We now unpack the message and search for the correct channel
@@ -66,10 +68,10 @@ export class MessengerReceiveMsgHandler extends FunctionHandler<IncomingWhatsApp
     // So we get registered channel information by passing the business phone number id Channel Data Service
     const _channelService$ = new ChannelDataService(tools, sanitizedResponse);
 
-    const communicationChannel = await _channelService$.getChannelInfo(sanitizedResponse.platformId) as WhatsAppCommunicationChannel;
+    const communicationChannel = await _channelService$.getChannelInfo(sanitizedResponse.pageId) as MessengerCommunicationChannel;
 
     if (!communicationChannel) {
-      tools.Logger.error(() => `[ChannelInfo].getChannelInfo - This phone number has not been registered to a channel: ID: ${sanitizedResponse.platformId}`);
+      tools.Logger.error(() => `[ChannelInfo].getChannelInfo - This phone number has not been registered to a channel: ID: ${sanitizedResponse.pageId}`);
 
       return { status: 500 } as RestResult;
     }
@@ -80,28 +82,31 @@ export class MessengerReceiveMsgHandler extends FunctionHandler<IncomingWhatsApp
     //         We need to create the active channel so that the engine can use it to process and send the message
     //         The active channel contains the Communication Channel and a send message function
     //         So here we create an instance of the SendWhatsAppMessageModel which implements the active channel
-    const whatsappActiveChannel = new WhatsappActiveChannel(tools, communicationChannel);
+    const messengerActiveChannel = new MessengerActiveChannel(tools, communicationChannel);
 
     // STEP 5: Create the bot engine and process the message.
     //        Since we receive different types of messages e.g. text message, location,
-    const engine = new EngineBotManager(tools, tools.Logger, whatsappActiveChannel);
+    const engine = new EngineBotManager(tools, tools.Logger, messengerActiveChannel);
 
-    const _msgDataService$ = new MessagesDataService(tools);
+    const messengerIncomingMessageParser = new MessengerIncomingMessageParser().resolve(sanitizedResponse.type);
 
-    const whatsappIncomingMessageParser = new WhatsappIncomingMessageParser().resolve(sanitizedResponse.type);
-
-    const message = whatsappIncomingMessageParser.parse(sanitizedResponse.message);
+    const message = messengerIncomingMessageParser.parse(sanitizedResponse);
 
     // Don't process the message if we cannot parse it
     if (!message) return { status: 500, message: `Failed to parse incoming message: ${sanitizedResponse.message}` } as RestResult;
+
+    const messengerEndUser: MessengerEndUser = {
+      id: generateEndUserId(sanitizedResponse.endUserPageId, PlatformType.Messenger, communicationChannel.n),
+      endUserPageId: sanitizedResponse.endUserPageId,
+    }
     
     // STEP 6: Pass the standardized message and run the bot engine
-    return engine.run(message);
+    return engine.run(message, messengerEndUser);
     } catch (error) {
       tools.Logger.error(() => `[MessengerReceiveMsgHandler].execute - Error processing message: ${error}`);
-      
+
       return { status: 500 } as RestResult; 
     }
-  }
+  }  
 }
 
