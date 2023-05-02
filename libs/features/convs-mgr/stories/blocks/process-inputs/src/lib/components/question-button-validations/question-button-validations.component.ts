0a3c19fa9ae245d81db32ed9bcb4b02963031f2d
Snow <onyangogeorge954@gmail.com>
2023-05-02 21:57:17 +0300

    add question-valodations component

---
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/components/question-button-validations/question-button-validations.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-button-validations',
+  templateUrl: './question-button-validations.component.html',
+  styleUrls: ['./question-button-validations.component.scss'],
+})
+export class QuestionButtonValidationsComponent {}
