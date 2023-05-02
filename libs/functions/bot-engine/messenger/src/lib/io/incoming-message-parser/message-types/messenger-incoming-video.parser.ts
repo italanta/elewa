11b0c5339bcf4e7129e3ff79162e6aea6cfd1c59
Reagan Charana <rgncharana2@gmail.com>
2023-05-02 12:16:46 +0300

    :art: :truck: clean up parsers

--- libs/functions/bot-engine/messenger/src/lib/io/incoming-message-parser/message-types/messenger-incoming-video.parser.ts
+++ libs/functions/bot-engine/messenger/src/lib/io/incoming-message-parser/message-types/messenger-incoming-video.parser.ts
@@ -1,6 +1,5 @@
 import { IncomingVideoMessageParser } from "@app/functions/bot-engine";
 import { IncomingMessagePayload, VideoMessage } from "@app/model/convs-mgr/conversations/messages";
-import { MessageTypes , VideoPayload } from "@app/model/convs-mgr/functions";
 
 export class MessengerIncomingVideoParser extends IncomingVideoMessageParser {
 
