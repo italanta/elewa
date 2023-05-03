6fb2596f0f57c6568d460e726f1453b184d3a312
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-05-03 09:46:24 +0300

    :sparkles: feat: generate conditional block component

---
+++ libs/features/convs-mgr/stories/blocks/library/conditional-block/src/lib/component/conditional-block/conditional-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conditional-block',
+  templateUrl: './conditional-block.component.html',
+  styleUrls: ['./conditional-block.component.scss'],
+})
+export class ConditionalBlockComponent {}
--- libs/features/convs-mgr/stories/blocks/library/conditional-block/src/lib/features-convs-mgr-stories-blocks-library-conditional-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/conditional-block/src/lib/features-convs-mgr-stories-blocks-library-conditional-block.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConditionalBlockComponent } from './component/conditional-block/conditional-block.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ConditionalBlockComponent],
 })
 export class FeaturesConvsMgrStoriesBlocksLibraryConditionalBlockModule {}
