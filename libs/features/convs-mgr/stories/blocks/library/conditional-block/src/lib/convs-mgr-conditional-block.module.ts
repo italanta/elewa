ba470aa762407a65694b743b1c9dfdb3a4e46581
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-05-03 12:37:08 +0300

    :sparkles: feat: rename conditional block module file name

---
+++ libs/features/convs-mgr/stories/blocks/library/conditional-block/src/lib/convs-mgr-conditional-block.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  FlexLayoutModule,
+  MaterialBricksModule,
+  MaterialDesignModule,
+} from '@iote/bricks-angular';
+
+import { MultiLangModule } from '@ngfi/multi-lang';
+
+import { ConvsMgrReusableTextAreaModule } from '@app/features/convs-mgr/stories/blocks/library/reusable-text-area';
+import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/blocks/library/block-options';
+
+import { ConditionalBlockComponent } from './component/conditional-block/conditional-block.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MultiLangModule,
+    FlexLayoutModule,
+    MaterialBricksModule,
+    MaterialDesignModule,
+    ConvsMgrBlockOptionsModule,
+    ConvsMgrReusableTextAreaModule,
+  ],
+  declarations: [ConditionalBlockComponent],
+  exports: [ConditionalBlockComponent],
+})
+export class ConvsMgrConditionalBlockModule {}
--- libs/features/convs-mgr/stories/blocks/library/conditional-block/src/lib/features-convs-mgr-stories-blocks-library-conditional-block.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ConditionalBlockComponent } from './component/conditional-block/conditional-block.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [ConditionalBlockComponent],
-})
-export class FeaturesConvsMgrStoriesBlocksLibraryConditionalBlockModule {}
