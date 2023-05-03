8638144fbf57ce9400bfc10ab8d20613e8b06ae5
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-05-03 16:09:42 +0300

    :sparkles: feat: collect all variables from existing blocks

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/providers/process-input.service.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/providers/process-input.service.ts
@@ -5,22 +5,33 @@ import { StoryBlock } from '@app/model/convs-mgr/stories/blocks/main';
 import { StoryBlocksStore } from '@app/state/convs-mgr/stories/blocks';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProcessInputService {
   /** list of blocks with variables already set */
   blocksWithVars$: Observable<StoryBlock[]>;
 
   constructor(private _blockStore$$: StoryBlocksStore) {
-    this.blocksWithVars$ = this._blockStore$$
-      .get()
-      .pipe(
-        map((blocks) =>
-          blocks.filter(
-            (block) =>
-              !block.deleted && block.type < 1000 && block.variable?.name !== ''
-          )
+    this.blocksWithVars$ = this.getBlocksWithVars()
+  }
+
+  getBlocksWithVars() {
+    return this._blockStore$$
+    .get()
+    .pipe(
+      map((blocks) =>
+        blocks.filter(
+          (block) =>
+          // ignore deleted blocks, end blocks and blocks with the variables as an empty string or undefined
+            !block.deleted && block.type < 1000 && block.variable?.name !== '' && block.variable?.name != undefined
         )
-      );
+      )
+    );
+  }
+
+  getAllVariables() : Observable<string[]> {
+    return this.blocksWithVars$.pipe(
+      map((blocks) => blocks.map((block) => block.variable?.name) as string[])
+    );
   }
 }
