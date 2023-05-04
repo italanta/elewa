aa2b797255f49deeba84f445c8090c8ce034cb92
Reagan Charana <rgncharana2@gmail.com>
2023-05-04 14:50:29 +0300

    :wrench: :sparkles: implement parsing of messenger media msg

--- libs/functions/bot-engine/messenger/src/lib/io/incoming-message-parser/message-types/messenger-incoming-image.parser.ts
+++ libs/functions/bot-engine/messenger/src/lib/io/incoming-message-parser/message-types/messenger-incoming-image.parser.ts
@@ -1,5 +1,6 @@
 import { IncomingImageMessageParser } from "@app/functions/bot-engine";
-import { ImageMessage, IncomingMessagePayload } from "@app/model/convs-mgr/conversations/messages";
+import { ImageMessage } from "@app/model/convs-mgr/conversations/messages";
+import { IncomingMessengerMessage, MessageTypes, MessengerAttachments } from "@app/model/convs-mgr/functions";
 
 export class MessengerIncomingImageParser extends IncomingImageMessageParser {
 
@@ -8,7 +9,14 @@ export class MessengerIncomingImageParser extends IncomingImageMessageParser {
     super();
   }
 
-  parse(incomingMessage: IncomingMessagePayload): ImageMessage {
-    return null;
+  parse(incomingMessage: IncomingMessengerMessage): ImageMessage {
+    const imagePayload = incomingMessage.message as MessengerAttachments
+    // Create the base message object
+    const newMessage: ImageMessage = {
+      id: incomingMessage.timeStamp.toString(),
+      type: MessageTypes.IMAGE,
+      url: imagePayload.attachments[0].payload.url,
+    };
+    return newMessage;
   }
 }
