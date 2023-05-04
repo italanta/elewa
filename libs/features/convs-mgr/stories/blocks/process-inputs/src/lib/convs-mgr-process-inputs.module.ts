4e1164d38f46c67a30a65624ba2bbf6b18b31eb5
George Onyango <69570282+SnowJunior@users.noreply.github.com>
2023-05-04 11:14:39 +0300

    Update convs-mgr-process-inputs.module.ts

--- libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
+++ libs/features/convs-mgr/stories/blocks/process-inputs/src/lib/convs-mgr-process-inputs.module.ts
@@ -9,7 +9,6 @@ import { NameValidationsComponent } from './components/name-validations/name-val
 import { EmailValidationsComponent } from './components/email-validations/email-validations.component';
 import { PhoneValidationsComponent } from './components/phone-validations/phone-validations.component';
 import { LocationValidationsComponent } from './components/location-validations/location-validations.component';
-import { QuestionButtonValidationsComponent } from './components/question-button-validations/question-button-validations.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, MultiLangModule],
@@ -19,7 +18,6 @@ import { QuestionButtonValidationsComponent } from './components/question-button
     EmailValidationsComponent,
     PhoneValidationsComponent,
     LocationValidationsComponent,
-    QuestionButtonValidationsComponent,
   ],
   exports: [VariableInputComponent],
 })
