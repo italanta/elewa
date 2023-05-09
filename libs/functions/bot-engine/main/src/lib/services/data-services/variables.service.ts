5a34412758e6a2948291af4c76f431f4c81c3fdd
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-05-09 16:45:09 +0300

    :sparkles: feat: add variables data service

---
+++ libs/functions/bot-engine/main/src/lib/services/data-services/variables.service.ts
@@ -0,0 +1,32 @@
+import { HandlerTools } from '@iote/cqrs';
+
+import { BotDataService } from './data-service-abstract.class';
+
+/**
+ * Contains all the required database flow methods for the chat-status collection
+ */
+export class VariablesDataService extends BotDataService<any>{
+  private _docPath: string;
+  tools: HandlerTools;
+
+  constructor(tools: HandlerTools, orgId: string, endUserId: string) 
+  {
+    super(tools);
+    this.tools = tools;
+    this._init(orgId, endUserId);
+  }
+
+  protected _init(orgId: string, endUserId:string): void 
+  {
+    this._docPath = `orgs/${orgId}/end-users/${endUserId}/variables`;
+  }
+
+  public async getAllVariables() {
+    return this.getDocumentById('values',this._docPath);
+  }
+
+  public async getSpecificVariable(varName: string) {
+    const allVariables = await this.getAllVariables()
+    return allVariables[varName]
+  }
+}
