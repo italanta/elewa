f5ee46ca84a33b55cc3a9cfa1caf588810c7ac7a
Reagan Charana <rgncharana2@gmail.com>
2023-05-12 14:08:48 +0300

    :scroll: create script to download and extract dummy data

---
+++ emulator-data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the script is called with a file argument
+curl -O https://firebasestorage.googleapis.com/v0/b/elewa-conv-learning-prod.appspot.com/o/emulator-data.zip\?alt\=media\&token\=bab68cc2-a84d-4d14-99d4-d7e8ecd71efe
+
+filename="emulator-data.zip"
+
+# Create a directory to extract the contents
+target_dir="."
+mkdir -p "$target_dir"
+
+# Extract the contents of the zip file
+if command -v bsdtar >/dev/null; then
+  bsdtar -xf "$filename" -C "$target_dir"
+elif command -v unzip >/dev/null; then
+  unzip "$filename" -d "$target_dir"
+else
+  echo "Error: Neither 'bsdtar' nor 'unzip' commands found."
+  exit 1
+fi
+
+rm "$filename"
+
+echo "Extraction complete. Contents extracted to: $target_dir"
