64035abc9f05167713d7e256937e9bd267f40e8e
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-05-15 16:50:24 +0300

    :lipstick: style: add menu bar to select trackmode and also styling for that

--- libs/features/analytics/group-based/progress/src/lib/group-based-progress.module.ts
+++ libs/features/analytics/group-based/progress/src/lib/group-based-progress.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialDesignModule, MaterialBricksModule } from '@iote/bricks-angular';
+
 import { GroupBasedProgressChartComponent } from './components/group-based-progress-chart/group-based-progress-chart.component';
 
 @NgModule({
   declarations: [GroupBasedProgressChartComponent],
   exports: [GroupBasedProgressChartComponent], 
 
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialBricksModule, MaterialDesignModule],
 })
 export class GroupBasedProgressModule {}
