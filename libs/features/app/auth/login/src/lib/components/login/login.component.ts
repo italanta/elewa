351ac3b3f6ee4803f8d991455f82a675d1f70ae7
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-05-18 22:15:15 +0300

    🎨 :Added dialog box for logins and validations.

--- libs/features/app/auth/login/src/lib/components/login/login.component.ts
+++ libs/features/app/auth/login/src/lib/components/login/login.component.ts
@@ -1,61 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EventLogger } from '@iote/bricks-angular';
+import { FormBuilder, Validators } from '@angular/forms';
 
 import { AuthService } from '@ngfi/angular';
 import { ForgotPasswordModalComponent } from '../../modals/forgot-password-modal/forgot-password-modal.component';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit
-{
-
+export class LoginComponent implements OnInit {
   isLoading: boolean;
   email: string;
   password: string;
+  loginForm: any;
+  loginFailed: boolean;
+  isLogin: boolean;
 
   // isLoading: boolean;
-  isLogin = true;
+  isLoginFormValid(): boolean {
+    return this.loginForm.valid;
+  }
 
-  constructor( private _authService: AuthService,
-              private _dialog: MatDialog,
-              private _analytics: EventLogger)
-  {  }
+  constructor(
+    private _authService: AuthService,
+    private _dialog: MatDialog,
+    private _analytics: EventLogger,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit()
-  {
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
   validateLoginCred = () => this.email && this.password;
 
   // When user clicks enter, try log in.
-  detectEnter = (event: any) => (event.key === "Enter") ? this.loginUser() : 'noop';
-
-  loginUser()
-  {
+  detectEnter(event: any): void {
+    if (event.key === 'Enter' && this.isLoginFormValid()) {
+      this.loginUser();
+    }
+  }
+  loginUser(): void {
     this.isLoading = true;
 
-    if(this.validateLoginCred())
-      this._authService.loginWithEmailAndPassword(this.email, this.password)
-                      .then(()=> this._analytics.logEvent('login'))
-                      .catch((error) => { this.isLoading = false;
-                                          this._analytics.logEvent('login_error', {"errorMsg": error})
-                                        });
+    if (this.isLoginFormValid()) {
+      const { email, password } = this.loginForm.value;
+
+      this._authService
+        .loginWithEmailAndPassword(email, password)
+        .then(() => this._analytics.logEvent('login'))
+        .catch((error) => {
+          this.isLoading = false;
+          this._analytics.logEvent('login_error', { errorMsg: error });
+          this.loginFailed = true;
+        });
+    }
   }
 
   forgotPass() {
-    this._dialog
-          .open(ForgotPasswordModalComponent, {
-            width: '500px'
-          });
+    this._dialog.open(ForgotPasswordModalComponent, {
+      width: '500px',
+    });
   }
 
-  toggleMode = () => this.isLogin = ! this.isLogin;
-
+  toggleMode = () => (this.isLogin = !this.isLogin);
 
   loginGoogle() {
     return this._authService.loadGoogleLogin();
@@ -70,5 +84,4 @@ export class LoginComponent implements OnInit
   loginMicrosoft() {
     return this._authService.loadMicrosoftLogin();
   }
-
 }
