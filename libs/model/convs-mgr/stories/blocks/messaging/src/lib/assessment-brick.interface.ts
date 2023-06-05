114f6e28520bcb2a0430e0af620059e87e7bba25
Reagan Charana <rgncharana2@gmail.com>
2023-06-05 15:21:44 +0300

    :wrench: add missing assessment id property

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/assessment-brick.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/assessment-brick.interface.ts
@@ -2,6 +2,9 @@ import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 
 export interface AssessmentBrick extends StoryBlock
 {
+  /** Id if the selected assessment */
+  assessmentId: string;
+  
   /** Actual question */
   defaultTarget?: string;
 
