091dc840622e91d1d7484239131c3aa70e98f21b
Reagan Charana <rgncharana2@gmail.com>
2023-06-09 15:54:59 +0300

    :wrench: add assessment services to switch cases

--- libs/functions/bot-engine/main/src/lib/services/process-operation-block/process-operation-block.factory.ts
+++ libs/functions/bot-engine/main/src/lib/services/process-operation-block/process-operation-block.factory.ts
@@ -12,6 +12,8 @@ import { EndStoryBlockService } from "./block-type/end-story-block.service";
 import { FailBlockService } from "./block-type/fail-story-block.service";
 import { ConditionalBlockService } from "./block-type/conditional-block.service";
 import { EventBlockService } from "./block-type/event-block.service";
+import { AssessmentBlockService } from "./block-type/assessment-block.service";
+import { AssessmentQuestionBlockService } from "./block-type/assessment-question.service";
 
 
 export class OperationBlockFactory
@@ -31,8 +33,12 @@ export class OperationBlockFactory
         return new FailBlockService(this._blockDataService, this._connDataService, this.tools);
       case StoryBlockTypes.Conditional:
         return new ConditionalBlockService(this._blockDataService, this._connDataService, this.tools);
+      case StoryBlockTypes.Assessment:
+        return new AssessmentBlockService(this._blockDataService, this._connDataService, this.tools);
+      case StoryBlockTypes.AssessmentQuestionBlock:
+        return new AssessmentQuestionBlockService(this._blockDataService, this._connDataService, this.tools);
       case StoryBlockTypes.Event:
-        return new EventBlockService(this._blockDataService, this._connDataService, this.tools)
+        return new EventBlockService(this._blockDataService, this._connDataService, this.tools);
       default:
         break;
     }
