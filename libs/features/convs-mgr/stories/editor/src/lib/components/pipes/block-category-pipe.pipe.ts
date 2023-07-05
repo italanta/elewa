0dc0f1ba5f1c5c811e96484f3cc0b8df9f555250
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-07-05 22:23:01 +0300

    Implement custom pipe for block category filtering

--- libs/features/convs-mgr/stories/editor/src/lib/components/pipes/block-category-pipe.pipe.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/components/pipes/block-category-pipe.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { StoryBlock } from '@app/model/convs-mgr/stories/blocks/main';
 
 @Pipe({
   name: 'blockCategoryPipe'
 })
 export class BlockCategoryPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
-
+  transform(blocks: StoryBlock[], category: string, filter: string): boolean {
+     const filteredBlocks = blocks.filter((block: StoryBlock) => {
+      return block.blockCategory === category && block.message?.toString().toLowerCase().includes(filter);
+     })
+     return filteredBlocks.length > 0
+  }  
 }
