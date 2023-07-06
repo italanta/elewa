4ff160352338cac4f22558afb403ac61960baf14
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-06 11:05:19 +0300

    :sparkles: feat: move grouped blocks section and logic to it's own separate component

--- libs/features/convs-mgr/stories/editor/src/lib/components/grouped-blocks/grouped-blocks.component.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/components/grouped-blocks/grouped-blocks.component.ts
@@ -1,8 +1,137 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+
+import { SubSink } from 'subsink';
+
+import {
+  StoryBlock,
+  StoryBlockTypes,
+} from '@app/model/convs-mgr/stories/blocks/main';
+
+import { Coordinate } from '../../model/coordinates.interface';
+import { StoryEditorFrame } from '../../model/story-editor-frame.model';
+import { DragDropService } from '../../providers/drag-drop.service';
 
 @Component({
   selector: 'convl-grouped-blocks',
   templateUrl: './grouped-blocks.component.html',
   styleUrls: ['./grouped-blocks.component.scss'],
 })
-export class GroupedBlocksComponent {}
+export class GroupedBlocksComponent implements OnInit, OnDestroy {
+  @Input() groupedBlocks: StoryBlock[];
+  @Input() frame: StoryEditorFrame;
+
+  private _sBs = new SubSink();
+
+  coordinates: Coordinate;
+
+  constructor(private dragService: DragDropService) {}
+
+  ngOnInit() {
+    this._sBs.sink = this.dragService.coord$.subscribe((position) => (this.coordinates = position));
+  }
+
+  addBlock(type: number, coordinates?: Coordinate) {
+    switch (type) {
+      case StoryBlockTypes.EndStoryAnchorBlock:
+        this.frame.newBlock(StoryBlockTypes.EndStoryAnchorBlock, coordinates);
+        break;
+      case StoryBlockTypes.TextMessage:
+        this.frame.newBlock(StoryBlockTypes.TextMessage, coordinates);
+        break;
+      case StoryBlockTypes.Input:
+        this.frame.newBlock(StoryBlockTypes.Input, coordinates);
+        break;
+      case StoryBlockTypes.IO:
+        this.frame.newBlock(StoryBlockTypes.IO, coordinates);
+        break;
+      case StoryBlockTypes.Location:
+        this.frame.newBlock(StoryBlockTypes.Location, coordinates);
+        break;
+      case StoryBlockTypes.Image:
+        this.frame.newBlock(StoryBlockTypes.Image, coordinates);
+        break;
+      case StoryBlockTypes.QuestionBlock:
+        this.frame.newBlock(StoryBlockTypes.QuestionBlock, coordinates);
+        break;
+      case StoryBlockTypes.Document:
+        this.frame.newBlock(StoryBlockTypes.Document, coordinates);
+        break;
+      case StoryBlockTypes.Audio:
+        this.frame.newBlock(StoryBlockTypes.Audio, coordinates);
+        break;
+      case StoryBlockTypes.Structural:
+        this.frame.newBlock(StoryBlockTypes.Structural, coordinates);
+        break;
+      case StoryBlockTypes.Name:
+        this.frame.newBlock(StoryBlockTypes.Name, coordinates);
+        break;
+      case StoryBlockTypes.Email:
+        this.frame.newBlock(StoryBlockTypes.Email, coordinates);
+        break;
+      case StoryBlockTypes.PhoneNumber:
+        this.frame.newBlock(StoryBlockTypes.PhoneNumber, coordinates);
+        break;
+      case StoryBlockTypes.Video:
+        this.frame.newBlock(StoryBlockTypes.Video, coordinates);
+        break;
+      case StoryBlockTypes.Sticker:
+        this.frame.newBlock(StoryBlockTypes.Sticker, coordinates);
+        break;
+      case StoryBlockTypes.List:
+        this.frame.newBlock(StoryBlockTypes.List, coordinates);
+        break;
+      case StoryBlockTypes.Reply:
+        this.frame.newBlock(StoryBlockTypes.Reply, coordinates);
+        break;
+      case StoryBlockTypes.JumpBlock:
+        this.frame.newBlock(StoryBlockTypes.JumpBlock, coordinates);
+        break;
+      case StoryBlockTypes.MultipleInput:
+        this.frame.newBlock(StoryBlockTypes.MultipleInput, coordinates);
+        break;
+      case StoryBlockTypes.FailBlock:
+        this.frame.newBlock(StoryBlockTypes.FailBlock, coordinates);
+        break;
+      case StoryBlockTypes.ImageInput:
+        this.frame.newBlock(StoryBlockTypes.ImageInput, coordinates);
+        break;
+      case StoryBlockTypes.LocationInputBlock:
+        this.frame.newBlock(StoryBlockTypes.LocationInputBlock, coordinates);
+        break;
+      case StoryBlockTypes.AudioInput:
+        this.frame.newBlock(StoryBlockTypes.AudioInput, coordinates);
+        break;
+      case StoryBlockTypes.VideoInput:
+        this.frame.newBlock(StoryBlockTypes.VideoInput, coordinates);
+        break;
+      case StoryBlockTypes.WebhookBlock:
+        this.frame.newBlock(StoryBlockTypes.WebhookBlock, coordinates);
+        break;
+      case StoryBlockTypes.OpenEndedQuestion:
+        this.frame.newBlock(StoryBlockTypes.OpenEndedQuestion, coordinates);
+        break;
+      case StoryBlockTypes.MultiContentInput:
+        this.frame.newBlock(StoryBlockTypes.MultiContentInput, coordinates);
+        break;
+      case StoryBlockTypes.keyword:
+        this.frame.newBlock(StoryBlockTypes.keyword, coordinates);
+        break;
+      case StoryBlockTypes.Event:
+        this.frame.newBlock(StoryBlockTypes.Event, coordinates);
+        break;
+      case StoryBlockTypes.Assessment:
+        this.frame.newBlock(StoryBlockTypes.Assessment, coordinates);
+        break;
+      case StoryBlockTypes.Conditional:
+        this.frame.newBlock(StoryBlockTypes.Conditional, coordinates);
+    }
+  }
+
+  onDragEnd(blockType: number) {
+    this.addBlock(blockType, this.coordinates);
+  }
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
+  }
+}
