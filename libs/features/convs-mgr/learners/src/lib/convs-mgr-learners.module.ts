0364a560d4f3e442b008604fc2edae5ff9ac6a91
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-10 17:08:18 +0300

    :sparkles: feat: add learners page to default route

--- libs/features/convs-mgr/learners/src/lib/convs-mgr-learners.module.ts
+++ libs/features/convs-mgr/learners/src/lib/convs-mgr-learners.module.ts
@@ -4,8 +4,10 @@ import { LearnersPageComponent } from './pages/learners-page/learners-page.compo
 
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
 
+import { LearnersRouterModule } from './learners.router';
+
 @NgModule({
-  imports: [CommonModule, ConvlPageModule],
+  imports: [CommonModule, ConvlPageModule, LearnersRouterModule],
   declarations: [LearnersPageComponent],
 })
 export class ConvsMgrLearnersModule {}
