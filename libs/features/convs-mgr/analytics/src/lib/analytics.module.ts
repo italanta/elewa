3cdd2bc2bc8a574f7085ef62f64472ae82230b93
Reagan Charana <rgncharana2@gmail.com>
2023-07-17 13:01:57 +0300

    :wrench: declare safe pipe in module

--- libs/features/convs-mgr/analytics/src/lib/analytics.module.ts
+++ libs/features/convs-mgr/analytics/src/lib/analytics.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
 
 import { AnalyticsPageComponent } from './pages/analytics-page/analytics-page.component';
-
 import { AnalyticsRouterModule } from './analytics.router.module';
+import { SafePipe } from './pages/safe.pipe';
 
 @NgModule({
   imports: [CommonModule, AnalyticsRouterModule, ConvlPageModule],
-  declarations: [AnalyticsPageComponent]
+  declarations: [AnalyticsPageComponent, SafePipe]
 })
 export class AnalyticsModule {}
