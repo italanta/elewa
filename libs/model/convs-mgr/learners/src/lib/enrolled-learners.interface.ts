44ac657b3026137c339867df464b207d0cff9e29
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-19 16:35:50 +0300

    :memo: refactor: update comments on enrolledUser interface

--- libs/model/convs-mgr/learners/src/lib/enrolled-learners.interface.ts
+++ libs/model/convs-mgr/learners/src/lib/enrolled-learners.interface.ts
@@ -1,16 +1,39 @@
 import { IObject } from "@iote/bricks";
 
+
 /**
- * instance of an enrolled learner
+ * Represents an enrolled learner.
  */
 export interface EnrolledEndUser extends IObject {
+  /**
+   * The name of the enrolled learner.
+   */
   name: string;
+
+  /**
+   * The phone number of the enrolled learner.
+   */
   phoneNumber: string;
+
+  /**
+   * The ID of the class to which the learner is enrolled.
+   */
   classId: string;
-  endUserId?: string; // To link to the end-users collection
+
+  /**
+   * Optional field to link to the end-users collection using their ID.
+   */
+  endUserId?: string;
+
+  /**
+   * The status of the enrolled learner.
+   */
   status: EnrolledEndUserStatus;
 }
 
+/**
+ * Enum representing the status of an enrolled learner.
+ */
 enum EnrolledEndUserStatus {
   active = 1,
   inactive = 2
