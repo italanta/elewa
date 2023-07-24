96b66fea33ba7478c2561813117dbf546ed7ebd3
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-24 09:58:07 +0300

    :sparkles: feat: add reports page route

--- libs/features/convs-mgr/analytics/src/lib/analytics.router.module.ts
+++ libs/features/convs-mgr/analytics/src/lib/analytics.router.module.ts
@@ -4,15 +4,19 @@ import { Route, RouterModule } from '@angular/router';
 import { AnalyticsPageComponent } from './pages/analytics-page/analytics-page.component';
 
 const ANALYTICS_ROUTES: Route[] = [
-  { path: '',   redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'dashboard',
+    component: AnalyticsPageComponent,
+  },
+  {
+    path: 'reports',
     component: AnalyticsPageComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(ANALYTICS_ROUTES)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AnalyticsRouterModule { }
+export class AnalyticsRouterModule {}
