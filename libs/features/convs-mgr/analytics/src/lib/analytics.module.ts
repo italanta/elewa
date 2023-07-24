18858062aae4698b457f89ce2992601947cf50a7
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-24 09:57:54 +0300

    :sparkles: feat: generate reports page component

--- libs/features/convs-mgr/analytics/src/lib/analytics.module.ts
+++ libs/features/convs-mgr/analytics/src/lib/analytics.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
 
 import { AnalyticsPageComponent } from './pages/analytics-page/analytics-page.component';
+import { ReportsPageComponent } from './pages/reports-page/reports-page.component';
 
 import { AnalyticsRouterModule } from './analytics.router.module';
 
 @NgModule({
   imports: [CommonModule, AnalyticsRouterModule, ConvlPageModule],
-  declarations: [AnalyticsPageComponent]
+  declarations: [AnalyticsPageComponent, ReportsPageComponent]
 })
 export class AnalyticsModule {}
