db818e3d0638a5063b687b580d19ed739d636321
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-26 14:59:01 +0300

    :sparkles: feat: add selection to learners table

--- libs/features/convs-mgr/learners/src/lib/convs-mgr-learners.module.ts
+++ libs/features/convs-mgr/learners/src/lib/convs-mgr-learners.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {
+  MaterialBricksModule,
+  MaterialDesignModule,
+} from '@iote/bricks-angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
 
 import { LearnersPageComponent } from './pages/learners-page/learners-page.component';
@@ -8,7 +14,14 @@ import { LearnersPageComponent } from './pages/learners-page/learners-page.compo
 import { LearnersRouterModule } from './learners.router';
 
 @NgModule({
-  imports: [CommonModule, ConvlPageModule, LearnersRouterModule],
+  imports: [
+    CommonModule,
+    ConvlPageModule,
+    LearnersRouterModule,
+    MultiLangModule,
+    MaterialBricksModule,
+    MaterialDesignModule,
+  ],
   declarations: [LearnersPageComponent],
 })
 export class ConvsMgrLearnersModule {}
--- libs/features/convs-mgr/learners/src/lib/pages/learners-page/learners-page.component.ts
+++ libs/features/convs-mgr/learners/src/lib/pages/learners-page/learners-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { SelectionModel } from '@angular/cdk/collections';
 
 import { SubSink } from 'subsink';
 import { Observable, map, switchMap, of, combineLatest } from 'rxjs';
@@ -26,13 +29,16 @@ export class LearnersPageComponent implements OnInit, OnDestroy {
 
   allLearners$: Observable<EnrolledEndUser[]>;
   allLearners: EnrolledEndUser[];
-  learnersColumns = ['name', 'phone', 'course', 'class'];
+  learnersColumns = ['select', 'name', 'phone', 'course', 'class', 'status'];
+
+  dataSource = new MatTableDataSource<EnrolledEndUser>();
+  selection = new SelectionModel<EnrolledEndUser>(true, []);
 
   ngOnInit() {
     this.getCurrentCourseForUsers();
   }
 
-  // TODO @LemmyMwaura: We now update the enrolled user's current milestone from the event brick so this method is redundant. 
+  // TODO @LemmyMwaura: We now update the enrolled user's current milestone from the event brick so this method is redundant.
   // TODO: to be removed after incremental adoption of the event brick(as a way to mark entry into a new milstone) on existing prod versions.
   getCurrentCourseForUsers() {
     this.allLearners$ = this._eLearners.getAllLearners$().pipe(
@@ -57,6 +63,34 @@ export class LearnersPageComponent implements OnInit, OnDestroy {
         return combineLatest(endUsers);
       })
     );
+
+    this._sBs.sink = this.allLearners$.subscribe(
+      (alllearners) => (this.dataSource.data = alllearners)
+    );
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    // if there is a selection then clear that selection
+    if (this.isSomeSelected()) {
+      this.selection.clear();
+    } else {
+      this.isAllSelected()
+        ? this.selection.clear()
+        : this.dataSource.data.map((row) => this.selection.select(row));
+    }
+  }
+
+  isSomeSelected() {
+    console.log(this.selection.selected);
+    return this.selection.selected.length > 0;
   }
 
   sortData(sortState: Sort) {
