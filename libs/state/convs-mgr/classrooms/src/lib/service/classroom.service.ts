f7c9ac9d3b3a4f4271c1c27715a3ebd5702f9d8b
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-27 10:44:57 +0300

    :sparkles: feat: create state store and it's corresponding state service

---
+++ libs/state/convs-mgr/classrooms/src/lib/service/classroom.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { Classroom } from '@app/model/convs-mgr/classroom';
+import { ClassroomStore } from '../store/classroom.store';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EndUserService {
+  constructor(private _classroom$$: ClassroomStore) {}
+
+  getAllClassrooms() {
+    return this._classroom$$.get();
+  }
+
+  getSpecificClassroom(id: string) {
+    return this._classroom$$.getOne(id);
+  }
+
+  deleteClassroom(classroom: Classroom) {
+    return this._classroom$$.remove(classroom);
+  }
+
+  updateClassroom(classroom: Classroom) {
+    return this._classroom$$.update(classroom);
+  }
+}
