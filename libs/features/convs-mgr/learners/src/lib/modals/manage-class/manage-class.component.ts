680ff8766e99b9c2c749a57eb8b79605667d08ef
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-27 16:17:03 +0300

    :sparkles: feat: add contents to manage class modal

--- libs/features/convs-mgr/learners/src/lib/modals/manage-class/manage-class.component.ts
+++ libs/features/convs-mgr/learners/src/lib/modals/manage-class/manage-class.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Classroom } from '@app/model/convs-mgr/classroom';
+import { EnrolledEndUser } from '@app/model/convs-mgr/learners';
 
 @Component({
   selector: 'app-manage-class',
   templateUrl: './manage-class.component.html',
   styleUrls: ['./manage-class.component.scss'],
 })
-export class ManageClassComponent {}
+export class ManageClassComponent {
+  selectedClass: Classroom;
+
+  constructor(
+    public dialogRef: MatDialogRef<ManageClassComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { selectedStudent: EnrolledEndUser }
+  ) {}
+
+  get selectedStudent() {
+    return this.data.selectedStudent;
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  onChange(e: any) {
+    this.selectedClass = e.target.value;
+  }
+
+  submitAction() {
+    // set user to class
+  }
+}
