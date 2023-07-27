a325512af0f1ab63276ac40850db78def966f352
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-27 10:46:28 +0300

    :sparkles: feat: update classroom state module - will be initialised with the store

--- libs/state/convs-mgr/classrooms/src/lib/classrooms-state.module.ts
+++ libs/state/convs-mgr/classrooms/src/lib/classrooms-state.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ClassroomStore } from './store/classroom.store';
 
 @NgModule({
   imports: [CommonModule],
 })
-export class ClassroomsStateModule {}
+export class ClassroomStateModule {
+  static forRoot(): ModuleWithProviders<ClassroomStateModule> {
+    return {
+      ngModule: ClassroomStateModule,
+      providers: [ClassroomStore],
+    };
+  }
+};
