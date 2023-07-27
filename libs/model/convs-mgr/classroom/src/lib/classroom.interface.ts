27c23a83219b62725dbb18c2f42945d5432d0bc6
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-27 11:12:41 +0300

    :house: feat: update jsdoc comments on classroom interface

--- libs/model/convs-mgr/classroom/src/lib/classroom.interface.ts
+++ libs/model/convs-mgr/classroom/src/lib/classroom.interface.ts
@@ -1,7 +1,13 @@
 import { IObject } from "@iote/bricks";
 
+/** Represents a classroom/userGroup */
 export interface Classroom extends IObject {
+  /** The name of the classroom */ 
   className: string;
+
+  /** A brief description of the classroom */
   description: string;
+
+  /** A boolean flag indicating whether the classroom has been deleted */
   deleted: boolean;
-}
+};
