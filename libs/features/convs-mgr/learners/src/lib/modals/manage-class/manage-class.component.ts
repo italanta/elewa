9ca540a542ca017a5bc41e4dcc49a6322d01ea8a
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-28 10:43:25 +0300

    :sparkles: feat: update enrolled user with selectedclass

--- libs/features/convs-mgr/learners/src/lib/modals/manage-class/manage-class.component.ts
+++ libs/features/convs-mgr/learners/src/lib/modals/manage-class/manage-class.component.ts
@@ -1,23 +1,29 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+import { Observable } from 'rxjs';
 
 import { ClassroomService } from '@app/state/convs-mgr/classrooms';
 
 import { Classroom } from '@app/model/convs-mgr/classroom';
 import { EnrolledEndUser } from '@app/model/convs-mgr/learners';
-import { Observable } from 'rxjs';
+import { EnrolledLearnersService } from '@app/state/convs-mgr/learners';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-manage-class',
   templateUrl: './manage-class.component.html',
   styleUrls: ['./manage-class.component.scss'],
 })
-export class ManageClassComponent implements OnInit {
+export class ManageClassComponent implements OnInit, OnDestroy {
   selectedClass: Classroom;
   classrooms$: Observable<Classroom[]>;
 
+  private _sBs = new SubSink();
+
   constructor(
     private _classroom$: ClassroomService,
+    private _enrolledUser$: EnrolledLearnersService,
     public dialogRef: MatDialogRef<ManageClassComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { enrolledUser: EnrolledEndUser, mode: string }
   ) {}
@@ -43,6 +49,16 @@ export class ManageClassComponent implements OnInit {
   }
 
   submitAction() {
-    // set user to class
+    if (this.selectedClass.id) {
+      this.enrolledUser.classId = this.selectedClass.id;
+
+      this._sBs.sink = this._enrolledUser$.updateLearner$(this.enrolledUser).subscribe(() => {
+        this.dialogRef.close();
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
   }
 }
