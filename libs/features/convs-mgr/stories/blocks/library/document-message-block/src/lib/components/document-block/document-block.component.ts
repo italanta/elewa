b81cbe9d33fed3a867c6e3db183590ede9bd6a9a
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-01 14:50:18 +0300

    :sparkles: feat: check document onSelect for file limits

--- libs/features/convs-mgr/stories/blocks/library/document-message-block/src/lib/components/document-block/document-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/document-message-block/src/lib/components/document-block/document-block.component.ts
@@ -2,14 +2,10 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { SubSink } from 'subsink';
-
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
-import { DocumentMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
-import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
-
 import { FileStorageService } from '@app/state/file';
-
+import { DocumentMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
 @Component({
   selector: 'app-document-block',
@@ -25,15 +21,10 @@ export class DocumentBlockComponent implements OnInit, OnDestroy  {
 
   file: File;
   docInputId: string;
-  docName = '';
-  defaultLink = "";
   isDocLoading = false;
-  docLink: string =  this.defaultLink;
-  
-  type: StoryBlockTypes;
-  documentType = StoryBlockTypes.Document;
+  byPassedLimits: any[] = [];
 
-  private _sBs = new SubSink();  //SubSink instance
+  private _sBs = new SubSink();
 
   constructor(private _docUploadService: FileStorageService){ }
 
@@ -42,28 +33,35 @@ export class DocumentBlockComponent implements OnInit, OnDestroy  {
     this.docInputId = `docs-${this.id}`;
   }
 
-  async processDocs(event: any)
-  {   
-
+  async processDocs(event: any) {   
     const allowedFileTypes = ['application/pdf'];
+    this.file = event.target.files[0];
 
     if (!allowedFileTypes.includes(event.target.files[0].type)) {
       this._docUploadService.openErrorModal("Invalid File Type", "Please select a .pdf only.");
       return;
-    }
+    };
 
-    if (event.target.files[0]) {
-      this.file = event.target.files[0];
+    // Check if file bypasses size limit.
+    const fileSizeInKB = this.file.size / 1024;
+    this.byPassedLimits = this._checkSizeLimit(fileSizeInKB, 'sticker');
+
+    if (this.file && !this.byPassedLimits.length) {
       this.isDocLoading = true;
-    } else {
-      this.docLink = this.defaultLink;
-    }
-  
-    this.isDocLoading= true;
 
-    const docFilePath = `docs/${this.file.name}_${new Date().getTime()}`;
-    const response = await this._docUploadService.uploadSingleFile(this.file, docFilePath)
-    this._sBs.sink = response.subscribe(url => this._autofillDocUrl(url))
+      //Step 1 - Create the file path that will be in firebase storage
+      const docFilePath = `docs/${this.file.name}_${new Date().getTime()}`;
+
+      //Step 2 - Upload file to firestore
+      const response = await this._docUploadService.uploadSingleFile(this.file, docFilePath);
+
+      //Step 3 - PatchValue to Block
+      this._sBs.sink = response.subscribe(url => this._autofillDocUrl(url));
+    }
+  }
+
+  private _checkSizeLimit(size:number, type:string) {
+    return this._docUploadService.checkFileSizeLimits(size, type)
   }
 
   private _autofillDocUrl(url: any) {
