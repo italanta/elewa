dcede68a8d54b909d137773b801ae5a028f47b8f
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-01 12:53:32 +0300

    :recycle: refactor the sticker block to use the new filestorage service

--- libs/features/convs-mgr/stories/blocks/library/sticker-message-block/src/lib/components/sticker-block/sticker-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/sticker-message-block/src/lib/components/sticker-block/sticker-block.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import { take } from 'rxjs';
+import { SubSink } from 'subsink';
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
+import { FileStorageService } from '@app/state/file';
 import { StickerMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
-
-import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
-import { UploadFileService } from '@app/state/file';
-
 
 @Component({
   selector: 'app-sticker-block',
   templateUrl: './sticker-block.component.html',
   styleUrls: ['./sticker-block.component.scss'],
 })
-
-export class StickerBlockComponent implements OnInit {
-
+export class StickerBlockComponent implements OnInit, OnDestroy {
   @Input() id: string;
   @Input() block: StickerMessageBlock;
   @Input() stickerMessageForm: FormGroup;
@@ -24,36 +21,39 @@ export class StickerBlockComponent implements OnInit {
 
   file: File;
   isLoadingSticker: boolean;
-  isStickerLoaded: string;
-  stickerLink = "";
   stickerInputId: string;
-  defaultImage = "assets/images/lib/block-builder/sticker-block-placeholder.png"
 
-  type: StoryBlockTypes;
-  stickerType = StoryBlockTypes.Sticker;
-  blockFormGroup: FormGroup;
+  private _sBs = new SubSink();
 
+  constructor(private _stickerUploadService: FileStorageService) {}
 
-  constructor(private _stickerUploadService: UploadFileService) 
-  { }
-
-  ngOnInit(): void
-  {
-    this.stickerInputId = `stckr-${this.id}`
+  ngOnInit(): void {
+    this.stickerInputId = `stckr-${this.id}`;
   }
 
-  async processSticker(event: any) 
-  {
-    if (event.target.files && event.target.files[0]) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => this.stickerLink = e.target.result;
-      reader.readAsDataURL(event.target.files[0]);
-      this.file = event.target.files[0];
-    } else {
-      this.stickerLink = this.defaultImage;
+  async processSticker(event: any) {
+    this.file = event.target.files[0];
+
+    if (this.file) {
+      this.isLoadingSticker = true;
+
+      //Step 1 - Create the file path that will be in firebase storage
+      const stickerFilePath = `stickers/${this.file.name}_${new Date().getTime()}`;
+
+      //Step 2 - Upload file to firestore
+      const response = await this._stickerUploadService.uploadSingleFile(this.file, stickerFilePath);
+
+      //Step 3 - PatchValue to Block
+      this._sBs.sink = response.pipe(take(1)).subscribe((url) => this._autofillUrl(url));
     }
-    this.isLoadingSticker = true;
-    const stickerFilePath = `stickers/${this.file.name}_${new Date().getTime()}`;
-    (await this._stickerUploadService.uploadFile(this.file, this.block, stickerFilePath)).subscribe();
+  };
+
+  private _autofillUrl(url: string) {
+    this.stickerMessageForm.patchValue({ fileSrc: url });
+    this.isLoadingSticker = false;
+  };
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
   }
 }
