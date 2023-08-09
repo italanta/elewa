22f9591b9fb4f379033942c153234aea2d3b4b63
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-08-09 15:40:11 +0300

    :package: Added the single learner page component

---
+++ libs/features/convs-mgr/learners/src/lib/pages/single-learner-page/single-learner-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-learner-page',
+  templateUrl: './single-learner-page.component.html',
+  styleUrls: ['./single-learner-page.component.scss'],
+})
+export class SingleLearnerPageComponent {}
