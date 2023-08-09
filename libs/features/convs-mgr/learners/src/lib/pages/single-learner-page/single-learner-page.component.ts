f1d31a03726334e8abdadad86b62b9572c5f20f9
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-08-09 15:50:49 +0300

    :sparkles: Added the single learner page details

--- libs/features/convs-mgr/learners/src/lib/pages/single-learner-page/single-learner-page.component.ts
+++ libs/features/convs-mgr/learners/src/lib/pages/single-learner-page/single-learner-page.component.ts
@@ -1,8 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SubSink } from 'subsink';
+
+import { EnrolledLearnersService } from '@app/state/convs-mgr/learners';
+import { switchMap } from 'rxjs';
+import { EnrolledEndUser } from '@app/model/convs-mgr/learners';
 
 @Component({
   selector: 'app-single-learner-page',
   templateUrl: './single-learner-page.component.html',
   styleUrls: ['./single-learner-page.component.scss'],
 })
-export class SingleLearnerPageComponent {}
+export class SingleLearnerPageComponent implements OnInit, OnDestroy {
+  constructor(
+    private _route: ActivatedRoute,
+    private _elearners: EnrolledLearnersService
+  ) {}
+
+  private _sBS = new SubSink();
+  currentLearner!: EnrolledEndUser;
+
+  ngOnInit() {
+    this._sBS.sink = this._route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const id = params.get('id') as string;
+          return this._elearners.getSpecificLearner$(id);
+        })
+      )
+      .subscribe(learner => {
+        this.currentLearner = learner as EnrolledEndUser
+        console.log(this.currentLearner)
+        return this.currentLearner
+      });
+  }
+
+  getBreadcrumbTitle() {
+    return this.currentLearner.name || this.currentLearner.phoneNumber || this.currentLearner.id
+  }
+
+  ngOnDestroy() {
+    this._sBS.unsubscribe();
+  }
+}
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SubSink } from 'subsink';
+
+import { EnrolledLearnersService } from '@app/state/convs-mgr/learners';
+import { switchMap } from 'rxjs';
+import { EnrolledEndUser } from '@app/model/convs-mgr/learners';
+
+@Component({
+  selector: 'app-single-learner-page',
+  templateUrl: './single-learner-page.component.html',
+  styleUrls: ['./single-learner-page.component.scss'],
+})
+export class SingleLearnerPageComponent implements OnInit, OnDestroy {
+  constructor(
+    private _route: ActivatedRoute,
+    private _elearners: EnrolledLearnersService
+  ) {}
+
+  private _sBS = new SubSink();
+  currentLearner!: EnrolledEndUser;
+
+  ngOnInit() {
+    this._sBS.sink = this._route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const id = params.get('id') as string;
+          return this._elearners.getSpecificLearner$(id);
+        })
+      )
+      .subscribe(learner => {
+        this.currentLearner = learner as EnrolledEndUser
+        console.log(this.currentLearner)
+        return this.currentLearner
+      });
+  }
+
+  getBreadcrumbTitle() {
+    return this.currentLearner.name || this.currentLearner.phoneNumber || this.currentLearner.id
+  }
+
+  ngOnDestroy() {
+    this._sBS.unsubscribe();
+  }
+}
