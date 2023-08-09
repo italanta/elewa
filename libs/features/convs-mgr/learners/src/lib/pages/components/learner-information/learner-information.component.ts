75f91fea3b5df7abb70cbabaf22b117d7178be05
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-08-09 16:01:20 +0300

    :package: Added the learner information component for the single learner information page

---
+++ libs/features/convs-mgr/learners/src/lib/pages/components/learner-information/learner-information.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learner-information',
+  templateUrl: './learner-information.component.html',
+  styleUrls: ['./learner-information.component.scss'],
+})
+export class LearnerInformationComponent {}
