1dc26875bb02ec05c6b67e62f9236c424dc3adc5
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-08-09 16:33:34 +0300

    :package: Added the Learner's assessment history and connected it to  the single learner page

---
+++ libs/features/convs-mgr/learners/src/lib/components/learner-assessment-history/learner-assessment-history.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learner-assessment-history',
+  templateUrl: './learner-assessment-history.component.html',
+  styleUrls: ['./learner-assessment-history.component.scss'],
+})
+export class LearnerAssessmentHistoryComponent {}
