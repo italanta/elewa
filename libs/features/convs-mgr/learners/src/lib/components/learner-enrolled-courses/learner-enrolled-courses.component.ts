9a78224c250918ca8376cd7ed5af635b83f79778
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-08-09 16:28:27 +0300

    :package: Added the Learner's enrolled courses component for the single learner page

---
+++ libs/features/convs-mgr/learners/src/lib/components/learner-enrolled-courses/learner-enrolled-courses.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learner-enrolled-courses',
+  templateUrl: './learner-enrolled-courses.component.html',
+  styleUrls: ['./learner-enrolled-courses.component.scss'],
+})
+export class LearnerEnrolledCoursesComponent {}
