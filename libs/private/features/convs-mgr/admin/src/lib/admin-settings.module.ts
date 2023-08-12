79ef4e62b65dced94b65b4c54de2ca3feeb7e89c
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-12 18:56:21 +0300

    :sparkles: feat: setup mat-table tabs on main-profile tabs

--- libs/private/features/convs-mgr/admin/src/lib/admin-settings.module.ts
+++ libs/private/features/convs-mgr/admin/src/lib/admin-settings.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {
+  MaterialBricksModule,
+  MaterialDesignModule,
+} from '@iote/bricks-angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
 
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
@@ -8,7 +14,14 @@ import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { AdminSettingsRouterModule } from './admin.routing';
 
 @NgModule({
-  imports: [CommonModule, AdminSettingsRouterModule, ConvlPageModule],
+  imports: [
+    CommonModule,
+    AdminSettingsRouterModule,
+    ConvlPageModule,
+    MaterialBricksModule,
+    MaterialDesignModule,
+    MultiLangModule,
+  ],
   declarations: [AdminPageComponent],
 })
 export class AdminSettingsModule {}
