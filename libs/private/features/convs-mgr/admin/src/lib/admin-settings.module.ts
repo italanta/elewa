b439f79655073a80ee877953c1a7f04a6f6276a4
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-12 14:48:59 +0300

    :sparkles: feat: add admin settings routing

--- libs/private/features/convs-mgr/admin/src/lib/admin-settings.module.ts
+++ libs/private/features/convs-mgr/admin/src/lib/admin-settings.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
+import { AdminSettingsRouterModule } from './admin.routing';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AdminSettingsRouterModule],
   declarations: [AdminPageComponent],
 })
 export class AdminSettingsModule {}
