a23a946b2e4b5258149407491c7e578678b57c81
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-12 19:56:47 +0300

    :art: style: add default ui elements for the teams-settings section

--- libs/private/features/convs-mgr/admin/src/lib/components/teams-settings/teams-settings.component.ts
+++ libs/private/features/convs-mgr/admin/src/lib/components/teams-settings/teams-settings.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-teams-settings',
   templateUrl: './teams-settings.component.html',
   styleUrls: ['./teams-settings.component.scss'],
 })
-export class TeamsSettingsComponent {}
+export class TeamsSettingsComponent {
+  displayedColumns = ['logo', 'name', 'email', 'status', 'role', 'actions'];
+  dataSource = new MatTableDataSource();
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
+
+  sortData(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction} ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+}
