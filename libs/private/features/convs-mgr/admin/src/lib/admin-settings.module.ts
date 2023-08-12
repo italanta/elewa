4bc4313c71b38052da2500eed9aae022a53698e0
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-12 18:30:48 +0300

    :sparkles: feat: wrap page with convl-page component

--- libs/private/features/convs-mgr/admin/src/lib/admin-settings.module.ts
+++ libs/private/features/convs-mgr/admin/src/lib/admin-settings.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ConvlPageModule } from '@app/elements/layout/page-convl';
+
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 import { AdminSettingsRouterModule } from './admin.routing';
 
 @NgModule({
-  imports: [CommonModule, AdminSettingsRouterModule],
+  imports: [CommonModule, AdminSettingsRouterModule, ConvlPageModule],
   declarations: [AdminPageComponent],
 })
 export class AdminSettingsModule {}
