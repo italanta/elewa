dd969bc770d98ceb73ccde605e116271d3906afa
japhethnyaranga <nyaranga4@gmail.com>
2023-08-14 15:55:01 +0300

    💄 created cmi5-block component

---
+++ libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/components/cmi5-block/cmi5-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cmi5-block',
+  templateUrl: './cmi5-block.component.html',
+  styleUrls: ['./cmi5-block.component.scss'],
+})
+export class Cmi5BlockComponent {}
--- libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/libs-private-features-convs-mgr-stories-blocks-library-cmi5-block.module.ts
+++ libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/libs-private-features-convs-mgr-stories-blocks-library-cmi5-block.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Cmi5BlockComponent } from './components/cmi5-block/cmi5-block.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [Cmi5BlockComponent],
 })
 export class LibsPrivateFeaturesConvsMgrStoriesBlocksLibraryCmi5BlockModule {}
