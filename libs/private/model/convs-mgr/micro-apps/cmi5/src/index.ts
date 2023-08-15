83c8d2ba4327760c5931b16e68aa245129598546
Reagan Charana <rgncharana2@gmail.com>
2023-08-15 14:45:57 +0300

    :truck: export context template interface

--- libs/private/model/convs-mgr/micro-apps/cmi5/src/index.ts
+++ libs/private/model/convs-mgr/micro-apps/cmi5/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/assignable-unit.interface';
 export * from './lib/context-template.interface';
 export * from './lib/launch-data.interface';
 export * from './lib/session.interface';
+export * from './lib/context-template.interface';
