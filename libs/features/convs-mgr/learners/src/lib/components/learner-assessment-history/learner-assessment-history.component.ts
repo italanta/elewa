a0b74fee9c1c4870371d61d2f9edc2cbb7391c37
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-08-15 13:03:00 +0300

    :sparkles: Added the assessment input to the component

--- libs/features/convs-mgr/learners/src/lib/components/learner-assessment-history/learner-assessment-history.component.ts
+++ libs/features/convs-mgr/learners/src/lib/components/learner-assessment-history/learner-assessment-history.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { AssessmentCursor } from '@app/model/convs-mgr/conversations/admin/system';
 
 @Component({
   selector: 'app-learner-assessment-history',
   templateUrl: './learner-assessment-history.component.html',
   styleUrls: ['./learner-assessment-history.component.scss'],
 })
-export class LearnerAssessmentHistoryComponent {
+export class LearnerAssessmentHistoryComponent implements OnInit{
+  @Input() assessments: AssessmentCursor[];
+
   displayedColumns: string[] = ['Assessment Name', 'Score', 'Duration Taken', 'Date Taken'];
-  dataSource = [];
+  dataSource: AssessmentCursor[]= [];
+  
+  ngOnInit() {
+    this.dataSource = this.assessments;
+  }
 }
