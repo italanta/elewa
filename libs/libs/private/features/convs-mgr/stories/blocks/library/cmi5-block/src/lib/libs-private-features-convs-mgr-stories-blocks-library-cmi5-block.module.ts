ccb48b04bf5c6fb22fc3304cefc190e37078422f
japhethnyaranga <nyaranga4@gmail.com>
2023-08-15 12:44:00 +0300

    💄 changed module name for cmi5 module

--- libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/libs-private-features-convs-mgr-stories-blocks-library-cmi5-block.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Cmi5BlockComponent } from './components/cmi5-block/cmi5-block.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [Cmi5BlockComponent],
-})
-export class LibsPrivateFeaturesConvsMgrStoriesBlocksLibraryCmi5BlockModule {}
