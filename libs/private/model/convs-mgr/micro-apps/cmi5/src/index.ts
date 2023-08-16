d4c4729a2d7393a415beb159aba831f18b964128
Reagan Charana <rgncharana2@gmail.com>
2023-08-16 12:04:54 +0300

    :truck: export interfaces

--- libs/private/model/convs-mgr/micro-apps/cmi5/src/index.ts
+++ libs/private/model/convs-mgr/micro-apps/cmi5/src/index.ts
@@ -4,3 +4,5 @@ export * from './lib/launch-data.interface';
 export * from './lib/session.interface';
 export * from './lib/context-template.interface';
 export * from './lib/actor.interface';
+export * from './lib/xAPI-statement.interface';
+export * from './lib/au-status-types.enum';
