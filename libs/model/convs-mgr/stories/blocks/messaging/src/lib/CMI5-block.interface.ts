725315ddc73b521f41f9e85332c45896363c66a7
japhethnyaranga <nyaranga4@gmail.com>
2023-08-16 09:36:42 +0300

    added other fields to the cmi5 interface

--- libs/model/convs-mgr/stories/blocks/messaging/src/lib/CMI5-block.interface.ts
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/CMI5-block.interface.ts
@@ -1,9 +1,24 @@
 import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+import { ButtonsBlockButton } from "@app/model/convs-mgr/stories/blocks/scenario";
 
 export interface CMI5Block extends StoryBlock
 {
 
   /** Once the zip file has been uploaded, we get the generated id of the course and save it here. */
   courseId?: string;
+
+  defaultTarget?: string;
+
+  /** Response options */
+  options?: ButtonsBlockButton<Button>[];
+
+  fileSrc?: string;
   
+}
+interface Button {
+  id: string;
+  /** Message to display as answer */
+  message: string;
+  /** Value the answer holds. */
+  value?: string;
 }
