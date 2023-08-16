10955dbf199fbe85d591f329772966f9479026f0
Reagan Charana <rgncharana2@gmail.com>
2023-08-16 10:03:56 +0300

    :wrench: :sparkles: create session service

---
+++ libs/private/functions/micro-apps/cmi5/src/lib/session.service.ts
@@ -0,0 +1,55 @@
+import { randomUUID } from "crypto";
+
+import { HandlerTools } from "@iote/cqrs";
+import { Query } from "@ngfi/firestore-qbuilder";
+
+import { AUStatus, LMSLaunchData, LearnerSession } from "@app/private/model/convs-mgr/micro-apps/cmi5";
+
+export class LearnerSessionService {
+  learnerSession: LearnerSession;
+
+  constructor(private tools: HandlerTools) { }
+
+  public generateSessionID() { 
+    this.learnerSession.id = randomUUID().slice(0, 22);
+
+    return this.learnerSession.id;
+  }
+
+  public async getSession(orgId: string, auId: string, sessionID: string) { 
+    const courseId = auId.split('/')[0];
+
+    const sessionRepo$ = this.tools.getRepository<LearnerSession>(`orgs/${orgId}/course-packages/${courseId}/sessions`);
+
+    const session = await sessionRepo$.getDocumentById(sessionID);
+
+    if (session) { 
+      return session;
+    } else {
+      this.tools.Logger.warn(() => `No session found for AU :: ${auId}`);
+
+      return null;
+    }
+  }
+
+  public create(orgId: string, endUserId: string, auId: string, state: LMSLaunchData) {
+    const courseId = auId.split('_')[0];
+
+    const sessionRepo$ = this.tools.getRepository<LearnerSession>(`orgs/${orgId}/course-packages/${courseId}/sessions`);
+
+    // Create session object
+    this.learnerSession = {
+      ...this.learnerSession,
+      currentUnit: auId,
+      stateData: state,
+      start: new Date(),
+      learnerId: endUserId,
+      courseId: courseId,
+
+      // Set status to launched
+      status: AUStatus.Launched
+    }
+
+    return sessionRepo$.create(this.learnerSession);
+  }
+}
