c52478deab3e69debbac8b92fc82a7432906f069
Reagan Charana <rgncharana2@gmail.com>
2023-08-16 10:09:05 +0300

    :construction: create cmi5 listener

--- libs/private/functions/micro-apps/cmi5/src/lib/cmi5-listener.handler.ts
+++ libs/private/functions/micro-apps/cmi5/src/lib/cmi5-listener.handler.ts
@@ -1,3 +1,15 @@
-export function privateFunctionsMicroAppsCmi5(): string {
-  return 'private-functions-micro-apps-cmi5';
+import { HandlerTools } from '@iote/cqrs';
+
+import { FunctionHandler, RestResult200, FunctionContext } from '@ngfi/functions';
+
+export class CMI5Listener extends FunctionHandler<any, RestResult200>
+{
+  /**
+   * Put a break on execution and halt the system to talk to a Human agent. */
+  public async execute(req: any, context: FunctionContext, tools: HandlerTools)
+  {
+
+    return {success: true} as RestResult200;
+  }
+
 }
