ded8c70d52729c91097415e78df64e7b39e46018
japhethnyaranga <nyaranga4@gmail.com>
2023-08-17 11:02:59 +0300

    imported take and subsink for the component and declared them , then added has document boolen value to check if file is available

--- libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/components/cmi5-block/cmi5-block.component.ts
+++ libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/components/cmi5-block/cmi5-block.component.ts
@@ -3,9 +3,11 @@
 // "Since the xAPI specification is highly generalized to support many different use cases, a set of “extra rules” (called a “profile”) is needed to ensure interoperability for a given use case. The cmi5 profile ensures plug and play interoperability between learning content and LMS systems."
 
 // and this: @see https://aicc.github.io/CMI-5_Spec_Current/flows/cmi5-overview.html
-
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+import { take } from 'rxjs';
+import { SubSink } from 'subsink';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 import { TranslateService } from '@ngfi/multi-lang';
@@ -14,17 +16,12 @@ import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 import { CMI5Block } from '@app/model/convs-mgr/stories/blocks/messaging';
 import { FileStorageService } from '@app/state/file';
 
-
-
-
-
 @Component({
   selector: 'app-cmi5-block',
   templateUrl: './cmi5-block.component.html',
   styleUrls: ['./cmi5-block.component.scss'],
 })
 export class Cmi5BlockComponent implements OnInit {
- 
   @Input() id: string;
   @Input() block: CMI5Block;
   @Input() CMI5BlockForm: FormGroup;
@@ -33,70 +30,72 @@ export class Cmi5BlockComponent implements OnInit {
   file: File;
   courseId: string;
   docName = '';
-  defaultLink = "";
+  defaultLink = '';
   isDocLoading = false;
-  docLink: string =  this.defaultLink;
-  
+  docLink: string = this.defaultLink;
   type: StoryBlockTypes;
   documentType = StoryBlockTypes.Document;
+  hasDocument = false;
 
+  private _sBs = new SubSink(); //SubSink instance
 
   optionClass: string;
   CMI5BlockOptions: any[];
 
-  
   jumpType = StoryBlockTypes.JumpBlock;
   blockFormGroup: FormGroup;
 
   constructor(
-    private _docUploadService: FileStorageService,
+    private _cmi5UploadService: FileStorageService,
     private _fb: FormBuilder,
-     private _translate: TranslateService,){ }
+    private _translate: TranslateService
+  ) {}
 
-  ngOnInit(): void 
-  {
+  ngOnInit(): void {
     this.courseId = `docs-${this.id}`;
     this.setCMI5BlockOptions();
   }
 
-  get options(): FormArray
-  {
+  get options(): FormArray {
     return this.CMI5BlockForm.controls['options'] as FormArray;
   }
 
-
-  addCMI5Options(option?: any)
-  {
+  addCMI5Options(option?: any) {
     return this._fb.group({
       id: [option?.id ?? `${this.id}-${this.options.length + 1}`],
       message: [option?.message ?? ''],
-      value: [option?.value ?? '']
+      value: [option?.value ?? ''],
     });
   }
 
-  setCMI5BlockOptions()
-  {
-    this.CMI5BlockOptions = [{
-      message: this._translate.translate("PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.SUCCESS"),
-      value: "success"
-    },
-    {
-      message: this._translate.translate("PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.FAILED"),
-      value: "failed"
-    }
+  setCMI5BlockOptions() {
+    this.CMI5BlockOptions = [
+      {
+        message: this._translate.translate(
+          'PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.SUCCESS'
+        ),
+        value: 'success',
+      },
+      {
+        message: this._translate.translate(
+          'PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.FAILED'
+        ),
+        value: 'failed',
+      },
     ];
     this.CMI5BlockOptions.forEach((option) => {
-    this.options.push(this.addCMI5Options(option));
-  });
+      this.options.push(this.addCMI5Options(option));
+    });
   }
 
-  async processDocs(event: any)
-  {   
-
+  async processDocs(event: any) {
     const allowedFileTypes = ['application/zip'];
 
     if (!allowedFileTypes.includes(event.target.files[0].type)) {
-      this._docUploadService.openErrorModal("Invalid File Type", "Please select a .zip only.");
+      this._cmi5UploadService.openErrorModal(
+        'Invalid File Type',
+        'Please select a .zip only.'
+      );
       return;
     }
 
@@ -105,16 +104,11 @@ export class Cmi5BlockComponent implements OnInit {
       this.isDocLoading = true;
     } else {
       this.docLink = this.defaultLink;
+      this.isDocLoading = true;
     }
-  
-    this.isDocLoading= true;
   }
 
   private _autofillDocUrl(url: any) {
     this.isDocLoading = false;
   }
-
 }
-
-
-
