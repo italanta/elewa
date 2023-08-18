afc20068084c5ab806f30df50c6de4f691f0fc02
japhethnyaranga <nyaranga4@gmail.com>
2023-08-18 12:54:12 +0300

    Exported CMI5 service

--- libs/state/file/store/src/index.ts
+++ libs/state/file/store/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/model/file-upload.interface';
 export * from './lib/providers/file-storage.service';
 export * from  './lib/providers/upload-file.service';
 export * from './lib/file-state.module';
+export * from './lib/providers/cmi5-block.service';
