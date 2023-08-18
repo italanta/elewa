25185c19df038662417d2b567f822ac19506cd0d
japhethnyaranga <nyaranga4@gmail.com>
2023-08-18 13:04:31 +0300

    injected angularFirefunctions , UserStore, filestorageService and matDialog to the service

--- libs/state/file/store/src/lib/providers/cmi5-block.service.ts
+++ libs/state/file/store/src/lib/providers/cmi5-block.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AngularFireFunctions } from '@angular/fire/compat/functions';
+
+import { from, switchMap } from 'rxjs';
+
+import { UserStore } from '@app/state/user';
+
+import { FileStorageService } from './file-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class CMI5BlockService {
-  constructor() {}
+  constructor(
+    private _aFF: AngularFireFunctions,
+    private authService: UserStore,
+    private fileStorageService: FileStorageService,
+    private dialog: MatDialog
+  ) {}
+
+
+
+ 
 }
