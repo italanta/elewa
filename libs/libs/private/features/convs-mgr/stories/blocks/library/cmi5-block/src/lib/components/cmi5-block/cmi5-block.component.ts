597f0ad7529eaca1c22d823550dfccacde9a581d
japhethnyaranga <nyaranga4@gmail.com>
2023-08-18 12:58:02 +0300

    imported CMI5 block service and obtained value of active organization id

--- libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/components/cmi5-block/cmi5-block.component.ts
+++ libs/libs/private/features/convs-mgr/stories/blocks/library/cmi5-block/src/lib/components/cmi5-block/cmi5-block.component.ts
@@ -14,98 +14,111 @@ import { TranslateService } from '@ngfi/multi-lang';
 
 import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 import { CMI5Block } from '@app/model/convs-mgr/stories/blocks/messaging';
-import { FileStorageService } from '@app/state/file';
+import { CMI5BlockService } from '@app/state/file';
+import { ActiveOrgStore } from '@app/state/organisation';
 
 @Component({
-selector: 'app-cmi5-block',
-templateUrl: './cmi5-block.component.html',
-styleUrls: ['./cmi5-block.component.scss'],
+  selector: 'app-cmi5-block',
+  templateUrl: './cmi5-block.component.html',
+  styleUrls: ['./cmi5-block.component.scss'],
 })
-export class Cmi5BlockComponent implements OnInit{
-@Input() id: string;
-@Input() block: CMI5Block;
-@Input() CMI5BlockForm: FormGroup;
-@Input() jsPlumb: BrowserJsPlumbInstance;
+export class Cmi5BlockComponent implements OnInit, OnDestroy {
+  @Input() id: string;
+  @Input() block: CMI5Block;
+  @Input() CMI5BlockForm: FormGroup;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
 
-file: File;
-courseId: string;
-docName = '';
-defaultLink = '';
-isDocLoading = false;
-docLink: string = this.defaultLink;
-type: StoryBlockTypes;
-documentType = StoryBlockTypes.Document;
-hasDocument = false;
+  orgId: string;
+  file: File;
+  courseId: string;
+  docName = '';
+  defaultLink = '';
+  isDocLoading = false;
+  docLink: string = this.defaultLink;
+  type: StoryBlockTypes;
+  documentType = StoryBlockTypes.Document;
+  hasDocument = false;
 
-private _sBs = new SubSink(); //SubSink instance
+  private _sBs = new SubSink(); //SubSink instance
 
-optionClass: string;
-CMI5BlockOptions: any[];
+  optionClass: string;
+  CMI5BlockOptions: any[];
 
-jumpType = StoryBlockTypes.JumpBlock;
-blockFormGroup: FormGroup;
+  jumpType = StoryBlockTypes.JumpBlock;
+  blockFormGroup: FormGroup;
 
-constructor(
-private _cmi5UploadService: FileStorageService,
-private _fb: FormBuilder,
-private _translate: TranslateService
-) {}
+  constructor(
+    private _cmi5UploadService: CMI5BlockService,
+    private _fb: FormBuilder,
+    private _translate: TranslateService,
+    private _activeOrgStore: ActiveOrgStore
+  ) {}
 
-ngOnInit(): void {
-this.courseId = `docs-${this.id}`;
-this.setCMI5BlockOptions();
-}
+  ngOnInit(): void {
+    this._sBs.sink = this._activeOrgStore.get().subscribe((activeOrg) => {
+      if (activeOrg) {
+        this.orgId = activeOrg.id as string;
+      }
+    });
+    this.courseId = `docs-${this.id}`;
+    this.setCMI5BlockOptions();
+  }
 
-get options(): FormArray {
-return this.CMI5BlockForm.controls['options'] as FormArray;
-}
+  get options(): FormArray {
+    return this.CMI5BlockForm.controls['options'] as FormArray;
+  }
 
-addCMI5Options(option?: any) {
-return this._fb.group({
-id: [option?.id ?? `${this.id}-${this.options.length + 1}`],
-message: [option?.message ?? ''],
-value: [option?.value ?? ''],
-});
-}
+  addCMI5Options(option?: any) {
+    return this._fb.group({
+      id: [option?.id ?? `${this.id}-${this.options.length + 1}`],
+      message: [option?.message ?? ''],
+      value: [option?.value ?? ''],
+    });
+  }
 
-setCMI5BlockOptions() {
-this.CMI5BlockOptions = [
-{
-message: this._translate.translate(
-'PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.SUCCESS'
-),
-value: 'success',
-},
-{
-message: this._translate.translate(
-'PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.FAILED'
-),
-value: 'failed',
-},
-];
-this.CMI5BlockOptions.forEach((option) => {
-this.options.push(this.addCMI5Options(option));
-});
-}
+  setCMI5BlockOptions() {
+    this.CMI5BlockOptions = [
+      {
+        message: this._translate.translate(
+          'PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.SUCCESS'
+        ),
+        value: 'success',
+      },
+      {
+        message: this._translate.translate(
+          'PAGE-CONTENT.BLOCK.BUTTONS.JUMP-BLOCK.FAILED'
+        ),
+        value: 'failed',
+      },
+    ];
+    this.CMI5BlockOptions.forEach((option) => {
+      this.options.push(this.addCMI5Options(option));
+    });
+  }
 
-async processDocs(event: any) {
-const allowedFileTypes = ['application/zip'];
+  async processDocs(event: any) {
+    const allowedFileTypes = ['application/zip'];
 
-if (!allowedFileTypes.includes(event.target.files[0].type)) {
-this._cmi5UploadService.openErrorModal(
-   this._translate.translate("PAGE-CONTENT.BLOCK.UPLOAD-WARNING.CMI5-BLOCK.INVALID-FILE-TYPE"),
-   this._translate.translate("PAGE-CONTENT.BLOCK.UPLOAD-WARNING.CMI5-BLOCK.PLEASE-SELECT-ZIP-ONLY"),
-   
-);
-return;
-}
+    if (!allowedFileTypes.includes(event.target.files[0].type)) {
+      this._cmi5UploadService.openDialog(
+        this._translate.translate(
+          'PAGE-CONTENT.BLOCK.UPLOAD-WARNING.CMI5-BLOCK.INVALID-FILE-TYPE'
+        ),
+        this._translate.translate(
+          'PAGE-CONTENT.BLOCK.UPLOAD-WARNING.CMI5-BLOCK.PLEASE-SELECT-ZIP-ONLY'
+        )
+      );
+      return;
+    }
 
-if (event.target.files[0]) {
-this.file = event.target.files[0];
-this.isDocLoading = true;
-} else {
-this.docLink = this.defaultLink;
-}
-}
+    if (event.target.files[0]) {
+      this.file = event.target.files[0];
+      this.isDocLoading = true;
+
+
+    } else {
+      this.docLink = this.defaultLink;
+    }
+  }
 
 }
