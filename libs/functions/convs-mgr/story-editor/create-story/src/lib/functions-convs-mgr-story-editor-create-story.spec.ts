f0095f23c580b7ef44e44ab1a5be1a886d36ef3d
japhethnyaranga <nyaranga4@gmail.com>
2023-08-24 15:37:00 +0300

    created a node library for creatingStory

---
+++ libs/functions/convs-mgr/story-editor/create-story/src/lib/functions-convs-mgr-story-editor-create-story.spec.ts
@@ -0,0 +1,9 @@
+import { functionsConvsMgrStoryEditorCreateStory } from './functions-convs-mgr-story-editor-create-story';
+
+describe('functionsConvsMgrStoryEditorCreateStory', () => {
+  it('should work', () => {
+    expect(functionsConvsMgrStoryEditorCreateStory()).toEqual(
+      'functions-convs-mgr-story-editor-create-story'
+    );
+  });
+});
