1e601df9cb0a0be2517356f5e17878df8db72639
japhethnyaranga <nyaranga4@gmail.com>
2023-08-25 15:40:11 +0300

    removed some files

--- libs/functions/convs-mgr/story-editor/create-story/src/lib/functions-convs-mgr-story-editor-create-story.spec.ts
+++
@@ -1,9 +0,0 @@
-import { functionsConvsMgrStoryEditorCreateStory } from './functions-convs-mgr-story-editor-create-story';
-
-describe('functionsConvsMgrStoryEditorCreateStory', () => {
-  it('should work', () => {
-    expect(functionsConvsMgrStoryEditorCreateStory()).toEqual(
-      'functions-convs-mgr-story-editor-create-story'
-    );
-  });
-});
