c021afd152416f681c75b795e97dec43cef51419
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-08-31 12:29:47 +0300

    :art: home summaries

---
+++ libs/features/convs-mgr/home/src/lib/components/home-summaries/home-summaries.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'italanta-apps-home-summaries',
+  templateUrl: './home-summaries.component.html',
+  styleUrls: ['./home-summaries.component.scss'],
+})
+export class HomeSummariesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
