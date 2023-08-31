5e2bc0c516591f734fde9f8d334ea9bf4f3ad2e3
japhethnyaranga <nyaranga4@gmail.com>
2023-08-31 10:02:56 +0300

    Added comment to show what the function does

--- apps/conv-lm-backend/src/app/story/create-Story.function.ts
+++ apps/conv-lm-backend/src/app/story/create-Story.function.ts
@@ -4,6 +4,7 @@ import { SaveStoryHandler } from "@app/functions/convs-mgr/story-editor/create-s
 
 import { ConvLearnFunction } from "../../conv-learn-func.class";
 
+// this is a backend function that is used to save blocks and connections to firebase firestore database storage
 const saveStoryHandler = new SaveStoryHandler();
 
 
