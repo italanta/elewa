e1ad1ec1113697d58cfd4623ca6499ee8d736166
japhethnyaranga <nyaranga4@gmail.com>
2023-09-06 10:54:01 +0300

    Removed extra spaces form the class

--- libs/functions/bot-engine/main/src/lib/services/process-operation-block/block-type/cmi5-block.service.ts
+++ libs/functions/bot-engine/main/src/lib/services/process-operation-block/block-type/cmi5-block.service.ts
@@ -13,14 +13,9 @@ import { BlockDataService } from '../../data-services/blocks.service';
 import { ConnectionsDataService } from '../../data-services/connections.service';
 import { IProcessOperationBlock } from '../models/process-operation-block.interface';
 
-
-
-
-
 export class CMI5BackendBlockService implements IProcessOperationBlock {
   // Operations service(firstblock.id)
   private cmi5Service: CMI5Service; // Create an instance of the CMI5Service
-
   /**
    * Creates an instance of CMI5BackendBlockService.
    * @param _blockDataService The data service for handling blocks.
@@ -35,9 +30,7 @@ export class CMI5BackendBlockService implements IProcessOperationBlock {
   {
     this.cmi5Service = new CMI5Service(tools); 
   }
-
   sideOperations: Promise<any>[];
-
   /**
    * Handles a CMI5 block and prepares it for launch.
    * @param storyBlock The CMI5 block to handle.
@@ -46,7 +39,6 @@ export class CMI5BackendBlockService implements IProcessOperationBlock {
    * @param endUser The end user for whom the block is being handled.
    * @returns An object containing the launch link and the new cursor.
    */
-
   async handleBlock(
     storyBlock: CMI5Block,
     updatedCursor: Cursor,
@@ -54,25 +46,18 @@ export class CMI5BackendBlockService implements IProcessOperationBlock {
     endUser: EndUser
   ) {
     try {
-   
       // Fetch the details of the first AU
       const firstAUs = await this.getAssignableUnits(orgId, storyBlock.id);
- 
-
       if (firstAUs.length === 0) {
         console.error('No assignable units found.');
         return;
       }
-
       const firstAU = firstAUs[0];
       // Assuming you want the first item in the array
       const firstAULocationURL = firstAU.urlPath;
-
       const firstAUId = firstAU.id;
-
       // Prepare the AU for launch
       await this.cmi5Service.prepareForLaunch(orgId, endUser.id, firstAUId);
-
       // Define the actor properties here
       const actor: Actor = {
         objectType: 'Agent',
@@ -82,7 +67,6 @@ export class CMI5BackendBlockService implements IProcessOperationBlock {
           name: endUser.name,
         },
       };
-
       // Generate the launch link using the AU details and actor properties
       const launchLink = this.cmi5Service.createLaunchURL(
         firstAULocationURL, // Use firstAULocationURL
@@ -90,33 +74,24 @@ export class CMI5BackendBlockService implements IProcessOperationBlock {
         endUser.id,
         firstAUId
       );
-
       const launchBlock : CMI5LaunchBlock = {
         link: launchLink,   
       }
-
-
       const response = {
         storyBlock: launchBlock,
         newCursor: updatedCursor,
       };
-
       return response;
-
-
     } catch (error) {
       console.error('Error handling CMI5 block:', error);
     }
   }
-
-
    /**
    * Retrieves a list of assignable units for a given organization and course ID.
    * @param orgId The organization ID.
    * @param courseId The course ID.
    * @returns A Promise that resolves to an array of AssignableUnit objects.
    */
-
     private async getAssignableUnits(
       orgId: string,
       courseId: string
