5f79799b2d99f2511547279d4f8d39b60d36d2f2
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-07 11:54:40 +0300

    :fire:  Removed spec file in the flow error lib

--- libs/functions/convs-mgr/story-editor/check-story-for-errors/src/lib/functions-convs-mgr-story-editor--check-story-for-errors.spec.ts
+++
@@ -1,9 +0,0 @@
-import { functionsConvsMgrStoryEditorCheckStoryForErrors } from './functions-convs-mgr-story-editor--check-story-for-errors';
-
-describe('functionsConvsMgrStoryEditorCheckStoryForErrors', () => {
-  it('should work', () => {
-    expect(functionsConvsMgrStoryEditorCheckStoryForErrors()).toEqual(
-      'functions-convs-mgr-story-editor--check-story-for-errors'
-    );
-  });
-});
