c713799dcf5eb6833ac8db58011a2ff8944b2c45
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-08 10:42:39 +0300

    :necktie:  Added logic to check opions source id

--- libs/functions/convs-mgr/story-editor/check-story-for-errors/src/lib/check-story-for-errors.handler.ts
+++ libs/functions/convs-mgr/story-editor/check-story-for-errors/src/lib/check-story-for-errors.handler.ts
@@ -3,7 +3,7 @@ import { HandlerTools } from '@iote/cqrs';
 import { FunctionHandler, FunctionContext, RestResult } from '@ngfi/functions';
 import { Query } from '@ngfi/firestore-qbuilder';
 
-import { FlowError, FlowErrorType } from '@app/model/convs-mgr/stories/main';
+import { StoryError, StoryErrorType } from '@app/model/convs-mgr/stories/main';
 import { StoryBlock } from '@app/model/convs-mgr/stories/blocks/main';
 import { ListMessageBlock, QuestionMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 import { Connection } from '@app/model/convs-mgr/conversations/chats';
@@ -11,7 +11,7 @@ import { Connection } from '@app/model/convs-mgr/conversations/chats';
 /**
  * Handler to find flow errors in story connections and blocks.
  */
-export class FindStoryErrorHandler extends FunctionHandler<any, FlowError[]> {
+export class FindStoryErrorHandler extends FunctionHandler<any, StoryError[]> {
 
   /**
    * Execute the function to find flow errors.
@@ -21,20 +21,20 @@ export class FindStoryErrorHandler extends FunctionHandler<any, FlowError[]> {
    * @returns Promise<FlowError[]> - Array of detected flow errors.
    */
 
-  public async execute(req: { orgId: string, storyId: string }, context: FunctionContext, tools: HandlerTools): Promise<FlowError[]> {
-    const errors: FlowError[] = [];
+  public async execute(req: { orgId: string, storyId: string }, context: FunctionContext, tools: HandlerTools): Promise<StoryError[]> {
+    const errors: StoryError[] = [];
     const connectionIds = new Set();
 
-    // Extract block ids from the sourceId
-    function extractBlockId(sourceId: string) {
-      const parts = sourceId.split("-");
-      if (parts.length === 2) {
-        const blockId = parts[1];
-        return blockId;
-      } else {
-        return sourceId; 
-      }
-    }
+    // // Extract block ids from the sourceId
+    // function extractBlockId(sourceId: string) {
+    //   const parts = sourceId.split("-");
+    //   if (parts.length === 2) {
+    //     const blockId = parts[1];
+    //     return blockId;
+    //   } else {
+    //     return sourceId; 
+    //   }
+    // }
 
     // Retrieve connections for the given orgId and storyId.
     // TODO: implement promise all to get both connections and blocks concurrently
@@ -43,7 +43,7 @@ export class FindStoryErrorHandler extends FunctionHandler<any, FlowError[]> {
 
     // Iterate through connections and collect their sourceIds.
     connections.forEach((connection) => {
-      connectionIds.add(extractBlockId(connection.sourceId));
+      connectionIds.add(connection.sourceId);
     });
 
     // Retrieve blocks for the given orgId and storyId.
@@ -54,36 +54,44 @@ export class FindStoryErrorHandler extends FunctionHandler<any, FlowError[]> {
     blocks.forEach((block) => {
       // Errors are not applicable to the endblock
       if( block.id != 'story-end-anchor'){  
-        if (this.isMessageEmpty(block.message)) {
-          errors.push({ type: FlowErrorType.EmptyTextField, blockId: block.id });
-        }
-        // Check if the blockIdToCheck is not in the sourceIds array
-        if (!connectionIds.has(block.id) || !connectionIds.has(req.storyId)) {
-          errors.push({
-            type: FlowErrorType.MissingConnection,
-            blockId: block.id
-          })
-        }
-
+        // It's either a ListMessageBlock or a QuestionMessageBlock or Another block with opption
         if (this.blockHasOptions(block)) {
-          // It's either a ListMessageBlock or a QuestionMessageBlock or Another block with opption
           const options = block.options;
           if (options) {
             // Check for empty message in block options
             options.forEach(option => {
               if (this.isMessageEmpty(option.message)) {
-                errors.push({ type: FlowErrorType.EmptyTextField, blockId: block.id })
+                errors.push({ type: StoryErrorType.EmptyTextField, blockId: block.id, optionsId: option.id })
+              }
+              if (!connectionIds.has(option.id)){
+                errors.push({
+                  type: StoryErrorType.MissingConnection,
+                  blockId: block.id,
+                  optionsId: option.id
+                })
               }
             });
           }
         } 
+        if (this.isMessageEmpty(block.message)) {
+          errors.push({ type: StoryErrorType.EmptyTextField, blockId: block.id });
+        }
+        // Check if the blockIdToCheck is not in the sourceIds array
+        if (!connectionIds.has(block.id) || !connectionIds.has(req.storyId)) {
+          errors.push({
+            type: StoryErrorType.MissingConnection,
+            blockId: block.id
+          })
+        }
+
+        
       }
     })
 
     // Return the array of detected flow errors.
     return errors;
   }
-  
+
   // Type guard function to check if a block ha soptions (ListMessageBlock or QuestionMessageBlock)
   // TODO: Paul - Ask if there othe blocks with options 
   private blockHasOptions(block: StoryBlock): block is ListMessageBlock | QuestionMessageBlock {
