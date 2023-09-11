e9db322840dd9e6a885b0773d386aa932a418e1d
japhethnyaranga <nyaranga4@gmail.com>
2023-09-11 09:30:37 +0300

    Exported only the course completion handler

--- libs/private/functions/micro-apps/cmi5-course-completion/src/index.ts
+++ libs/private/functions/micro-apps/cmi5-course-completion/src/index.ts
@@ -1,2 +1,2 @@
-export * from './lib/private-functions-micro-apps-cmi5-course-completion';
+// export * from './lib/private-functions-micro-apps-cmi5-course-completion';
 export * from './lib/cmi5-course-completion.handler'
