84033df45ce059d4aa674396abde5fe49919e387
japhethnyaranga <nyaranga4@gmail.com>
2023-09-11 09:30:59 +0300

    Created course completion handler function

--- apps/conv-lm-backend/src/app/micro-apps/cmi5/cmi5-course-completion.function.ts
+++ apps/conv-lm-backend/src/app/micro-apps/cmi5/cmi5-course-completion.function.ts
@@ -1,10 +1,10 @@
 import { EndpointRegistrar } from "@ngfi/functions";
 
-import { CMI5CompletionHandler } from "@app/private/functions/micro-apps/cmi5-course-completion";
+import { CMICourseCompletionHandler } from "@app/private/functions/micro-apps/cmi5-course-completion"; 
 
 import { ConvLearnFunction } from "../../../conv-learn-func.class";
 
-const handler = new CMI5CompletionHandler;
+const handler = new CMICourseCompletionHandler();
 
 export const cmi5Listener= new ConvLearnFunction('cmi5CourseCompletion', 
                                                   new EndpointRegistrar(), 
