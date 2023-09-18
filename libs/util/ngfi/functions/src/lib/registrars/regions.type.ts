2c3c51c9fc1209319fc9398d2ab1e35ff13cb4c2
Reagan Charana <rgncharana2@gmail.com>
2023-09-18 15:51:26 +0300

    :heavy_plus_sign: add asia-south region

--- libs/util/ngfi/functions/src/lib/registrars/regions.type.ts
+++ libs/util/ngfi/functions/src/lib/registrars/regions.type.ts
@@ -1,1 +1,1 @@
-export type FIREBASE_REGIONS = "europe-west1" | "us-central1" | "us-east1" | "us-east4" | "europe-west2" | "europe-west3" | "asia-east2" | "asia-northeast1";
+export type FIREBASE_REGIONS = "europe-west1" | "us-central1" | "us-east1" | "us-east4" | "europe-west2" | "europe-west3" | "asia-east2" | "asia-northeast1" | "asia-south1";
