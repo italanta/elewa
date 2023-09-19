d07cc8d532c3d8f0c6562bb7c87290b2ddb7a040
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-19 11:08:41 +0300

    :truck:  :wrench:  Moved service to state

--- libs/state/convs-mgr/story-editor/src/index.ts
+++ libs/state/convs-mgr/story-editor/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/model/story-editor-state.model';
 export * from './lib/providers/story-editor-state.service';
+export * from './lib/providers/check-story-errors.service';
 
 export * from './lib/stories-state.module';
