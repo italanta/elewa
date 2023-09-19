ef341ab7c422b999860efdca44511b0aedf9a2f0
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-19 10:17:31 +0300

    :truck:  Added a toast module

---
+++ libs/elements/layout/toast/src/lib/toast.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastComponent } from './components/toast/toast.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ToastComponent],
+})
+export class ToastModule {}
--- libs/state/convs-mgr/stories/main/src/index.ts
+++ libs/state/convs-mgr/stories/main/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/stores/labels.store';
 
 export * from './lib/services/labels-state.service';
 export * from './lib/stories-state.module';
+
+export * from './lib/services/snackbar.service';
