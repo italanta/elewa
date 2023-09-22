a2e96fa8e843278d74fa3611582ebdf71d60eb68
japhethnyaranga <nyaranga4@gmail.com>
2023-09-22 10:05:26 +0300

    🔧 imported platform type

--- libs/state/convs-mgr/learners/src/lib/services/learners.service.ts
+++ libs/state/convs-mgr/learners/src/lib/services/learners.service.ts
@@ -4,6 +4,7 @@ import { map, switchMap, of, combineLatest } from 'rxjs';
 
 import { EnrolledEndUser } from '@app/model/convs-mgr/learners';
 import { EndUserService } from '@app/state/convs-mgr/end-users';
+import { PlatformType } from '@app/model/convs-mgr/conversations/admin/system';
 
 import { LearnersStore } from '../store/learners.store';
 
