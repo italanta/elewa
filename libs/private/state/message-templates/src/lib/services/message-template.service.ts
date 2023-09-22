14bce5bdcf91970749f3d58a262bd0fd0a2b3cc4
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-22 14:29:06 +0300

    :pencil:  Edits bassed on the proposed approach

--- libs/private/state/message-templates/src/lib/services/message-template.service.ts
+++ libs/private/state/message-templates/src/lib/services/message-template.service.ts
@@ -1,44 +1,33 @@
 import { Injectable } from '@angular/core';
-
-import { take, map } from 'rxjs/operators';
-
+import { AngularFireFunctions } from '@angular/fire/compat/functions';
 import { MessageTemplate } from '@app/private/model/message-template';
-import { ActiveOrgStore } from '@app/private/state/organisation/main';
-
-import { MessageTemplateStore } from '../store/message-template.store'; 
+import { Observable } from 'rxjs';
+import { MessageTemplateStore } from '../store/message-template.store';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MessageTemplateService {
-  constructor(
-    private _messageTemplateStore: MessageTemplateStore, 
-    private _orgId$$: ActiveOrgStore
-  ) {}
+export class MessageTemplatesService {
+  constructor(private _aff:  AngularFireFunctions, private _messageTemplateStore: MessageTemplateStore) {}
 
   getMessageTemplates$() {
     return this._messageTemplateStore.get();
   }
-
-  createMessageTemplate(template: MessageTemplate) {
-    return this._messageTemplateStore.createMessageTemplate(template);
+  // Adjust data types once I know what the functions return
+  createTemplate(payload: MessageTemplate): Observable<any> {
+    return this.callFunction('create', { payload });
   }
 
-  updateMessageTemplate(template: MessageTemplate) {
-    return this._messageTemplateStore.updateMessageTemplate(template);
+  deleteTemplate(templateId: string): Observable<any> {
+    return this.callFunction('delete', { templateId });
   }
 
-  deleteMessageTemplate(template: MessageTemplate) {
-    return this._messageTemplateStore.deleteMessageTemplate(template);
+  updateTemplate(payload: MessageTemplate): Observable<any> {
+    return this.callFunction('update', { payload });
   }
 
-  getMessageTemplateOrg$() {
-    return this._orgId$$.get().pipe(take(1), map((_org) => _org.id));
-  }
-
-  getMessageTemplateById(templateId: string) {
-    return this._messageTemplateStore.getMessageTemplateById(templateId);
+  private callFunction(action: string, data: any): Observable<any> {
+    const templateRef = this._aff.httpsCallable('messageTemplateAPI');
+    return templateRef({ action, ...data });
   }
 }
-
-
