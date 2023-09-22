c926037816d656a5a39009462bf766b8294f987d
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-22 10:48:07 +0300

    :bulb: added the interface documantation

--- libs/private/model/message-template/src/lib/message-template.interface.ts
+++ libs/private/model/message-template/src/lib/message-template.interface.ts
@@ -1,7 +1,26 @@
-export interface privateModelMessageTemplate {
+/**
+ * Represents a message template model.
+ * This interface defines the structure of a message template.
+ */
+export interface ModelMessageTemplate {
+  /**
+   * The title of the message template.
+   */
   title: string;
   
+  /**
+   * The header content of the message template.
+   */
   header: string;
+
+  /**
+   * The body content of the message template.
+   */
   body: string;
+
+  /**
+   * The footer content of the message template.
+   */
   footer: string;
 }
+
