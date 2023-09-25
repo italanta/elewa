b266754e8e6759bd1c48db9b73a957cd7c153281
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-25 08:51:00 +0300

    :package:  message template home

--- libs/private/features/convs-mgr/message-template/src/lib/private-features-convs-mgr-message-template.module.ts
+++ libs/private/features/convs-mgr/message-template/src/lib/private-features-convs-mgr-message-template.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessageTemplateHomeComponent } from './pages/message-template-home/message-template-home/message-template-home.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [MessageTemplateHomeComponent],
 })
 export class PrivateFeaturesConvsMgrMessageTemplateModule {}
