c7f04f333445baac80cb421430bb1cc123492cb5
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-25 16:19:30 +0300

    :sparkles: feat: add create class modal

---
+++ libs/features/convs-mgr/learners/src/lib/modals/create-class-modal/create-class-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-class-modal',
+  templateUrl: './create-class-modal.component.html',
+  styleUrls: ['./create-class-modal.component.scss'],
+})
+export class CreateClassModalComponent {}
