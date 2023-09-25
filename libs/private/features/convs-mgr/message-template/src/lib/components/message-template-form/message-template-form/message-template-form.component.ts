0decdbc6fd1954fdf325977d74bb88f6b7cc764a
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-25 08:52:21 +0300

    :package:  message template form page

---
+++ libs/private/features/convs-mgr/message-template/src/lib/components/message-template-form/message-template-form/message-template-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-template-form',
+  templateUrl: './message-template-form.component.html',
+  styleUrls: ['./message-template-form.component.scss'],
+})
+export class MessageTemplateFormComponent {}
--- libs/private/features/convs-mgr/message-template/src/lib/private-features-convs-mgr-message-template.module.ts
+++ libs/private/features/convs-mgr/message-template/src/lib/private-features-convs-mgr-message-template.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageTemplateHomeComponent } from './pages/message-template-home/message-template-home/message-template-home.component';
+import { MessageTemplateFormComponent } from './components/message-template-form/message-template-form/message-template-form.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MessageTemplateHomeComponent],
+  declarations: [MessageTemplateHomeComponent, MessageTemplateFormComponent],
 })
 export class PrivateFeaturesConvsMgrMessageTemplateModule {}
