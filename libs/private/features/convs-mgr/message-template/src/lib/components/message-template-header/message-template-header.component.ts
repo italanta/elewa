399639897fc23a6538587d56ce73df8a34d39891
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-25 15:10:51 +0300

    :package:  message template header component

---
+++ libs/private/features/convs-mgr/message-template/src/lib/components/message-template-header/message-template-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-template-header',
+  templateUrl: './message-template-header.component.html',
+  styleUrls: ['./message-template-header.component.scss'],
+})
+export class MessageTemplateHeaderComponent {}
