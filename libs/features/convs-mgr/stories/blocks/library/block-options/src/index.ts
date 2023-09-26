a5b81f90234e9a3496da185c7b66a2939d63c73a
japhethnyaranga <nyaranga4@gmail.com>
2023-09-26 14:49:47 +0300

    🔧 exported option input

--- libs/features/convs-mgr/stories/blocks/library/block-options/src/index.ts
+++ libs/features/convs-mgr/stories/blocks/library/block-options/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/convs-mgr-block-options.module';
 export * from  './lib/providers/jsplumb-decorator.function';
+export * from './lib/components/option-input-field/option-input-field.component';
