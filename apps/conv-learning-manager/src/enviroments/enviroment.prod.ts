5d9ad7d7600fbb996edcd8358a802b6448ada46c
MuchinaWamburia <lucymuchina20@gmail.com>
2023-09-27 13:47:12 +0300

    enviroment setup

---
+++ apps/conv-learning-manager/src/enviroments/enviroment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: true,
+  
+    useEmulators: false,
+  
+    project: {
+      name: 'Elewa Manager',
+      info: 'Conversational Learning',
+    },
+  
+    firebase: {
+        // Paste your firebase config
+    apiKey: "AIzaSyBrjbkCmlctCpkas_Sxql3ag6d9n2KyF2k",
+    authDomain: "elewa-66408.firebaseapp.com",
+    projectId: "elewa-66408",
+    storageBucket: "elewa-66408.appspot.com",
+    messagingSenderId: "695117508825",
+    appId: "1:695117508825:web:81d40726a749df6f81cec8"
+  
+
+    },
+  };
+  
---
+++ apps/conv-learning-manager/src/enviroments/enviroment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: false,
+
+  useEmulators: false,
+
+  project: {
+    name: 'Elewa Manager',
+    info: 'Conversational Learning',
+  },
+
+  firebase: {
+    // Mine (delete)
+    // Paste your firebase config
+  apiKey: "AIzaSyBrjbkCmlctCpkas_Sxql3ag6d9n2KyF2k",
+  authDomain: "elewa-66408.firebaseapp.com",
+  projectId: "elewa-66408",
+  storageBucket: "elewa-66408.appspot.com",
+  messagingSenderId: "695117508825",
+  appId: "1:695117508825:web:81d40726a749df6f81cec8"
+
+  },
+};
