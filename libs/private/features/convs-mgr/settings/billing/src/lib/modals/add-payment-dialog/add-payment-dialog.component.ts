02c5de06d3cf642fc4ae9f5d0b884ae045c50ca3
japhethnyaranga <nyaranga4@gmail.com>
2023-09-28 09:01:49 +0300

    🔥 changed billing module name and deleted old name

---
+++ libs/private/features/convs-mgr/settings/billing/src/lib/modals/add-payment-dialog/add-payment-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-payment-dialog',
+  templateUrl: './add-payment-dialog.component.html',
+  styleUrls: ['./add-payment-dialog.component.scss'],
+})
+export class AddPaymentDialogComponent {}
