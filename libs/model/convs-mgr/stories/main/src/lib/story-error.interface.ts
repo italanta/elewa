e30da064d60a2e26eda2b9d960cd113b3d89d468
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-09-28 11:20:27 +0300

    Revert "Merge branch 'develop' into CLM-133-Throw-error-warning-if-the-flow-has-errors"
    This reverts commit 12f37d3dc77fa47132a49ecc13d5514f4101d1dc, reversing
    changes made to f6cb17fde60d8109c7b38f5a1eb5c63450faecf1.

---
+++ libs/model/convs-mgr/stories/main/src/lib/story-error.interface.ts
@@ -0,0 +1,28 @@
+/**
+ * Interface for the story errors
+ */
+export interface StoryError 
+{
+/** The type of the error as defined above */
+  type: StoryErrorType,
+  
+  /** The id of the block affected by the error
+  * If the type of the error is a missing connection, this will be the block
+  * that the connection is supposed to originate from
+  **/
+  blockId: string,
+
+  // The id of the option that is missing a connection in the case of multiple input options
+  optionsId?: string,
+}
+
+/**
+ * Enumeration representing different types of errors that can occur in a flow or story.
+ */
+export enum StoryErrorType {
+  // Indicates a missing connection error.
+  MissingConnection = 10,
+   
+  // Indicates an error related to an empty text field.
+  EmptyTextField = 20     
+}
