0d8ee6a020e61b0d34b71c3ce825f4166bb1a785
japhethnyaranga <nyaranga4@gmail.com>
2023-09-28 12:11:38 +0300

    💄 restructured the code and added payment dialog option

--- libs/private/features/convs-mgr/settings/billing/src/lib/components/billing/billing.component.ts
+++ libs/private/features/convs-mgr/settings/billing/src/lib/components/billing/billing.component.ts
@@ -1,29 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { NewUserDialogComponent } from 'libs/private/features/convs-mgr/settings/users/src/lib/modals/new-user-dialog/new-user-dialog.component';
+import { AddPaymentDialogComponent } from '../../modals/add-payment-dialog/add-payment-dialog.component';
 
-@Component({
-  selector: 'app-billing',
-  templateUrl: './billing.component.html',
-  styleUrls: ['./billing.component.scss'],
-})
-
-
-export class BillingComponent {
-
-  constructor(private dialog: MatDialog){}
-
-  displayedColumns: string[] = ['position', 'name', 'weight', ''];
-  dataSource = ELEMENT_DATA;
-
-  addPaymentMethod() {
-    const dialogRef = this.dialog.open(NewUserDialogComponent, {
-      minWidth: '500px',
-      minHeight: '200px',
-      
-    });
-  }
-}
 
 export interface PeriodicElement {
   name: string;
@@ -36,9 +14,40 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-
+ 
 ];
+
+@Component({
+  selector: 'app-billing',
+  templateUrl: './billing.component.html',
+  styleUrls: ['./billing.component.scss'],
+})
+
+
+
+export class BillingComponent {
+  org: any;
+
+  constructor(private dialog: MatDialog){}
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+
+  addPaymentMethod() {
+    const dialogRef = this.dialog.open(AddPaymentDialogComponent, {
+      minWidth: '500px',
+      minHeight: '200px',
+      data: this.org
+    });
+  }
+
+  selectedOption: string = 'subscription'; // Initialize with the default selected option
+
+  // You can have a method to update the selected option based on user interaction
+  selectOption(option: string) {
+    this.selectedOption = option;
+  }
+}
+
 
 
