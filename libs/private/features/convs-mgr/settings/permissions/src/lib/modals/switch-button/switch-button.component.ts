42c72cc4950abdd15093b5cd45a96deb9cc666ec
japhethnyaranga <nyaranga4@gmail.com>
2023-09-29 11:56:56 +0300

    💄 created separate componentfor the switch button

---
+++ libs/private/features/convs-mgr/settings/permissions/src/lib/modals/switch-button/switch-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-button',
+  templateUrl: './switch-button.component.html',
+  styleUrls: ['./switch-button.component.scss'],
+})
+export class SwitchButtonComponent {}
