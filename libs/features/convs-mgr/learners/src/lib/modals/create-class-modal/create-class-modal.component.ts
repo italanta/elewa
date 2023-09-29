15bbe9b2986faa20c0060f1e0e42447e2e1968e1
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-29 15:42:38 +0300

    :sparkles: feat: add create class modal and it's functionality

---
+++ libs/features/convs-mgr/learners/src/lib/modals/create-class-modal/create-class-modal.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { SubSink } from 'subsink';
+
+import { ClassroomService } from '@app/state/convs-mgr/classrooms';
+
+@Component({
+  selector: 'app-create-class-modal',
+  templateUrl: './create-class-modal.component.html',
+  styleUrls: ['./create-class-modal.component.scss'],
+})
+export class CreateClassModalComponent implements OnInit, OnDestroy {
+  createClassForm: FormGroup;
+
+  private _sBs = new SubSink();
+
+  constructor(
+    private _fb: FormBuilder,
+    private _classroomServ$: ClassroomService,
+    public dialogRef: MatDialogRef<CreateClassModalComponent>
+  ) {}
+
+  ngOnInit() {
+    this.createClassForm = this.buildFormGroup();
+  }
+
+  buildFormGroup() {
+    return this._fb.group({
+      className: ['', Validators.required],
+      description: ['', Validators.required],
+      deleted: [false],
+    });
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  submitAction() {
+    this._sBs.sink = this._classroomServ$
+      .addClassroom(this.createClassForm.value)
+      .subscribe(() => {
+        this.dialogRef.close();
+      });
+  }
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
+  }
+}
