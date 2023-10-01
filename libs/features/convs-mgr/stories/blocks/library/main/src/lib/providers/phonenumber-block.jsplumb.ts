f1bfc59e2485580051beb9f1f744604b617c6d5f
mutagah <kihahumutagah@gmail.com>
2023-10-01 14:04:19 +0300

    :lipstick:feat: Add second jsplumb and change co-ordinates of the anchors.

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/phonenumber-block.jsplumb.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/phonenumber-block.jsplumb.ts
@@ -22,7 +22,17 @@ export function _PhoneBlockDecoratePlumb(block: PhoneMessageBlock, comp: Compone
     target: true,
     cssClass:"block_endpoint",
     endpoint: "Dot",
-    anchor: "Left",
+    anchor: [0,0.3,0,1],
+    maxConnections: -1
+  });
+
+  // Adding the second static jsPlumb connector
+  jsPlumb.addEndpoint(comp.location.nativeElement, {
+    // Whether the anchor is target (Other Block -> This Block)
+    target: true,
+    cssClass:"block_endpoint",
+    endpoint: "Dot",
+    anchor: [1, 0.8,0,1],
     maxConnections: -1
   });
 
