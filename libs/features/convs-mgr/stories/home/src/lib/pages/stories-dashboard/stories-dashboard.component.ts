ef8b166436c65164309f80ed311142605b61cd33
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-02 15:52:22 +0300

    :sparkles: feat:refactor bot list component

--- libs/features/convs-mgr/stories/home/src/lib/pages/stories-dashboard/stories-dashboard.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/pages/stories-dashboard/stories-dashboard.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-import { SubSink } from 'subsink';
 
 import { Observable } from 'rxjs';
 
@@ -16,52 +13,31 @@ import { StoriesStore } from '@app/state/convs-mgr/stories';
 
 import { HOME_CRUMB } from '@app/elements/nav/convl/breadcrumbs';
 
-import { CreateBotModalComponent } from '../../modals/create-bot-modal/create-bot-modal.component';
-
 @Component({
   selector: 'italanta-apps-stories-dashboard',
   templateUrl: './stories-dashboard.component.html',
   styleUrls: ['./stories-dashboard.component.scss'],
 })
 export class StoriesDashboardComponent implements OnInit {
-
-  private _sb = new SubSink();
-
   title: string;
-
   breadcrumbs: Breadcrumb[] = [];
 
   stories$: Observable<Story[]>;
   org$: Observable<Organisation>;
 
   loading = true;
-  showAllCourses: boolean = false;
+  showAllCourses = false;
 
-  constructor(private _org$$: ActiveOrgStore,
-              private _stories$$: StoriesStore,
-              private _router$$: Router,
-              private dialog : MatDialog
-  )
-  {
+  constructor(
+    private _org$$: ActiveOrgStore,
+    private _stories$$: StoriesStore,
+    private _router$$: Router
+  ) {
     this.breadcrumbs = [HOME_CRUMB(_router$$, true)];
-    this.org$ = _org$$.get();
+  }
+
+  ngOnInit(): void {
+    this.org$ = this._org$$.get();
     this.stories$ = this._stories$$.get();
-  }
-
-  ngOnInit(): void {}
-
-  openCreateDialog(){
-    this.dialog.open(CreateBotModalComponent, {
-      data: {
-        isEditMode: false,
-      },
-      minHeight: 'fit-content',
-      minWidth: '600px'
-    });
-  }
-
-  ngOnDestroy()
-  {
-    this._sb.unsubscribe();
   }
 }
