6416f2e0296e5ec46006822188c4de7f62bcfa25
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-02 12:50:05 +0300

    :sparkles: feat: create module and lesson modals

---
+++ libs/features/convs-mgr/stories/home/src/lib/modals/create-lesson-modal/create-lesson-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'italanta-apps-create-lesson-modal',
+  templateUrl: './create-lesson-modal.component.html',
+  styleUrls: ['./create-lesson-modal.component.scss'],
+})
+export class CreateLessonModalComponent {}
---
+++ libs/features/convs-mgr/stories/home/src/lib/modals/create-module-modal/create-module-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'italanta-apps-create-module-modal',
+  templateUrl: './create-module-modal.component.html',
+  styleUrls: ['./create-module-modal.component.scss'],
+})
+export class CreateModuleModalComponent {}
