b761e7af3f2ea1ff7ab59ee1f8848accdb7867ba
Reagan Charana <rgncharana2@gmail.com>
2023-10-02 10:16:11 +0300

    :horse_racing: :sparkles: implement send templates handler

--- libs/functions/bot-engine/send-message/src/index.ts
+++ libs/functions/bot-engine/send-message/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/send-outgoing-message.handler';
+export * from './lib/send-scheduled-messages.handler';
---
+++ libs/functions/bot-engine/send-message/src/lib/send-scheduled-messages.handler.ts
@@ -0,0 +1,39 @@
+import { HandlerTools } from "@iote/cqrs";
+import { FunctionHandler, FunctionContext } from "@ngfi/functions";
+
+import { ScheduledMessage } from "@app/model/convs-mgr/functions";
+import { PlatformType } from "@app/model/convs-mgr/conversations/admin/system";
+
+import { SendOutgoingMsgHandler } from "./send-outgoing-message.handler";
+
+export class SendScheduledMessagesHandler extends FunctionHandler<ScheduledMessage, any>
+{
+  async execute(cmd: ScheduledMessage, context: FunctionContext, tools: HandlerTools) 
+  {
+    return this._sendMessages(cmd, tools);
+  }
+
+  private async _sendMessages(msgToSend: ScheduledMessage, tools: HandlerTools)
+  {
+    const message: any = {
+      ...msgToSend.message,
+      n: msgToSend.n
+    };
+
+    const sendMessage = new SendOutgoingMsgHandler();
+
+    for (let receieveID of msgToSend.endUsers) {
+      if (msgToSend.plaform === PlatformType.WhatsApp) {
+
+        message.phoneNumber = receieveID;
+        await sendMessage.execute(message, null, tools);
+
+      } else if (msgToSend.plaform === PlatformType.Messenger) {
+        message.receipientId = receieveID;
+        await sendMessage.execute(message, null, tools);
+
+      }
+    }
+
+  }
+}
