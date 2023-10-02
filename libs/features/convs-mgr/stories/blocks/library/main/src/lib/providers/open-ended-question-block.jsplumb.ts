16d8098b17a58780e34c53fd1f6a9d16ee17ff88
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-10-02 16:01:39 +0300

    :art: added and styled the jsPlumb endpoints

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/open-ended-question-block.jsplumb.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/open-ended-question-block.jsplumb.ts
@@ -21,8 +21,16 @@ export function _OpenEndedQuestionBlockDecoratePlumb(block: OpenEndedQuestionBlo
     // Whether the anchor is target (Other Block -> This Block)
     target: true,
     cssClass: 'block_endpoint',
-    endpoint: 'Rectangle',
-    anchor: "Left",
+    endpoint: 'Dot',
+    anchor: [0, 0.25, 0, 1],
+    maxConnections: -1
+  });
+
+  jsPlumb.addEndpoint(comp.location.nativeElement, {
+    source: true,
+    cssClass:"block_endpoint",
+    endpoint: "Dot",
+    anchor: [1, 0.9, 0, 1],
     maxConnections: -1
   });
   
