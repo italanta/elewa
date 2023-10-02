5f3698f99bd3bdc735d538e05a43326cd8a80caf
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-02 14:43:55 +0300

    :sparkles: feat: add create lesson and create module form

--- libs/features/convs-mgr/stories/home/src/lib/modals/create-lesson-modal/create-lesson-modal.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/modals/create-lesson-modal/create-lesson-modal.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { NewStoryService } from '../../services/new-story.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'italanta-apps-create-lesson-modal',
   templateUrl: './create-lesson-modal.component.html',
   styleUrls: ['./create-lesson-modal.component.scss'],
 })
-export class CreateLessonModalComponent {}
+export class CreateLessonModalComponent {
+  lessonForm: FormGroup;
+  modalMode = false;
+
+  constructor(
+    private _addStory$: NewStoryService,
+    private _formBuilder: FormBuilder
+  ) {}
+
+  createFormGroup() {
+    this.lessonForm = this._formBuilder.group({
+      botName: [this._addStory$.generateName()],
+      botDesc: [''],
+      module: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    console.log(this.lessonForm.value);
+    //
+  }
+}
--- libs/features/convs-mgr/stories/home/src/lib/modals/create-module-modal/create-module-modal.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/modals/create-module-modal/create-module-modal.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { NewStoryService } from '../../services/new-story.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'italanta-apps-create-module-modal',
   templateUrl: './create-module-modal.component.html',
   styleUrls: ['./create-module-modal.component.scss'],
 })
-export class CreateModuleModalComponent {}
+export class CreateModuleModalComponent {
+  moduleForm: FormGroup;
+  modalMode = false;
+
+  constructor(
+    private _addStory$: NewStoryService,
+    private _formBuilder: FormBuilder
+  ) {}
+
+  createFormGroup() {
+    this.moduleForm = this._formBuilder.group({
+      botName: [this._addStory$.generateName()],
+      botDesc: [''],
+      module: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    console.log(this.moduleForm.value);
+    //
+  }
+}
