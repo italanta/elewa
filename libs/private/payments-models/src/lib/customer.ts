0e0fd3c2cb57bace8f42c7d3ffaebeef071b0536
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-10-03 09:35:22 +0300

    Generating the base customer model

---
+++ libs/private/payments-models/src/lib/customer.ts
@@ -0,0 +1,8 @@
+
+/**The customer id is returned by the customer API and will be saved to db */
+export interface Customer{
+    customerId:     string
+    name:           string;
+    email:          string;
+    isPremium:      boolean;
+}
