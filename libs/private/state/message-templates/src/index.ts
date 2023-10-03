9f0d040a178fa56471298fb62bf0c5d505eaed06
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-03 13:58:11 +0300

    :rocket:  exported the stores

--- libs/private/state/message-templates/src/index.ts
+++ libs/private/state/message-templates/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/private-state-message-templates.module';
 export * from './lib/services/message-template.service'
 export * from './lib/store/message-template.store'
+export * from './lib/store/active-message-template.store'
