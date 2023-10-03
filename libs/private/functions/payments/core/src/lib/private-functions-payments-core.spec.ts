e6e509b6ad22626dcf0dd9226aa592b120115ced
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-10-03 15:14:06 +0300

    📦 Generate private/function/payment/core

---
+++ libs/private/functions/payments/core/src/lib/private-functions-payments-core.spec.ts
@@ -0,0 +1,9 @@
+import { privateFunctionsPaymentsCore } from './private-functions-payments-core';
+
+describe('privateFunctionsPaymentsCore', () => {
+  it('should work', () => {
+    expect(privateFunctionsPaymentsCore()).toEqual(
+      'private-functions-payments-core'
+    );
+  });
+});
--- libs/private/payments-models/src/lib/payment-method.ts
+++ libs/private/payments-models/src/lib/payment-method.ts
@@ -3,8 +3,15 @@ export interface PaymentMethod {
     metadata:       any;
 }
 
+
 export enum PaymentType {
     debitCard = 1,
     creditCard  = 2,
     mPesa = 3
 }
+
+/**comments  on type and params*/
+
+//end point api/rcv payment 
+//handler
+//private/model/payments
