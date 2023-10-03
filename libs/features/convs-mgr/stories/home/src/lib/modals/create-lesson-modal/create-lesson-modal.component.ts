0962043d6b29499996b9facf5cd8079b25d5f208
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-03 11:56:11 +0300

    :sparkles: refactor: create and mutate lesson and module

--- libs/features/convs-mgr/stories/home/src/lib/modals/create-lesson-modal/create-lesson-modal.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/modals/create-lesson-modal/create-lesson-modal.component.ts
@@ -1,31 +1,73 @@
-import { Component } from '@angular/core';
-import { NewStoryService } from '../../services/new-story.service';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Story } from '@app/model/convs-mgr/stories/main';
+import { StoryStateService } from '@app/state/convs-mgr/stories';
+
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from 'unique-names-generator';
 
 @Component({
   selector: 'italanta-apps-create-lesson-modal',
   templateUrl: './create-lesson-modal.component.html',
   styleUrls: ['./create-lesson-modal.component.scss'],
 })
-export class CreateLessonModalComponent {
+export class CreateLessonModalComponent implements OnInit {
   lessonForm: FormGroup;
   modalMode = false;
 
+  story: Story;
+  isSavingStory = false;
+
   constructor(
-    private _addStory$: NewStoryService,
+    private _stateStoryServ$: StoryStateService,
     private _formBuilder: FormBuilder
   ) {}
 
+  ngOnInit () {
+    this.createFormGroup();
+  }
+
+  generateName() {
+    const defaultName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] });
+    return defaultName;
+  }
+
   createFormGroup() {
     this.lessonForm = this._formBuilder.group({
-      botName: [this._addStory$.generateName()],
+      botName: [this.generateName()],
       botDesc: [''],
       module: ['', Validators.required],
     });
   }
 
+  add() {
+    const newStory: Story = {
+      name: this.lessonForm.value.botName,
+      description: this.lessonForm.value.botDesc,
+      orgId: '',
+    };
+
+    this._stateStoryServ$.createStory(newStory).subscribe(() => {
+      // send output event to move to next step
+    });
+  }
+
+  update() {
+    this.story.name = this.lessonForm.value.botName;
+    this.story.description = this.lessonForm.value.botDesc;
+
+    this._stateStoryServ$.updateStory(this.story).subscribe(() => {
+      // send output event to close dialog
+    });
+  }
+
   submitForm() {
-    console.log(this.lessonForm.value);
-    //
+    this.isSavingStory = true;
+    this.modalMode ? this.update() : this.add();
   }
 }
--- libs/features/convs-mgr/stories/home/src/lib/modals/create-module-modal/create-module-modal.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/modals/create-module-modal/create-module-modal.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
-import { NewStoryService } from '../../services/new-story.service';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BotModulesStateService } from '@app/state/convs-mgr/modules';
+
+import { NewStoryService } from '../../services/new-story.service';
 
 @Component({
   selector: 'italanta-apps-create-module-modal',
   templateUrl: './create-module-modal.component.html',
   styleUrls: ['./create-module-modal.component.scss'],
 })
-export class CreateModuleModalComponent {
+export class CreateModuleModalComponent implements OnInit {
   moduleForm: FormGroup;
   modalMode = false;
 
   constructor(
+    private _botModulesServ: BotModulesStateService,
     private _addStory$: NewStoryService,
     private _formBuilder: FormBuilder
   ) {}
+
+  ngOnInit () {
+    this.createFormGroup();
+  }
 
   createFormGroup() {
     this.moduleForm = this._formBuilder.group({
