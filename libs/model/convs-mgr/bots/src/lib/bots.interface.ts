1a636c232ff77c92e3d44ea927ee2f0471bdc16d
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-03 11:54:22 +0300

    :house: feat: create botMode enum and update existing interface

--- libs/model/convs-mgr/bots/src/lib/bots.interface.ts
+++ libs/model/convs-mgr/bots/src/lib/bots.interface.ts
@@ -7,12 +7,24 @@ import { IObject } from '@iote/bricks';
  * Interface representing a bot.
  */
 export interface Bot extends IObject {
+  /* Foreign Key to Organisation holding the story. */
+  orgId: string;
+
   /** The name of the bot.*/
   name: string;
 
   /** A brief description of the bot. */
   description?: string;
 
+  /** The bot's image */
+  imageField?: string;
+
   /** An array of module names associated with the bot.*/
   modules: string[];
+}
+
+/** botmutation enum - has the different bot mutations you can perfom on a bot */
+export enum BotMutationEnum {
+  EditMode = 'Edit Bot',
+  CreateMode = 'Create Bot',
 }
