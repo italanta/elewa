e55567f062956c14043a018e260a213ea4e6ed3f
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-03 14:00:02 +0300

    :recycle:  mesage template pages

--- libs/private/features/convs-mgr/message-template/src/lib/pages/message-template-create/message-template-create.component.ts
+++ libs/private/features/convs-mgr/message-template/src/lib/pages/message-template-create/message-template-create.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageTemplatesService } from '@app/private/state/message-templates';
 
 @Component({
   selector: 'app-message-template-create',
   templateUrl: './message-template-create.component.html',
   styleUrls: ['./message-template-create.component.scss'],
 })
-export class MessageTemplateCreateComponent {}
+export class MessageTemplateCreateComponent {
+  // action: string;
+  // constructor(
+  //   private _route$$: Router,
+  //   private _route:ActivatedRoute,
+  //   private _snackBar: MatSnackBar,
+  //   private _templateService: MessageTemplatesService,
+  // ) {}
+
+  // ngOnInit(): void {
+  //     this.action = this._route$$.url.split('/')[2];
+  //     if (this.action === 'create') {
+  //       this.initializeEmptyAssessmentForm();
+  //     } else {
+  //       this.initPage();
+  //     }
+  // }
+}
