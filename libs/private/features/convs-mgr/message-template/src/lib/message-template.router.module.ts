28ef88cbf8f51de47ab628268769207f33bdd5c1
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-03 17:46:12 +0300

    :signal_strength:  Route added for help

--- libs/private/features/convs-mgr/message-template/src/lib/message-template.router.module.ts
+++ libs/private/features/convs-mgr/message-template/src/lib/message-template.router.module.ts
@@ -3,7 +3,7 @@ import { Route, RouterModule } from '@angular/router';
 
 import { MessageTemplateHomeComponent } from './pages/message-template-home/message-template-home.component';
 import { MessageTemplateCreateComponent } from './pages/message-template-create/message-template-create.component';
-import { MessageTemplateViewComponent } from './pages/message-template-view/message-template-view.component';
+import { MessageTemplateViewComponent } from './pages/message-template-help/message-template-view.component';
 
 const MESSAGING_ROUTES: Route[] = [
   {
@@ -17,6 +17,10 @@ const MESSAGING_ROUTES: Route[] = [
   {
     path: ':id', 
     component: MessageTemplateCreateComponent, 
+  },
+  {
+    path: 'help', 
+    component: MessageTemplateViewComponent, 
   },
 
 ];
