cd0373417fb16e29a386c77af975fe24ac227107
japhethnyaranga <nyaranga4@gmail.com>
2023-10-03 08:21:35 +0300

    💄 billing content fetched dynamically

--- libs/private/features/convs-mgr/settings/billing/src/lib/components/billing/billing.component.ts
+++ libs/private/features/convs-mgr/settings/billing/src/lib/components/billing/billing.component.ts
@@ -3,7 +3,6 @@ import { MatDialog } from '@angular/material/dialog';
 import { AddPaymentDialogComponent } from '../../modals/add-payment-dialog/add-payment-dialog.component';
 import { EditPaymentDialogComponent } from '../../modals/edit-payment-dialog/edit-payment-dialog.component';
 
-
 export interface PeriodicElement {
   name: string;
   position: any;
@@ -11,11 +10,16 @@ export interface PeriodicElement {
   symbol: string;
 }
 
+export interface PaymentData {
+  id: number;
+  cardNumber: number;
+  expiry: string;
+}
+
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: "23 August 2023", name: 'Plan 1', weight: 1200, symbol: 'Download'},
-  {position: "23 July 2023", name: 'Plan 1', weight: 1200, symbol: 'Download'},
-  {position: "23 July 2023", name: 'plan 1', weight: 1200, symbol: 'Download'},
- 
+  { position: "23 August 2023", name: 'Plan 1', weight: 1200, symbol: 'Download' },
+  { position: "23 July 2023", name: 'Plan 1', weight: 1200, symbol: 'Download' },
+  { position: "23 July 2023", name: 'plan 1', weight: 1200, symbol: 'Download' },
 ];
 
 @Component({
@@ -23,13 +27,19 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.scss'],
 })
-
-
-
 export class BillingComponent {
   org: any;
+  selectedOption: string = 'subscription'; // Initialize with the default selected option
 
-  constructor(private dialog: MatDialog){}
+  // Define ELEMENT_CARD as a property of the class
+  ELEMENT_CARD: PaymentData[] = [
+    { id: 1, cardNumber: 1234578908, expiry: '02/24' },
+    { id: 2, cardNumber: 1234578909, expiry: '03/25' },
+    { id: 3, cardNumber: 1234578910, expiry: '04/26' },
+    { id: 4, cardNumber: 1234578911, expiry: '05/27' },
+  ];
+
+  constructor(private dialog: MatDialog) {}
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
@@ -41,23 +51,17 @@ export class BillingComponent {
       data: this.org,
     });
   }
-  
 
   editPaymentMethod() {
     const dialogRef = this.dialog.open(EditPaymentDialogComponent, {
       minWidth: '500px',
       minHeight: '200px',
-      data: this.org
+      data: this.org,
     });
   }
-
-  selectedOption: string = 'subscription'; // Initialize with the default selected option
 
   // You can have a method to update the selected option based on user interaction
   selectOption(option: string) {
     this.selectedOption = option;
   }
 }
-
-
-
