bdcc72b2dab06f360f1433ba9d84dea9e3c74a14
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-05 15:17:39 +0300

    :sparkles: feat: generate lessons library

---
+++ libs/features/convs-mgr/lessons/src/lib/lessons.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [CommonModule],
+})
+
+// a story is a lesson
+export class ConvsMgrLessonsModule {
+  
+}
