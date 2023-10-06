a4af359c7dee0eb7f863f4bf6814209ce222ce8f
Beulah Mathenge <Beulah-Matt@users.noreply.github.com>
2023-10-06 17:12:22 +0300

    :recycle: Switching to mollie client for api communication

--- libs/private/functions/payments/core/src/lib/services/payment-core.service.ts
+++ libs/private/functions/payments/core/src/lib/services/payment-core.service.ts
@@ -1,14 +1,25 @@
 //import { Payment } from ;
+import { HandlerTools } from "@iote/cqrs";
 import { Payment } from "../models/payment";
 import axios, {AxiosRequestHeaders} from 'axios';
 
+import { createMollieClient } from '@mollie/api-client';
+
 export class PaymentCoreService {
+
     apiKey: string;
-    paymentObject: Payment
+    paymentObject: any
     paymentURL: string;
-    constructor(private _apiKey: string, private _paymentObject: Payment, private _paymentURL: string){
-        this. apiKey = _apiKey;
-        this._paymentObject = _paymentObject;
+
+    mollieClient = createMollieClient({ apiKey: this._apiKey });
+
+    constructor(private _apiKey: string, 
+                private _paymentObject: any, 
+                private _paymentURL: string,
+                private tools: HandlerTools
+    ) {
+        this.apiKey = _apiKey;
+        this.paymentObject = _paymentObject;
         this.paymentURL = _paymentURL;
     }
 
@@ -17,7 +28,6 @@ export class PaymentCoreService {
             'Authorization': `Bearer ${this.apiKey}`
         };
         return headers;
-
     }
 
     async getPayment(id:string){
@@ -25,7 +35,7 @@ export class PaymentCoreService {
     }
 
     async requestPayment(){
-        const payment = await axios.post(`${this.paymentURL}/payments`, this.paymentObject)
-        return payment.data;
+        this.tools.Logger.log(() => `PaymentCoreService : requestPayment`);
+        return await this.mollieClient.payments.create(this.paymentObject);
     }
 }
