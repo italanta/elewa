9bc408500c92bd9c78dc2cc664b42a411d063e6f
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-09 16:58:49 +0300

    :sparkles: feat: compute and visualise assessments

---
+++ libs/features/monitoring/src/lib/components/assessment-progress-chart/assessment-progress-chart.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { map, switchMap } from 'rxjs';
+import { SubSink } from 'subsink';
+
+import { AssessmentService } from '@app/state/convs-mgr/conversations/assessments';
+import { AssessmentMetricsService } from '@app/features/convs-mgr/conversations/assessments';
+
+import { EndUserService } from '@app/state/convs-mgr/end-users';
+import { Assessment } from '@app/model/convs-mgr/conversations/assessments';
+
+interface AssessmentScore {
+  assessment: Assessment;
+  scores: number[];
+}
+
+@Component({
+  selector: 'app-assessment-progress-chart',
+  templateUrl: './assessment-progress-chart.component.html',
+  styleUrls: ['./assessment-progress-chart.component.scss'],
+})
+export class AssessmentProgressChartComponent implements OnInit, OnDestroy {
+  private _sBs = new SubSink();
+
+  highestScore: number;
+  lowestScore: number;
+  averageScore: string;
+  totalQuestions: number;
+  assessments: Assessment[];
+  assessmentScores: AssessmentScore[];
+
+  selectedAssessment: Assessment;
+
+  constructor(
+    private _assessmentServ$: AssessmentService,
+    private _endUserService: EndUserService,
+    private _aMetrics: AssessmentMetricsService
+  ) {}
+
+  ngOnInit(): void {
+    this.getMetrics();
+  }
+
+  getMetrics() {
+    this._sBs.sink = this._assessmentServ$
+      .getAssessments$()
+      .pipe(
+        switchMap((assessments) => {
+          this.assessments = assessments;
+
+          return this._endUserService.getUserDetailsAndTheirCursor().pipe(
+            map((endUsers) => {
+              this.assessmentScores = assessments.map((assessment) => {
+                const { scores } = this._aMetrics.computeMetrics(
+                  endUsers,
+                  assessments[0]
+                );
+
+                return { assessment, scores } as AssessmentScore;
+              });
+
+              console.log(this.assessmentScores);
+            })
+          );
+        })
+      )
+      .subscribe();
+  }
+
+  switchAssessment(assessment: Assessment) {
+    const assessmentScore = this.assessmentScores.find((assess) => assess.assessment.id === assessment.id) as AssessmentScore;
+
+    const scores = assessmentScore.scores;
+
+    this.computeScores(scores);
+  }
+
+  computeScores(scores: number[]) {
+    console.log({ scores });
+
+    if (!scores.length) return;
+
+    this.highestScore = Math.max(...scores);
+    this.lowestScore = Math.min(...scores);
+
+    const sum = scores.reduce((prev, next) => prev + next);
+    this.averageScore = (sum / scores.length).toFixed(2);
+  }
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
+  }
+}
