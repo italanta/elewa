6c05d17fae9520c4a3c36bcccc6679f25a15537e
kiplangat01 <meshnobie@gmail.com>
2023-10-10 01:33:02 +0300

    :beers: positioned the left anchor to fit the proposed design

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/assessment-brick.jsplumb.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/assessment-brick.jsplumb.ts
@@ -24,7 +24,7 @@ export function _AssessmentBrickDecoratePlumb(
     target: true,
     cssClass: 'block_endpoint',
     endpoint: 'Dot',
-    anchor: 'Left',
+    anchor: [0, 0.13, -1, 0],
     maxConnections: -1,
   });
 
