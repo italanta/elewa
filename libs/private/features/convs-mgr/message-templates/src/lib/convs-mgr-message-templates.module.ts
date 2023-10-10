4db8895591308307ae407465460c592003ffec3b
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-10 04:28:40 +0300

    :rocket:  exports

---
+++ libs/private/features/convs-mgr/message-templates/src/lib/convs-mgr-message-templates.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageTemplateHomeComponent } from './pages/message-template-home/message-template-home.component';
+import { MessageTemplateHelpComponent } from './pages/message-template-help/message-template-help.component';
+import { MessageTemplateCreateComponent } from './pages/message-template-create/message-template-create.component';
+import { MessageTemplateFormComponent } from './components/message-template-form/message-template-form.component';
+import { MessageTemplateHeaderComponent } from './components/message-template-header/message-template-header.component';
+import { MessageTemplateListComponent } from './components/message-template-list/message-template-list.component';
+import { MessageTemplateSingleSettingsComponent } from './components/message-template-single-settings/message-template-single-settings.component';
+import { MessageTemplateSingleComponent } from './components/message-template-single/message-template-single.component';
+import { ConvlPageModule } from '@app/elements/layout/page-convl';
+import {
+  MaterialBricksModule,
+  MaterialDesignModule,
+} from '@iote/bricks-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MessageTemplateStore,
+  MessageTemplatesService,
+} from '@app/private/state/message-templates';
+import { SpecificTimeModalComponent } from './modals/specific-time-modal/specific-time-modal.component';
+import { AfterInactivityModalComponent } from './modals/after-inactivity-modal/after-inactivity-modal.component';
+import { MilestoneReachedModalComponent } from './modals/milestone-reached-modal/milestone-reached-modal.component';
+import { MessageTemplateRouterModule } from './message-template.router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ConvlPageModule,
+    MessageTemplateRouterModule,
+    MaterialBricksModule,
+    MaterialDesignModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  declarations: [
+    MessageTemplateHomeComponent,
+    MessageTemplateHelpComponent,
+    MessageTemplateCreateComponent,
+    MessageTemplateFormComponent,
+    MessageTemplateHeaderComponent,
+    MessageTemplateListComponent,
+    MessageTemplateSingleSettingsComponent,
+    MessageTemplateSingleComponent,
+    SpecificTimeModalComponent,
+    AfterInactivityModalComponent,
+    MilestoneReachedModalComponent,
+  ],
+  providers: [MessageTemplatesService, MessageTemplateStore],
+})
+export class ConvsMgrMessageTemplatesModule {}
