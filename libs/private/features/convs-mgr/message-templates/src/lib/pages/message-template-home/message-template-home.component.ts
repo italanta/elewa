c14295520efa8eae605af4f740b306a87f6bda76
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-10 04:22:54 +0300

    :package:  message template home

---
+++ libs/private/features/convs-mgr/message-templates/src/lib/pages/message-template-home/message-template-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-template-home',
+  templateUrl: './message-template-home.component.html',
+  styleUrls: ['./message-template-home.component.scss'],
+})
+export class MessageTemplateHomeComponent {}
