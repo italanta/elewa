7d40586ad02fd1c460a6af7c99b84fad506ac20b
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-10 05:07:20 +0300

    imports handled

--- libs/private/state/message-templates/src/lib/private-state-message-templates.module.ts
+++ libs/private/state/message-templates/src/lib/private-state-message-templates.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageTemplateStore } from './store/message-template.store';
 import { MessageTemplatesService } from './service/message-template.service';
+import { ScheduleMessageService } from './service/schedule-message.service';
+import { ScheduledMessageStore } from './store/scheduled-message.store';
 
 @NgModule({
   imports: [CommonModule],
   providers: [
     MessageTemplateStore,
-    MessageTemplatesService
+    MessageTemplatesService,
+    ScheduleMessageService,
+    ScheduledMessageStore
   ]
 })
 export class PrivateStateMessageTemplatesModule {}
