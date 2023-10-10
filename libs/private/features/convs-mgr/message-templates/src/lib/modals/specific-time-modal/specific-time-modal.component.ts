05e8421eb76f9acc29386170d3b2f3a055ef7900
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-10 04:24:04 +0300

    :watch:  :package:  specific time modal

---
+++ libs/private/features/convs-mgr/message-templates/src/lib/modals/specific-time-modal/specific-time-modal.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+
+import { MessageTemplatesService } from '@app/private/state/message-templates';
+
+@Component({
+  selector: 'app-specific-time-modal',
+  templateUrl: './specific-time-modal.component.html',
+  styleUrls: ['./specific-time-modal.component.scss'],
+})
+export class SpecificTimeModalComponent {
+  @Output() dateTimeSelected = new EventEmitter<{ date: Date, time: string }>();
+
+  selectedDate: Date;
+  selectedTime: string;
+  action: string;
+
+  constructor(
+    private dialogRef: MatDialogRef<SpecificTimeModalComponent>, 
+    private _route$$: Router,
+    ) {}
+
+  ngOnInit(): void {
+    this.action = this._route$$.url.split('/')[2];
+  }
+
+  dateChanged(event: any): void {
+    this.selectedDate = event.value;
+  }
+
+  saveDateTime(): void {
+    if (this.selectedDate && this.selectedTime) {
+      const selectedDateTime = new Date(this.selectedDate);
+      const timeParts = this.selectedTime.split(':');
+      selectedDateTime.setHours(Number(timeParts[0]));
+      selectedDateTime.setMinutes(Number(timeParts[1]));
+      
+      this.dateTimeSelected.emit({ date: selectedDateTime, time: this.selectedTime });
+
+      this.dialogRef.close();
+    }
+  }
+}
