3cbee0f70280c1b6c727ec7bf80a79e4a900b062
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-11 10:38:27 +0300

    :art:  constants added in one file

---
+++ libs/private/features/convs-mgr/message-templates/src/lib/utils/constants.ts
@@ -0,0 +1,11 @@
+export const categoryOptions = [
+    { value: 'AUTHENTICATION', display: 'Authentication' },
+    { value: 'MARKETING', display: 'Marketing' },
+    { value: 'UTILITY', display: 'Utility' },
+  ];
+  export const languageOptions = [
+    { value: 'en_US', display: 'English' },
+    { value: 'fr', display: 'French' },
+    { value: 'sw', display: 'Swahili' },
+  ];
+    
