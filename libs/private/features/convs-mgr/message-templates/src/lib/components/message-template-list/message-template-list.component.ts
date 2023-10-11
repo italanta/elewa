d8025eb82397b6a62032b471da9ec4a63d3f56fb
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-11 08:54:07 +0300

    :pencil: order of imports

--- libs/private/features/convs-mgr/message-templates/src/lib/components/message-template-list/message-template-list.component.ts
+++ libs/private/features/convs-mgr/message-templates/src/lib/components/message-template-list/message-template-list.component.ts
@@ -2,10 +2,12 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+
+import { SubSink } from 'subsink';
+import { Observable, combineLatest, map } from 'rxjs';
+
 import { MessageTemplate, TemplateHeaderTypes, TextHeader } from '@app/model/convs-mgr/functions';
 import { MessageTemplatesService, MessageStatusRes } from '@app/private/state/message-templates';
-import { Observable, combineLatest, map } from 'rxjs';
-import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-message-template-list',
