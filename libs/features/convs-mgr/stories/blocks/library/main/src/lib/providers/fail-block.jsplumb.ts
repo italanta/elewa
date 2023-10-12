358d9769e20531e9e51f84f4f6af409838295829
mutagah <kihahumutagah@gmail.com>
2023-10-12 21:44:33 +0300

    :lipstick:feat: Removal of the second jsplumb.

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/fail-block.jsplumb.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/fail-block.jsplumb.ts
@@ -26,15 +26,5 @@ export function _FailBlockDecoratePlumb(block: FailBlock, comp: ComponentRef<Blo
     maxConnections: -1
   });
 
-  // Adding the second static jsPlumb connector
-  jsPlumb.addEndpoint(comp.location.nativeElement, {
-    // Whether the anchor is target (Other Block -> This Block)
-    source: true,
-    cssClass:"block_endpoint",
-    endpoint: "Dot",
-    anchor: [1, 0.87, 0, 1],
-    maxConnections: -1
-  });
-  
   return comp;
 }
