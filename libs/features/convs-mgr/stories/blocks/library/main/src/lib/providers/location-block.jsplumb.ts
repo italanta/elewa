de83d05db3e9d782907bdee381039720a234d64b
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-10-13 17:36:06 +0300

    :lipstick: styled the jsPlumb endpoints

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/location-block.jsplumb.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/location-block.jsplumb.ts
@@ -22,7 +22,7 @@ export function _LocationBlockDecoratePlumb(block: LocationMessageBlock, comp: C
     target: true,
     cssClass:"block_endpoint",
     endpoint: "Dot",
-    anchor: "Left",
+    anchor: [0, 0.12, 0, 1],
     maxConnections: -1
   });
 
