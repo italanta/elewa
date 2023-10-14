bf0d6b1df04dbe98f38783d406a677de01dac805
Levy <lnjiru43@gmail.com>
2023-10-14 21:18:35 +0300

    updated position of the jsplum connectors

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/webhook-block.jsplumb.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/providers/webhook-block.jsplumb.ts
@@ -22,7 +22,7 @@ export function _WebhookBlockDecoratePlumb(block: WebhookBlock, comp: ComponentR
     target: true,
     cssClass:"block_endpoint",
     endpoint: "Dot",
-    anchor: [0, 0.07 , 0, 1],
+    anchor: [0, 0.05 , 0, 1],
     maxConnections: -1
   });
 
@@ -31,7 +31,7 @@ export function _WebhookBlockDecoratePlumb(block: WebhookBlock, comp: ComponentR
     target: true,
     cssClass:"block_endpoint",
     endpoint: "Dot",
-    anchor: [1, 0.9 , 0, 1],
+    anchor: [1, 0.88 , 0, 1],
     maxConnections: -1
   });
 
