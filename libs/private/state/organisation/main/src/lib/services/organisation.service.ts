70ede7be020262629984f9b16fada9816b23893c
japhethnyaranga <nyaranga4@gmail.com>
2023-10-16 00:47:33 +0300

    💄 edited image styles and removed reload feature

--- libs/private/state/organisation/main/src/lib/services/organisation.service.ts
+++ libs/private/state/organisation/main/src/lib/services/organisation.service.ts
@@ -87,11 +87,7 @@ export class OrganisationService {
   updateOrgDetails(org: Organisation, fileUrl?: string) {
     org.logoUrl = fileUrl ?? '';
 
-    this._orgs$$.update(org).subscribe((org) => {
-      if (org) {
-        window.location.reload();
-      }
-    });
+    this._orgs$$.update(org).subscribe();
   }
 
   getOrgPermissions () {
