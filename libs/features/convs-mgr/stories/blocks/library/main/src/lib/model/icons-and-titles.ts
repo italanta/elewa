dadfff618babf0731a6112c7390dc4193fb4e311
ndush <mdamarisnduku@gmail.com>
2023-10-17 00:40:43 +0300

    added svg icon field

--- libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/icons-and-titles.ts
+++ libs/features/convs-mgr/stories/blocks/library/main/src/lib/model/icons-and-titles.ts
@@ -59,6 +59,7 @@ export const iconsAndTitles: any = {
   16: {
     title: 'PAGE-CONTENT.BLOCK.TITLES.LIST',
     icon: 'fas fa-list-ul',
+    svgIcon: '/assets/icons/question-light.svg',
   },
   17: {
     title: 'PAGE-CONTENT.BLOCK.TITLES.REPLY',
