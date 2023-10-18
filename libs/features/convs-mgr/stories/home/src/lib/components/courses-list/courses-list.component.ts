62846cd057fb5d1ffc4e1a65270dcd07ee1f715c
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-10-18 10:32:20 +0300

    :art: style: design course list item

--- libs/features/convs-mgr/stories/home/src/lib/components/courses-list/courses-list.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/components/courses-list/courses-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 import { Observable } from 'rxjs';
 
 import { BotModule } from '@app/model/convs-mgr/bot-modules';
-import { Bot } from '@app/model/convs-mgr/bots';
+import { Bot, BotMutationEnum } from '@app/model/convs-mgr/bots';
 import { Story } from '@app/model/convs-mgr/stories/main';
+
+import { CreateLessonModalComponent } from '../../modals/create-lesson-modal/create-lesson-modal.component';
 
 @Component({
   selector: 'italanta-apps-courses-list',
@@ -23,4 +26,18 @@ export class CoursesListComponent {
       >;
     }[]
   >;
+  
+  constructor(private _dialog: MatDialog){}
+
+  createLesson(moduleId: string) {
+    const dialogData = {
+      botMode: BotMutationEnum.CreateMode,
+      botModId: moduleId
+    };
+
+    this._dialog.open(CreateLessonModalComponent, {
+      minWidth: '600px',
+      data: dialogData,
+    });
+  }
 }
