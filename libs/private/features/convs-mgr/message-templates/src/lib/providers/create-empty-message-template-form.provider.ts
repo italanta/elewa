1121c20616a319acfb6f68b9d0e79a4fd8cba4b8
k0imburi <35740939+paulkahura@users.noreply.github.com>
2023-10-18 11:22:37 +0300

    :art:  throw warning if name is invalid

--- libs/private/features/convs-mgr/message-templates/src/lib/providers/create-empty-message-template-form.provider.ts
+++ libs/private/features/convs-mgr/message-templates/src/lib/providers/create-empty-message-template-form.provider.ts
@@ -1,8 +1,10 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { customTextValidator } from '../utils/validator';
+
 export function createEmptyTemplateForm(fb: FormBuilder): FormGroup {
   return fb.group({
-    name: ['', Validators.required],
+    name: ['', [Validators.required, Validators.pattern(/^[a-z0-9_-]{1,512}$/), customTextValidator]],
     category: ['', Validators.required],
     channelId: ['', Validators.required],
     language: ['', Validators.required],
