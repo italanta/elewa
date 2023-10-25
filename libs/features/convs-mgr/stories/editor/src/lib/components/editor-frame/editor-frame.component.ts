07ede45e03d753d88dc3cc709cfa55124ea1307f
Jente Rosseel <rosseeljente@gmail.com>
2023-10-25 18:45:44 +0300

    :wrench: Move zoom down to frame level control + integrate jsPlumb zoom

--- libs/features/convs-mgr/stories/editor/src/lib/components/editor-frame/editor-frame.component.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/components/editor-frame/editor-frame.component.ts
@@ -6,6 +6,7 @@ import { StoryEditorState } from '@app/state/convs-mgr/story-editor';
 import { StoryEditorFrame } from '../../model/story-editor-frame.model';  
 import { StoryEditorInitialiserService } from '../../providers/story-editor-initialiser.service';
 import { SubSink } from 'subsink';
+import { STORY_EDITOR_HEIGHT, STORY_EDITOR_WIDTH } from '../../utils/frame-size';
 
 @Component({
   selector: 'convl-story-editor-frame',
@@ -27,6 +28,12 @@ export class StoryEditorFrameComponent implements AfterViewInit, OnDestroy //imp
   /** Observable bounding box of the viewport */
   public viewportBounds$: Observable<DOMRect> = this._viewportBounds$$.pipe(filter(f => !!f));
 
+  // Set frame width and height at minimum on load.
+  private _zoom = 1;
+  frameWidth = STORY_EDITOR_WIDTH;
+  frameHeight = STORY_EDITOR_HEIGHT;
+
+  private _frame: StoryEditorFrame;
   private _frameState$$ = new BehaviorSubject<StoryEditorState>(null as any as StoryEditorState);
   public frameState$: Observable<StoryEditorState> = this._frameState$$.pipe(filter(f => !!f));
 
@@ -34,26 +41,24 @@ export class StoryEditorFrameComponent implements AfterViewInit, OnDestroy //imp
 
 
   ngAfterViewInit() {
-    const frame = this._frameInitialiser.initialiseEditor(this.editorVC, this.drawArea);
+    this._frame = this._frameInitialiser.initialiseEditor(this.editorVC, this.drawArea);
 
     // Transfer listener for frame state changes.
     //    This transfer is necessary as the listener is initialised async and 
     //      can thus be null in the child components in case they render too fast.
     this._sbS.sink =
-      frame.frameChanges$.subscribe(f => 
+      this._frame.frameChanges$.subscribe(f => 
       {
         if(f) 
           this._frameState$$.next(f);
 
-        this.viewPortScrolled();
+        this.viewPortScrolled(null);
       });
 
-    this.frameLoaded.emit(frame);
-
-    
+    this.frameLoaded.emit(this._frame); 
   }
 
-  viewPortScrolled(): void 
+  viewPortScrolled(event: any): void 
   { 
     const editorPosition     = this.editorVC.nativeElement.getBoundingClientRect();
     const viewportDimensions = this.viewport.nativeElement.getBoundingClientRect();
@@ -68,9 +73,58 @@ export class StoryEditorFrameComponent implements AfterViewInit, OnDestroy //imp
     this._viewportBounds$$.next(viewportBounds);
   }
 
+  //
+  // SECTION - ZOOM
+
   onPinch(level:number){
-    this.pinchZoom.emit(level)
+    return level > 0 ? this.increaseFrameZoom() : this.decreaseFrameZoom();
   }
+
+  /** Increase the frame zoom size */
+  increaseFrameZoom() {
+    if (this._zoom < 1)
+    {
+      this._zoom = this._zoom += 0.03;
+      this._applyZoom();
+    }
+
+    return this._zoom;
+  }
+
+  /** Decrease the frame zoom size */
+  decreaseFrameZoom() {
+    if (this._zoom >= 0.25)
+    {
+      this._zoom = this._zoom -= 0.03;
+      this._applyZoom();
+    }
+    return this._zoom;
+  }
+
+  /** Apply a zoom to the story editor */
+  setFrameZoom(value: number) {
+    if(value >= 0.25 && value <= 1)
+    {
+      this._zoom = value;
+      this._applyZoom();
+    }
+
+    return this._zoom;
+  }
+
+  private _applyZoom()
+  {
+    // this.viewport.nativeElement.style.transform = `scale(${this._zoom})`;
+    this.editorVC.nativeElement.style.transform = `scale(${this._zoom})`;
+    this._frame.setZoom(this._zoom);
+
+    // Apply bounding box transformations
+    this.frameWidth = STORY_EDITOR_WIDTH * this._zoom;
+    this.frameHeight = STORY_EDITOR_HEIGHT * this._zoom;
+  }
+
+  // SECTION - CLEANUP
+  //
 
   ngOnDestroy(): void {
     this._sbS.unsubscribe();
