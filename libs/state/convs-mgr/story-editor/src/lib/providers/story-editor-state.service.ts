9b0cd72baed57b749895a82b8799290d5ddc2a24
Jente Rosseel <rosseeljente@gmail.com>
2023-11-01 10:37:09 +0300

    Revert ":bug: :zap: quick fix for story saving issue"
    This reverts commit 208f1a79517a10b385e32ec874fe465d8ab3782f.

--- libs/state/convs-mgr/story-editor/src/lib/providers/story-editor-state.service.ts
+++ libs/state/convs-mgr/story-editor/src/lib/providers/story-editor-state.service.ts
@@ -93,7 +93,7 @@ export class StoryEditorStateService
                           : of(false);
 
     const blockActions$ = this._determineBlockActions(state.blocks);
-    const actions$ = shouldAddNewConns ? blockActions$.concat(addNewConnections$ as any)
+    const actions$ = shouldAddNewConns ? blockActions$.concat(addNewConnections$ as Observable<StoryBlockConnection[]> as any)
                                        : blockActions$;
 
     // Persist the story and all the blocks
@@ -146,12 +146,8 @@ export class StoryEditorStateService
     // Todo: check for deleted connections and upated conections
     private _determineConnections(connections: StoryBlockConnection[]) 
     {
-      const oldConns = (this._lastLoadedState as StoryEditorState).connections;
       // fetches only the new connections
-      let newConnections = this.fetchNewJsPlumbConnections(connections);
-
-      // Filter out old connections
-      // newConnections = newConnections.filter(newConn => !oldConns.find(oCon => newConn.id === oCon.id));
+      const newConnections = this.fetchNewJsPlumbConnections(connections);
 
       //TODO: chain connections actions -> refer to determineBlockActions
       return newConnections;
@@ -159,7 +155,7 @@ export class StoryEditorStateService
 
     /** Creates a block. */
     private _createBlock(block: StoryBlock) {
-      return this._blocks$$.addBlock(block);
+      return this._blocks$$.add(block, block.id);
     }
 
     /** We cannot just delete blocks as active chat users might have their cursor on that block. 
