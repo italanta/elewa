6a0a2200346e2720e2c2f779c972181d8192012a
japhethnyaranga <nyaranga4@gmail.com>
2023-11-03 16:18:21 +0300

    💄 created add-=channel modal

--- libs/private/features/convs-mgr/settings/organisation-details/src/lib/modals/add-channel-modal/add-channel-modal.component.ts
+++ libs/private/features/convs-mgr/settings/organisation-details/src/lib/modals/add-channel-modal/add-channel-modal.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-channel-modal',
   templateUrl: './add-channel-modal.component.html',
   styleUrls: ['./add-channel-modal.component.scss'],
 })
-export class AddChannelModalComponent {}
+export class AddChannelModalComponent {
+  selectedPlatform: string | null = null;
+  showWhatsAppForm = false;
+  showMessengerForm = false;
+  showOptionBoxes = true;
+  continueButtonText = 'Continue';
+
+  constructor(
+    private _dialog: MatDialog,
+  ){}
+
+
+  showForm() {
+    if (this.selectedPlatform === 'WhatsApp') {
+      this.showWhatsAppForm = true;
+      this.showMessengerForm = false;
+      this.showOptionBoxes = false;
+      this.continueButtonText = 'Add Channel';
+    } else if (this.selectedPlatform === 'Messenger') {
+      this.showWhatsAppForm = false;
+      this.showMessengerForm = true;
+      this.showOptionBoxes = false;
+      this.continueButtonText = 'Add Channel';
+    }
+  }
+  
+
+  closeModal() {
+    // this._dialog.close();
+  }
+}
