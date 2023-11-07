df61b0c9fef289818577024ca8abe82027a94708
Beulah Mathenge <beulahdaizy@gmail.com>
2023-11-07 14:47:13 +0300

    :label: The payments model

---
+++ libs/model/payments/mollie-payments/src/lib/payment.interface.ts
@@ -0,0 +1,33 @@
+import { IObject } from "@iote/bricks";
+ 
+/**
+* The payment object expected by Mollie API.
+* Amount, description, redirect url, are compulsory.
+* Making method an optional param so that when a user is redirected to mollie, they can choose the method they prefer
+*/
+export interface Payment extends IObject{
+    amount: Amount;
+    description: string;
+    redirectUrl: string;
+    cancelUrl: string;
+    webhookUrl?: string;
+    sequenceType?: SequenceType;
+    method : string  
+    customerId: string;
+    status: string;
+}
+ 
+/**
+* The amount object expected should have currency and value keys.
+* The value is a string of numbers without the thousand assertion ie 10000 != 10,000
+*/
+export interface Amount {
+    currency: string;
+    value: string;
+}
+ 
+export interface SequenceType {
+    Oneoff:  "oneoff",
+    First: "first",
+    Reccuring: "recurring"
+}
