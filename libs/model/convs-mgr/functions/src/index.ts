f8acb7e8a114c2cc9b74f996cd34821168ef0b48
Reagan Charana <rgncharana2@gmail.com>
2023-11-07 16:04:55 +0300

    :house: update interfaces to support surveys

--- libs/model/convs-mgr/functions/src/index.ts
+++ libs/model/convs-mgr/functions/src/index.ts
@@ -33,3 +33,4 @@ export * from './lib/whatsapp/message-templates/whatsapp-template-component-type
 export * from './lib/whatsapp/message-templates/whatsapp-template-components.interface';
 export * from './lib/whatsapp/message-templates/whatsapp-template-header-component';
 export * from './lib/base/schedule/send-message-template.interface';
+export * from './lib/base/schedule/schedule-options.interface';
