03474df6788c5e5ae99501b179360fd34b04db72
Beulah Mathenge <beulahdaizy@gmail.com>
2023-11-07 15:17:37 +0300

    :label: Base mollie subscription object

---
+++ libs/model/payments/mollie-payments/src/lib/mollie-subscription.interface.ts
@@ -0,0 +1,17 @@
+import { IObject } from "@iote/bricks";
+import { Amount } from "./payment.interface";
+
+ 
+/**
+* Interval takes types monthy, weekly, yearly
+* @see https://docs.mollie.com/payments/recurring on how to handle recurring payments
+*/
+export interface Subscription extends IObject{
+    amount:            Amount;
+    interval:          string;
+    description:       string;
+    orgId:             string;
+    webhookUrl:        string;
+    customerId:        string;
+    sequenceType:      string;   
+}
