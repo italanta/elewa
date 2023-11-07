6823ba9e0a7d8363d74c9a1dd7465a33cfcab313
Beulah Mathenge <beulahdaizy@gmail.com>
2023-11-07 15:40:21 +0300

    :sparkles: Constructor method for the create customer service

---
+++ libs/private/functions/payments/core/src/lib/services/customer-core.service.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+ 
+import { createMollieClient } from '@mollie/api-client';
+
+import { HandlerTools } from '@iote/cqrs';
+
+export class MollieCustomerService 
+{
+    private mollieClient;
+    private _apiKey: string;
+
+    constructor(
+        private tools: HandlerTools
+    ){
+        this._apiKey = process.env.MOLLIE_API_KEY as string;
+        this.mollieClient = createMollieClient({apiKey: this._apiKey});
+        this.tools = tools;
+    }
+}
