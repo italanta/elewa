852e3b8fd55ea4fd3c6d654eb3d6c1e70e1793d8
Beulah Mathenge <beulahdaizy@gmail.com>
2023-11-07 15:02:10 +0300

    :label: Customer interface

---
+++ libs/model/payments/mollie-payments/src/lib/customer.interface.ts
@@ -0,0 +1,10 @@
+import { IObject } from "@iote/bricks";
+ 
+/**
+* The customer id is returned by the customer API and will be saved to db
+*/
+export interface Customer extends IObject {
+    customerId?:     string
+    name:           string;
+    email:          string;
+}
