0c13fbbf33afb262ae55c2d070898299e1f99767
japhethnyaranga <nyaranga4@gmail.com>
2023-11-16 08:50:19 +0300

    🎨 closed dialog modal on cancel

--- libs/private/features/convs-mgr/settings/user-profile/src/lib/modals/deactivate-user/deactivate-user.component.ts
+++ libs/private/features/convs-mgr/settings/user-profile/src/lib/modals/deactivate-user/deactivate-user.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-deactivate-user',
   templateUrl: './deactivate-user.component.html',
   styleUrls: ['./deactivate-user.component.scss'],
 })
-export class DeactivateUserComponent {}
+export class DeactivateUserComponent {
+  constructor(
+    private _dialog: MatDialog,
+  ){}
+
+
+  closeModal(){
+    this._dialog.closeAll()
+  }
+}
