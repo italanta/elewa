a84771bfe542e04defaca7d5e4c5f3e54c1e6f1a
Reagan Charana <rgncharana2@gmail.com>
2023-11-21 14:21:21 +0300

    :bug: :sparkles: create delete overlay on editor initialize

--- libs/features/convs-mgr/stories/editor/src/lib/providers/story-editor-initialiser.service.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/providers/story-editor-initialiser.service.ts
@@ -6,7 +6,7 @@ import { BrowserJsPlumbInstance, ContainmentType, newInstance as initJsPlumb } f
 import { BlockInjectorService } from '@app/features/convs-mgr/stories/blocks/library/main';
 
 import { StoryEditorFrame } from '../model/story-editor-frame.model';
-import { CreateDeleteButton } from './manage-jsPlumb-connections.function';
+import { DeleteConnOverlaySpec } from '../utils/overlays/delete-connection-overlay.util';
 
 @Injectable()
 export class StoryEditorInitialiserService
@@ -28,29 +28,12 @@ export class StoryEditorInitialiserService
       dragOptions:{
         containment: ContainmentType.parent
       },
-      connectionOverlays: [
-        {
-          // Specify the type of overlay as "Custom"
-          type: 'Custom',
-          options: {
-            // Set the id of the overlay to the connection id
-            // id: "3",
-            create: (component: any, conn: any) => {
-              // Create the delete button element and return it
-              return CreateDeleteButton();
-            },
-            // Set the location of the overlay as 0.5
-            location: 0.5,
-            events: {
-              // Add a double-click event to the overlay
-              dblclick: ((overlayData: any) => {
-                debugger
-                return DeleteConnectorbyID(overlayData)
-              }).bind(this)
-            },
-          },
-        },
-      ],
+      
+      // Specify connection overlays here so that
+      //  they can be initialized with the editor and
+      //   will be default for all the created connections
+      connectionOverlays: [ DeleteConnOverlaySpec ]
+      
       // paintStyle: { strokeWidth: 1 },
       // anchors: [["Left", "Right", "Bottom"], ["Top", "Bottom"]],
     });
@@ -59,20 +42,5 @@ export class StoryEditorInitialiserService
 
     return new StoryEditorFrame(this._fb, _jsplumb, this._blocksInjector, viewport, editorContainer);
   }
-}
-
-export function DeleteConnectorbyID(overlayData: any) {
-  const connections = overlayData.overlay.instance.connections;
-  const connectionId = overlayData.overlay.component.id;
-  // Get an array of all connections using the jsPlumb library
-  // const connections: any = jsPlumb.getConnections();
-
-  // Filter the connections obtained from the jsPlumb library by the filtered connection ID
-  const dCon = connections.filter((c: any) => (c.id === connectionId));
-  debugger
-  // Delete each filtered connection using the jsPlumb.deleteConnection method
-  dCon.forEach((cn: any) => {
-    overlayData.overlay.instance.deleteConnection(cn);
-  });
 }
 
