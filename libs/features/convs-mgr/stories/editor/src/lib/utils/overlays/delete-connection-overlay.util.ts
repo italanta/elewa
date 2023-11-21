153a422a233ee6d30334ab09e43cff274f23e770
Reagan Charana <rgncharana2@gmail.com>
2023-11-21 14:22:44 +0300

    :art: clean up code

--- libs/features/convs-mgr/stories/editor/src/lib/utils/overlays/delete-connection-overlay.util.ts
+++ libs/features/convs-mgr/stories/editor/src/lib/utils/overlays/delete-connection-overlay.util.ts
@@ -7,7 +7,8 @@ export const DeleteConnOverlaySpec = {
   options: {
     // Set the id of the overlay to the connection id
     id: "deleteConnection",
-    create: (component: any, conn: any) => {
+    create: (component: any, conn: any) =>
+    {
       // Create the delete button element and return it
       return CreateDeleteButton();
     },
@@ -15,14 +16,16 @@ export const DeleteConnOverlaySpec = {
     location: 0.5,
     events: {
       // Add a double-click event to the overlay
-      dblclick: ((overlayData: any) => {
-        return DeleteConnectorbyID(overlayData)
+      dblclick: ((overlayData: any) =>
+      {
+        return DeleteConnectorbyID(overlayData);
       }).bind(this)
     },
   },
-}
+};
 
-function DeleteConnectorbyID(overlayData: any) {
+function DeleteConnectorbyID(overlayData: any)
+{
   const connections = overlayData.overlay.instance.connections;
   const connectionId = overlayData.overlay.component.id;
   // Get an array of all connections using the jsPlumb library
@@ -32,7 +35,8 @@ function DeleteConnectorbyID(overlayData: any) {
   const dCon = connections.filter((c: any) => (c.id === connectionId));
 
   // Delete each filtered connection using the jsPlumb.deleteConnection method
-  dCon.forEach((cn: any) => {
+  dCon.forEach((cn: any) =>
+  {
     overlayData.overlay.instance.deleteConnection(cn);
   });
 
@@ -40,7 +44,8 @@ function DeleteConnectorbyID(overlayData: any) {
 }
 
 
-function CreateDeleteButton() {
+function CreateDeleteButton()
+{
   // Create a button element
   const deleteButton = document.createElement("button");
 
@@ -51,11 +56,12 @@ function CreateDeleteButton() {
   deleteButton.id = "overlay-delete-button";
   // deleteButton.classList.add("overlay-button ");
 
-   // Add event listener for mouseover event
-   deleteButton.addEventListener("mouseover", () => {
+  // Add event listener for mouseover event
+  deleteButton.addEventListener("mouseover", () =>
+  {
     deleteButton.title = "Double click to delete";
   });
 
   // Return the created button
-  return deleteButton
+  return deleteButton;
 }
