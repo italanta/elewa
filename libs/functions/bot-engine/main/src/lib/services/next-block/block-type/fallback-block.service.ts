e4ef87fda452ac68a6448b047f19b7e2136a020a
Reagan Charana <rgncharana2@gmail.com>
2023-11-22 17:46:56 +0300

    :wrench: :horse_racing: create service to manage fallbacks

---
+++ libs/functions/bot-engine/main/src/lib/services/next-block/block-type/fallback-block.service.ts
@@ -0,0 +1,93 @@
+import { CommunicationChannel, Cursor } from "@app/model/convs-mgr/conversations/admin/system";
+import { StoryBlock, StoryBlockTypes } from "@app/model/convs-mgr/stories/blocks/main";
+import { Button, QuestionMessageBlock } from "@app/model/convs-mgr/stories/blocks/messaging";
+import { ButtonsBlockButton } from "@app/model/convs-mgr/stories/blocks/scenario";
+import { Message, QuestionMessage } from "@app/model/convs-mgr/conversations/messages";
+
+import { BlockDataService } from "../../data-services/blocks.service";
+
+export class FallBackBlockService 
+{
+
+  getBlock(failedBlockId: string)
+  {
+    /**
+     * The fall back block will reuse the structure of the question block. But
+     *   they block type is different so that we can handle it differently on 
+     *    the operation block level.
+     */
+    const block: QuestionMessageBlock = {
+      id: `${failedBlockId}-fallback-${Date.now()}`,
+      type: StoryBlockTypes.FallbackBlock,
+      position: null,
+      deleted: false,
+      blockTitle: '',
+      blockIcon: '',
+      message: `Sorry, I could not understand your response. Please select of the below:`,
+      options: this.__getFallBackOptions()
+    };
+    return block;
+  }
+
+  private __getFallBackOptions()
+  {
+    const fallBackOptions: ButtonsBlockButton<Button>[] = [
+      {
+        id: "1",
+        message: "Resend last message",
+      },
+      {
+        id: "2",
+        message: "Restart Lesson",
+      },
+      {
+        id: "3",
+        message: "Restart Conversation",
+      },
+    ];
+
+    return fallBackOptions;
+  }
+
+  async fallBack(channel: CommunicationChannel, currentCursor: Cursor, blockDataService: BlockDataService, message: Message)
+	{
+    let newCursor: Cursor;
+    let nextBlock: StoryBlock;
+
+    const response =  message as QuestionMessage;
+    const firstStory = channel.defaultStory;
+    const orgId = channel.orgId;
+    const currentStory = currentCursor.position.storyId;
+    const lastBlockId = currentCursor.position.blockId.split('-')[0];
+    
+    switch (response.options[0].optionId) {
+      case '1':
+        // Resend the last block
+        newCursor.position.blockId = lastBlockId;
+        nextBlock = await blockDataService.getBlockById(lastBlockId, orgId, currentStory);
+        break;
+      case '2':
+        // Restart the current lesson
+        nextBlock = await blockDataService.getFirstBlock(orgId, currentStory);
+
+        newCursor.position.blockId = nextBlock.id;
+        break;  
+      case '3':
+        // Restart the conversation from the beginning
+        nextBlock = await blockDataService.getFirstBlock(orgId, firstStory);
+        newCursor.position.blockId = nextBlock.id;
+        newCursor.position.storyId = firstStory;
+        break;
+      default:
+        // Resend the last block
+        nextBlock = await blockDataService.getBlockById(lastBlockId, orgId, currentStory);
+        newCursor.position.blockId = lastBlockId;
+        break;
+    }
+
+		return {
+			nextBlock,
+			newCursor
+		};
+	}
+}
