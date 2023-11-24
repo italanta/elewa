6d6a2c1c053162e3c9512a9ab460512d9f9fda39
Lemmy <lemminton@gmail.com>
2023-11-24 11:10:31 +0300

    :sparkles: feat: compute learner progress

--- libs/features/convs-mgr/learners/src/lib/components/learner-enrolled-courses/learner-enrolled-courses.component.ts
+++ libs/features/convs-mgr/learners/src/lib/components/learner-enrolled-courses/learner-enrolled-courses.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { map } from 'rxjs';
+
+import { BotsStateService } from '@app/state/convs-mgr/bots';
+import { BotModulesStateService } from '@app/state/convs-mgr/modules';
+
+import { EnrolledUserBotModule } from 'libs/model/convs-mgr/learners/src/lib/enrolled-user-courses.interface';
+import { EnrolledEndUser } from '@app/model/convs-mgr/learners';
+import { BotModule } from '@app/model/convs-mgr/bot-modules';
 
 @Component({
   selector: 'app-learner-enrolled-courses',
@@ -8,7 +17,47 @@ import { Component } from '@angular/core';
 export class LearnerEnrolledCoursesComponent {
   isOpen = false;
 
+  @Input() currentLearner: EnrolledEndUser;
+
+  constructor(
+    private _botsStateServ$: BotsStateService,
+    private _botModStateServ$: BotModulesStateService
+  ) {}
+
   toggleCollapsible() {
-    return this.isOpen = !this.isOpen;
+    return (this.isOpen = !this.isOpen);
+  }
+
+  /**  */
+  getLearnerProgress() {
+    return this.currentLearner.courses?.map((course) =>
+      this._botsStateServ$.getBotById(course.courseId).pipe(
+        map((bot) => {
+          return {
+            name: bot?.name,
+            modules: course.modules.map((usrProgModule) =>
+              this.computeLearnerProgress(usrProgModule)
+            ),
+          };
+        })
+      )
+    );
+  }
+
+  /**  */
+  computeLearnerProgress(userprogModule: EnrolledUserBotModule) {
+    return this._botModStateServ$
+      .getBotModuleById(userprogModule.moduleId).pipe(
+        map((mod) => {
+          const totalLessons = (mod as BotModule)?.stories.length | 0;
+          const lessonsTaken = userprogModule.lessons.length;
+          const percentage = totalLessons === 0 ? 0 : (lessonsTaken / totalLessons) * 100;
+
+          return {
+            name: mod?.name,
+            progress: Math.round(percentage),
+          };
+        })
+      );
   }
 }
