4363f6b6a236bbdb2d1fc34d60553876e04c0fb7
Lemmy <lemminton@gmail.com>
2023-11-28 08:48:24 +0300

    :recycle: refactor: rename default classroom's name to Not assigned

--- libs/model/convs-mgr/classroom/src/lib/classroom.interface.ts
+++ libs/model/convs-mgr/classroom/src/lib/classroom.interface.ts
@@ -21,7 +21,7 @@ export enum ClassroomUpdateEnum {
 // TODO: move elsewhere, potentially to DB - to be decided
 /** example of a classroom - we use to group ungrouped users (classroom) */
 export const defaultClassroom: Classroom = {
-  className: 'default',
+  className: 'Not Assigned',
   description: 'the default classroom',
   deleted: false
 }
