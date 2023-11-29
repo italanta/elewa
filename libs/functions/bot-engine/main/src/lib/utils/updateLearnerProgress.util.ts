93aeabcfb42bdefc857dfba844c2ca786c9f507c
Lemmy <lemminton@gmail.com>
2023-11-29 14:58:10 +0300

    :robot: feat: update progress tracking utility

--- libs/functions/bot-engine/main/src/lib/utils/updateLearnerProgress.util.ts
+++ libs/functions/bot-engine/main/src/lib/utils/updateLearnerProgress.util.ts
@@ -6,44 +6,47 @@ import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
 import { EnrolledUserDataService } from "../services/data-services/enrolled-user.service";
 import { BotModuleDataService } from "../services/data-services/botmodules.service";
 import { StoriesDataService } from "../services/data-services/stories.service";
+import { CoursesDataService } from "../services/data-services/courses.service";
 
 /** update the enrolled user's progress with the just completed story(lesson's) details*/
 export async function updateLearnerProgress (currentStory: string, lastBlock:StoryBlock, endUser:EndUser, tools:HandlerTools, orgId:string) {
   const enrolledDataServ = new EnrolledUserDataService(tools, orgId);
   const botModDataService = new BotModuleDataService(tools, orgId);
   const storiesDataService = new StoriesDataService(tools, orgId);
+  const courseDataService = new CoursesDataService(tools, orgId);
 
   const enrolledUser = await enrolledDataServ.getEnrolledUser(endUser.enrolledUserId ?? '');
 
-  const parentModule = (await storiesDataService.getStory(currentStory))?.parentModule;
-  const parentCourse = (await botModDataService.getBotModule(parentModule))?.parentBot;
+  const lesson = (await storiesDataService.getStory(currentStory));
+  const parentModule = (await botModDataService.getBotModule(lesson.parentModule));
+  const parentCourse = (await courseDataService.getBot(parentModule.parentBot));
 
   if (!enrolledUser) return;
 
   if (enrolledUser.courses == null) enrolledUser.courses = [];
 
   // Find or create the course
-  let theCourse = enrolledUser.courses.find(course => course.courseId === parentCourse);
+  let theCourse = enrolledUser.courses.find(course => course.courseId === parentCourse.id);
 
   if (!theCourse) {
-    theCourse = { courseId: parentCourse, modules: [] };
+    theCourse = { courseId: parentCourse.id, courseName: parentCourse.name, modules: [] };
     enrolledUser.courses.push(theCourse);
-  }
+  } else theCourse.courseName = parentCourse.name;
 
   // Find or create the module
-  let theModule = theCourse.modules.find(mod => mod.moduleId === parentModule);
+  let theModule = theCourse.modules.find(mod => mod.moduleId === parentModule.id);
 
   if (!theModule) {
-    theModule = { moduleId: parentModule, lessons: [] };
+    theModule = { moduleId: parentModule.id, moduleName: parentModule.name, lessons: [] };
     theCourse.modules.push(theModule);
-  }
+  } else theModule.moduleName = parentModule.name;
 
   let theLesson = theModule.lessons.find(lesson => lesson.lessonId === currentStory);
 
   if (!theLesson) {
-    theLesson = { lessonId: currentStory, blocks: [] };
+    theLesson = { lessonId: currentStory, lessonName: lesson.name,  blocks: [] };
     theModule.lessons.push(theLesson);
-  }
+  } else theLesson.lessonName = lesson.name;
 
   const block = theLesson.blocks.find((blockId) => blockId === lastBlock.id)
   if (!block) theLesson.blocks.push(lastBlock.id);
--- libs/model/convs-mgr/learners/src/lib/enrolled-user-courses.interface.ts
+++ libs/model/convs-mgr/learners/src/lib/enrolled-user-courses.interface.ts
@@ -1,16 +1,19 @@
 /** progress covered by the enrolled user */
 export interface EnrolledUserCourse {
   courseId: string;
+  courseName: string;
   modules: EnrolledUserBotModule[];
 };
 
 // Define a Module interface
 export interface EnrolledUserBotModule {
   moduleId: string;
+  moduleName: string;
   lessons: EnrolledUserLesson[];
 };
 
 export interface EnrolledUserLesson {
   lessonId: string;
+  lessonName: string;
   blocks: string[];
 };
