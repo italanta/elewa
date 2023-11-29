33752849b47d8b122f9eb1f134b226f981f7c48b
Lemmy <lemminton@gmail.com>
2023-11-29 15:37:37 +0300

    :sparkles: feat: simplify stream to get the users progress

--- libs/features/convs-mgr/learners/src/lib/components/learner-enrolled-courses/learner-enrolled-courses.component.ts
+++ libs/features/convs-mgr/learners/src/lib/components/learner-enrolled-courses/learner-enrolled-courses.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { SubSink } from 'subsink';
-import { combineLatest, map, of, switchMap, tap } from 'rxjs';
+import { combineLatest, map, of, tap } from 'rxjs';
 
-import { BotsStateService } from '@app/state/convs-mgr/bots';
-import { BotModulesStateService } from '@app/state/convs-mgr/modules';
-import { StoryBlocksStore } from '@app/state/convs-mgr/stories/blocks';
 import { StoryStateService } from '@app/state/convs-mgr/stories';
 
 import {
@@ -31,16 +28,11 @@ export class LearnerEnrolledCoursesComponent implements OnInit, OnDestroy {
 
   @Input() currentLearner: EnrolledEndUser;
 
-  constructor(
-    private _botsStateServ$: BotsStateService,
-    private _botModStateServ$: BotModulesStateService,
-    private _lessonStateService$: StoryStateService,
-    private _blocksStateService$: StoryBlocksStore
-  ) {}
+  constructor(private _lessonStateService$: StoryStateService) {}
 
   ngOnInit(): void {
-    this._sBs.sink = this.getLearnerProgress().subscribe((progresss) =>{
-      this.leanerProgress = progresss
+    this._sBs.sink = this.getLearnerProgress().subscribe((progresss) => {
+      this.leanerProgress = progresss;
     });
   }
 
@@ -51,62 +43,45 @@ export class LearnerEnrolledCoursesComponent implements OnInit, OnDestroy {
   /** gets the learner progress */
   getLearnerProgress() {
     this.isLoading = true;
-  
+
     if (!this.currentLearner.courses) {
       return of([]);
     }
 
     const progress = this.currentLearner.courses.map((course) =>
-      this._botsStateServ$.getBotById(course.courseId).pipe(
-        switchMap((bot) =>
-          combineLatest(
-            course.modules.map((usrProgModule) => this.computeProgressAtModuleLevel(usrProgModule))
-          ).pipe(
-            map((modules) => ({
-              name: bot?.name ?? bot?.id,
-              modules: modules,
-            }))
-          )
+      combineLatest(
+        course.modules.map((usrProgModule) =>
+          this.computeProgressAtModuleLevel(usrProgModule)
         )
-      )
+      ).pipe(map((modules) => ({ name: course.courseName, modules: modules })))
     );
 
-    return combineLatest(progress).pipe(tap(() => this.isLoading = false))
+    return combineLatest(progress).pipe(tap(() => (this.isLoading = false)));
   }
 
   /** computes learner progress per module (lessons covered) */
   computeProgressAtModuleLevel(userProgModule: EnrolledUserBotModule) {
     return combineLatest(
-      userProgModule.lessons.map((lesson) => this.computeLearnerProgress(lesson))
-    ).pipe(
-      switchMap((lessons) =>
-        this._botModStateServ$.getBotModuleById(userProgModule.moduleId).pipe(
-          map((mod) => ({
-            name: mod?.name ?? mod?.id,
-            lessons: lessons,
-          }))
-        )
+      userProgModule.lessons.map((lesson) =>
+        this.computeLearnerProgress(lesson)
       )
-    );
+    ).pipe(map((lessons) => ({ name: userProgModule.moduleName, lessons })));
   }
 
   /** computes leaner progress per lesson(blocks covered) */
   computeLearnerProgress(userprogModule: EnrolledUserLesson) {
     return this._lessonStateService$.getStoryById(userprogModule.lessonId).pipe(
-      switchMap((story) =>
-        this._blocksStateService$.getBlocksByStory(story?.id as string, story?.orgId).pipe(
-          map((blocks) => {
-            const totalBlocks = blocks.length ? blocks.length - 2 : 0;
-            const blockedPassed = userprogModule?.blocks?.length ?? 0;
-            const percentage = totalBlocks === 0 ? 0 : (blockedPassed / totalBlocks) * 100;
+      map((story) => {
+        const totalBlocks = story?.blocksCount ? story.blocksCount - 2 : 0;
+        const blockedPassed = userprogModule?.blocks?.length ?? 0;
+        const percentage =
+          totalBlocks === 0 ? 0 : (blockedPassed / totalBlocks) * 100;
 
-            return {
-              name: story?.name ?? story?.id,
-              progress: Math.round(percentage),
-            };
-          })
-        )
-      )
+        return {
+          name: story?.name ?? story?.id,
+          progress: Math.round(percentage),
+        };
+      })
     );
   }
 
