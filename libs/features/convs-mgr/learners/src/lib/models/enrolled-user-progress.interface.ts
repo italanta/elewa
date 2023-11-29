42e1151f205aee511f38c88c83daec2ab9ccd294
Lemmy <lemminton@gmail.com>
2023-11-29 16:12:31 +0300

    :recycle: refactor: break enrolled user progress interface into more types

--- libs/features/convs-mgr/learners/src/lib/models/enrolled-user-progress.interface.ts
+++ libs/features/convs-mgr/learners/src/lib/models/enrolled-user-progress.interface.ts
@@ -1,11 +1,18 @@
+/** user progress per course */
 export interface EnrolledUserProgress {
   name?: string;
   enrollmentDate: Date;
-  modules: {
-    name?: string;
-    lessons: {
-      name?: string;
-      progress: number;
-    }[];
-  }[];
+  modules: Module[];
+}
+
+/** user progress per module */
+export interface Module {
+  name?: string;
+  lessons: Lesson[];
+}
+
+/** user progress per lesson */
+export interface Lesson {
+  name?: string;
+  progress: number;
 }
