e69b28ff8ab6ada5218f48e2c873e139664ff736
ndush <mdamarisnduku@gmail.com>
2023-11-30 10:21:45 +0300

    done radio button functionality

--- libs/features/convs-mgr/stories/blocks/library/list-message-block/src/lib/components/list-block/list-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/list-message-block/src/lib/components/list-block/list-block.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
@@ -12,26 +12,30 @@ import { OptionInputFieldComponent, __FocusCursorOnNextInputOfBlock } from '@app
   templateUrl: './list-block.component.html',
   styleUrls: ['./list-block.component.scss'],
 })
-export class ListBlockComponent<T> implements AfterViewInit 
-{
+export class ListBlockComponent<T> implements AfterViewInit, OnInit {
   @Input() id: string;
   @Input() block: ListMessageBlock;
   @Input() listMessageBlock: FormGroup;
   @Input() jsPlumb: BrowserJsPlumbInstance;
   @ViewChildren('optionInputFields') optionInputFields: QueryList<OptionInputFieldComponent>;
 
-  private currentIndex = 0; 
+  private currentIndex = 0;
 
   readonly listOptionInputLimit = 24;
   readonly listOptionsArrayLimit = 10;
 
-  constructor(private _fb: FormBuilder) 
-  { }
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    if (!this.listMessageBlock.get('RadioButtonControl')) {
+      this.listMessageBlock.addControl('RadioButtonControl', this._fb.control(null));
+    }
+  }
 
   ngAfterViewInit(): void {
     this.block.options?.forEach((listItem) => {
       this.listItems.push(this.addListOptions(listItem));
-    })
+    });
   }
 
   get listItems(): FormArray {
@@ -42,24 +46,48 @@ export class ListBlockComponent<T> implements AfterViewInit
     return this._fb.group({
       id: [listItem?.id ?? `${this.id}-${this.listItems.length + 1}`],
       message: [listItem?.message ?? ''],
-      value: [listItem?.value ?? '']
-    })
+      value: [listItem?.value ?? ''],
+    });
   }
 
   addNewOption() {
-    if (this.listItems.length < this.listOptionsArrayLimit) this.listItems.push(this.addListOptions());
-    setTimeout(() => {
-      this.setFocusOnNextInput();
-    });
+    if (this.listItems.length < this.listOptionsArrayLimit) {
+      this.listItems.push(this.addListOptions());
+      setTimeout(() => {
+        this.setFocusOnNextInput();
+      });
+    }
   }
+
   deleteInput(i: number) {
     this.listItems.removeAt(i);
+    this.hideOptionField();
   }
 
   setFocusOnNextInput() {
-    this.currentIndex = __FocusCursorOnNextInputOfBlock(
-      this.currentIndex,
-      this.optionInputFields
-    );
-}
+    this.currentIndex = __FocusCursorOnNextInputOfBlock(this.currentIndex, this.optionInputFields);
+  }
+
+  updateRadioValue(index: number) {
+    const radioControl = this.listMessageBlock.get('RadioButtonControl') as FormControl;
+    const currentRadioValue = radioControl.value;
+
+    if (currentRadioValue === index) {
+      radioControl.setValue(null);
+    } else {
+      radioControl.setValue(index);
+    }
+
+    this.hideOptionField();
+  }
+
+  hideOptionField() {
+    const radioControl = this.listMessageBlock.get('RadioButtonControl') as FormControl;
+    if (!this.isOptionFieldVisible(radioControl.value)) {
+    }
+  }
+
+  isOptionFieldVisible(selectedIndex: number): boolean {
+    return selectedIndex !== null;
+  }
 }
