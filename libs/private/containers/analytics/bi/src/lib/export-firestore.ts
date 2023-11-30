6b4b4eeaf3b59f13100936a1b0b1c84640b56ba8
Reagan Charana <rgncharana2@gmail.com>
2023-11-30 15:52:53 +0300

    :wrench: export to the same folder

--- libs/private/containers/analytics/bi/src/lib/export-firestore.ts
+++ libs/private/containers/analytics/bi/src/lib/export-firestore.ts
@@ -10,7 +10,7 @@ const project = process.env.PROJECT || 'default';
 app.get('/', (req, res) => {
     const bucketName = 'analytics-bi';
 
-    const command = `gcloud firestore export gs://${bucketName}/${project}/export-${Date.now()} --collection-ids=${collectionIds.join(',')}`;
+    const command = `gcloud firestore export gs://${bucketName}/${project}/exports --collection-ids=${collectionIds.join(',')}`;
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
