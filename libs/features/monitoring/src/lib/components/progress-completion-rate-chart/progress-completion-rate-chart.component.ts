886dca993736d2eb1a691c6d0956a756e0fb1f26
Lemmy <lemminton@gmail.com>
2023-12-05 12:18:09 +0300

    :recycle: refactor: refactor progress chart generation logic

--- libs/features/monitoring/src/lib/components/progress-completion-rate-chart/progress-completion-rate-chart.component.ts
+++ libs/features/monitoring/src/lib/components/progress-completion-rate-chart/progress-completion-rate-chart.component.ts
@@ -1,43 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 
 import { Chart } from 'chart.js/auto';
 import { SubSink } from 'subsink';
+import { switchMap } from 'rxjs';
 
 import { ProgressMonitoringService } from '@app/state/convs-mgr/monitoring';
 
 import { BotModulesStateService } from '@app/state/convs-mgr/modules'
 import { BotsStateService } from '@app/state/convs-mgr/bots';
-import { ClassroomService } from '@app/state/convs-mgr/classrooms';
 
 import { Bot } from '@app/model/convs-mgr/bots';
 import { BotModule } from '@app/model/convs-mgr/bot-modules';
 import { CompletionRateProgress } from '@app/model/analytics/group-based/progress';
 
-import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-progress-completion-rate-chart',
   templateUrl: './progress-completion-rate-chart.component.html',
   styleUrls: ['./progress-completion-rate-chart.component.scss'],
 })
-export class ProgressCompletionRateChartComponent {
+export class ProgressCompletionRateChartComponent implements OnDestroy {
   chart: Chart<'doughnut'>;
 
   private _sBs = new SubSink();
 
   courses: Bot[];
   activeCourse: Bot;
+
   allBotModules: BotModule[];
   modulesInActiveCourse: BotModule[];
   selectedModule: BotModule;
 
-  completionRateData: CompletionRateProgress;
-
   showData = false;
+  completionRateData: CompletionRateProgress;
 
   constructor (
     private _progressService: ProgressMonitoringService,
-    private _clasroomServ$: ClassroomService,
     private _botModServ$: BotModulesStateService,
     private _botServ$: BotsStateService
   ) {}
@@ -60,7 +58,7 @@ export class ProgressCompletionRateChartComponent {
     });
   }
 
-  /** initialise the data layer (fetch bots, modules and classrooms) */
+  /** initialise the data layer (fetch bots, modules) */
   initDataLayer() {
     this._sBs.sink = this._botServ$.getBots().pipe(
       switchMap(bots => {
@@ -73,11 +71,26 @@ export class ProgressCompletionRateChartComponent {
   /** select progress tracking periodicals */
   selectActiveCourse(course: Bot) {
     if (!this.completionRateData) return;
+    this.modulesInActiveCourse = this.allBotModules.filter((botMod) => botMod.parentBot === course.id);
+    this.selectedModule = this.modulesInActiveCourse[0];
 
-    if (this.activeCourse.name === 'All') {
-      this.modulesInActiveCourse = [];
-    } else {
-      this.modulesInActiveCourse = this.allBotModules.filter((botMod) => botMod.parentBot === course.id);
+    this.updateChart();
+  }
+
+  // called when a module is selected
+  onSelectChange() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    this.updateChart();
+  }
+
+  private updateChart() {
+    if (this.chart) this.chart.destroy();
+    
+    if (this.showData) {
+      this.chart = this._loadChart(this.completionRateData);
     }
   }
 
@@ -86,7 +99,6 @@ export class ProgressCompletionRateChartComponent {
       this.chart.destroy();
     }
 
-    // don't generate graph if no data is present
     return new Chart('completion-chart', {
       type: 'doughnut',
       data: {
@@ -96,8 +108,8 @@ export class ProgressCompletionRateChartComponent {
     });
   }
 
-
   getChartData(chartData: CompletionRateProgress) {
+    // if all course is selected we return avgProgress for allcourses;
     if (this.activeCourse.name === 'All') {
       return {
         label: 'All Courses',
@@ -107,16 +119,30 @@ export class ProgressCompletionRateChartComponent {
       }
     }
 
-    const averageProgress =
-      chartData.progressData[this.activeCourse.id as string]?.modules[
-        this.selectedModule.id as string
-      ]?.avgModuleProgress;
-      
-    return {
-      label: this.selectedModule.name,
-      data: [averageProgress],
-      backgroundColor: ['rgba(31, 124, 142, 1)'],
-      hoverOffset: 4
+    // if a course is selected but no module in the course is selcted we return the avg progress for the entire course;
+    if (!this.selectedModule) {
+      return {
+        label: this.activeCourse.name,
+        data: [chartData.progressData.find((course) => course.courseId)?.avgCourseProgress ?? 0],
+        backgroundColor: ['rgba(31, 124, 142, 1)'],
+        hoverOffset: 4
+      }
+    } 
+    
+    // if a course is selected and a course is selected we return the avg progress for the module;
+    else {
+      const avgProg = chartData.progressData.find((course) => course.courseId)?.modules.find((mod) => mod.moduleId)?.avgModuleProgress ?? 0;
+
+      return {
+        label: this.selectedModule.name,
+        data: [avgProg],
+        backgroundColor: ['rgba(31, 124, 142, 1)'],
+        hoverOffset: 4
+      }
     }
+  }
+
+  ngOnDestroy(): void {
+    this._sBs.unsubscribe();
   }
 }
