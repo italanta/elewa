80f72b6f964f8b635e3438eb45a5c144cd576f8d
lev-web <lnjiru43@gmail.com>
2023-12-06 20:32:35 +0300

    :sparkles: :lipstick: styled individual assessment history display

--- libs/features/convs-mgr/learners/src/lib/components/learner-assessment-history/learner-assessment-history.component.ts
+++ libs/features/convs-mgr/learners/src/lib/components/learner-assessment-history/learner-assessment-history.component.ts
@@ -1,19 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { AssessmentCursor } from '@app/model/convs-mgr/conversations/admin/system';
+import { SubSink } from 'subsink';
+
+import { AssessmentService } from '@app/state/convs-mgr/conversations/assessments';
+import { Assessment } from '@app/model/convs-mgr/conversations/assessments';
+
 
 @Component({
   selector: 'app-learner-assessment-history',
   templateUrl: './learner-assessment-history.component.html',
   styleUrls: ['./learner-assessment-history.component.scss'],
 })
-export class LearnerAssessmentHistoryComponent implements OnInit{
+export class LearnerAssessmentHistoryComponent implements OnInit, OnDestroy{
   @Input() assessments: AssessmentCursor[];
+  private _sBs = new SubSink();
 
-  displayedColumns: string[] = ['Assessment Name', 'Score', 'Duration Taken', 'Date Taken'];
-  dataSource: AssessmentCursor[]= [];
+  allAssessments: Assessment[] = []
+
+  dataSource = new MatTableDataSource<AssessmentCursor>();
+
+  displayedColumns: string[] = ['Assessment Name', 'Score', 'Date done', 'Duration taken'];
+
+  constructor(
+    private _assessments: AssessmentService,
+  ) {}
   
   ngOnInit() {
-    this.dataSource = this.assessments;
+    this.getAllAssessments()
+    this.dataSource.data = this.assessments;
   }
+
+  getAllAssessments(){
+    this._sBs.sink = this._assessments.getAssessments$().subscribe((assessment) => {
+      this.allAssessments = assessment
+    });
+  }
+  
+  getAssessmentName(id: string) {
+    return this.allAssessments.find((assessment => assessment.id === id))?.title
+  }
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
+  }
+
 }
