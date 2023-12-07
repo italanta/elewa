24bd4e1d132a74dfe3cc8336c1f2c13c124586fe
Lemmy <lemminton@gmail.com>
2023-12-07 13:13:59 +0300

    :sparkles: feat: add breadcrumb logic to fetch breadcrumb from dynamic routes

--- libs/features/convs-mgr/stories/home/src/lib/stories.router.ts
+++ libs/features/convs-mgr/stories/home/src/lib/stories.router.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 
 import { IsLoggedInGuard } from '@app/elements/base/authorisation';
+import { BotResolverService } from '@app/elements/layout/ital-bread-crumb';
+import { Bot } from '@app/model/convs-mgr/bots';
 
 import { StoriesDashboardComponent } from './pages/stories-dashboard/stories-dashboard.component';
 import { CoursesViewAllPageComponent } from './pages/courses-view-all-page/courses-view-all-page.component';
@@ -12,14 +14,22 @@ const BOTS_ROUTES: Route[] = [
   {
     path: 'dashboard',
     component: StoriesDashboardComponent,
+    data: { breadCrumb: 'Bots' },
     canActivate: [IsLoggedInGuard],
   },
   {
     path: 'view-all',
     component: CoursesViewAllPageComponent,
+    data: { breadCrumb: 'All-Bots' },
     canActivate: [IsLoggedInGuard],
   },
-  { path: ':id', component: BotPageComponent, canActivate: [IsLoggedInGuard] },
+  {
+    path: ':id',
+    component: BotPageComponent,
+    data: { breadCrumb: (data: { bot: Bot }) => `${data.bot.name}` },
+    resolve: { bot: BotResolverService },
+    canActivate: [IsLoggedInGuard],
+  },
 ];
 
 @NgModule({
