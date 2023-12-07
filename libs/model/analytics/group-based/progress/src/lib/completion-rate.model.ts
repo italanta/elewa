39d6e3d27ecd63af536173e26f8b5720060121c4
Lemmy <lemminton@gmail.com>
2023-12-07 10:38:46 +0300

    :house: refactor: update completion rate progress models

--- libs/model/analytics/group-based/progress/src/lib/completion-rate.model.ts
+++ libs/model/analytics/group-based/progress/src/lib/completion-rate.model.ts
@@ -1,13 +1,22 @@
+/** course completion rate progress */
 export interface CompletionRateProgress {
   allCourseAverage: number;
+  progressData: CourseProgress[];
+}
 
-  // TODO: split into better types.
-  progressData: {
-    courseId: string;
-    avgCourseProgress: number;
-    modules: {
-      moduleId: string;
-      avgModuleProgress: number;
-    }[];
-  }[]
-};
+/** progress data at course level */
+export interface CourseProgress {
+  courseId: string;
+  avgCourseProgress: number;
+  modules: ModuleProgress[];
+}
+
+/** progress data at module level */
+export interface ModuleProgress {
+  moduleId: string;
+  avgModuleProgress: number;
+}
+
+/** progress data record */
+export type ProgressDataRecord = Record<string, { avgCourseProgress: number; modules: Record<string, { avgModuleProgress: number }> }>;
+
