ceb6ed90f43e97181e888830aace67fdc7cfc23f
Beryl-ajuoga <berrylajuoga@gmail.com>
2023-12-21 09:57:25 +0300

    :package:  Added the user group list component

---
+++ libs/features/convs-mgr/user-groups/src/lib/components/user-groups-list/user-groups-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-groups-list',
+  templateUrl: './user-groups-list.component.html',
+  styleUrls: ['./user-groups-list.component.scss'],
+})
+export class UserGroupsListComponent {}
