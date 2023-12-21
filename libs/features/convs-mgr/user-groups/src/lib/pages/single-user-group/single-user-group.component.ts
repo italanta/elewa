36cb6602300bebaa5d7f1152431353a0d5d6284b
Beryl-ajuoga <berrylajuoga@gmail.com>
2023-12-21 09:51:04 +0300

    :package:  Single user group component added

---
+++ libs/features/convs-mgr/user-groups/src/lib/pages/single-user-group/single-user-group.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-user-group',
+  templateUrl: './single-user-group.component.html',
+  styleUrls: ['./single-user-group.component.scss'],
+})
+export class SingleUserGroupComponent {}
