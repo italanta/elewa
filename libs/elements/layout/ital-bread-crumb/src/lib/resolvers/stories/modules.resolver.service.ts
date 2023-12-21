8ca7617f129ca93acf53693035d80b16b1477dd8
Lemmy <lemminton@gmail.com>
2023-12-21 12:15:08 +0300

    :sparkles: feat: prefetch then filter modules on breadcrumb resolver

--- libs/elements/layout/ital-bread-crumb/src/lib/resolvers/stories/modules.resolver.service.ts
+++ libs/elements/layout/ital-bread-crumb/src/lib/resolvers/stories/modules.resolver.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable } from 'rxjs';
+
+import { Observable, of } from 'rxjs';
+import { filter, switchMap, take } from 'rxjs/operators';
 
 import { BotModule } from '@app/model/convs-mgr/bot-modules';
 
@@ -10,10 +12,31 @@ import { BotModulesStateService } from '@app/state/convs-mgr/modules';
   providedIn: 'root',
 })
 export class BotModuleResolverService implements Resolve<BotModule> {
-  constructor(private _botModulesService: BotModulesStateService) {}
+  private fetchedBotMods$: Observable<BotModule[]>;
+
+  constructor(private _botModulesService: BotModulesStateService) {
+    //since this is provided in root and we call it in the constructor, The original bots are thus fetched once.
+    //why? Trying to optimise this as we use the resolver in multiple routes and also the user will navigate to and from diff routes multiple times.
+
+    this.fetchedBotMods$ = this._botModulesService.getBotModules().pipe(take(1)); 
+  }
 
   resolve(route: ActivatedRouteSnapshot): Observable<BotModule> {
     const id = route.params['id'];
-    return this._botModulesService.getBotModuleById(id) as Observable<BotModule>;
+
+    return this.fetchedBotMods$.pipe(
+      switchMap((bots) => {
+        const existingBot = bots.find((bot) => bot.id === id);
+
+        if (existingBot) {
+          return of(existingBot);
+        } else {
+          return (this._botModulesService.getBotModuleById(id) as Observable<BotModule>).pipe(
+            filter((botMod) => !!botMod),
+            take(1)
+          );
+        }
+      })
+    );
   };
 }
