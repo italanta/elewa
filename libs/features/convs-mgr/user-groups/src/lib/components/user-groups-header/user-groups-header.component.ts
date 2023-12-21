c7e06ab900ca4e2b096dbc15baae57cdffef100a
Beryl-ajuoga <berrylajuoga@gmail.com>
2023-12-21 09:57:00 +0300

    :package:  Added the user group header component

---
+++ libs/features/convs-mgr/user-groups/src/lib/components/user-groups-header/user-groups-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-groups-header',
+  templateUrl: './user-groups-header.component.html',
+  styleUrls: ['./user-groups-header.component.scss'],
+})
+export class UserGroupsHeaderComponent {}
