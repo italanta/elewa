f5bc1f8eea32c1d72d1f06112afd881be4556ebc
Beryl-ajuoga <berrylajuoga@gmail.com>
2023-12-21 09:51:36 +0300

    :rocket:  Export UserGroup and SingleUserGroup components

--- libs/features/convs-mgr/user-groups/src/lib/features-convs-mgr-user-groups.module.ts
+++ libs/features/convs-mgr/user-groups/src/lib/features-convs-mgr-user-groups.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserGroupsComponent } from './pages/user-groups/user-groups.component';
+import { SingleUserGroupComponent } from './pages/single-user-group/single-user-group.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [UserGroupsComponent, SingleUserGroupComponent],
 })
 export class FeaturesConvsMgrUserGroupsModule {}
