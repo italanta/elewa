054e8ba41f77d4df9fe12af3228f7f150c05828a
Beryl-ajuoga <berrylajuoga@gmail.com>
2023-12-21 09:57:53 +0300

    :rocket:  Export the user group list and user group header components

--- libs/features/convs-mgr/user-groups/src/lib/features-convs-mgr-user-groups.module.ts
+++ libs/features/convs-mgr/user-groups/src/lib/features-convs-mgr-user-groups.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserGroupsComponent } from './pages/user-groups/user-groups.component';
 import { SingleUserGroupComponent } from './pages/single-user-group/single-user-group.component';
+import { UserGroupsHeaderComponent } from './components/user-groups-header/user-groups-header.component';
+import { UserGroupsListComponent } from './components/user-groups-list/user-groups-list.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [UserGroupsComponent, SingleUserGroupComponent],
+  declarations: [
+    UserGroupsComponent,
+    SingleUserGroupComponent,
+    UserGroupsHeaderComponent,
+    UserGroupsListComponent,
+  ],
 })
 export class FeaturesConvsMgrUserGroupsModule {}
