404aafe3eb8492055eb49b1fd9da89b2db966aca
Beryl-ajuoga <berrylajuoga@gmail.com>
2023-12-21 10:00:01 +0300

    :rocket:  Export create user group modal

--- libs/features/convs-mgr/user-groups/src/lib/features-convs-mgr-user-groups.module.ts
+++ libs/features/convs-mgr/user-groups/src/lib/features-convs-mgr-user-groups.module.ts
@@ -4,6 +4,7 @@ import { UserGroupsComponent } from './pages/user-groups/user-groups.component';
 import { SingleUserGroupComponent } from './pages/single-user-group/single-user-group.component';
 import { UserGroupsHeaderComponent } from './components/user-groups-header/user-groups-header.component';
 import { UserGroupsListComponent } from './components/user-groups-list/user-groups-list.component';
+import { CreateUserGroupComponent } from './modals/create-user-group/create-user-group.component';
 
 @NgModule({
   imports: [CommonModule],
@@ -12,6 +13,7 @@ import { UserGroupsListComponent } from './components/user-groups-list/user-grou
     SingleUserGroupComponent,
     UserGroupsHeaderComponent,
     UserGroupsListComponent,
+    CreateUserGroupComponent,
   ],
 })
 export class FeaturesConvsMgrUserGroupsModule {}
