3718e34b918515899d48511bb7274360ac4a7557
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-03 08:27:56 +0300

    🎨Incorporated Modal Handling, created and styled table stracture

--- libs/features/convs-mgr/user-groups/src/lib/components/user-groups-header/user-groups-header.component.ts
+++ libs/features/convs-mgr/user-groups/src/lib/components/user-groups-header/user-groups-header.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { CreateUserGroupComponent } from '../../modals/create-user-group/create-user-group.component';
+import { modalState } from '../../models/modal-state';
 
 @Component({
   selector: 'app-user-groups-header',
   templateUrl: './user-groups-header.component.html',
   styleUrls: ['./user-groups-header.component.scss'],
 })
-export class UserGroupsHeaderComponent {}
+export class UserGroupsHeaderComponent {
+  constructor(private _dialog: MatDialog) {}
+  
+  openCreateModal() {
+    const dialogRef = this._dialog.open(CreateUserGroupComponent, {
+      width: '610px',
+    });
+    const dialogInstance = dialogRef.componentInstance;
+    dialogInstance.modalType = modalState.Create;
+  }
+
+}
