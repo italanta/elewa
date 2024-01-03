91d2bf20c0435e4b45e7320215295f197f9777ce
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-03 11:29:08 +0300

    ✨Added delete user from group Modal

---
+++ libs/features/convs-mgr/user-groups/src/lib/modals/delete-user-from-group-modal/delete-user-from-group-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-user-from-group-modal',
+  templateUrl: './delete-user-from-group-modal.component.html',
+  styleUrls: ['./delete-user-from-group-modal.component.scss'],
+})
+export class DeleteUserFromGroupModalComponent {}
