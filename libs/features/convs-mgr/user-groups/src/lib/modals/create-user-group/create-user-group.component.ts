cff3ee9293ea55570ea7f67b0bafdeba349aadda
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-03 11:26:40 +0300

    🎨Refactored Modal Component for User Group Editing and Creation

--- libs/features/convs-mgr/user-groups/src/lib/modals/create-user-group/create-user-group.component.ts
+++ libs/features/convs-mgr/user-groups/src/lib/modals/create-user-group/create-user-group.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { modalState } from '../../models/modal-state';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-user-group',
   templateUrl: './create-user-group.component.html',
   styleUrls: ['./create-user-group.component.scss'],
 })
-export class CreateUserGroupComponent {}
+export class CreateUserGroupComponent implements OnInit{
+[x: string]: any;
+  @Input() modalType: modalState;
+
+  createUserGroupForm: FormGroup;
+  isLoading: boolean;
+
+  constructor(private _fb: FormBuilder, private _dialog: MatDialog) {}
+  
+  ngOnInit(): void {
+    this.createUserGroupForm = this.buildFormGroup();
+
+  }
+  buildFormGroup() {
+    return this._fb.group({});
+  }
+
+  closeModal() {
+    this._dialog.closeAll();
+  }
+}
