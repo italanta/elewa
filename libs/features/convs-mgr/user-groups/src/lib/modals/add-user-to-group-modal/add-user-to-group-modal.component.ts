c9a548527535c0656e5896cb7c62c0f1657b9a9a
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-03 11:12:37 +0300

    🎨Base layout for adding user to group modal created

---
+++ libs/features/convs-mgr/user-groups/src/lib/modals/add-user-to-group-modal/add-user-to-group-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-user-to-group-modal',
+  templateUrl: './add-user-to-group-modal.component.html',
+  styleUrls: ['./add-user-to-group-modal.component.scss'],
+})
+export class AddUserToGroupModalComponent {}
