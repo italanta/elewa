2a5e27190c84d2466da556dff738ba2996f0f91e
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-03 07:58:46 +0300

    📦️ single-group-header component added

---
+++ libs/features/convs-mgr/user-groups/src/lib/components/single-group-header/single-group-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-group-header',
+  templateUrl: './single-group-header.component.html',
+  styleUrls: ['./single-group-header.component.scss'],
+})
+export class SingleGroupHeaderComponent {  
+}
