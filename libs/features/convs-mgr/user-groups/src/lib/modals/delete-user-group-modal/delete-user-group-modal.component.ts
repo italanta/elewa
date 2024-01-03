36705d61be4ee74d3cac8fa53e41e9a8f83b9993
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-03 12:17:33 +0300

    💄Added stylings and Localization for delete modal

---
+++ libs/features/convs-mgr/user-groups/src/lib/modals/delete-user-group-modal/delete-user-group-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-user-group-modal',
+  templateUrl: './delete-user-group-modal.component.html',
+  styleUrls: ['./delete-user-group-modal.component.scss'],
+})
+export class DeleteUserGroupModalComponent {}
