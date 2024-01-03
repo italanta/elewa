53065a98d632c2e7153297b43d3a5e7e8c5de787
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-03 09:20:03 +0300

    🎨Refine Angular Module for User Groups Feature

--- libs/features/convs-mgr/user-groups/src/lib/convs-mgr-user-groups.module.ts
+++ libs/features/convs-mgr/user-groups/src/lib/convs-mgr-user-groups.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+import { ConvlPageModule } from '@app/elements/layout/page-convl';
+
 import { UserGroupsComponent } from './pages/user-groups/user-groups.component';
 import { SingleUserGroupComponent } from './pages/single-user-group/single-user-group.component';
 import { UserGroupsHeaderComponent } from './components/user-groups-header/user-groups-header.component';
 import { UserGroupsListComponent } from './components/user-groups-list/user-groups-list.component';
 import { CreateUserGroupComponent } from './modals/create-user-group/create-user-group.component';
-import { ConvlPageModule } from '@app/elements/layout/page-convl';
-import { UserGroupsRouterModule } from './user-groups.router';
+import { UserGroupsRouterModule } from './user-groups.router.module';
+import { MultiLangModule } from '@ngfi/multi-lang';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { DeleteUserGroupModalComponent } from './modals/delete-user-group-modal/delete-user-group-modal.component';
+import { SingleGroupHeaderComponent } from './components/single-group-header/single-group-header.component';
+import { SingleGroupUserListComponent } from './components/single-group-user-list/single-group-user-list.component';
+import { AddUserToGroupModalComponent } from './modals/add-user-to-group-modal/add-user-to-group-modal.component';
+import { DeleteUserFromGroupModalComponent } from './modals/delete-user-from-group-modal/delete-user-from-group-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ConvlPageModule,
-    UserGroupsRouterModule
+    UserGroupsRouterModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MultiLangModule,
+    ReactiveFormsModule,
+    MatMenuModule,
   ],
   declarations: [
     UserGroupsComponent,
@@ -20,6 +37,11 @@ import { UserGroupsRouterModule } from './user-groups.router';
     UserGroupsHeaderComponent,
     UserGroupsListComponent,
     CreateUserGroupComponent,
+    DeleteUserGroupModalComponent,
+    SingleGroupHeaderComponent,
+    SingleGroupUserListComponent,
+    AddUserToGroupModalComponent,
+    DeleteUserFromGroupModalComponent,
   ],
 })
 export class ConvsMgrUserGroupsModule {}
