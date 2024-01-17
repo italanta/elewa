6e06cc22be3e6fe103a97db9e3933be0df78d2f1
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-18 01:05:38 +0300

    ✨implemented single-group-user-list component

--- libs/features/convs-mgr/user-groups/src/lib/components/single-group-header/single-group-header.component.ts
+++ libs/features/convs-mgr/user-groups/src/lib/components/single-group-header/single-group-header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserGroups } from '@app/model/convs-mgr/user-groups';
+import { UserGroupService } from '@app/state/convs-mgr/user-group';
 
 @Component({
   selector: 'app-single-group-header',
@@ -6,4 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-group-header.component.scss'],
 })
 export class SingleGroupHeaderComponent {  
+
+  userGroupName = '';
+
+
+  constructor(
+    private _router: Router,
+    private _userGroupService: UserGroupService
+  ) {}
+
+  ngOnInit() {
+    this.loadUserGroup();
+  }
+
+  loadUserGroup() {
+    const userGroupId = this._router.url.split('/')[2].toString();
+    this._userGroupService.getUserGroup$(userGroupId).subscribe(
+      (data: any) => {
+        console.log("", data)
+        this.userGroupName = data.className;
+      }
+    );                
+  }
 }
--- libs/features/convs-mgr/user-groups/src/lib/components/single-group-user-list/single-group-user-list.component.ts
+++ libs/features/convs-mgr/user-groups/src/lib/components/single-group-user-list/single-group-user-list.component.ts
@@ -1,40 +1,86 @@
-import { Component, ViewChild } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+import { UserGroups } from '@app/model/convs-mgr/user-groups';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { AddUserToGroupModalComponent } from '../../modals/add-user-to-group-modal/add-user-to-group-modal.component';
+import { result } from 'lodash';
+import { GroupsService } from '../../services/groups.service';
+import { UserGroupService } from '@app/state/convs-mgr/user-group';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-single-group-user-list',
   templateUrl:'./single-group-user-list.component.html',
   styleUrls: ['./single-group-user-list.component.scss'],
 })
-export class SingleGroupUserListComponent {
+export class SingleGroupUserListComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+
   displayedColumns = [
-    'name',
+    'className',
     'phonenumber',
     'date',
     'status',
     'actions'
   ];
 
-  dataSource = new MatTableDataSource();
+  dataSource = new MatTableDataSource<any>();
+  dialog: any;
+  userGroup: any;
 
-  allUsers = [{
-    "name": "peter",
-    "phoenenumber": 2,
-    "date": "Today",
-    "status": "Inactive"
-  }];
-  
+  userGroupId = '';
+
+  constructor(
+    private userGroupsService: UserGroupService,
+    private router$$: Router
+    ) {
+      this.userGroupId = this.router$$.url.split('/')[2].toString();
+    }
+
+  ngOnInit() {
+    this.loadUserGroup();
+    // const userGroup = this.userGroup as UserGroups;
+    // const users = userGroup.users as any[];
+    // this.dataSource.data = users;
+  }
+
+  loadUserGroup() {
+    this.userGroupsService.getUserGroup$(this.userGroupId).subscribe(
+      (data) => {
+        this.userGroup = data;
+        const userGroup = this.userGroup as UserGroups;
+        const users = userGroup.users as any[];
+        this.dataSource.data=users;
+      }
+    );
+  }
+
+  // addUserGroup(userGroups: UserGroups) {
+  //   this.userGroupsService.addUserGroups(userGroups).subscribe(() => {
+  //     this.loadUserGroups();
+  //   });
+  // }
+
   openAddModal() {
+    const dialogRef = this.dialog.open(AddUserToGroupModalComponent, {
+      width: '400px', 
+    });
 
+    dialogRef.afterClosed().subscribe((result:UserGroups)=>{
+      if(result){
+        this.addUserGroup(result)
+      }
+    })
   }
-
-
-  openDeleteModal() {
-
-  }
+  addUserGroup(userGroups: UserGroups) {
+    this.userGroupsService.addUserGroups(userGroups).subscribe(() => {
+      this.loadUserGroup();
+    });
+  }   
+  // openDeleteModal() {}
 }
