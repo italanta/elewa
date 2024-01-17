b1931c009abac765b7051610b28596fd3e3b86a7
Beryl-ajuoga <berrylajuoga@gmail.com>
2024-01-18 01:17:29 +0300

    ⚡️ Introduce form initialization in the constructor for class and description fields

--- libs/features/convs-mgr/user-groups/src/lib/modals/create-user-group/create-user-group.component.ts
+++ libs/features/convs-mgr/user-groups/src/lib/modals/create-user-group/create-user-group.component.ts
@@ -2,27 +2,37 @@ import { Component, Input, OnInit } from '@angular/core';
 import { modalState } from '../../models/modal-state';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { GroupsService } from '../../services/groups.service';
+import { take } from 'lodash';
 
 @Component({
   selector: 'app-create-user-group',
   templateUrl: './create-user-group.component.html',
   styleUrls: ['./create-user-group.component.scss'],
 })
-export class CreateUserGroupComponent implements OnInit{
+export class CreateUserGroupComponent  {
 [x: string]: any;
   @Input() modalType: modalState;
 
   createUserGroupForm: FormGroup;
   isLoading: boolean;
 
-  constructor(private _fb: FormBuilder, private _dialog: MatDialog) {}
-  
-  ngOnInit(): void {
-    this.createUserGroupForm = this.buildFormGroup();
-
+  constructor(private _fb: FormBuilder, private _dialog: MatDialog, private _group:GroupsService) {
+    this.createUserGroupForm = this._fb.group({
+      className:[''],
+      descriptionName:['']
+    })
   }
+  
   buildFormGroup() {
     return this._fb.group({});
+  }
+
+  submitedUserForm(){
+   
+    console.log(this.createUserGroupForm.value)
+    this._group.createGroup(this.createUserGroupForm.value)
+    
   }
 
   closeModal() {
