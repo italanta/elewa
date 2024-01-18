a0924261cc1db45e3f8d524a12e0ca9ef3cf2cc6
Reagan Charana <rgncharana2@gmail.com>
2024-01-18 16:03:46 +0300

    :recycle: move user count logic to utils

---
+++ libs/private/functions/analytics/progress/main/src/lib/utils/get-user-count.util.ts
@@ -0,0 +1,70 @@
+import { HandlerTools } from "@iote/cqrs";
+
+import { UserMetricsService } from "../data-services/user-metrics.service";
+
+/**
+ * Gets the number of enrolled users created today, in the past week (if it's friday), and in the past month (if end of month).
+ * @param {UserMetricsService} userMetricsService - The user metrics service.
+ * @param {string} orgId - The organization ID.
+ */
+
+export async function getEnrolledUserCreationCount(userMetricsService: UserMetricsService, orgId: string, tools: HandlerTools, timeInUnix: number)
+{
+  const timeToCollect = new Date(timeInUnix);
+  const dayOfWeek = timeToCollect.getDay(); // 0 = Sunday, 1 = Monday, ...
+  const isLastDayOfMonth = new Date(timeToCollect.getFullYear(), timeToCollect.getMonth() + 1, 0).getDate() === timeToCollect.getDate();
+
+  const dailyCount = (await userMetricsService.getDayUserCount(orgId, timeInUnix)).length;
+
+  let pastWeekCount = 0;
+  let pastMonthCount = 0;
+
+  if (dayOfWeek === 5) { // Friday (0-based index, where 5 represents Friday)
+    pastWeekCount = (await userMetricsService.getPastWeekUserCount(orgId, timeInUnix)).length;
+  }
+
+  if (isLastDayOfMonth) {
+    pastMonthCount = (await userMetricsService.getPastMonthUserCount(orgId, timeInUnix)).length;
+  }
+
+  tools.Logger.log(() => `[measureGroupProgressHandler].execute - Enrolled user creation count completed`);
+
+  return {
+    dailyCount,
+    pastWeekCount,
+    pastMonthCount
+  };
+}
+
+/**
+ * Gets the number of users who engaged with the bot today, in the past week (if it's friday), and in the past month (if end of month).
+ * @param {UserMetricsService} userMetricsService - The user metrics service.
+ * @param {string} orgId - The organization ID.
+ */
+export async function getEngagedUserCount(userMetricsService: UserMetricsService, orgId: string, tools: HandlerTools, timeInUnix: number)
+{
+  const timeToCollect = new Date(timeInUnix);
+  const dayOfWeek = timeToCollect.getDay(); // 0 = Sunday, 1 = Monday, ...
+  const isLastDayOfMonth = new Date(timeToCollect.getFullYear(), timeToCollect.getMonth() + 1, 0).getDate() === timeToCollect.getDate();
+
+  const dailyCount = (await userMetricsService.getDayUserEngagement(orgId, timeInUnix)).length;
+
+  let pastWeekCount = 0;
+  let pastMonthCount = 0;
+
+  if (dayOfWeek === 5) { // Friday (0-based index, where 5 represents Friday)
+    pastWeekCount = (await userMetricsService.getPastWeekUserEngagement(orgId, timeInUnix)).length;
+  }
+
+  if (isLastDayOfMonth) {
+    pastMonthCount = (await userMetricsService.getPastMonthUserEngagement(orgId, timeInUnix)).length;
+  }
+
+  tools.Logger.log(() => `[measureGroupProgressHandler].execute - Enrolled user creation count completed`);
+
+  return {
+    dailyCount,
+    pastWeekCount,
+    pastMonthCount
+  };
+}
