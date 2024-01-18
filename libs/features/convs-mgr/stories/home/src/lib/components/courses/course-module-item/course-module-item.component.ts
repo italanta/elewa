474f7f6db4d04fb9e019c2ffbc8fb8429c750548
japhethnyaranga <nyaranga4@gmail.com>
2024-01-18 22:18:40 +0300

    📝 updated multi lang

--- libs/features/convs-mgr/stories/home/src/lib/components/courses/course-module-item/course-module-item.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/components/courses/course-module-item/course-module-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 import { Bot, BotMutationEnum } from '@app/model/convs-mgr/bots';
 import { BotModule } from '@app/model/convs-mgr/bot-modules';
@@ -13,6 +13,7 @@ import {
   CreateLessonModalComponent
 } from '@app/elements/layout/convs-mgr/story-elements';
 import { BotsModuleService } from '../../../services/bots-module.service';
+import { ConnectToChannelModalComponent } from '../../../modals/connect-to-channel-modal/connect-to-channel-modal.component';
 
 @Component({
   selector: 'italanta-apps-course-module-item',
@@ -26,7 +27,7 @@ export class CourseModuleItemComponent implements OnInit{
 
   constructor(private _dialog: MatDialog, private _router$: Router, private _botsModuleService$:BotsModuleService) {}
 
-  specificBot: Bot | undefined; /**adding undefined here since it is described that way in the store service, removing might break something, check on this */
+  specificBot: Bot | any; /**adding undefined here since it is described that way in the store service, removing might break something, check on this */
 
   ngOnInit(): void {
     this._botsModuleService$.getSpecificBot(this.botModule.parentBot).subscribe(response =>{
@@ -62,4 +63,28 @@ export class CourseModuleItemComponent implements OnInit{
       }
     }).afterClosed();
   }
+
+  connectToChannel(botId:string){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '30rem'; // Set the width of the dialog
+    dialogConfig.height = '20rem'; // Set the height of the dialog
+    dialogConfig.data = { botId: botId }; // Pass the botId to the dialog
+    this._dialog.open(ConnectToChannelModalComponent, dialogConfig);
+  }
+  deleteBot(botId:Bot){
+    this._botsModuleService$.deleteBot(botId)
+  } 
+
+  archiveBot(bot:Bot){
+    bot.archived = true;
+    this._botsModuleService$.archiveBot(bot)
+  }
+  publishBot(bot:Bot){
+    bot.isPublishing = true;
+    bot.isPublished = true;
+    this._botsModuleService$.publishBot(bot)
+      .subscribe(() => {
+        bot.isPublishing = false;
+      });
+   }
 }
