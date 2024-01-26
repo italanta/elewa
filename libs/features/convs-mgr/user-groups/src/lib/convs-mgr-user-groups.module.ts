3ae3591385ca0be2a0f44fc1f420c56cf6d67b5e
Reagan Charana <rgncharana2@gmail.com>
2024-01-26 15:40:59 +0300

    :truck: order imports

--- libs/features/convs-mgr/user-groups/src/lib/convs-mgr-user-groups.module.ts
+++ libs/features/convs-mgr/user-groups/src/lib/convs-mgr-user-groups.module.ts
@@ -3,23 +3,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MultiLangModule } from '@ngfi/multi-lang';
+import { MaterialBricksModule, MaterialDesignModule } from '@iote/bricks-angular';
 
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
 
+import { DeleteUserGroupModalComponent } from './modals/delete-user-group-modal/delete-user-group-modal.component';
+import { SingleGroupHeaderComponent } from './components/single-group-header/single-group-header.component';
+import { SingleGroupUserListComponent } from './components/single-group-user-list/single-group-user-list.component';
+import { AddUserToGroupModalComponent } from './modals/add-user-to-group-modal/add-user-to-group-modal.component';
+import { DeleteUserFromGroupModalComponent } from './modals/delete-user-from-group-modal/delete-user-from-group-modal.component';
 import { UserGroupsComponent } from './pages/user-groups/user-groups.component';
 import { SingleUserGroupComponent } from './pages/single-user-group/single-user-group.component';
 import { UserGroupsHeaderComponent } from './components/user-groups-header/user-groups-header.component';
 import { UserGroupsListComponent } from './components/user-groups-list/user-groups-list.component';
 import { CreateUserGroupComponent } from './modals/create-user-group/create-user-group.component';
 import { UserGroupsRouterModule } from './user-groups.router.module';
-import { MultiLangModule } from '@ngfi/multi-lang';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatMenuModule } from '@angular/material/menu';
-import { DeleteUserGroupModalComponent } from './modals/delete-user-group-modal/delete-user-group-modal.component';
-import { SingleGroupHeaderComponent } from './components/single-group-header/single-group-header.component';
-import { SingleGroupUserListComponent } from './components/single-group-user-list/single-group-user-list.component';
-import { AddUserToGroupModalComponent } from './modals/add-user-to-group-modal/add-user-to-group-modal.component';
-import { DeleteUserFromGroupModalComponent } from './modals/delete-user-from-group-modal/delete-user-from-group-modal.component';
 // import { UserGroupsStore } from 'libs/state/convs-mgr/user-groups/src/lib/store/user-groups.store';
 // import { UserGroupsService } from 'libs/state/convs-mgr/user-groups/src/lib/service/user-groups.service';
 
@@ -30,6 +32,8 @@ import { DeleteUserFromGroupModalComponent } from './modals/delete-user-from-gro
     UserGroupsRouterModule,
     MatTableModule,
     MatPaginatorModule,
+    MaterialBricksModule,
+    MaterialDesignModule,
     MultiLangModule,
     ReactiveFormsModule,
     MatMenuModule,
