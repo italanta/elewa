4571350ae75b516275acbea156805f5a5a67eea6
Reagan Charana <rgncharana2@gmail.com>
2024-02-14 16:26:25 +0300

    :lipstick: :recycle: remove connect bot logic from lesson

--- libs/features/convs-mgr/stories/home/src/lib/components/courses/course-module-item/course-module-item.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/components/courses/course-module-item/course-module-item.component.ts
@@ -3,37 +3,33 @@ import { Router } from '@angular/router';
 
 import { MatDialog  } from '@angular/material/dialog';
 
+import { SubSink } from 'subsink';
+
 import { Bot, BotMutationEnum } from '@app/model/convs-mgr/bots';
 import { BotModule } from '@app/model/convs-mgr/bot-modules';
 import { Story } from '@app/model/convs-mgr/stories/main';
 import { BotsStateService } from '@app/state/convs-mgr/bots';
-
 import { 
   DeleteElementsEnum,
   DeleteBotModalComponent,
   CreateLessonModalComponent
 } from '@app/elements/layout/convs-mgr/story-elements';
-import { ConnectToChannelModalComponent } from '../../../modals/connect-to-channel-modal/connect-to-channel-modal.component';
 
 @Component({
   selector: 'italanta-apps-course-module-item',
   templateUrl: './course-module-item.component.html',
   styleUrls: ['./course-module-item.component.scss'],
 })
-export class CourseModuleItemComponent implements OnInit{
+export class CourseModuleItemComponent {
   @Input() botModule: BotModule;
   @Input() story: Story;
 
+  _sBs = new SubSink();
 
-  constructor(private _dialog: MatDialog, private _router$: Router, private _botsService$: BotsStateService) {}
+  constructor(private _dialog: MatDialog, 
+              private _router$: Router) {}
 
   specificBot: Bot | undefined; /**adding undefined here since it is described that way in the store service, removing might break something, check on this */
-
-  ngOnInit(): void {
-    this._botsService$.getBotById(this.botModule.parentBot).subscribe(response =>{
-      this.specificBot = response;
-    })
-   }
 
   openModule(id: string) {
     this._router$.navigate(['modules', id]);
@@ -64,27 +60,4 @@ export class CourseModuleItemComponent implements OnInit{
     }).afterClosed();
   }
 
-  connectToChannel(botId: string) {
-    this._dialog.open(ConnectToChannelModalComponent,{
-      width: '30rem',
-       height: '20rem',
-       data: { botId: botId }
-    });
-   }
-  deleteBot(botId:Bot){
-    this._botsService$.deleteBot(botId)
-  } 
-
-  archiveBot(bot:Bot){
-    bot.isArchived = true;
-    this._botsService$.updateBot(bot)
-  }
-  publishBot(bot:Bot){
-    bot.isPublishing = true;
-    bot.isPublished = true;
-    this._botsService$.updateBot(bot)
-      .subscribe(() => {
-        bot.isPublishing = false;
-      });
-   }
 }
