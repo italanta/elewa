99b4fd95431c135c8c4e258d7b1eba233f34fc65
Reagan Charana <rgncharana2@gmail.com>
2024-02-14 16:24:41 +0300

    :lipstick: add menu to course header

--- libs/features/convs-mgr/stories/home/src/lib/components/courses/courses-list/courses-list.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/components/courses/courses-list/courses-list.component.ts
@@ -1,28 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
+import { SubSink } from 'subsink';
 import { Observable, of } from 'rxjs';
 
 import { CreateLessonModalComponent } from '@app/elements/layout/convs-mgr/story-elements';
-import { BotMutationEnum } from '@app/model/convs-mgr/bots';
+import { Bot, BotMutationEnum } from '@app/model/convs-mgr/bots';
+import { BotsStateService } from '@app/state/convs-mgr/bots';
 
 import { Course } from '../../../model/courses.interface';
+import { MainChannelModalComponent } from '../../../modals/main-channel-modal/main-channel-modal.component';
 
 @Component({
   selector: 'italanta-apps-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.scss'],
 })
-export class CoursesListComponent {
+export class CoursesListComponent implements OnDestroy {
   @Input() courses$: Observable<Course>;
   @Input()
   set searchValue(value: string) {
     this.filterString$ = of(value);
   }
 
+  _sBs = new SubSink();
+
   filterString$: Observable<string>;
 
-  constructor(private _dialog: MatDialog) {}
+  constructor(private _dialog: MatDialog, private _botsService$: BotsStateService) {}
 
   createLesson(moduleId: string) {
     const dialogData = {
@@ -35,4 +40,33 @@ export class CoursesListComponent {
       data: dialogData,
     });
   }
+
+  connectToChannel(botId: string)
+  {
+    this._dialog.open(MainChannelModalComponent, {
+      width: '30rem',
+      height: '27rem',
+      data: { botId: botId }
+    });
+  }
+
+  publishBot(bot:Bot){
+    bot.isPublishing = true;
+    this._sBs.sink = this._botsService$.publishBot(bot)
+      .subscribe(() => {
+        bot.isPublishing = false;
+      });
+  }
+
+  archiveBot(bot:Bot){
+    this._sBs.sink = this._botsService$.archiveBot(bot).subscribe();
+  }
+
+   deleteBot(bot:Bot){
+    this._sBs.sink = this._botsService$.deleteBot(bot).subscribe();
+   }
+
+   ngOnDestroy(): void {
+       this._sBs.unsubscribe();
+   }
 }
