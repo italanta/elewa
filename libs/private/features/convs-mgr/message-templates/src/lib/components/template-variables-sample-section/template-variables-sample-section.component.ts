90d06ac80ed2572b6f3560484b9c20ac62d5a361
Reagan Charana <rgncharana2@gmail.com>
2024-02-22 12:58:34 +0300

    :sparkles: :recycle: implement variable example section

--- libs/private/features/convs-mgr/message-templates/src/lib/components/template-variables-sample-section/template-variables-sample-section.component.ts
+++ libs/private/features/convs-mgr/message-templates/src/lib/components/template-variables-sample-section/template-variables-sample-section.component.ts
@@ -1,8 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+import { MessageTemplate, VariableExample } from '@app/model/convs-mgr/functions';
 
 @Component({
   selector: 'app-template-variables-sample-section',
   templateUrl: './template-variables-sample-section.component.html',
   styleUrls: ['./template-variables-sample-section.component.scss'],
 })
-export class TemplateVariablesSampleSectionComponent {}
+export class TemplateVariablesSampleSectionComponent implements OnInit {
+  @Input() templateForm: FormGroup;
+  @Input() template: MessageTemplate;
+  @Input() section: 'header' | 'body';
+
+  samplesArray: VariableExample[] = [];
+
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    if(this.template) {
+      this.template.examples?.forEach((example: VariableExample) => {
+        if(example.section == this.section) {
+          this.examples.push(this.createExampleFB(example));
+        }
+      })
+
+      // TEMP: Remove after implementation is complete
+      this.templateForm.get('examples')?.valueChanges.subscribe(changes=> {
+        console.log(changes)
+    })
+
+    }
+
+  }
+
+  get examples(): FormArray
+  {
+    return this.templateForm.controls['examples'] as FormArray;
+  }
+
+  createExampleFB(example?: VariableExample)
+  {
+    return this._fb.group({
+      name: [example?.name ?? ''],
+      value: [example?.value ?? ''],
+      section: [this.section]
+    });
+  }
+
+  addExample(name: string)
+  {
+    this.examples.push(this.createExampleFB({name}));
+  }
+
+  removeExample(name: string, section: 'body' | 'header')
+  {
+    const index = this.examples.value.findIndex((exm: VariableExample) => {
+      return exm.name === name && exm.section === section
+    });
+    if(index !== -1) this.examples.removeAt(index);
+  }
+}
