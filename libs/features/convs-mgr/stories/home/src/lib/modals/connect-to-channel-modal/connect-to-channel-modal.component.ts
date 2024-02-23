56ef9b6c7f05ab5855ea8b46210a5324ae533b24
Reagan Charana <rgncharana2@gmail.com>
2024-02-23 16:08:26 +0300

    :lipstick: improve style of connect to channel tab

--- libs/features/convs-mgr/stories/home/src/lib/modals/connect-to-channel-modal/connect-to-channel-modal.component.ts
+++ libs/features/convs-mgr/stories/home/src/lib/modals/connect-to-channel-modal/connect-to-channel-modal.component.ts
@@ -5,15 +5,11 @@ import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { CommunicationChannel } from '@app/model/convs-mgr/conversations/admin/system';
 import { PlatformType } from '@app/model/convs-mgr/conversations/admin/system';
 
-
-
 @Component({
   selector: 'italanta-apps-connect-to-channel-modal',
   templateUrl: './connect-to-channel-modal.component.html',
   styleUrls: ['./connect-to-channel-modal.component.scss'],
 })
-
-
 
 export class ConnectToChannelModalComponent {
   @Output() platformAndBotSelected = new EventEmitter<{selectedPlatform: PlatformType, botId: string}>();
@@ -31,13 +27,10 @@ export class ConnectToChannelModalComponent {
     private _dialog: MatDialog
     ){}
 
-
-
   onPlatformSelected(){
     this.platformAndBotSelected.emit({selectedPlatform: this.selectedPlatform, botId: this.data});
   }
 
-   
    closeDialog(){
     this._dialog.closeAll();
    }
