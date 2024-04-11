3106c4e2293d2bea0b7e0e0a265c9ab4576d3996
Reagan Charana <rgncharana2@gmail.com>
2024-04-11 09:43:12 +0300

    :lipstick: update phone block edit to match design

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/phone-block-edit/phone-block-edit.component.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/phone-block-edit/phone-block-edit.component.ts
@@ -9,6 +9,7 @@ import { FormGroup } from '@angular/forms';
 export class PhoneBlockEditComponent implements OnInit {
   @Input() form: FormGroup;
   @Input() title: string;
+  @Input() icon: string;
   validate: boolean;
 
   ngOnInit() {
