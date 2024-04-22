441f57e4ac42999c1a11244710f8ba4ab036fdf4
Lemmy <lemminton@gmail.com>
2024-04-22 13:04:32 +0300

    refactor: add fallback main page and router

--- libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
+++ libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FallbackMainPageComponent } from './pages/fallback-main-page/fallback-main-page.component';
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [FallbackMainPageComponent],
 })
 export class FallbacksModule {}
---
+++ libs/features/convs-mgr/fallbacks/src/lib/pages/fallback-main-page/fallback-main-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fallback-main-page',
+  templateUrl: './fallback-main-page.component.html',
+  styleUrls: ['./fallback-main-page.component.scss'],
+})
+export class FallbackMainPageComponent {}
