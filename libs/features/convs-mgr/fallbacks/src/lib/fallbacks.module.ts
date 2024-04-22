79ce00b826e7fbb4cf726aabbfbf97482bcf00cb
Lemmy <lemminton@gmail.com>
2024-04-22 14:45:42 +0300

    style: add routing and setup default styles to page

--- libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
+++ libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ConvlPageModule } from '@app/elements/layout/page-convl';
+
 import { FallbackMainPageComponent } from './pages/fallback-main-page/fallback-main-page.component';
 
+import { FallbackRouterModule } from './fallbacks.router';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ConvlPageModule, FallbackRouterModule],
   declarations: [FallbackMainPageComponent],
 })
 export class FallbacksModule {}
