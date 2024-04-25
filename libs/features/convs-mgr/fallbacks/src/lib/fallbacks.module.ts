b1b7b9b0d3895984ab94e2bc2e9ccaa3f72ba0e4
Lemmy <lemminton@gmail.com>
2024-04-25 16:57:41 +0300

    refactor: add modal component

--- libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
+++ libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
@@ -6,9 +6,10 @@ import { ConvlPageModule } from '@app/elements/layout/page-convl';
 import { FallbackMainPageComponent } from './pages/fallback-main-page/fallback-main-page.component';
 
 import { FallbackRouterModule } from './fallbacks.router';
+import { FallbackModalComponent } from './modals/fallback-modal/fallback-modal.component';
 
 @NgModule({
   imports: [CommonModule, ConvlPageModule, FallbackRouterModule],
-  declarations: [FallbackMainPageComponent],
+  declarations: [FallbackMainPageComponent, FallbackModalComponent],
 })
 export class FallbacksModule {}
---
+++ libs/features/convs-mgr/fallbacks/src/lib/modals/fallback-modal/fallback-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fallback-modal',
+  templateUrl: './fallback-modal.component.html',
+  styleUrls: ['./fallback-modal.component.scss'],
+})
+export class FallbackModalComponent {}
