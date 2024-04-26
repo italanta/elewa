4c368631470d39e6e8bc633ec62dc3d3dd360273
Lemmy <lemminton@gmail.com>
2024-04-26 10:41:20 +0300

    feat: add starter model type

--- libs/features/convs-mgr/fallbacks/src/lib/utils/fallback-forms.models.ts
+++ libs/features/convs-mgr/fallbacks/src/lib/utils/fallback-forms.models.ts
@@ -1,6 +1,10 @@
 import { FormBuilder } from '@angular/forms';
+import { Fallback } from '@app/model/convs-mgr/fallbacks';
 
-export function initFallBackFormGroup(_fb: FormBuilder, fallbackForm: any) {
+export function initFallBackFormGroup(
+  _fb: FormBuilder,
+  fallbackForm: Fallback
+) {
   return _fb.group({
     id: ['' ?? fallbackForm.id],
     userSays: ['' ?? fallbackForm.userSays],
---
+++ libs/model/convs-mgr/fallbacks/src/lib/fallbackui.model.ts
@@ -0,0 +1,8 @@
+import { IObject } from '@iote/bricks';
+
+// TODO: set types
+export interface Fallback extends IObject {
+  userSays: any;
+  actions: any;
+  actionDelays: any;
+}
