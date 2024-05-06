90b3ded6eeae2ff4113011a3ffdf02a78304b2db
Lemmy <lemminton@gmail.com>
2024-05-06 09:51:47 +0300

    style: add toogle slide btn

--- libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
+++ libs/features/convs-mgr/fallbacks/src/lib/fallbacks.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { ConvlPageModule } from '@app/elements/layout/page-convl';
 
+import {
+  MaterialBricksModule,
+  MaterialDesignModule,
+} from '@iote/bricks-angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
 import { FallbackMainPageComponent } from './pages/fallback-main-page/fallback-main-page.component';
 
 import { FallbackRouterModule } from './fallbacks.router';
 import { FallbackModalComponent } from './modals/fallback-modal/fallback-modal.component';
 
 @NgModule({
-  imports: [CommonModule, ConvlPageModule, FallbackRouterModule],
+  imports: [
+    CommonModule,
+    ConvlPageModule,
+    FallbackRouterModule,
+    MaterialBricksModule,
+    MaterialDesignModule,
+    MultiLangModule,
+  ],
   declarations: [FallbackMainPageComponent, FallbackModalComponent],
 })
 export class FallbacksModule {}
