16f8a46dacabfb990f051a877367a0db40f25cbf
Lemmy <lemminton@gmail.com>
2024-05-06 12:43:55 +0300

    feat: create story breadcrumb component

---
+++ libs/elements/layout/ital-bread-crumb/src/lib/components/story-breadcrumb/story-breadcrumb.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-story-breadcrumb',
+  templateUrl: './story-breadcrumb.component.html',
+  styleUrls: ['./story-breadcrumb.component.scss'],
+})
+export class StoryBreadcrumbComponent {}
--- libs/elements/layout/ital-bread-crumb/src/lib/ital-bread-crumb.module.ts
+++ libs/elements/layout/ital-bread-crumb/src/lib/ital-bread-crumb.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { ItalBreadcrumbComponent } from './components/ital-bread-crumb/ital-breadcrumb.component';
+import { StoryBreadcrumbComponent } from './components/story-breadcrumb/story-breadcrumb.component';
 
 @NgModule({
-  declarations: [ItalBreadcrumbComponent],
+  declarations: [ItalBreadcrumbComponent, StoryBreadcrumbComponent],
   imports: [CommonModule, RouterModule],
-  exports: [ItalBreadcrumbComponent],
+  exports: [ItalBreadcrumbComponent, StoryBreadcrumbComponent],
 })
 export class ItalBreadCrumbModule {}
