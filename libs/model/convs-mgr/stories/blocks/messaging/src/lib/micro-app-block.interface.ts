d46f43309b61eda1cff36334e909c63be1d1f974
Beulah Mathenge <beulahdaizy@gmail.com>
2024-05-17 10:23:04 +0300

    :tada: Begin model definition

---
+++ libs/model/convs-mgr/stories/blocks/messaging/src/lib/micro-app-block.interface.ts
@@ -0,0 +1,13 @@
+import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+
+export interface MicroAppBlock extends StoryBlock 
+{
+  /** will be used to create the url */
+  appId: string;
+  
+  /** The name of the micro app. Will be used as the link display text */
+  appName: string;
+
+  /** From which platform is the microApp being called */
+  // platformType: PlatformType;
+}
