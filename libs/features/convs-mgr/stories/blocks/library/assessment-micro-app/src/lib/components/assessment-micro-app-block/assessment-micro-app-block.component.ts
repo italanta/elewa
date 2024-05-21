60790b01fa55fa2f3c772aa79b2703568d23273f
Beulah Mathenge <beulahdaizy@gmail.com>
2024-05-21 12:27:25 +0300

    :package: Generate assessment micro app block component

---
+++ libs/features/convs-mgr/stories/blocks/library/assessment-micro-app/src/lib/components/assessment-micro-app-block/assessment-micro-app-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assessment-micro-app-block',
+  templateUrl: './assessment-micro-app-block.component.html',
+  styleUrls: ['./assessment-micro-app-block.component.scss']
+})
+export class AssessmentMicroAppBlockComponent {
+
+}
--- libs/features/convs-mgr/stories/blocks/library/assessment-micro-app/src/lib/features-convs-mgr-stories-blocks-library-assessment-micro-app.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/assessment-micro-app/src/lib/features-convs-mgr-stories-blocks-library-assessment-micro-app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AssessmentMicroAppBlockComponent } from './components/assessment-micro-app-block/assessment-micro-app-block.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    AssessmentMicroAppBlockComponent
+  ],
 })
 export class AssessmentMicroAppModule {}
