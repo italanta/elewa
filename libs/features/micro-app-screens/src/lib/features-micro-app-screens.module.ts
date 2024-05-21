2827953debaccfff64f91820050b3fb660114202
Beulah Mathenge <beulahdaizy@gmail.com>
2024-05-21 16:49:45 +0300

    :package: Generate micro apps main page component

--- libs/features/micro-app-screens/src/lib/features-micro-app-screens.module.ts
+++ libs/features/micro-app-screens/src/lib/features-micro-app-screens.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FlexLayoutModule, MaterialDesignModule } from '@iote/bricks-angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
+import { MicroAppMainPageComponent } from './pages/micro-app-main-page/micro-app-main-page.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialDesignModule,
+    FlexLayoutModule,
+    MultiLangModule,
+  ],
+  declarations: [
+    MicroAppMainPageComponent,
+  ],
+  exports: [
+    MicroAppMainPageComponent
+  ]
 })
 export class MicroAppScreensModule {}
---
+++ libs/features/micro-app-screens/src/lib/pages/micro-app-main-page/micro-app-main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-micro-app-main-page',
+  templateUrl: './micro-app-main-page.component.html',
+  styleUrls: ['./micro-app-main-page.component.scss']
+})
+export class MicroAppMainPageComponent {
+
+}
