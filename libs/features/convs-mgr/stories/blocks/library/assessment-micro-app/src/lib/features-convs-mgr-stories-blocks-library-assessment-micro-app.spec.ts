1794e95c787bcc95ec6cdae8246b85b03a7df5a0
Beulah Mathenge <beulahdaizy@gmail.com>
2024-05-21 12:22:47 +0300

    :fire: Remove library without  a module

--- libs/features/convs-mgr/stories/blocks/library/assessment-micro-app/src/lib/features-convs-mgr-stories-blocks-library-assessment-micro-app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { assessmentMicroApp } from './features-convs-mgr-stories-blocks-library-assessment-micro-app';
-
-describe('featuresConvsMgrStoriesBlocksLibraryAssessmentMicroApp', () => {
-  it('should work', () => {
-    expect(assessmentMicroApp()).toEqual(
-      'features-convs-mgr-stories-blocks-library-assessment-micro-app'
-    );
-  });
-});
--- libs/features/convs-mgr/stories/blocks/library/assessment-micro-app/src/lib/features-convs-mgr-stories-blocks-library-assessment-micro-app.ts
+++
@@ -1,3 +0,0 @@
-export function assessmentMicroApp(): string {
-  return 'features-convs-mgr-stories-blocks-library-assessment-micro-app';
-}
