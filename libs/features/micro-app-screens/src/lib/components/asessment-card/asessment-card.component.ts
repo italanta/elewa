cca5fdc9bc90c7cfb43d51231ab37914ec4d9580
Beulah Mathenge <beulahdaizy@gmail.com>
2024-05-22 11:44:50 +0300

    :package: Generate question content cmponent

---
+++ libs/features/micro-app-screens/src/lib/components/asessment-card/asessment-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asessment-card',
+  templateUrl: './asessment-card.component.html',
+  styleUrls: ['./asessment-card.component.scss']
+})
+export class AsessmentCardComponent {
+
+}
--- libs/features/micro-app-screens/src/lib/features-micro-app-screens.module.ts
+++ libs/features/micro-app-screens/src/lib/features-micro-app-screens.module.ts
@@ -9,6 +9,7 @@ import { MicroAppMainPageComponent } from './pages/micro-app-main-page/micro-app
 import { MicroAppsHeaderComponent } from './components/micro-apps-header/micro-apps-header.component';
 import { MicroAppsFooterComponent } from './components/micro-apps-footer/micro-apps-footer.component';
 import { ContentSectionComponent } from './components/content-section/content-section.component';
+import { AsessmentCardComponent } from './components/asessment-card/asessment-card.component';
 
 @NgModule({
   imports: [
@@ -23,6 +24,7 @@ import { ContentSectionComponent } from './components/content-section/content-se
     MicroAppsHeaderComponent,
     MicroAppsFooterComponent,
     ContentSectionComponent,
+    AsessmentCardComponent,
   ],
   exports: [
     MicroAppMainPageComponent
