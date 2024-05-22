341270d815ea191ed6b1a79649b968344d3240e9
Beulah Mathenge <beulahdaizy@gmail.com>
2024-05-22 16:39:56 +0300

    :sparkles: Render questions as form arrays

--- libs/features/micro-app-screens/src/lib/components/content-section/content-section.component.ts
+++ libs/features/micro-app-screens/src/lib/components/content-section/content-section.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TEST_DATA } from '../../utils/test-data';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { MicroAppAssessmentQuestionFormService } from '../../services/microapp-assessment-questions-form.service';
 
 @Component({
   selector: 'app-content-section',
   templateUrl: './content-section.component.html',
   styleUrls: ['./content-section.component.scss']
 })
-export class ContentSectionComponent {
+export class ContentSectionComponent implements OnInit {
 
   assessmentQuestions = TEST_DATA
 
+  assessmentFormArray: FormArray;
+
+  assessmentForm: FormGroup
+
+  constructor ( private _assessFormService: MicroAppAssessmentQuestionFormService,
+                private _fb: FormBuilder,
+  ){}
+
+  ngOnInit() {
+    this.buildForms()
+  }
+
+  /**Building assessment forms */
+  buildForms(){
+    this.assessmentFormArray = this._assessFormService.createMicroAppAssessment(this.assessmentQuestions);
+    this.assessmentForm = this._fb.group({
+      assessmentFormArray: this.assessmentFormArray
+    });
+  }
 }
---
+++ libs/features/micro-app-screens/src/lib/services/microapp-assessment-questions-form.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { FormArray, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { AssessmentQuestion } from '../models/assessment-question.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MicroAppAssessmentQuestionFormService {
+
+  constructor(private _fb: FormBuilder) {}
+
+  /** A single question form */
+  createAssessmentQuestionForm(assessmentQuestion?: AssessmentQuestion): FormGroup {
+    return this._fb.group({
+      id: [assessmentQuestion?.id ?? ""],
+      question: [assessmentQuestion?.question ?? ""],
+      options: this._fb.array(assessmentQuestion?.options.map(option => new FormControl(option)) || [])
+    });
+  }
+
+  /** Adding all the questions into a form array */
+  createMicroAppAssessment(assessmentQuestions?: AssessmentQuestion[]): FormArray {
+    const questionsArray = assessmentQuestions?.map(question => this.createAssessmentQuestionForm(question)) || [];
+    return this._fb.array(questionsArray);
+  }
+
+  /** Method to add a new question form to the form array */
+  addQuestion(formArray: FormArray, assessmentQuestion?: AssessmentQuestion) {
+    formArray.push(this.createAssessmentQuestionForm(assessmentQuestion));
+  }
+
+  /** Method to remove a question form from the form array by index */
+  removeQuestion(formArray: FormArray, index: number) {
+    formArray.removeAt(index);
+  }
+}
