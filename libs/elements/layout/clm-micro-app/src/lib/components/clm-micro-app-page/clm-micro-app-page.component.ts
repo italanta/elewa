eaba9331054940941deb4578e2bf9558e8960f3b
Beulah Mathenge <beulahdaizy@gmail.com>
2024-06-02 22:13:14 +0300

    :package: Generate page layout component

---
+++ libs/elements/layout/clm-micro-app/src/lib/components/clm-micro-app-page/clm-micro-app-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clm-micro-app-page',
+  templateUrl: './clm-micro-app-page.component.html',
+  styleUrls: ['./clm-micro-app-page.component.scss'],
+})
+export class ClmMicroAppPageComponent {}
--- libs/elements/layout/clm-micro-app/src/lib/libs-elements-layout-clm-micro-app.module.ts
+++ libs/elements/layout/clm-micro-app/src/lib/libs-elements-layout-clm-micro-app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClmMicroAppPageComponent } from './components/clm-micro-app-page/clm-micro-app-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ClmMicroAppPageComponent],
 })
-export class LibsElementsLayoutClmMicroAppModule {}
+export class ClmMicroAppLayoutModule {}
