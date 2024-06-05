873b16b610e5e26491556cffbf884315899628b5
Reagan Charana <rgncharana2@gmail.com>
2024-06-05 10:43:05 +0300

    :pencil2: rename start page component

--- apps/conv-lm-micro-app/src/app/app.routing.module.ts
+++ apps/conv-lm-micro-app/src/app/app.routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule, inject } from '@angular/core';
 import { RouterModule, Route, PreloadAllModules }    from '@angular/router';
 import { CanAccessMicroAppGuard } from '@app/elements/base/authorisation';
 
-import { MicroAppContentPageComponent, MicroAppMainPageComponent, PlatformRedirectPageComponent } from '@app/features/micro-app';
+import { MicroAppContentPageComponent, MicroAppStartPageComponent, PlatformRedirectPageComponent } from '@app/features/micro-app';
 
 
 
@@ -13,7 +13,7 @@ export const APP_ROUTES: Route[] = [
 
   {
     path: 'start',
-    component: MicroAppMainPageComponent,
+    component: MicroAppStartPageComponent,
     // canActivate: [() => inject(CanAccessMicroAppGuard)],
     // canLoad: [() => inject(CanAccessMicroAppGuard)]
   },
--- libs/features/micro-app/src/index.ts
+++ libs/features/micro-app/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/features-micro-app-screens.module';
-export * from './lib/pages/micro-app-main-page/micro-app-start-page.component';
+export * from './lib/pages/micro-app-start-page/micro-app-start-page.component';
 export * from './lib/pages/micro-app-content-page/micro-app-content-page.component';
 export * from './lib/pages/platform-redirect-page/platform-redirect-page.component';
