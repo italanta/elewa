e19d0f941031e926ff8c65e409d4eb9c70401c29
Reagan Charana <rgncharana2@gmail.com>
2024-06-12 09:15:30 +0300

    :wrench: route to bots if id is not present in fallbacks

--- libs/features/convs-mgr/fallbacks/src/lib/fallbacks.router.ts
+++ libs/features/convs-mgr/fallbacks/src/lib/fallbacks.router.ts
@@ -4,6 +4,7 @@ import { Route, RouterModule } from '@angular/router';
 import { FallbackMainPageComponent } from './pages/fallback-main-page/fallback-main-page.component';
 
 const FALLBACK_ROUTES: Route[] = [
+  { path: '', redirectTo: '/bots/dashboard', pathMatch: 'full' },
   {
     path: ':id',
     component: FallbackMainPageComponent,
