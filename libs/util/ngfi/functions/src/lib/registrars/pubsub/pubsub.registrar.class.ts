d73be55c034ca45855c586a1e6e7e28b48f16dc7
Jente Rosseel <rosseeljente@gmail.com>
2024-06-13 15:46:24 +0300

    :rocket: PubSub registrar

--- libs/util/ngfi/functions/src/lib/registrars/pubsub/pubsub.registrar.class.ts
+++ libs/util/ngfi/functions/src/lib/registrars/pubsub/pubsub.registrar.class.ts
@@ -1,4 +1,6 @@
-import * as functions from 'firebase-functions';
+
+import { CloudEvent, CloudFunction } from 'firebase-functions/v2';
+import { onMessagePublished, PubSubOptions, Message } from 'firebase-functions/v2/pubsub';
 
 import { FunctionRegistrar } from "../function-registrar.interface";
 
@@ -11,22 +13,31 @@ import { FIREBASE_REGIONS } from '../regions.type';
 export class PubSubRegistrar<T> extends FunctionRegistrar<T, void>
 {
   constructor(private _topic: string,
-              private _region: FIREBASE_REGIONS = 'europe-west1')
+              private _region: FIREBASE_REGIONS = 'europe-west1',
+              private _opts?: PubSubOptions)
   { super(); }
 
-  register(func: (dataSnap: any, context: any) => Promise<void>): functions.CloudFunction<functions.pubsub.Message>
+  register(func: (dataSnap: any, context: any) => Promise<void>) : CloudFunction<CloudEvent<any>> 
   {
-    return functions.region(this._region)
-            .pubsub.topic(this._topic)
-            .onPublish((message: functions.pubsub.Message, context: functions.EventContext) => func(message, context));
+    const opts: PubSubOptions = {
+      topic: this._topic,
+      region: this._region,
+      ... this._opts ?? {}
+    };
+    
+    return onMessagePublished
+    (
+      opts, 
+      (event) => func(event.data.message, event)
+    );
   }
 
-  before(message: any, context: any): { data: T, context: FunctionContext; }
+  before(message: any, context: any): Promise<{ data: T, context: FunctionContext; }>
   {
-    const msg  = message as functions.pubsub.Message;
-    const data = JSON.parse(Buffer.from(msg.data, 'base64').toString()) as T;
+    const msg  = message as Message<T>;
+    const data = msg.json as T;
 
-    return { data, context };
+    return new Promise(resolve => resolve({ data, context: { eventContext: context, isAuthenticated: true, userId: 'root' } }));
   }
 
   after(result: void, _: any): any {
