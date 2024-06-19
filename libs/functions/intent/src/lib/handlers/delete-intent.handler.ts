7ff7b16069ce2883a2cd3fbadb16b6984a625062
Reagan Charana <rgncharana2@gmail.com>
2024-06-19 18:01:41 +0300

    :art: clean up code

--- libs/functions/intent/src/lib/handlers/delete-intent.handler.ts
+++ libs/functions/intent/src/lib/handlers/delete-intent.handler.ts
@@ -1,6 +1,6 @@
 import { HandlerTools } from '@iote/cqrs';
 
-import { FunctionHandler, RestResult, HttpsContext, RestResult200 } from '@ngfi/functions';
+import { FunctionHandler, RestResult, HttpsContext } from '@ngfi/functions';
 
 import { DialogflowCXIntent } from '@app/model/convs-mgr/fallbacks';
 
@@ -17,12 +17,12 @@ export class DeleteIntentHandler extends FunctionHandler<DialogflowCXIntent, Res
   public async execute(req: DialogflowCXIntent, context: HttpsContext, tools: HandlerTools) 
   { 
     this._intentService = new IntentService();
-    tools.Logger.log(() => `[CreateIntentHandler] - Creating Intent: ${JSON.stringify(req)}`);
+    tools.Logger.log(() => `[DeleteIntentHandler] - Deleting Intent: ${JSON.stringify(req)}`);
     try {
       const deleteResult = await this._intentService.deleteIntent(req, tools);
       return { status: 200, message: deleteResult } as unknown as RestResult;
     } catch (e){
-      tools.Logger.log(() => `[CreateIntentHandler] - Error Creating Intent: ${JSON.stringify(e)}`);
+      tools.Logger.log(() => `[DeleteIntentHandler] - Error Deleting Intent: ${JSON.stringify(e)}`);
       return { status: 500, message: e.message } as RestResult;
     }
   }
