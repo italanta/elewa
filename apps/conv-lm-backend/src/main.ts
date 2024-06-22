ae6f037f043bc9f64aa27a99d909196163d300f2
Reagan Charana <rgncharana2@gmail.com>
2024-06-22 12:56:08 +0300

    :bug: fix build errors on deploying v1 functions

--- apps/conv-lm-backend/src/main.ts
+++ apps/conv-lm-backend/src/main.ts
@@ -33,8 +33,6 @@ export * from './app/micro-apps/surveys/start-survey.function';
 
 export * from './app/user/create-new-user.function';
 
-export * from './app/story/check-story-for-errors.function';
-
 export * from './app/organisation/on-create-org-assign-user-to-org.function';
 
 
