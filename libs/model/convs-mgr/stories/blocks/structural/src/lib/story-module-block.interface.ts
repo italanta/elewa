3a86885862322098af60a30d7c596a9bb9c0d09b
Jente Rosseel <rosseeljente@gmail.com>
2024-06-22 10:55:23 +0300

    :house_with_garden: create StoryModule model
    Inherits from a block/flow element

---
+++ libs/model/convs-mgr/stories/blocks/structural/src/lib/story-module-block.interface.ts
@@ -0,0 +1,56 @@
+import { StoryBlock } from "@app/model/convs-mgr/stories/blocks/main";
+
+/**
+ * Block which represents a story module.
+ * 
+ *  Story modules are child stories that contain their own story flow.
+ *  They have a clear entry point (from the parent story) and can have one to many outputs (resulting in exit points on the representative block).
+ *   
+ */
+export interface StoryModuleBlock extends StoryBlock
+{
+  /** ID of the story module. Corresponds with the ID of the related story. */
+  id: string;
+
+  /** Type of the story */
+  storyType: StoryModuleTypes;
+
+  /** 
+   * Set of story module outputs.
+   *  Needs to be at least one!
+   */
+  outputs: [StoryModuleResult];
+}
+
+/**
+ * Story module results are the outcome of a story module.
+ * They represent 
+ */
+export interface StoryModuleResult 
+{
+  /** ID of the result endpoint */
+  id: string;
+  /** Label/name of the result */
+  label: string;
+}
+
+/**
+ * Different possible types of a story module
+ */
+export enum StoryModuleTypes
+{
+  /** The story module represents a child story */
+  SubStory = 1,
+
+  /** The story module represents a flow */
+  Flow = 2,
+
+  /** The story module represents an assessment */
+  Assessment = 3,
+
+  /** The story module represents a micro-app */
+  MicroApp = 4,
+
+  /** Future feature to experiment with @Jente */
+  CoachingModule = 5
+}
