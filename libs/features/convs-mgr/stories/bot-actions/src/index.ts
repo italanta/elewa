b0236c8ff6790a82d9c79b0806238286627c0f71
Jente Rosseel <rosseeljente@gmail.com>
2024-06-23 11:15:55 +0300

    :truck: move course actions into shared package bot-actions

--- libs/features/convs-mgr/stories/bot-actions/src/index.ts
+++ libs/features/convs-mgr/stories/bot-actions/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/features-convs-mgr-stories-bot-actions/features-convs-mgr-stories-bot-actions.component';
+export * from './lib/modals/channel/channel.component';
+export * from './lib/modals/confirm-archive-modal/confirm-archive-modal.component';
+export * from './lib/modals/confirm-publish-modal/confirm-publish-modal.component';
+export * from './lib/modals/connect-to-channel-modal/connect-to-channel-modal.component';
+export * from './lib/modals/main-channel-modal/main-channel-modal.component';
+export * from './lib/modals/spinner-modal/spinner-modal.component';
---
+++ libs/features/convs-mgr/stories/bot-actions/src/lib/stories-home.module.ts
@@ -0,0 +1,48 @@
+import { ItalBreadCrumbModule } from '@app/elements/layout/ital-bread-crumb';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
+
+import { FlexLayoutModule, MaterialDesignModule } from '@iote/bricks-angular';
+import { MultiLangModule } from '@ngfi/multi-lang';
+
+import { ConvlPageModule } from '@app/elements/layout/page-convl';
+import { ChatsRouterModule } from '@app/features/convs-mgr/conversations/chats';
+
+import { ConnectToChannelModalComponent } from './modals/connect-to-channel-modal/connect-to-channel-modal.component';
+import { SpinnerModalComponent } from './modals/spinner-modal/spinner-modal.component';
+import { ChannelComponent } from './modals/channel/channel.component';
+import { MainChannelModalComponent } from './modals/main-channel-modal/main-channel-modal.component';
+import { ConfirmPublishModalComponent } from './modals/confirm-publish-modal/confirm-publish-modal.component';
+import { ConfirmArchiveModalComponent } from './modals/confirm-archive-modal/confirm-archive-modal.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialDesignModule,
+    FlexLayoutModule,
+    ConvlPageModule,
+    MultiLangModule,
+    FormsModule,
+    ItalBreadCrumbModule,
+  ],
+  declarations: [
+    ConnectToChannelModalComponent,
+    SpinnerModalComponent,
+    ChannelComponent,
+    MainChannelModalComponent,
+    ConfirmPublishModalComponent,
+    ConfirmArchiveModalComponent,
+  ],
+  exports: [
+    ConnectToChannelModalComponent,
+    SpinnerModalComponent,
+    ChannelComponent,
+    MainChannelModalComponent,
+    ConfirmPublishModalComponent,
+    ConfirmArchiveModalComponent,
+  ]
+})
+export class ConvsMgrBotActionsModule {}
