d1b79764923096871840ced66501fa88de4ff19b
Jente Rosseel <rosseeljente@gmail.com>
2024-06-28 09:02:57 +0000

    :fire: remove unused imports

--- libs/features/convs-mgr/stories/builder/app/src/lib/bot-builder.module.ts
+++ libs/features/convs-mgr/stories/builder/app/src/lib/bot-builder.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule, MaterialBricksModule, MaterialDesignModule, MaterialFormBricksModule } from '@iote/bricks-angular';
 import { MultiLangModule } from '@ngfi/multi-lang';
 
 import { ItalBreadCrumbModule } from '@app/elements/layout/ital-bread-crumb';
