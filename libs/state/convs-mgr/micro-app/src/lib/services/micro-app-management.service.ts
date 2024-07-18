4e0e0069595f5da4403b0ba00be87a802fd95ac9
Reagan Charana <rgncharana2@gmail.com>
2024-07-18 16:07:42 +0300

    :wrench: use envs for progress endpoint

--- libs/state/convs-mgr/micro-app/src/lib/services/micro-app-management.service.ts
+++ libs/state/convs-mgr/micro-app/src/lib/services/micro-app-management.service.ts
@@ -40,9 +40,9 @@ export class MicroAppManagementService
 
 /** Updating a user's progress when they are done with an assessment, on the redirect page */
 progressCallBack(app?: MicroAppStatus, milestones?: AssessmentProgressUpdate) {
-  if (!app || !app.config || !app.config.callBackUrl) return;
+  if (!app || !app.config) return;
 
-  const URL = app.config.callBackUrl;
+  const URL = `${this._env.microAppUrl}/${PROGRESS_MICROAPP_ENDPOINT}`;
 
   if (app.config.type === MicroAppTypes.Assessment && milestones) {
     const assessmentPayload: AssessmentProgressUpdate = {
