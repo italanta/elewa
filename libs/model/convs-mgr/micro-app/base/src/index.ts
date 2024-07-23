0770dfd8db6ec8806d8cda192f0c55b62785e52f
Reagan Charana <rgncharana2@gmail.com>
2024-07-23 11:44:35 +0300

    :art: format code

--- libs/model/convs-mgr/micro-app/base/src/index.ts
+++ libs/model/convs-mgr/micro-app/base/src/index.ts
@@ -5,5 +5,5 @@ export * from './lib/micro-app-types.enum';
 export * from './lib/init-micro-app-cmd.interface';
 
 export * from './lib/pass-criteria-types.enum';
-export * from './lib/micro-app-progress.interface'
-export * from './lib/microapp-section-types.enum'
+export * from './lib/micro-app-progress.interface';
+export * from './lib/microapp-section-types.enum';
