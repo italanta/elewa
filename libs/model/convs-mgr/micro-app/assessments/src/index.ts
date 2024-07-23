031f58c7fbd661e70535010392c6445b43c62f8b
Reagan Charana <rgncharana2@gmail.com>
2024-07-23 11:46:08 +0300

    :house: add assessment status types

--- libs/model/convs-mgr/micro-app/assessments/src/index.ts
+++ libs/model/convs-mgr/micro-app/assessments/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/assessment-progress.interface';
 export * from './lib/assessment-attempt.interface';
 export * from './lib/assessment-question-response.interface';
 export * from './lib/assessment-progress-update.interface';
+export * from './lib/assessment-status-types.enum';
