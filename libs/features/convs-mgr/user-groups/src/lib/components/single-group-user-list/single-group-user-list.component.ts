fbcdbe1321cc3e32869b9c76645c54521f66d647
fraciah <karfraciah@gmail.com>
2024-08-16 14:51:39 +0300

    ⏪️ Revert platform case

--- libs/features/convs-mgr/user-groups/src/lib/components/single-group-user-list/single-group-user-list.component.ts
+++ libs/features/convs-mgr/user-groups/src/lib/components/single-group-user-list/single-group-user-list.component.ts
@@ -38,7 +38,7 @@ export class SingleGroupUserListComponent implements OnInit, OnDestroy
 
   _sbs = new SubSink();
 
-  allPlatforms: string[] = ['WhatsApp', 'Messenger'];
+  allPlatforms: string[] = ['whatsapp', 'messenger'];
   allStatus = [{name: 'Active', value: 1}, {name: 'Inactive', value: 2}];
 
   allLearners: EnrolledEndUser[] = [];
