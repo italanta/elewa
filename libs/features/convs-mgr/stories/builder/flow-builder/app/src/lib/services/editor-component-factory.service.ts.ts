e699e8cfcb60b7b98aa3d3e9a0c2d558216b0011
Beulah Mathenge <beulahdaizy@gmail.com>
2024-08-20 10:47:48 +0300

    :truck: Rename component factory

---
+++ libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/services/editor-component-factory.service.ts.ts
@@ -0,0 +1,60 @@
+import { Injectable, Type, ViewContainerRef } from '@angular/core';
+import { FlowPageTextSizesV31 } from "@app/model/convs-mgr/stories/flows";
+import { FlowControl, FlowControlType } from "../providers/flow-controls.const";
+import { FlowHeaderTextComponent } from '../components/flow-header-text/flow-header-text.component';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditorComponentFactory {
+  
+  createEditorComponent(flowControl: FlowControl, vcr: ViewContainerRef): any {
+    let componentType: Type<any>;
+
+    switch (flowControl.type) {
+        case FlowControlType.Header:
+        case FlowControlType.LightHeader:
+        case FlowControlType.Caption:
+        case FlowControlType.Text: {
+            componentType = FlowHeaderTextComponent;
+            break;
+        }
+        // case 'image': {
+        //     componentType = FlowImageComponent;
+        //     break;
+        // }
+        // case 'link': {
+        //     componentType = FlowLinkComponent;
+        //     break;
+        // }
+        // case 'select':
+        // case 'checkbox':
+        // case 'radio': {
+        //     componentType = FlowSelectComponent;
+        //     break;
+        // }
+        // case 'datepick': {
+        //     componentType = FlowDatePickerComponent;
+        //     break;
+        // }
+        default: {
+            componentType = FlowHeaderTextComponent;
+            break;
+        }
+    }
+
+    const componentRef = vcr.createComponent(componentType);
+
+    // Optionally set inputs
+    if (flowControl.type === FlowControlType.Header || flowControl.type === FlowControlType.LightHeader) {
+      componentRef.instance.size = flowControl.type === FlowControlType.Header ? FlowPageTextSizesV31.Header : FlowPageTextSizesV31.SubHeader;
+    } else if (flowControl.type === FlowControlType.Text) {
+      componentRef.instance.size = FlowPageTextSizesV31.Body;
+    } else if (flowControl.type === FlowControlType.Caption) {
+      componentRef.instance.size = FlowPageTextSizesV31.Caption;
+    }
+
+    return componentRef;
+  }
+}
