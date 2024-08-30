3c9720ea58667e654baad4c83d44f6025d3a0fb4
Reagan Charana <rgncharana2@gmail.com>
2024-08-30 17:03:07 +0300

    :lipstick: :construction: add image block form to edit component

--- libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/image-output-block-edit/image-output-block-edit.component.ts
+++ libs/features/convs-mgr/stories/blocks/edit/blocks-edit/src/lib/components/image-output-block-edit/image-output-block-edit.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
+
+import { ImageMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
+
 @Component({
   selector: 'app-image-output-block-edit',
   templateUrl: './image-output-block-edit.component.html',
   styleUrl: './image-output-block-edit.component.scss'
 })
 export class ImageOutputBlockEditComponent {
-  @Input() form: FormGroup;
+  @Input() id: string;
+  @Input() block: ImageMessageBlock;
   @Input() title: string;
   @Input() icon: string;
+  @Input() form: FormGroup;
+  @Input() jsPlumb: BrowserJsPlumbInstance;
 }
