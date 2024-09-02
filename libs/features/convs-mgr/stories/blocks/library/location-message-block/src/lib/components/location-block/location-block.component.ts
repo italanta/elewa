174f1cfbd9240f4fc3803d97e9289156b90720d0
Reagan Charana <rgncharana2@gmail.com>
2024-09-02 10:46:15 +0300

    :lipstick: :recycle: move location block form to separate component

--- libs/features/convs-mgr/stories/blocks/library/location-message-block/src/lib/components/location-block/location-block.component.ts
+++ libs/features/convs-mgr/stories/blocks/library/location-message-block/src/lib/components/location-block/location-block.component.ts
@@ -1,10 +1,9 @@
-import { AfterViewInit, Component, ElementRef, Input, NgZone, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, NgZone, Renderer2, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { GoogleMap } from '@angular/google-maps';
 
 import { BrowserJsPlumbInstance } from '@jsplumb/browser-ui';
 
-import { StoryBlockTypes } from '@app/model/convs-mgr/stories/blocks/main';
 import { LocationMessageBlock } from '@app/model/convs-mgr/stories/blocks/messaging';
 
 @Component({
@@ -12,7 +11,7 @@ import { LocationMessageBlock } from '@app/model/convs-mgr/stories/blocks/messag
   templateUrl: './location-block.component.html',
   styleUrls: ['./location-block.component.scss'],
 })
-export class LocationBlockComponent implements OnInit, AfterViewInit {
+export class LocationBlockComponent  {
 
   @ViewChild('mapsSearchField') searchElementRef: ElementRef;
   @ViewChild(GoogleMap) map: GoogleMap;
@@ -23,103 +22,6 @@ export class LocationBlockComponent implements OnInit, AfterViewInit {
 
   @Input() locationMessageForm: FormGroup;
 
-  locationInputId: string;
+  constructor() { }
 
-  type: StoryBlockTypes;
-  locationtype = StoryBlockTypes.Location;
-
-  blockFormGroup: FormGroup;
-
-  latitude: number;
-  longitude: number;
-  currentAddress: string = '';
-
-  zoom: number = 5;
-  address: string;
-
-
-  markerPositions: google.maps.LatLng;
-  markerOptions: google.maps.MarkerOptions = { draggable: false };
-
-  constructor(private ngZone: NgZone, private el: ElementRef, private renderer: Renderer2) { }
-
-  ngOnInit(): void {
-    if (this.locationMessageForm) {
-      this.latitude = this.locationMessageForm.value.locationInput.latitude;
-      this.longitude = this.locationMessageForm.value.locationInput.longitude;
-      this.currentAddress = this.locationMessageForm.value.locationInput.address;
-    }
-  }
-
-  ngAfterViewInit(): void {
-    this.setFocusOnInput();
-    this.findAdress();
-    if (this.locationMessageForm) {
-      this.checkIfAddressExists();
-    } 
-  }
-
-  private setFocusOnInput() {
-    const inputElement = this.el.nativeElement.querySelector(`input[formControlName="name"]`);
-    if (inputElement) {
-      this.renderer.selectRootElement(inputElement).focus();
-    }
-  }
-
-  checkIfAddressExists() {
-    let address = this.locationMessageForm.value.locationInput;
-
-    if (address && address.latitude && address.longitude) {
-      this.latitude = address.latitude;
-      this.longitude = address.longitude;
-      this.currentAddress = address.address;
-      this.markerPositions = new google.maps.LatLng(this.latitude, this.longitude);
-      let b = new google.maps.LatLngBounds(this.markerPositions);
-      this.map.fitBounds(b);
-    }
-  }
-
-  findAdress() {
-    const options = {}
-
-    let autocomplete = new google.maps.places.Autocomplete(
-      this.searchElementRef.nativeElement, options
-    );
-
-    autocomplete.addListener('place_changed', () => {
-      this.ngZone.run(() => {
-        let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-        if (place.geometry === undefined || place.geometry === null) {
-          return;
-        }
-
-        const bounds = new google.maps.LatLngBounds();
-
-        if (place.geometry.viewport) {
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location!);
-        }
-
-        this.zoom = 12;
-
-        if (place.geometry.location!.lat() && place.geometry.location!.lng()) {
-          this.latitude = place.geometry.location!.lat();
-          this.longitude = place.geometry.location!.lng();
-  
-          this.map.fitBounds(bounds)
-          this.markerPositions = new google.maps.LatLng(this.latitude, this.longitude)
-        }
-      });
-    });
-  }
-
-  addMarker(event: google.maps.MapMouseEvent) {
-    this.markerPositions = event.latLng!;
-  }
-
-  addressChanged(address: any) {
-    this.currentAddress = address.target.value;    
-  }
 }
--- libs/features/convs-mgr/stories/blocks/library/location-message-block/src/lib/convs-mgr-location-message-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/location-message-block/src/lib/convs-mgr-location-message-block.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { provideHttpClient, withInterceptorsFromDi, withJsonpSupport } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withJsonpSupport,
+} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
 
 import {
@@ -16,15 +20,26 @@ import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/bloc
 
 import { LocationBlockComponent } from './components/location-block/location-block.component';
 import { LocationBlockMapComponent } from './components/location-block-map/location-block-map.component';
+import { LocationBlockFormComponent } from './components/location-block-form/location-block-form.component';
 
-@NgModule({ declarations: [LocationBlockComponent, LocationBlockMapComponent],
-    exports: [LocationBlockComponent], imports: [CommonModule,
-        MultiLangModule,
-        MaterialDesignModule,
-        FlexLayoutModule,
-        MaterialBricksModule,
-        FormsModule,
-        ReactiveFormsModule,
-        GoogleMapsModule,
-        ConvsMgrBlockOptionsModule], providers: [provideHttpClient(withInterceptorsFromDi(), withJsonpSupport())] })
+@NgModule({
+  declarations: [
+    LocationBlockComponent,
+    LocationBlockMapComponent,
+    LocationBlockFormComponent,
+  ],
+  exports: [LocationBlockComponent],
+  imports: [
+    CommonModule,
+    MultiLangModule,
+    MaterialDesignModule,
+    FlexLayoutModule,
+    MaterialBricksModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GoogleMapsModule,
+    ConvsMgrBlockOptionsModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi(), withJsonpSupport())],
+})
 export class ConvsMgrLocationMessageBlockModule {}
