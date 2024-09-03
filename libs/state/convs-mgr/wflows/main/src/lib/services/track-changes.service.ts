6c02c1743aa83b7b93c5682e11a1cf1bd1f423fb
Beulah Mathenge <beulahdaizy@gmail.com>
2024-09-03 08:37:16 +0300

    :recycle: Getters and setters for behaviour subjects

--- libs/state/convs-mgr/wflows/main/src/lib/services/track-changes.service.ts
+++ libs/state/convs-mgr/wflows/main/src/lib/services/track-changes.service.ts
@@ -9,16 +9,27 @@ import { FlowsStore } from '../stores/wflow.store';
  * Service tracking user interactions
  */
 export class ChangeTrackerService {
+
+  private jsonArray: { controlId: string; newValue: any }[] = []
+
   private changeSubject = new BehaviorSubject<{ controlId: string; newValue: any }[]>([]);
+
+  constructor(private _wFlowStore: FlowsStore) {}
+
   public change$ = this.changeSubject.asObservable().pipe(
     debounceTime(1000), 
     distinctUntilChanged((prev, curr) => JSON.stringify(prev) === JSON.stringify(curr)) // Prevent redundant saves
   );
 
-  constructor(private _wFlowStore: FlowsStore) {}
+  setJsonArray(jsonArray: { controlId: string; newValue: any }[]) {
+    this.jsonArray = jsonArray;
+  }
 
   updateValue(controlId: string, newValue: any): void {
-    this.changeSubject.next([{ controlId, newValue }]);
+   this.jsonArray.push({ controlId, newValue })
+   this.changeSubject.next(this.jsonArray);
+   console.log(this.changeSubject.getValue())
+
   }
 
   clearChanges(): void {
