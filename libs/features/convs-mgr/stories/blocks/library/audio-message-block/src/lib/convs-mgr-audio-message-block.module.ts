3c88596f5b61751c4f0372f288dffda90dea46e5
Reagan Charana <rgncharana2@gmail.com>
2024-09-04 13:51:07 +0300

    :lipstick: :recycle:  move audio form to separate component

--- libs/features/convs-mgr/stories/blocks/library/audio-message-block/src/lib/convs-mgr-audio-message-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/audio-message-block/src/lib/convs-mgr-audio-message-block.module.ts
@@ -2,22 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { MultiLangModule } from '@ngfi/multi-lang';
-import { FlexLayoutModule, MaterialBricksModule, MaterialDesignModule } from '@iote/bricks-angular';
+import {
+  FlexLayoutModule,
+  MaterialBricksModule,
+  MaterialDesignModule,
+} from '@iote/bricks-angular';
 
 import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/blocks/library/block-options';
 
 import { AudioBlockComponent } from './components/audio-block/audio-block.component';
+import { AudioBlockFormComponent } from './components/audio-block-form/audio-block-form.component';
 
-@NgModule({ declarations: [AudioBlockComponent],
-    exports: [AudioBlockComponent], imports: [CommonModule,
-        MultiLangModule,
-        MaterialDesignModule,
-        FlexLayoutModule,
-        MaterialBricksModule,
-        FormsModule,
-        ReactiveFormsModule,
-        ConvsMgrBlockOptionsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class ConvsMgrAudioMessageBlockModule { }
+@NgModule({
+  declarations: [AudioBlockComponent, AudioBlockFormComponent],
+  exports: [AudioBlockComponent],
+  imports: [
+    CommonModule,
+    MultiLangModule,
+    MaterialDesignModule,
+    FlexLayoutModule,
+    MaterialBricksModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ConvsMgrBlockOptionsModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
+export class ConvsMgrAudioMessageBlockModule {}
