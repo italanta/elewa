2ca98f291906e29bb197bfcf07d455398025c579
Reagan Charana <rgncharana2@gmail.com>
2024-09-04 17:42:18 +0300

    :lipstick: :recycle: move video form to separate component

--- libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/convs-mgr-video-message-block.module.ts
+++ libs/features/convs-mgr/stories/blocks/library/video-message-block/src/lib/convs-mgr-video-message-block.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { ConvsMgrBlockOptionsModule } from '@app/features/convs-mgr/stories/blocks/library/block-options';
 
@@ -11,18 +14,29 @@ import {
   MaterialDesignModule,
 } from '@iote/bricks-angular';
 
-import {MultiLangModule}from '@ngfi/multi-lang';
+import { MultiLangModule } from '@ngfi/multi-lang';
 
 import { VideoBlockComponent } from './components/video-block/video-block.component';
 import { VideoUploadModalComponent } from './modals/video-upload-modal/video-upload-modal.component';
+import { VideoBlockFormComponent } from './components/video-block-form/video-block-form.component';
 
-@NgModule({ declarations: [VideoBlockComponent, VideoUploadModalComponent],
-    exports: [VideoBlockComponent], imports: [CommonModule,
-        MultiLangModule,
-        MaterialDesignModule,
-        FlexLayoutModule,
-        MaterialBricksModule,
-        FormsModule,
-        ReactiveFormsModule,
-        ConvsMgrBlockOptionsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    VideoBlockComponent,
+    VideoUploadModalComponent,
+    VideoBlockFormComponent,
+  ],
+  exports: [VideoBlockComponent],
+  imports: [
+    CommonModule,
+    MultiLangModule,
+    MaterialDesignModule,
+    FlexLayoutModule,
+    MaterialBricksModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ConvsMgrBlockOptionsModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
 export class ConvsMgrVideoMessageBlockModule {}
