0b3db8a576843b522d9b5cd0b79539e3e038227e
Beulah Mathenge <beulahdaizy@gmail.com>
2024-09-13 11:14:45 +0300

    :bug: Logo URL on app load

--- libs/elements/layout/clm-micro-app/src/lib/components/clm-micro-app-page/clm-micro-app-page.component.ts
+++ libs/elements/layout/clm-micro-app/src/lib/components/clm-micro-app-page/clm-micro-app-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {  MicroAppStore } from '@app/state/convs-mgr/micro-app';
-import { subscribe } from 'diagnostics_channel';
+
+import { take } from 'rxjs';
+import { SubSink } from 'subsink';
+
+import { MicroAppStatus } from '@app/model/convs-mgr/micro-app/base';
+import { MicroAppStore } from '@app/state/convs-mgr/micro-app';
+
 
 @Component({
   selector: 'app-clm-micro-app-page',
@@ -10,12 +15,32 @@ import { subscribe } from 'diagnostics_channel';
 export class ClmMicroAppPageComponent implements OnInit
 {
   logoUrl = ''
-
-  constructor (private _microApp$$: MicroAppStore){}
-
-  ngOnInit()
-  {
-    // Add method to get microapp logo
-    const app$ = this._microApp$$.get()
-  }
+   /** Comprehensive app data */
+   app: MicroAppStatus;
+  /** Tracking initialization (loading) */
+  isInitializing = true;
+ 
+   private _sbS = new SubSink();
+ 
+   constructor(private _microApp$$: MicroAppStore,
+   ) {}
+ 
+   ngOnInit()
+   {
+     // STEP 1. Get app ID
+      const app$ = this._microApp$$.get();
+      this._sbS.sink = 
+       app$.pipe(take(1)).subscribe(a => 
+        {
+         this.app = a;
+         this.isInitializing = false;
+         this.logoUrl = a.config.orgLogoUrl || ''
+        });
+   }
+ 
+   /** Unsubscribe from all subscriptions to prevent memory leaks */
+   ngOnDestroy()
+   {
+     this._sbS.unsubscribe();
+   }
 }
