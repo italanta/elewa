e57e28878e1af79ac37babf78ff38be197398ffc
k0imburi <pablokahura@gmail.com>
2024-09-16 14:17:41 +0300

    :package:  New library added for IVR

---
+++ libs/functions/bot-engine/interactive-voice-response/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/interactive-voice-response';
---
+++ libs/functions/bot-engine/interactive-voice-response/src/lib/interactive-voice-response.spec.ts
@@ -0,0 +1,7 @@
+import { interactiveVoiceResponse } from './interactive-voice-response';
+
+describe('interactiveVoiceResponse', () => {
+  it('should work', () => {
+    expect(interactiveVoiceResponse()).toEqual('interactive-voice-response');
+  });
+});
