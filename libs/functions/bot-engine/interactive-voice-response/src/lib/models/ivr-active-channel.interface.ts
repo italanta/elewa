b1140920875d4632f972afdfc6949c632afa9174
k0imburi <pablokahura@gmail.com>
2024-09-24 15:40:24 +0300

    Update ivr-active-channel.interface.ts

--- libs/functions/bot-engine/interactive-voice-response/src/lib/models/ivr-active-channel.interface.ts
+++ libs/functions/bot-engine/interactive-voice-response/src/lib/models/ivr-active-channel.interface.ts
@@ -6,7 +6,7 @@ export interface IVRActiveChannel {
      * Sends the outgoing message through the IVR system.
      * @param payload - Message details like phone number, voice settings, and IVR options.
      */
-    send: (payload: IVROutgoingMessage) => Promise<void>;
+    send?: (payload: IVROutgoingMessage) => Promise<void>;
   
     /**
      * Handles incoming payloads from Africa's Talking (e.g., user input like DTMF).
@@ -14,4 +14,4 @@ export interface IVRActiveChannel {
      * @returns The XML response for the IVR.
      */
     handleIncoming: (payload: any) => Promise<string>;
-  }
+}
