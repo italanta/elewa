c039a01039538b13d5c2626dadee269e8e5851df
k0imburi <pablokahura@gmail.com>
2024-09-30 16:40:45 +0300

    Update index.ts

--- libs/functions/bot-engine/interactive-voice-response/src/index.ts
+++ libs/functions/bot-engine/interactive-voice-response/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/interactive-voice-response';
+export * from './lib/services/azure-text-to-speech.service';
+export * from './lib/services/azure-blob-upload.service';
+export * from './lib/azure-audio-upload.handler';
