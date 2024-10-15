9762d8676fe195e50001b75616000ba46fa95837
k0imburi <pablokahura@gmail.com>
2024-10-15 11:44:36 +0300

    Update index.ts

--- libs/model/convs-mgr/stories/blocks/main/src/index.ts
+++ libs/model/convs-mgr/stories/blocks/main/src/index.ts
@@ -9,3 +9,4 @@ export * from './lib/variables.interface';
 export * from './lib/story-block-variables-config.interface';
 export * from './lib/http-methods.interface';
 export * from './lib/storyblock-variable.interface';
+export * from './lib/block-library-filter-strategy.interface';
