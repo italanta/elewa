33a803821e6d88eb977854c6ea42ceb20a19f07b
k0imburi <pablokahura@gmail.com>
2024-10-15 11:45:46 +0300

    Create audio-data.interface.ts

---
+++ libs/functions/bot-engine/interactive-voice-response/src/lib/models/audio-data.interface.ts
@@ -0,0 +1,23 @@
+/**
+ * Enum for voice gender options.
+ */
+enum VoiceGender {
+    Male = 'male',
+    Female = 'female'
+}
+
+/**
+ * Interface for audio data.
+ * 
+ * @property {ArrayBuffer} audioBuffer - The buffer containing audio data.
+ * @property {string} storyId - The ID of the story associated with the audio.
+ * @property {string} blockId - The ID of the block associated with the audio.
+ * @property {VoiceGender} voiceGender - The gender of the voice used for audio (male or female).
+ */
+interface AudioData {
+    audioBuffer: ArrayBuffer;
+    orgId?: string;
+    storyId: string;
+    blockId: string;
+    voiceGender: VoiceGender;
+}
